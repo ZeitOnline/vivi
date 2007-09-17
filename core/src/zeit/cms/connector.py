82dfba2ac4bdea065ac4f431ae9db01bb2c7544d
Christian Zagrodnick <cz@gocept.com>
2007-09-17 12:18:30 +0000

    finished moving connector out of zeit.cms

---
+++ core/src/zeit/cms/connector.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+# Backward compatibility
+from zeit.cms.content.adapter import xmlContentToResourceAdapterFactory
+from zeit.connector.resource import Resource
