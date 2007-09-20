959e35e85851a22c1ff9550ccb991de70f729a88
Christian Zagrodnick <cz@gocept.com>
2007-09-20 10:54:26 +0000

    added raw UI for stealing, locking and unlocking resources.

---
+++ core/src/zeit/workflow/browser/interfaces.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import zope.i18nmessageid
+import zope.interface
+import zope.schema
+
+
+_ = zope.i18nmessageid.MessageFactory('zeit.cms')
+
+
+class ILockFormSchema(zope.interface.Interface):
+
+    locked = zope.schema.Bool(
+        title=_('Locked'))
+
+    locker = zope.schema.TextLine(
+        title=_('Locker'),
+        required=False)
+
+    locked_until = zope.schema.Datetime(
+        title=_('Locked until'),
+        required=False)
---
+++ core/src/zeit/workflow/browser/lock.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import datetime
+
+import zope.cachedescriptors.property
+import zope.component
+import zope.i18nmessageid
+import zope.formlib.form
+
+import zeit.connector.interfaces
+
+import zeit.workflow.browser.interfaces
+
+
+_ = zope.i18nmessageid.MessageFactory('zeit.cms')
+
+
+def _stealable(form, action):
+    return form.lockable.isLockedOut()
+
+def _unlockable(form, action):
+    return form.lockable.ownLock()
+
+def _lockable(form, action):
+    return not form.lockable.locked()
+
+
+class Lock(zeit.cms.browser.form.FormBase, zope.formlib.form.Form):
+
+    form_fields = zope.formlib.form.Fields(
+        zeit.workflow.browser.interfaces.ILockFormSchema)
+
+    def get_data(self):
+        lockable = self.lockable
+        lockinfo = lockable.getLockInfo()
+        locked_until = None
+        created = None
+
+        if zeit.cms.content.interfaces.ILockInfo.providedBy(lockinfo):
+            locked_until = lockinfo.locked_until
+
+        return dict(
+            locked=lockable.locked(),
+            locker=lockable.locker(),
+            locked_until=locked_until,
+        )
+
+    @zope.formlib.form.action(_('Steal Lock'),
+                              condition=_stealable)
+    def steal(self, action, data):
+        self.lockable.breaklock()
+
+    @zope.formlib.form.action(_('Lock'),
+                             condition=_lockable)
+    def lock(self, action, data):
+        self.lockable.lock()
+
+    @zope.formlib.form.action(_('Unlock'),
+                             condition=_unlockable)
+    def unlock(self, action, data):
+        self.lockable.unlock()
+
+    @zope.cachedescriptors.property.Lazy
+    def lockable(self):
+        return zope.app.locking.interfaces.ILockable(self.context)
+
+    def _get_widgets(self, form_fields, ignore_request):
+        return zope.formlib.form.setUpDataWidgets(
+            form_fields, self.prefix, self.context, self.request,
+            data=self.get_data(), for_display=True,
+            ignore_request=ignore_request)
