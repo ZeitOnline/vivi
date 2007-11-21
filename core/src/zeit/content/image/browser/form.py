77ae5ab131a5d0fef3bb67346746757cb66c68b2
Christian Zagrodnick <cz@gocept.com>
2007-11-21 10:44:52 +0000

    formluar benutzbar gemacht (fixes bug #3826)

--- core/src/zeit/content/image/browser/form.py
+++ core/src/zeit/content/image/browser/form.py
@@ -7,21 +7,33 @@ import datetime
 import zope.formlib.form
 import zope.i18nmessageid
 
+import gocept.form.grouped
+
 import zeit.cms.browser.form
 import zeit.cms.interfaces
 import zeit.content.image.interfaces
 import zeit.content.image.image
 
+from zeit.cms.i18n import MessageFactory as _
 
-_ = zope.i18nmessageid.MessageFactory("zope")
+
+_zope = zope.i18nmessageid.MessageFactory("zope")
 
 
 class ImageFormBase(object):
 
-    widget_groups = ((u"Bild", zeit.cms.browser.form.REMAINING_FIELDS,
-                      "fullWidth"), )
+    field_groups = (
+        gocept.form.grouped.Fields(
+            _("Image data"), ('__name__', 'data', 'contentType', 'expires'),
+            css_class='column-left image-form'),
+        gocept.form.grouped.RemainingFields(
+            _("Texts"),
+            css_class='column-right image-form'),
+    )
 
 class AddForm(ImageFormBase, zeit.cms.browser.form.AddForm):
+
+    title = _("Add image")
 
     form_fields = (
         zope.formlib.form.Fields(
@@ -46,11 +58,12 @@ class AddForm(ImageFormBase, zeit.cms.browser.form.AddForm):
 
 class EditForm(ImageFormBase, zeit.cms.browser.form.EditForm):
 
+    title = _("Edit image")
     form_fields = zope.formlib.form.Fields(
         zeit.content.image.interfaces.IImageSchema,
         render_context=True, omit_readonly=False)
 
-    @zope.formlib.form.action(_("Apply"),
+    @zope.formlib.form.action(_zope("Apply"),
                               condition=zope.formlib.form.haveInputWidgets)
     def handle_edit_action(self, action, data):
         if not data['data']:
