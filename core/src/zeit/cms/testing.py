97c92895fd65c3edbe6bff68eb4bf23ff5b9c6e5
Christian Theune <ct@gocept.com>
2007-12-10 11:43:04 +0000

    Improved test isolation by resetting the mock connector after each test.

--- core/src/zeit/cms/testing.py
+++ core/src/zeit/cms/testing.py
@@ -6,10 +6,14 @@ import os
 
 from zope.testing import doctest
 
+import zope.component
+
 import zope.app.testing.functional
 
+import zeit.connector.interfaces
 
-CMSLayer = zope.app.testing.functional.ZCMLLayer(
+
+cms_layer = zope.app.testing.functional.ZCMLLayer(
     os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
     __name__, 'CMSLayer', allow_teardown=True)
 
@@ -18,10 +22,18 @@ def FunctionalDocFileSuite(*paths, **kw):
     try:
         layer = kw['layer']
     except KeyError:
-        layer = CMSLayer
+        layer = cms_layer
     else:
         del kw['layer']
     kw['package'] = doctest._normalize_module(kw.get('package'))
+
+    def tearDown(test):
+        zope.app.testing.functional.FunctionalTestSetup().tearDown()
+        connector = zope.component.getUtility(
+            zeit.connector.interfaces.IConnector)
+        connector._reset()
+    kw['tearDown'] = tearDown
+
     test = zope.app.testing.functional.FunctionalDocFileSuite(
         *paths, **kw)
     test.layer = layer
