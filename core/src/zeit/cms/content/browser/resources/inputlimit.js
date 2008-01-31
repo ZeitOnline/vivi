04d6c52a50debf9c4389755df68adc377bcdfbcb
Roman Joost <roman@bromeco.de>
2008-01-31 10:14:08 +0000

    - fixes #3976

--- core/src/zeit/cms/content/browser/resources/inputlimit.js
+++ core/src/zeit/cms/content/browser/resources/inputlimit.js
@@ -9,27 +9,32 @@ zeit.cms.InputValidation = Class.extend({
         this.notify_div = getFirstElementByTagAndClassName(
             'div', 'show-input-limit', this.input.parentNode);
         this.max_length = getNodeAttribute(this.notify_div, 'maxlength');
+        this.timer = null;
 
-        connect(this.input, 'onkeyup', this, 'check_length');
-        connect(this.input, 'onchange', this, 'check_length');
-
-        this.update_notification();
-    },
-
-    check_length: function(event) {
-        if (this.input.value.length > this.max_length) {
-            this.input.value = this.input.value.substring(0, this.max_length);
-        }
-        this.update_notification();
-    },
-
-    update_notification: function() {
         replaceChildNodes(
             this.notify_div,
             SPAN({class: 'current-length'}, this.input.value.length),
             '/',
             SPAN({class: 'max-length'}, this.max_length));
+        this.current_length = getFirstElementByTagAndClassName(
+            'span', 'current-length', this.notify_div);
 
-    }
+        connect(this.input, 'onkeyup', this, 'check_length');
+        connect(this.input, 'onchange', this, 'check_length');
+    },
+
+    check_length: function(event) {
+        if (this.input.value.length > this.max_length)
+            this.input.value = this.input.value.substring(0, this.max_length);
+
+        if (this.timer != null) {
+            this.timer.cancel();
+        }
+        this.timer = callLater(0.4, this.update_notification, this);
+    },
+
+    update_notification: function(self) {
+        self.current_length.innerHTML = self.input.value.length;
+    },
 
 });
