00b56c2418b70b0586ec63337135f72e7453ca80
Christian Zagrodnick <cz@gocept.com>
2008-02-07 13:07:47 +0000

    clear cache after each test

--- core/src/zeit/connector/test.py
+++ core/src/zeit/connector/test.py
@@ -9,10 +9,16 @@ import zope.app.testing.functional
 import zope.app.appsetup.product
 from zope.testing import doctest
 
+import zeit.connector.cache
+
 
 real_connector_layer = zope.app.testing.functional.ZCMLLayer(
     os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
-    __name__, 'ConnectorLayer', allow_teardown=True)
+    __name__, 'ConnectorLayer')
+
+
+def tearDown(test):
+    zeit.connector.cache._cleanup()
 
 
 def test_suite():
@@ -21,6 +27,7 @@ def test_suite():
         'connector.txt',
         'mock.txt',
         'cache.txt',
+        tearDown=tearDown,
         optionflags=(doctest.REPORT_NDIFF + doctest.NORMALIZE_WHITESPACE +
                      doctest.ELLIPSIS)))
     functional = zope.app.testing.functional.FunctionalDocFileSuite(
