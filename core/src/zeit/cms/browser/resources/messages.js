383dbb12a558b4d7fb03abd6fc9351a3f623d989
Roman Joost <roman@bromeco.de>
2008-02-12 12:44:34 +0000

    fixes #3994

--- core/src/zeit/cms/browser/resources/messages.js
+++ core/src/zeit/cms/browser/resources/messages.js
@@ -22,5 +22,44 @@ connect(window, 'onload', function(event) {
         toggleElementClass('hiddenMessages', messages);
     });
 
-    
+    new TimerWidget("messages_counter", 5,
+        function(){
+            toggleElementClass('hiddenMessages', messages);
+        });
+
+});
+
+
+var TimerWidget = Class.extend({
+
+    construct: function(widget_id, seconds, func_on_stop) {
+        this.widget_id = widget_id;
+        this.widget = $(widget_id);
+        this.element = getFirstElementByTagAndClassName('span', 'Counter');
+        this.seconds = seconds
+        this.timer = null;
+        this.onStopFunction = func_on_stop;
+        this.startTimer();
+    },
+
+    startTimer: function() {
+        this.element.innerHTML = this.seconds
+        this.timer = callLater(1, this.count, this);
+    },
+
+    count: function(othis) {
+        othis.element.innerHTML = othis.seconds
+        othis.seconds--
+        othis.timer.cancel()
+        if (othis.seconds >= 0)
+            othis.timer = callLater(1, othis.count, othis);
+        else
+            othis.onStop();
+    },
+
+    onStop: function() {
+        this.onStopFunction();
+        hideElement(this.widget);
+    }
+
 });
