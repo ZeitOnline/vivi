8b9ee2e582a06083f26235ff70fb1d62079d88af
Christian Zagrodnick <cz@gocept.com>
2008-04-10 08:37:08 +0000

    next would be 0.9.9

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='0.9.8dev',
+    version='0.9.9dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
