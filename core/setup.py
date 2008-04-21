fb0437bc17bb0ba0c1c4862e09b34761c3d40612
Christian Zagrodnick <cz@gocept.com>
2008-04-21 08:31:41 +0000

    next would be 0.2

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.objectlog',
-    version='0.1dev',
+    version='0.2dev',
     author='Christian Zagrodnick',
     author_email='cz@gocept.com',
     description="""\
