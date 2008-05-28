6f04a9e0090507f9d1cef68e7a058e836e953099
Christian Zagrodnick <cz@gocept.com>
2008-05-28 11:28:59 +0000

    - Der HTML-Titel im CMS beinhaltet jetzt den Titel des Objekts (bug #4152).

--- core/setup.py
+++ core/setup.py
@@ -39,7 +39,7 @@ setup(
         'zc.sourcefactory',
         'zc.table',
         'zdaemon',
-        'zeit.cms>=0.9.15',
+        'zeit.cms>=0.9.16dev',
         'zeit.connector>=0.14',
         'zeit.objectlog>=0.2',
         'zope.app.apidoc',
