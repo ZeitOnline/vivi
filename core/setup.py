05891fe8b6dab3d667c084e52ae39a77f9b330a6
Christian Zagrodnick <cz@gocept.com>
2008-06-10 06:48:50 +0000

    updated cms requirement

--- core/setup.py
+++ core/setup.py
@@ -39,7 +39,7 @@ setup(
         'zc.sourcefactory',
         'zc.table',
         'zdaemon',
-        'zeit.cms>=0.9.16',
+        'zeit.cms>=0.9.17',
         'zeit.connector>=0.14',
         'zeit.objectlog>=0.2',
         'zope.app.apidoc',
