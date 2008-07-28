53840e61e4d0b4bbec6fbda6c173d5732fb71e01
Christian Zagrodnick <cz@gocept.com>
2008-07-28 14:47:51 +0000

    create a common list representation for simple types

--- core/src/zeit/cms/repository/browser/adapter.py
+++ core/src/zeit/cms/repository/browser/adapter.py
@@ -13,11 +13,12 @@ import zeit.cms.interfaces
 import zeit.cms.repository.interfaces
 
 
-class UnknownResourceListing(zeit.cms.browser.listing.BaseListRepresentation):
+class CMSContentListRepresentation(
+    zeit.cms.browser.listing.BaseListRepresentation):
     """Adapter for listing unknown content resources"""
 
     zope.interface.implements(zeit.cms.browser.interfaces.IListRepresentation)
-    zope.component.adapts(zeit.cms.repository.interfaces.IUnknownResource,
+    zope.component.adapts(zeit.cms.interfaces.ICMSContent,
                           zope.publisher.interfaces.IPublicationRequest)
 
     @property
@@ -28,29 +29,4 @@ class UnknownResourceListing(zeit.cms.browser.listing.BaseListRepresentation):
     def searchableText(self):
         return self.title
 
-    @property
-    def metadata(self):
-        url = zope.traversing.browser.absoluteURL(self, self.request)
-        id = self.context.__name__
-        return ('<span class="Metadata">%s</span><span'
-                ' class="DeleteId">%s</span>' %(url, id))
-
     page = volume = year = ressort = workflowState = modifiedBy = author = None
-
-
-class CollectionListRepresentation(
-    zeit.cms.browser.listing.BaseListRepresentation):
-
-    zope.interface.implements(zeit.cms.browser.interfaces.IListRepresentation)
-    zope.component.adapts(zeit.cms.repository.interfaces.ICollection,
-                          zope.publisher.interfaces.IPublicationRequest)
-
-    author = subtitle = byline = ressort = volume = page = year = None
-
-    @zope.cachedescriptors.property.Lazy
-    def title(self):
-        return self.context.__name__
-
-    @zope.cachedescriptors.property.Lazy
-    def searchableText(self):
-        return self.title
