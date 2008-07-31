b9734a82873e8b7e9249fcbc6ba317bda10ec1b0
Christian Zagrodnick <cz@gocept.com>
2008-07-31 07:52:03 +0000

    changed workflow test setup so the tests work reliably even when zeit.cms is eggified

--- core/src/zeit/workflow/test.py
+++ core/src/zeit/workflow/test.py
@@ -4,6 +4,8 @@
 
 import os
 import unittest
+import stat
+import tempfile
 
 from zope.testing import doctest
 
@@ -12,16 +14,42 @@ import zope.app.testing.functional
 import zeit.cms.testing
 
 
-WorkflowLayer = zope.app.testing.functional.ZCMLLayer(
+class WorkflowLayer(zope.app.testing.functional.ZCMLLayer):
+    """Layer which copies the publish/retract scripts and makes them
+    executable."""
+
+    def __init__(self, *args, **kwargs):
+        zope.app.testing.functional.ZCMLLayer.__init__(self, *args, **kwargs)
+        self._tempfiles = []
+
+    def setUp(self):
+        zope.app.testing.functional.ZCMLLayer.setUp(self)
+        product_config['publish-script']  = self._make_copy('publish.sh')
+        product_config['retract-script'] = self._make_copy('retract.sh')
+
+
+    def tearDown(self):
+        for name in self._tempfiles:
+            os.remove(name)
+        zope.app.testing.functional.ZCMLLayer.tearDown(self)
+
+    def _make_copy(self, script):
+        source = os.path.join(os.path.dirname(__file__), script)
+        fd, destination = tempfile.mkstemp(suffix=script)
+        self._tempfiles.append(destination)
+        f = os.fdopen(fd, 'w')
+        f.write(open(source).read())
+        f.close()
+        os.chmod(destination, stat.S_IRUSR|stat.S_IXUSR)
+        return destination
+
+
+WorkflowLayer = WorkflowLayer(
     os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
     __name__, 'WorkflowLayer', allow_teardown=True)
 
 
 product_config = {
-    'publish-script': os.path.join(
-        os.path.dirname(__file__), 'publish.sh'),
-    'retract-script': os.path.join(
-        os.path.dirname(__file__), 'retract.sh'),
     'path-prefix': 'work',
 }
 
