ba79b9f2ccab72c041ca740499df95da9830d3f5
Christian Zagrodnick <cz@gocept.com>
2008-08-04 07:30:33 +0000

    - Import der Vortagszugriffszahlen in mehreren Transaktionen.

--- core/src/zeit/today/yesterday.py
+++ core/src/zeit/today/yesterday.py
@@ -5,6 +5,8 @@
 import logging
 
 import gocept.runner
+import zc.queue
+import zope.annotation.interfaces
 import zope.app.appsetup.product
 import zope.app.locking.interfaces
 import zope.component
@@ -50,28 +52,29 @@ def webdav_properties(context):
     return zeit.connector.interfaces.IWebDAVProperties(context.context, None)
 
 
-def update_lifetime_counters():
-    storage = zope.component.getUtility(zeit.today.interfaces.ICountStorage,
-                                        name='yesterday')
-    repository = zope.component.getUtility(
-        zeit.cms.repository.interfaces.IRepository)
+class UpdateLifetimecounters(object):
 
-    for unique_id in storage:
+    TICKS = 0.025
+
+    def __call__(self):
+        ticks = self.TICKS
+        if self.queue:
+            self.process_one()
+            if not self.queue:
+                ticks = None
+        else:
+            self.fill_queue()
+
+        return ticks
+
+    def process_one(self):
+        unique_id, count_date, count = self.queue.pull()
         __traceback_info__ = (unique_id,)
         try:
-            content = repository.getContent(unique_id)
+            content = self.repository.getContent(unique_id)
         except (KeyError, ValueError):
-            log.error("Could not find %s" % unique_id)
-            continue
-
-        lifetime = zeit.today.interfaces.ILifeTimeCounter(content)
-        count_date = storage.get_count_date(unique_id)
-        count = storage.get_count(unique_id)
-
-        if (lifetime.last_count is not None
-            and lifetime.last_count >= count_date):
-            # Already counted
-            continue
+            log.warning("Could not find %s" % unique_id)
+            return
 
         lockable = zope.app.locking.interfaces.ILockable(content)
         try:
@@ -79,9 +82,15 @@ def update_lifetime_counters():
         except zope.app.locking.interfaces.LockingError:
             log.warning("Could not update %s because it is locked." %
                         unique_id)
-            continue
+            return
 
         try:
+            lifetime = zeit.today.interfaces.ILifeTimeCounter(content)
+
+            if (lifetime.last_count is not None
+                and lifetime.last_count >= count_date):
+                # Already counted
+                return
             if lifetime.first_count is None:
                 lifetime.first_count = count_date
                 lifetime.total_hits = count
@@ -93,10 +102,38 @@ def update_lifetime_counters():
             lockable.unlock()
         log.debug("Updated %s (%s hits)" % (unique_id, lifetime.total_hits))
 
+    def fill_queue(self):
+        for unique_id in self.storage:
+            count_date = self.storage.get_count_date(unique_id)
+            count = self.storage.get_count(unique_id)
+            self.queue.put((unique_id, count_date, count))
+
+    @zope.cachedescriptors.property.Lazy
+    def storage(self):
+        return zope.component.getUtility(zeit.today.interfaces.ICountStorage,
+                                         name='yesterday')
+    @zope.cachedescriptors.property.Lazy
+    def repository(self):
+        return zope.component.getUtility(
+            zeit.cms.repository.interfaces.IRepository)
+
+    @zope.cachedescriptors.property.Lazy
+    def queue(self):
+        site = zope.app.component.hooks.getSite()
+        annotations = zope.annotation.interfaces.IAnnotations(site)
+        try:
+            queue = annotations[__name__]
+        except KeyError:
+            queue = annotations[__name__] = zc.queue.CompositeQueue()
+        return queue
+
+
+def update_lifetime_counters():
+    ticks = UpdateLifetimecounters()()
+    return ticks
+
 
 # XXX the principal should go to product config
 @gocept.runner.appmain(ticks=3600, principal='zope.hitimporter')
 def main():
-    log.info("Updating hit counters.")
-    update_lifetime_counters()
-    log.info("Finished updating hit counters.")
+    return update_lifetime_counters()
