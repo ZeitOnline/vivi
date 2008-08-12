6971f81f42ece6f3a1615ed98021289ddaaa7e47
Thomas Lotze <tl@gocept.com>
2008-08-12 13:25:55 +0000

    register an ILogProcessor adapter that cuts down object log in the workflow view to the latest 20 entries

---
+++ core/src/zeit/workflow/browser/objectlog.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2008 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zope.component
+import zope.interface
+
+import zeit.objectlog.interfaces
+
+
+class ProcessForDisplay(object):
+
+    zope.component.adapts(zope.interface.Interface)
+    zope.interface.implements(zeit.objectlog.interfaces.ILogProcessor)
+
+    def __init__(self, context):
+        pass
+
+    def __call__(self, entries):
+        return tuple(entries)[-5:]
