abde88058d723579ab374f447fa326df5b6c00f4
Christian Zagrodnick <cz@gocept.com>
2008-08-27 11:15:52 +0000

    package structure

---
+++ core/setup.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2008 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.content.rawxml',
+    version='0.1dev',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='https://svn.gocept.com/repos/gocept-int/zeit.cms/zeit.content.rawxml',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages = ['zeit', 'zeit.content'],
+    install_requires=[
+        'setuptools',
+        'zeit.cms',
+    ],
+    extras_require={
+        'test': [
+            'z3c.etestbrowser',
+        ],
+    },
+)
---
+++ core/src/zeit/content/rawxml/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (c) 2008 gocept gmbh & co. kg
+# See also LICENSE.txt
+
