a5e164437a4fd5c3f79db37489b1f1e124808e82
Christian Zagrodnick <cz@gocept.com>
2008-08-28 09:01:23 +0000

    braucht cms 1.1.8

--- core/setup.py
+++ core/setup.py
@@ -19,7 +19,7 @@ setup(
     namespace_packages = ['zeit', 'zeit.content'],
     install_requires=[
         'setuptools',
-        'zeit.cms',
+        'zeit.cms>=1.1.8',
     ],
     extras_require={
         'test': [
