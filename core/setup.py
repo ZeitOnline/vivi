6d31c12d6350c5fdca69cb004b003deda19636be
Thomas Lotze <tl@gocept.com>
2008-10-23 07:38:43 +0000

    fixed version requirement for zeit.cms

--- core/setup.py
+++ core/setup.py
@@ -39,7 +39,7 @@ setup(
         'zc.sourcefactory',
         'zc.table',
         'zdaemon',
-        'zeit.cms>=1.2.4',
+        'zeit.cms>=1.2.3',
         'zeit.connector>=0.14',
         'zeit.objectlog>=0.2',
         'zope.app.apidoc',
