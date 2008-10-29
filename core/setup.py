232306f376d1c96985be23a1d31d3473e98bdc4c
Christian Zagrodnick <cz@gocept.com>
2008-10-29 09:15:03 +0000

    vb

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='1.2.5dev',
+    version='1.2.6dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
