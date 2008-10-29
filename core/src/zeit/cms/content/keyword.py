b2042b83ca62fa35b09b8aff7d0a6ee4460f5395
Christian Zagrodnick <cz@gocept.com>
2008-10-29 09:06:33 +0000

    - Keywords werden alle 5 Minuten neu geladen (Bug #4501).

--- core/src/zeit/cms/content/keyword.py
+++ core/src/zeit/cms/content/keyword.py
@@ -1,23 +1,22 @@
 # Copyright (c) 2007-2008 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
 
+import datetime
 import logging
+import lxml.objectify
+import pytz
+import rwproperty
+import threading
 import urllib2
 import weakref
-
-import rwproperty
-
-import zope.interface
 import zope.app.appsetup.product
-
-import gocept.lxml.objectify
+import zope.interface
 
 import zeit.cms.content.interfaces
 import zeit.cms.content.property
 
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class Keyword(object):
@@ -45,12 +44,14 @@ class Keyword(object):
 class KeywordUtility(object):
 
     zope.interface.implements(zeit.cms.content.interfaces.IKeywords)
-    _loaded = False
+    reload_interval = datetime.timedelta(seconds=360)
+    loaded = None
+    load_lock = threading.Lock()
 
-    @zope.cachedescriptors.property.readproperty
+    @property
     def root(self):
         self._load_keywords()
-        return self.root
+        return self._root
 
     def __contains__(self, code):
         self._load_keywords()
@@ -67,63 +68,86 @@ class KeywordUtility(object):
             if searchterm.lower() in x.lower())
 
     def _load_keywords(self):
-        if self._loaded:
-            return
+        if self.loaded is None:
+            self.load_lock.acquire()
+        else:
+            locked = False
+            if self.loaded + self.reload_interval < datetime.datetime.now(
+                pytz.UTC):
+                locked = self.load_lock.acquire(False)
+            if not locked:
+                return
+        try:
 
-        def pcv(node_name):
-            pcv_ns = 'http://prismstandard.org/namespaces/1.2/pcv/'
-            return '{%s}%s' % (pcv_ns, node_name)
+            def pcv(node_name):
+                pcv_ns = 'http://prismstandard.org/namespaces/1.2/pcv/'
+                return '{%s}%s' % (pcv_ns, node_name)
 
-        def rdf(node_name):
-            rdf_ns = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-            return '{%s}%s' % (rdf_ns, node_name)
+            def rdf(node_name):
+                rdf_ns = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+                return '{%s}%s' % (rdf_ns, node_name)
 
+            prism_tree = self.load_tree()
+            if prism_tree is None:
+                # Doesn't parse etc.
+                return
+
+            keywords = {}
+            descriptors = []
+            root_keyword = None
+            keywords_by_code = {}
+
+            # Iteration 1: Create keywords
+            for descriptor in prism_tree[pcv('Descriptor')][:]:
+                code = descriptor[pcv('code')]
+                label = descriptor[pcv('label')]
+                rdf_id = descriptor.get(rdf('ID'))
+
+                keyword = Keyword(code, label, True)
+                keywords[rdf_id] = keyword
+                keywords_by_code[unicode(code)] = keyword
+
+                descriptors.append((rdf_id, descriptor))
+
+            # Iteration 2: Connect keywords
+            for rdf_id, descriptor in descriptors:
+                keyword = keywords[rdf_id]
+                try:
+                    narrower_terms = descriptor[pcv('narrowerTerm')][:]
+                except AttributeError:
+                    pass
+                else:
+                    for narrower in narrower_terms:
+                        narrower_id = narrower.get(rdf('resource'))
+                        keyword.narrower.append(keywords[narrower_id])
+                try:
+                    broader = descriptor[pcv('broaderTerm')]
+                except AttributeError:
+                    if root_keyword is not None:
+                        raise ValueError("Found multiple root keywords.")
+                    root_keyword = keyword
+                else:
+                    broader_id = broader.get(rdf('resource'))
+                    keyword.broader = keywords[broader_id]
+
+            self._root = root_keyword
+            self._keywords_by_code = keywords_by_code
+            self.loaded = datetime.datetime.now(pytz.UTC)
+        finally:
+            self.load_lock.release()
+
+    def load_tree(self):
         cms_config = zope.app.appsetup.product.getProductConfiguration(
             'zeit.cms')
         url = cms_config['source-keyword']
-        logger.info('Loading keywords from %s' % url)
+        log.info('Loading keywords from %s' % url)
         request = urllib2.urlopen(url)
-        prism_tree = gocept.lxml.objectify.fromfile(request)
-        keywords = {}
-        descriptors = []
-        root_keyword = None
-        self._keywords_by_code = {}
+        try:
+            return lxml.objectify.parse(request).getroot()
+        except lxml.etree.XMLSyntaxError:
+            log.error('Could not load keywords.', exc_info=True)
+            return None
 
-        # Iteration 1: Create keywords
-        for descriptor in prism_tree[pcv('Descriptor')][:]:
-            code = descriptor[pcv('code')]
-            label = descriptor[pcv('label')]
-            rdf_id = descriptor.get(rdf('ID'))
-
-            keyword = Keyword(code, label, True)
-            keywords[rdf_id] = keyword
-            self._keywords_by_code[unicode(code)] = keyword
-
-            descriptors.append((rdf_id, descriptor))
-
-        # Iteration 2: Connect keywords
-        for rdf_id, descriptor in descriptors:
-            keyword = keywords[rdf_id]
-            try:
-                narrower_terms = descriptor[pcv('narrowerTerm')][:]
-            except AttributeError:
-                pass
-            else:
-                for narrower in narrower_terms:
-                    narrower_id = narrower.get(rdf('resource'))
-                    keyword.narrower.append(keywords[narrower_id])
-            try:
-                broader = descriptor[pcv('broaderTerm')]
-            except AttributeError:
-                if root_keyword is not None:
-                    raise ValueError("Found multiple root keywords.")
-                root_keyword = keyword
-            else:
-                broader_id = broader.get(rdf('resource'))
-                keyword.broader = keywords[broader_id]
-
-        self.root = root_keyword
-        self._loaded = True
 
 
 class KeywordsProperty(zeit.cms.content.property.MultiPropertyBase):
@@ -143,7 +167,7 @@ class KeywordsProperty(zeit.cms.content.property.MultiPropertyBase):
         rdf_id = unicode(node)
         if rdf_id in taxonomy:
             return taxonomy[rdf_id]
-        logger.warning("Ignored keyword %s, not in taxonomy." % rdf_id)
+        log.warning("Ignored keyword %s, not in taxonomy." % rdf_id)
         return None
 
     def _node_factory(self, entry, tree):
