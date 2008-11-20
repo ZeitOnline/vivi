d6a20fbb06ace8ae2c900032245bfc23eff4e790
Christian Zagrodnick <cz@gocept.com>
2008-11-20 14:09:29 +0000

    vb

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.seo',
-    version='1.2.6dev',
+    version='1.3dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
