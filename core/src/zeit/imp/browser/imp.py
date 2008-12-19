2ca56624b93e2d73352c7417b08863d0eb68c974
Christian Zagrodnick <cz@gocept.com>
2008-12-19 09:03:03 +0000

    added api for the image bar

--- core/src/zeit/imp/browser/imp.py
+++ core/src/zeit/imp/browser/imp.py
@@ -1,6 +1,10 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import cjson
+import zeit.cms.browser.view
+import zeit.content.image.interfaces
+
 
 class Imp(object):
 
@@ -11,3 +15,18 @@ class Imp(object):
     @property
     def height(self):
         return self.context.getImageSize()[1]
+
+
+class ImageBar(zeit.cms.browser.view.Base):
+
+    def __call__(self):
+        result = []
+        for obj in self.context.__parent__.values():
+            if not zeit.content.image.interfaces.IImage.providedBy(obj):
+                continue
+            if obj == self.context:
+                continue
+            result.append(dict(
+                url=self.url(obj),
+                name=obj.__name__))
+        return cjson.encode(result)
--- core/src/zeit/imp/browser/test.py
+++ core/src/zeit/imp/browser/test.py
@@ -1,10 +1,64 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os.path
+import cjson
+import pkg_resources
+import transaction
 import unittest
+import zeit.cms.repository.interfaces
 import zeit.cms.testing
 import zeit.imp.test
+import zope.app.testing.functional
+import zope.component
+
+
+class ImageBarTest(zope.app.testing.functional.BrowserTestCase):
+
+    layer=zeit.imp.test.imp_layer
+
+    def setUp(self):
+        super(ImageBarTest, self).setUp()
+        self.setSite(self.getRootFolder())
+        self.repository = zope.component.getUtility(
+            zeit.cms.repository.interfaces.IRepository)
+
+    def tearDown(self):
+        del self.repository
+        self.setSite(None)
+        super(ImageBarTest, self).tearDown()
+
+    def get_image_bar_data(self):
+        response = self.publish(
+            '/++skin++cms/repository/2006/DSC00109_2.JPG/@@imp-image-bar',
+            basic='user:userpw')
+        return cjson.decode(response.getBody())
+
+    def assertAPI(self, expected):
+        self.assertEquals(expected, self.get_image_bar_data())
+
+    def test_no_other_images_but_other_objects_return_empty_list(self):
+        self.assertAPI([])
+
+    def test_other_images(self):
+        image = zeit.content.image.image.LocalImage()
+        image.open('w').write(pkg_resources.resource_string(
+            __name__, 'testdata/01.jpg'))
+        self.repository['2006']['foo-240x120.jpg'] = image
+        self.assertAPI([{
+            'url': 'http://localhost/++skin++cms/repository/2006/foo-240x120.jpg',
+            'name': 'foo-240x120.jpg'}])
+
+        # Another image
+        image = zeit.content.image.image.LocalImage()
+        image.open('w').write(pkg_resources.resource_string(
+            __name__, 'testdata/02.jpg'))
+        self.repository['2006']['foo-artikel.jpg'] = image
+        transaction.commit()
+        self.assertAPI([
+            {'url': 'http://localhost/++skin++cms/repository/2006/foo-240x120.jpg',
+             'name': 'foo-240x120.jpg'},
+            {'url': 'http://localhost/++skin++cms/repository/2006/foo-artikel.jpg',
+             'name': 'foo-artikel.jpg'}])
 
 
 def test_suite():
@@ -12,4 +66,5 @@ def test_suite():
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
         layer=zeit.imp.test.imp_layer))
+    suite.addTest(unittest.makeSuite(ImageBarTest))
     return suite
