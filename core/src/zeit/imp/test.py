bab9e0b900474c2882fecd26204c3d91ffc6986f
Christian Zagrodnick <cz@gocept.com>
2008-12-19 09:14:37 +0000

    pkg_resources statt os.path

--- core/src/zeit/imp/test.py
+++ core/src/zeit/imp/test.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os.path
+import pkg_resources
 import unittest
 import zeit.imp.mask
 import zope.app.testing.functional
 
 
 imp_layer = zope.app.testing.functional.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+    pkg_resources.resource_filename(__name__, 'ftesting.zcml'),
     __name__, 'ImpLayer', allow_teardown=True)
 
 
@@ -18,8 +18,8 @@ class TestLayerMask(unittest.TestCase):
         # Create a 20x30 mask in an 150x100 image
         mask = zeit.imp.mask.Mask((150, 100), (20, 30))
         mask_data = mask.open('r').read()
-        expected_data = open(os.path.join(
-            os.path.dirname(__file__), 'test_mask.png')).read()
+        expected_data = pkg_resources.resource_string(
+            __name__, 'test_mask.png')
         self.assertEquals(expected_data, mask_data,
                           "Mask doesn't match expected mask.")
 
