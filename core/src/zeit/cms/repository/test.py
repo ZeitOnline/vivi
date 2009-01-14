08ec371a7c29a93c6a35eec4018e2c570e57a7f1
Christian Zagrodnick <cz@gocept.com>
2009-01-14 10:28:01 +0000

    split unrelated tests from readme.txt into separate files

--- core/src/zeit/cms/repository/test.py
+++ core/src/zeit/cms/repository/test.py
@@ -12,5 +12,7 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
+        'cache.txt',
+        'preference.txt',
         'file.txt'))
     return suite
