446fa11c030160e7065f1b3197f10c239e2a3dd9
Christian Zagrodnick <cz@gocept.com>
2009-02-02 14:42:07 +0000

    move tests to selenium.py

--- core/src/zeit/cms/repository/browser/selenium.py
+++ core/src/zeit/cms/repository/browser/selenium.py
@@ -5,7 +5,29 @@ import xml.sax.saxutils
 import zeit.cms.selenium
 
 
-class Test(zeit.cms.selenium.Test):
+class TestTree(zeit.cms.selenium.Test):
+
+    def test_tree_keeps_state(self):
+        s = self.selenium
+
+        s.comment(
+            "Delete the tree state cookie to have a defined starting point")
+        s.deleteCookie('zeit.cms.repository.treeState', '/')
+        self.open('/')
+        s.verifyTextPresent('online')
+
+        s.comment("Open `online`")
+        s.click('//li[@uniqueid="http://xml.zeit.de/online"]')
+        s.waitForElementPresent(
+            '//li[@uniqueid="http://xml.zeit.de/online/2007"]')
+
+        s.comment("Tree is still open after reload.")
+        self.open('/')
+        s.verifyElementPresent(
+            '//li[@uniqueid="http://xml.zeit.de/online/2007"]')
+
+
+class TestListing(zeit.cms.selenium.Test):
 
     def test_tablelisting_filter(self):
         s = self.selenium
