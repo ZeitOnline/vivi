ca266c09721d28287d7172f60824e3ea6f425b09
Christian Zagrodnick <cz@gocept.com>
2009-03-03 09:53:13 +0000

    image_groups koÌnnen mit "lokaler" datei angelegt werden (benoÌtigt vom IMP)

--- core/src/zeit/content/image/test.py
+++ core/src/zeit/content/image/test.py
@@ -9,11 +9,12 @@ import zeit.content.image.image
 import zeit.content.image.imagegroup
 import zope.app.testing.functional
 import zope.component
+import pkg_resources
 from zope.testing import doctest
 
 
 ImageLayer = zope.app.testing.functional.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+    pkg_resources.resource_filename(__name__, 'ftesting.zcml'),
     __name__, 'ImageLayer', allow_teardown=True)
 
 
@@ -28,7 +29,7 @@ def test_suite():
     return suite
 
 
-def create_image_group():
+def create_image_group(file_name=None):
     repository = zope.component.getUtility(
         zeit.cms.repository.interfaces.IRepository)
     group = zeit.content.image.imagegroup.ImageGroup()
@@ -40,22 +41,26 @@ def create_image_group():
         image = zeit.content.image.image.LocalImage()
         image.contentType = 'image/jpeg'
         fh = image.open('w')
-        fh.write(open(os.path.join(
-            os.path.dirname(__file__),
-            'browser', 'testdata', filename), 'rb').read())
+        if file_name is None:
+            file_name = pkg_resources.resource_filename(
+                __name__, 'browser/testdata/%s' % filename)
+        fh.write(open(file_name, 'rb').read())
         fh.close()
         group[filename] = image
     return group
 
 
-def create_image_group_with_master_image():
+def create_image_group_with_master_image(file_name=None):
     repository = zope.component.getUtility(
         zeit.cms.repository.interfaces.IRepository)
     group = zeit.content.image.imagegroup.ImageGroup()
     group.master_image = u'master-image.jpg'
     repository['group'] = group
     image = zeit.content.image.image.LocalImage()
-    image.open('w').write(
-        repository['2006']['DSC00109_2.JPG'].open().read())
+    if file_name is None:
+        fh = repository['2006']['DSC00109_2.JPG'].open()
+    else:
+        fh = open(file_name)
+    image.open('w').write(fh.read())
     repository['group']['master-image.jpg'] = image
     return repository['group']
