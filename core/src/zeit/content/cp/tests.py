b01932d64244001fa04b509dc3a1837c59afa632
Christian Zagrodnick <cz@gocept.com>
2009-03-30 06:56:06 +0000

    use pkg_resources instead of os.foo

--- core/src/zeit/content/cp/tests.py
+++ core/src/zeit/content/cp/tests.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os
+import pkg_resources
 import unittest
 import zeit.cms.testing
 import zope.app.testing.functional
 
 
 layer = zope.app.testing.functional.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+    pkg_resources.resource_filename(__name__, 'ftesting.zcml'),
     __name__, 'zeit.content.cp.tests.layer', allow_teardown=True)
 
 
