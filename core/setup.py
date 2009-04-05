7c4114ae0626d6b9d61dda351b0bedaa2eca26c0
Christian Zagrodnick <cz@gocept.com>
2009-04-05 14:04:30 +0000

    vb

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.objectlog',
-    version='0.8dev',
+    version='0.9dev',
     author='Christian Zagrodnick',
     author_email='cz@gocept.com',
     description="""\
