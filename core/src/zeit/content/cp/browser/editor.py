6d232cebdf4eb0d35d009cbf4d419f09e860f0a8
Christian Zagrodnick <cz@gocept.com>
2009-04-14 13:49:21 +0000

    remove unused imports

--- core/src/zeit/content/cp/browser/editor.py
+++ core/src/zeit/content/cp/browser/editor.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import zope.component
-import zope.viewlet.interfaces
-import zope.viewlet.viewlet
 from zeit.content.cp.i18n import MessageFactory as _
 
 
