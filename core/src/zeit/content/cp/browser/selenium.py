40d11db931deb1e4a0ac6c48933c644ef90dd248
Christian Zagrodnick <cz@gocept.com>
2009-04-16 09:45:31 +0000

    added selenium test for deleting
     (re #4805)

--- core/src/zeit/content/cp/browser/selenium.py
+++ core/src/zeit/content/cp/browser/selenium.py
@@ -2,7 +2,10 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import zeit.cms.repository.interfaces
 import zeit.cms.selenium
+import zeit.cms.testcontenttype.testcontenttype
+import zope.component
 
 
 class Test(zeit.cms.selenium.Test):
@@ -25,14 +28,14 @@ class TestGenericEditing(Test):
     def test_insert(self):
         self.open_centerpage()
         s = self.selenium
-        s.verifyElementNotPresent('//a[@class="choose-block"]')
+        s.verifyElementNotPresent('css=a.choose-block')
         s.click('link=*Add block*')
-        s.waitForElementPresent('//a[@class="choose-block"]')
-        s.click('//a[@class="choose-block"]')
-        s.waitForElementPresent('//div[@class="block-types"]')
+        s.waitForElementPresent('css=a.choose-block')
+        s.click('css=a.choose-block')
+        s.waitForElementPresent('css=div.block-types')
         s.click('link=List of teasers')
-        s.waitForElementPresent('//div[@class="block type-teaser"]')
-        s.click('//div[@class="block type-teaser"]/*/div[@class="edit"]/a')
+        s.waitForElementPresent('css=div.block.type-teaser')
+        s.click('css=div.block.type-teaser > * > div.edit > a')
         s.waitForElementPresent('id=lightbox.form')
         s.type('form.title', 'Holladrio')
         s.click('form.actions.apply')
@@ -93,6 +96,26 @@ class TestGenericEditing(Test):
 
 class TestTeaserList(Test):
 
+    def create_clip(self):
+        # Creat clip
+        s = self.selenium
+        s.click('id=clip-add-folder-link')
+        s.type('id=clip-add-folder-title', 'Clip')
+        s.click('id=clip-add-folder-submit')
+        s.waitForElementPresent('link=Clip')
+        # Open clip
+        s.click('//li[@uniqueid="Clip"]')
+        s.waitForElementPresent('//li[@uniqueid="Clip"][@action="collapse"]')
+
+    def clip_object(self, match):
+        s = self.selenium
+        s.click('xpath=//td[contains(string(.), "%s")]' % match)
+        s.waitForElementPresent('css=div#bottomcontent > div')
+        s.dragAndDropToObject(
+            'xpath=//td[contains(string(.), "%s")]' % match,
+            '//li[@uniqueid="Clip"]')
+        s.pause(500)
+
     def create_teaserlist(self):
         self.open_centerpage()
         s = self.selenium
@@ -103,27 +126,13 @@ class TestTeaserList(Test):
         s.click('link=List of teasers')
         s.waitForElementPresent('css=div.type-teaser')
 
-    def test_adding_via_drag_and_drop(self):
+    def test_adding_via_drag_and_drop_from_clipboard(self):
         self.open('/')
         s = self.selenium
 
-        # First, we need to fill the clipboard.
-        # Creat clip
-        s.click('id=clip-add-folder-link')
-        s.type('id=clip-add-folder-title', 'Clip')
-        s.click('id=clip-add-folder-submit')
-        s.waitForElementPresent('link=Clip')
-        # Open clip
-        s.click('//li[@uniqueid="Clip"]')
-        s.waitForElementPresent('//li[@uniqueid="Clip"][@action="collapse"]')
-
+        self.create_clip()
         s.clickAndWait('link=Dateiverwaltung')
-        s.click('xpath=//td[contains(string(.), "testcontent")]')
-        s.waitForElementPresent('css=div#bottomcontent > div')
-        s.dragAndDropToObject(
-            'xpath=//td[contains(string(.), "testcontent")]',
-            '//li[@uniqueid="Clip"]')
-        s.pause(500)
+        self.clip_object('testcontent')
 
         self.create_teaserlist()
 
@@ -131,6 +140,49 @@ class TestTeaserList(Test):
             '//li[@uniqueid="Clip/testcontent"]',
             'css=div.type-teaser')
         s.waitForElementPresent('css=div.supertitle')
+
+    def test_delete(self):
+        s = self.selenium
+        # Create test content
+        s.open('/@@create-cp-test-content')
+        self.open('/')
+        self.create_clip()
+        s.clickAndWait('link=Dateiverwaltung')
+        self.clip_object('c1')
+        self.clip_object('c2')
+        self.clip_object('c3')
+
+        self.create_teaserlist()
+
+        s.dragAndDropToObject(
+            '//li[@uniqueid="Clip/c3"]',
+            'css=div.type-teaser')
+        s.waitForTextPresent('c3 teaser')
+        s.dragAndDropToObject(
+            '//li[@uniqueid="Clip/c2"]',
+            'css=div.type-teaser')
+        s.waitForTextPresent('c2 teaser')
+        s.dragAndDropToObject(
+            '//li[@uniqueid="Clip/c1"]',
+            'css=div.type-teaser')
+        s.waitForTextPresent('c1 teaser')
+
+        s.click('link=Edit teaser list')
+        s.waitForElementPresent('css=div.teaser-list-edit-box')
+
+        s.verifyXpathCount(
+            '//div[@id="lightbox"]//a[@class="delete-link"]', 3)
+        s.click(
+            '//div[@id="lightbox"]//li[contains(string(.), "c2 teaser")]/'
+            'a[@class="delete-link"]')
+        s.waitForElementPresent('css=.confirm-delete')
+        s.click('css=.confirm-delete > a')
+        s.waitForXpathCount(
+            '//div[@id="lightbox"]//a[@class="delete-link"]', 2)
+
+        # When closing the lightbox the c2 teaser goes away
+        s.click('css=a.CloseButton')
+        s.waitForTextNotPresent('c2 teaser')
 
 
 class TestTeaserMosaic(Test):
@@ -190,5 +242,22 @@ class TestTeaserMosaic(Test):
             '//div[@class="block type-teaser-bar"][2]@id', '${bar2}')
         s.verifyAttribute(
             '//div[@class="block type-teaser-bar"][3]@id', '${bar1}')
+
+
+class CreateTestContent(object):
+
+    def __call__(self):
+        repository = zope.component.getUtility(
+            zeit.cms.repository.interfaces.IRepository)
+        c1 = zeit.cms.testcontenttype.testcontenttype.TestContentType()
+        c1.teaserTitle = c1.shortTeaserTitle = u'c1 teaser'
+        repository['c1'] = c1
+        c2 = zeit.cms.testcontenttype.testcontenttype.TestContentType()
+        c2.teaserTitle = c2.shortTeaserTitle = u'c2 teaser'
+        repository['c2'] = c2
+        c3 = zeit.cms.testcontenttype.testcontenttype.TestContentType()
+        c3.teaserTitle = c3.shortTeaserTitle = u'c3 teaser'
+        repository['c3'] = c3
+        return 'Done.'
 
 
