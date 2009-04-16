1b45e64dd0bf96c7c6442ea466b242769eee7ba6
Sebastian Wehrmann <sw@gocept.com>
2009-04-16 09:25:49 +0000

    - Add logging output.

--- core/src/zeit/content/article/cds.py
+++ core/src/zeit/content/article/cds.py
@@ -9,11 +9,15 @@ import zeit.content.article.interfaces
 import zeit.cms.workflow.interfaces
 import zeit.cms.content.interfaces
 import zeit.connector.interfaces
+import logging
+
+log = logging.getLogger(__name__)
 
 def get_cds_filestore():
     config = zope.app.appsetup.product.getProductConfiguration(
         'zeit.content.article')
     store_dir = config.get('cds-export')
+    log.debug('Filestore is set to %s.' % store_dir)
     if store_dir is None:
         return None
     result = gocept.filestore.FileStore(store_dir)
@@ -24,15 +28,20 @@ def get_cds_filestore():
     zeit.content.article.interfaces.IArticle,
     zeit.cms.workflow.interfaces.IPublishedEvent)
 def export(object, event):
+    log.debug('Start export to Content-Drehscheibe')
     if not object.export_cds:
+        log.debug('Export flag is not set. Exiting.')
         return
     fs = get_cds_filestore()
     if fs is None:
+        log.debug('Filestore is not set. Exiting.')
         return
     uuid = zeit.cms.content.interfaces.IUUID(object).id
     filename = '%s.xml' % uuid
+    log.info('Exporting %s to Content-Drehscheibe.' % filename)
     f = fs.create(filename)
     data = zeit.connector.interfaces.IResource(object)
     f.write(data.data.read())
     f.close()
     fs.move(filename, 'tmp', 'new')
+    log.debug('File %s was created. Leaving.' % filename)
