9ee5fd9395ce6e51cd636f12ff4cc5d97ccd28a9
Martijn Faassen <faassen@startifact.com>
2009-04-16 08:14:56 +0000

    A refactoring to make the test and the code reuse the way to get to the cds store.

--- core/src/zeit/content/article/cds.py
+++ core/src/zeit/content/article/cds.py
@@ -11,19 +11,25 @@ import zeit.cms.workflow.interfaces
 import zeit.cms.content.interfaces
 import zeit.connector.interfaces
 
+def get_cds_filestore():
+    config = zope.app.appsetup.product.getProductConfiguration(
+        'zeit.content.article') 
+    store_dir = config.get('cds_filestore')
+    if store_dir is None:
+        return None
+    result = gocept.filestore.FileStore(store_dir)
+    result.prepare()
+    return result
+
 @zope.component.adapter(
     zeit.content.article.interfaces.IArticle,
     zeit.cms.workflow.interfaces.IPublishedEvent)
 def export(object, event):
-    config = zope.app.appsetup.product.getProductConfiguration(
-        'zeit.content.article')
-    store_dir = config.get('cds_filestore')
+    fs = get_cds_filestore()
     uuid = zeit.cms.content.interfaces.IUUID(object).id
-    filename = '%s.xml' % uuid
-    if store_dir is None or uuid is None:
+    if fs is None or uuid is None:
         return
-    fs = gocept.filestore.FileStore(store_dir)
-    fs.prepare()
+    filename = '%s.xml' % uuid
     f = fs.create(filename)
     data = zeit.connector.interfaces.IResource(object)
     f.write(data.data.read())
