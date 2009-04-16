5901f19be341768c3a94966c6b41882872b56762
Christian Zagrodnick <cz@gocept.com>
2009-04-16 11:36:28 +0000

    cleanup

--- core/src/zeit/cms/settings/test.py
+++ core/src/zeit/cms/settings/test.py
@@ -2,15 +2,12 @@
 # See also LICENSE.txt
 
 import unittest
-
+import zeit.cms.testing
 from zope.testing import doctest
 
-import zeit.cms.testing
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
-        optionflags=(doctest.REPORT_NDIFF + doctest.NORMALIZE_WHITESPACE +
-                     doctest.ELLIPSIS + doctest.INTERPRET_FOOTNOTES)))
     return suite
