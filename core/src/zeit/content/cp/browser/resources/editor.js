b9178b1c546d6d5b34af72bef8a399138d586f44
Christian Zagrodnick <cz@gocept.com>
2009-04-17 15:39:43 +0000

    Refactored connect/disconnect handling into contexts so a class doesn't need to implement any of the context switching things.
    enabled sorting of teasers in the teaserlist box
    
    (re #4805)

--- core/src/zeit/content/cp/browser/resources/editor.js
+++ core/src/zeit/content/cp/browser/resources/editor.js
@@ -90,17 +90,111 @@ MochiKit.Signal.connect(window, 'cp-editor-initialized', function() {
     zeit.content.cp.block_hover = new zeit.content.cp.BlockHover();
 });
 
+
+//
+// Define context managers
+//
+zeit.content.cp.in_context = {};
+
+
+zeit.content.cp.in_context.Base = gocept.Class.extend({
+
+    __name__: 'zeit.content.cp.in_context.Base',
+
+    construct: function(context_aware) {
+        var self = this;
+        log("Creating " + self.__name__ + " for " + context_aware.__name__)
+        self.context_aware = context_aware;
+        self.events = [];
+        
+        self.init();
+
+        self.events.push(MochiKit.Signal.connect(
+            zeit.content.cp.editor, 'single-context-start',
+            self, self.deactivate));
+        self.events.push(MochiKit.Signal.connect(
+            zeit.content.cp.editor, 'single-context-end',
+            self, self.activate));
+    },
+
+    destroy: function() {
+        var self = this;
+        while(self.events.length) {
+          MochiKit.Signal.disconnect(self.events.pop());
+        }
+    },
+
+    activate: function() {
+        var self = this;
+        log('Activating ' + self.context_aware.__name__);
+        self.context_aware.connect.call(self.context_aware);
+    },
+
+    deactivate: function() {
+        var self = this;
+        log('Deactivating ' + self.context_aware.__name__);
+        self.context_aware.disconnect.call(self.context_aware);
+    },
+});
+
+
+zeit.content.cp.in_context.Editor = zeit.content.cp.in_context.Base.extend({
+
+    __name__: 'zeit.content.cp.in_context.Editor',
+
+    init: function() {
+        var self = this;
+        // Those handlers stay forever
+        self.events.push(MochiKit.Signal.connect(
+            zeit.content.cp.editor, 'after-reload',
+            self, self.activate));
+        self.events.push(MochiKit.Signal.connect(
+            zeit.content.cp.editor, 'before-reload',
+            self, self.deactivate));
+    },
+
+});
+
+
+zeit.content.cp.in_context.Lightbox = zeit.content.cp.in_context.Base.extend({
+
+    __name__: 'zeit.content.cp.in_context.Lightbox',
+
+    init: function() {
+        var self = this;
+        MochiKit.Signal.signal(zeit.content.cp.editor, 'single-context-start');
+        self.activate();
+        self.events.push(MochiKit.Signal.connect(
+            zeit.content.cp.lightbox.lightbox, 'before-close',
+            self, self.deactivate));
+        self.events.push(MochiKit.Signal.connect(
+            zeit.content.cp.lightbox, 'before-reload',
+            self, self.deactivate));
+    },
+
+    deactivate: function() {
+        var self = this;
+        arguments.callee.$.deactivate.call(self);
+        self.destroy();
+    },
+});
+
+
 zeit.content.cp.ContentActionBase = gocept.Class.extend({
+
+    __name__: 'zeit.content.cp.ContentActionBase',
+    context: null,  // define in sublcasses
 
     construct: function() {
         var self = this;
         self.editor = zeit.content.cp.editor;
-        MochiKit.Signal.connect(
-            self.editor, 'before-reload', self, 'disconnect');
-        MochiKit.Signal.connect(
-            self.editor, 'after-reload', self, 'connect');
         self.dnd_objects = [];
         self.events = [];
+        if (isNull(self.context)) {
+            log("No context for "+self.__name__);
+        } else {
+            new self.context(self);
+        }
     },
 
     disconnect: function() {
@@ -118,6 +212,9 @@ zeit.content.cp.ContentActionBase = gocept.Class.extend({
 
 zeit.content.cp.ContentDropper = zeit.content.cp.ContentActionBase.extend({
     // Handle dropping of content objects.
+
+    __name__: 'zeit.content.cp.ContentDropper',
+    context: zeit.content.cp.in_context.Editor,
 
     connect: function() {
         var self = this;
@@ -154,60 +251,66 @@ zeit.content.cp.ContentDropper = zeit.content.cp.ContentActionBase.extend({
     },
 });
 
+
 MochiKit.Signal.connect(window, 'cp-editor-initialized', function() {
     zeit.content.cp.content_dropper = new zeit.content.cp.ContentDropper();
 });
 
 
-zeit.content.cp.TeaserBarSorter = zeit.content.cp.ContentActionBase.extend({
+zeit.content.cp.Sortable = zeit.content.cp.ContentActionBase.extend({
     // Sort the teaser bars.
     // This class is very specific right now. I hope to make it reusable for
     // the other sort tasks.
 
+    __name__: 'zeit.content.cp.Sortable',
+    default_options: {
+        onChange: MochiKit.Base.noop,
+        scroll: 'cp-content',
+    },
+
+    construct: function(container, passed_options) {
+        var self = this;
+
+        self.container = container;
+        var options = MochiKit.Base.update(
+            MochiKit.Base.clone(self.default_options), passed_options);
+        MochiKit.Sortable.sortables[container] = options;
+
+        arguments.callee.$.construct.call(self);
+    },
+
     connect: function() {
         var self = this;
-        self.container = $('cp-teasermosaic');
-        var bars = self.get_sortable_nodes();
-        forEach(bars, function(bar) {
-            var handle = MochiKit.Selector.findChildElements(
-                bar, ['> .block-inner > .edit > .dragger'])[0];
+        var container = $(self.container);
+
+        var nodes = self.get_sortable_nodes();
+        forEach(nodes, function(node) {
+            var handle = self.get_handle(node);
             self.dnd_objects.push(
-                new MochiKit.DragAndDrop.Draggable(bar, {
+                new MochiKit.DragAndDrop.Draggable(node, {
                     constraint: 'vertical',
                     handle: handle,
                     ghosting: false,
                     revert: true,
-                    scroll: $('cp-content'),
+                    scroll: self.options()['scroll'],
                     selectclass: 'hover',
                     zindex: 10000,
             }));
             self.dnd_objects.push(
-                new MochiKit.DragAndDrop.Droppable(bar, {
-                    containment: [self.container],
+                new MochiKit.DragAndDrop.Droppable(node, {
+                    containment: [container],
                     onhover: MochiKit.Sortable.onHover,
                     overlap: 'vertical',
            }));
         });
-        var options = {
-            onChange: MochiKit.Base.noop,
-            onUpdate: function(element) { self.on_update(element) },
-        }
-        MochiKit.Sortable.sortables[self.container.id] = options;
         
-        options.lastValue = self.serialize(self.container);
+        self.options().lastValue = self.serialize();
         this.events.push(MochiKit.Signal.connect(
             MochiKit.DragAndDrop.Draggables, 'start',
-            function(draggable) { self.on_start(self.container, draggable); }));
+            function(draggable) { self.on_start(container, draggable); }));
         this.events.push(MochiKit.Signal.connect(
             MochiKit.DragAndDrop.Draggables, 'end',
-            function(draggable) { self.on_end(self.container, draggable); }));
-    },
-
-    get_sortable_nodes: function() {
-        var self = this;
-        return MochiKit.Selector.findChildElements(
-            self.editor.contents,
-            ['#cp-teasermosaic > div.block.type-teaser-bar']);
+            function(draggable) { self.on_end(container, draggable); }));
     },
 
     on_start: function(element, draggable) {
@@ -220,18 +323,24 @@ zeit.content.cp.TeaserBarSorter = zeit.content.cp.ContentActionBase.extend({
         MochiKit.Sortable.unmark();
         if (!MochiKit.Base.arrayEqual(self.options().lastValue,
                                       self.serialize())) {
-            self.options().onUpdate(element);
+            self.on_update(element);
         }
     },
 
     on_update: function(element) {
         var self = this;
         var keys = MochiKit.Base.serializeJSON(self.serialize());
-        var url = self.container.getAttribute('cms:url') + '/@@updateOrder';
+        var url = $(self.container).getAttribute('cms:url') + '/@@updateOrder';
         var d = MochiKit.Async.doSimpleXMLHttpRequest(url, {keys:keys});
         // We don't have do anything on success as the ordering is already
         // applied in the HTML.
         // XXX error handling!
+        return d;
+    },
+
+
+    get_handle: function(element) {
+        return null;  // make the whole element draggable
     },
 
     serialize: function() {
@@ -242,11 +351,34 @@ zeit.content.cp.TeaserBarSorter = zeit.content.cp.ContentActionBase.extend({
 
     options: function() {
         var self = this;
-        return MochiKit.Sortable.options(self.container.id)
+        return MochiKit.Sortable.options(self.container)
     },
 
 });
 
+
+zeit.content.cp.TeaserBarSorter = zeit.content.cp.Sortable.extend({
+
+    __name__: 'zeit.content.cp.TeaserBarSorter',
+    context: zeit.content.cp.in_context.Editor,
+
+    construct: function() {
+        var self = this;
+        arguments.callee.$.construct.call(self, 'cp-teasermosaic');
+    },
+
+    get_sortable_nodes: function() {
+        var self = this;
+        return MochiKit.Selector.findChildElements(
+            self.editor.contents,
+            ['#cp-teasermosaic > div.block.type-teaser-bar']);
+    },
+
+    get_handle: function(element) {
+        return MochiKit.Selector.findChildElements(
+            element, ['> .block-inner > .edit > .dragger'])[0];
+    },
+});
 
 MochiKit.Signal.connect(window, 'cp-editor-initialized', function() {
     zeit.content.cp.teaser_bar_sorter = new zeit.content.cp.TeaserBarSorter();
@@ -262,6 +394,7 @@ zeit.content.cp.modules.LightBoxForm = zeit.cms.LightboxForm.extend({
 
     construct: function(context_element) {
         var self = this;
+        self.closed = false;
         zeit.content.cp.lightbox = self;
         var url = context_element.getAttribute('href');
         arguments.callee.$.construct.call(self, url, $('content'));
@@ -271,6 +404,12 @@ zeit.content.cp.modules.LightBoxForm = zeit.cms.LightboxForm.extend({
         self.close_event_handle = MochiKit.Signal.connect(
             self.lightbox, 'before-close',
             self, self.on_close);
+    },
+
+    reload: function() {
+        var self = this;
+        MochiKit.Signal.signal(self, 'before-reload');
+        arguments.callee.$.reload.call(self);
     },
 
     handle_submit: function(action) {
---
+++ core/src/zeit/content/cp/browser/resources/teaser.js
@@ -0,0 +1,36 @@
+zeit.content.cp.teaser = {};
+
+zeit.content.cp.teaser.Sortable = zeit.content.cp.Sortable.extend({
+
+    __name__: 'zeit.content.cp.teaser.Sortable',
+    context: zeit.content.cp.in_context.Lightbox,
+
+    construct: function() {
+        var self = this;
+        arguments.callee.$.construct.call(
+            self, 'teaser-list-edit-box-sorter');
+    },
+
+    get_sortable_nodes: function() {
+        var self = this;
+        return MochiKit.Sortable.findChildren(
+            $(self.container), null, false, 'li');
+    },
+
+    serialize: function() {
+        var self = this;
+        return MochiKit.Base.map(
+            function(e) { return e.getAttribute('cms:uniqueId')},
+            self.get_sortable_nodes());
+    },
+
+    on_update: function(element) {
+        var self = this;
+        var d = arguments.callee.$.on_update.call(self);
+        d.addCallback(function(result) {
+            zeit.content.cp.lightbox.reload();
+        });
+        return d;
+    },
+    
+});
