02ff1b7fe48d565ebbdc21382235b2a039783e10
Martijn Faassen <faassen@startifact.com>
2009-04-20 11:37:42 +0000

    Make the json-template story work.

--- core/src/zeit/find/browser/resources/find.js
+++ core/src/zeit/find/browser/resources/find.js
@@ -1,32 +1,25 @@
 (function() {
 
-var json_callback = function(result) {
-    log('success');
-    /*
+var json_callback = function(json) {
     var template_url = json['template_url'];
     var d = MochiKit.Async.doSimpleXMLHttpRequest(template_url);
     d.addCallback(function(result) {
-      var t = jsontemplate.JSONTemplate(result.responseText);
+      var t = jsontemplate.Template(result.responseText);
       var s = t.expand(json);
       $('search_result').innerHTML = s;
     });
     return d;
-    */
 };
 
 var json_errback = function(error) {
-    log('error');
+    log('error when getting find json');
 }
 
 var render = function() {
-  log('enter render');
-  var d = MochiKit.Async.doSimpleXMLHttpRequest('find');
-  log('render I');
+  var d = MochiKit.Async.loadJSONDoc('find');
   d.addCallbacks(json_callback, json_errback);
-  log('render II');
 };
 
-//MochiKit.Signal.connect(window, 'onload', render);
-render();
+MochiKit.Signal.connect(window, 'onload', render);
 
 })();
