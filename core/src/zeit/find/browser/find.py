ec0b19c3ed8209530ba387138b4f898fae8d980b
Sebastian Wehrmann <sw@gocept.com>
2009-04-20 13:56:23 +0000

    - Extend the results template.

--- core/src/zeit/find/browser/find.py
+++ core/src/zeit/find/browser/find.py
@@ -21,11 +21,39 @@ class Find(zeit.cms.browser.view.Base):
             "template_url": resources(self.request)['result.jsont'](),
             "results":
                 [{'uniqueId': 'foo',
+                  'icon': 'http://localhost:8080/@@/zeit.find/article.png',
+                  'favorited':
+                      'http://localhost:8080/@@/zeit.find/favorite.png',
+                  'publication_status':
+                      'http://localhost:8080/@@/zeit.find/published.png',
                   'teaser_title': 'Obama still alive',
-                  'teaser_text': 'Obama is still alive and well.'},
+                  'teaser_text': 'Obama is still alive and well.',
+                  'preview_url': '',
+                  'date': '13.02.2009',
+                  'date_filter': '',
+                  'week': '13/2009',
+                  'week_filter': '',
+                  'topics': 'Politik',
+                  'topics_filter': '',
+                  'author': 'Martijn Faassen',
+                  'author_filter': ''},
                  {'uniqueId': 'bar',
+                  'icon': 'http://localhost:8080/@@/zeit.find/article.png',
+                  'favorited':
+                      'http://localhost:8080/@@/zeit.find/not_favorit.png',
+                  'publication_status':
+                      'http://localhost:8080/@@/zeit.find/not_published.png',
                   'teaser_title': 'Obama journalism too extreme',
-                  'teaser_text': 'Reporters report that there is too much news about Obama.'}]
+                  'teaser_text': 'Reporters report that there is too much news about Obama.',
+                  'preview_url': '',
+                  'date': '19.02.2009',
+                  'date_filter': '',
+                  'week': '14/2009',
+                  'week_filter': '',
+                  'topics': 'Newspapers',
+                  'topics_filter': '',
+                  'author': 'Christian Zagrodnick',
+                  'author_filter': ''}]
                  }
         return cjson.encode(result)
 
