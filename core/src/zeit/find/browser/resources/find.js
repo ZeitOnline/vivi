055c1774e46fe678cba509cf4dba9468fc40a1d9
Sebastian Wehrmann <sw@gocept.com>
2009-04-20 12:20:34 +0000

    - Add caching of the template rendering.

--- core/src/zeit/find/browser/resources/find.js
+++ core/src/zeit/find/browser/resources/find.js
@@ -1,19 +1,41 @@
 (function() {
 
-var json_callback = function(json) {
-    var template_url = json['template_url'];
+var template_cache = {};
+
+var load_template = function(template_url, json, id) {
     var d = MochiKit.Async.doSimpleXMLHttpRequest(template_url);
     d.addCallback(function(result) {
-      var t = jsontemplate.Template(result.responseText);
-      var s = t.expand(json);
-      $('search_result').innerHTML = s;
+        t = make_template(template_url, result.responseText);
+        expand_template(t, json, id);
     });
     return d;
 };
 
+var make_template = function(template_url, template_source) {
+    var t = jsontemplate.Template(template_source);
+    template_cache[template_url] = t;
+    return t;
+};
+
+var expand_template = function(template, json, id) {
+    var s = t.expand(json);
+    $(id).innerHTML = s;
+};
+
+var json_callback = function(json) {
+    var id = 'search_result';
+    var template_url = json['template_url'];
+    var template = template_cache[template_url];
+    if (!isUndefinedOrNull(template)) {
+        expand_template(template, json, id);
+        return;
+    }
+    load_template(template_url, json, id);
+};
+
 var json_errback = function(error) {
     log('error when getting find json');
-}
+};
 
 var render = function() {
   var d = MochiKit.Async.loadJSONDoc('find');
