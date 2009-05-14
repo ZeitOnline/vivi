262e10060c223b759d501b578971490816ce6939
Wolfgang Schnerring <ws@gocept.com>
2009-05-14 11:50:57 +0000

    Re #5273: - style validation-errors. Put the background-color for blocks on block-inner, for areas on the area itself. - html-escape validation messages (the feed errors can contain <Exception...>)

--- core/src/zeit/content/cp/browser/rule.py
+++ core/src/zeit/content/cp/browser/rule.py
@@ -1,12 +1,17 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import cgi
 import zeit.content.cp.interfaces
 
 
 def validate(context):
     validator = zeit.content.cp.interfaces.IValidator(context)
-    css_class = ''
     if validator.status:
         css_class = 'validation-%s' % validator.status
-    return (css_class, '\n'.join(validator.messages))
+        messages = '\n'.join(validator.messages)
+        messages = cgi.escape(messages)
+    else:
+        css_class = ''
+        messages = ''
+    return (css_class, messages)
