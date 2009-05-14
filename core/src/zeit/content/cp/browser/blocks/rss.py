c547f870f5ca2532d1288305e85afd0d320c36ac
Sebastian Wehrmann <sw@gocept.com>
2009-05-14 06:12:29 +0000

    Re #5273: Display the error message in the editor if available.

--- core/src/zeit/content/cp/browser/blocks/rss.py
+++ core/src/zeit/content/cp/browser/blocks/rss.py
@@ -5,6 +5,19 @@
 import zeit.content.cp.browser.blocks.av
 import zeit.content.cp.interfaces
 import zope.formlib.form
+import zope.security.proxy
+
+class Display(object):
+
+    def css_class(self):
+        if self.error():
+            return 'validation-error'
+        return ''
+
+    def error(self):
+        feed = zope.security.proxy.removeSecurityProxy(self.context.feed)
+        return feed.xml.get('error')
+
 
 class EditProperties(zeit.content.cp.browser.blocks.av.EditProperties):
 
