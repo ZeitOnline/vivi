c06674ebb260ab99edb251663ec6ffb3f499f912
Christian Zagrodnick <cz@gocept.com>
2009-05-15 14:06:51 +0000

    - Alle ``test.py`` nach ``tests.py`` umbenannt.
    - Keine eigene Translationdomain mehr, Übersetzungen via zeit.locales.

--- src/zeit/seo/i18n.py
+++ src/zeit/seo/i18n.py
@@ -1,7 +1,4 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import zope.i18nmessageid
-
-
-MessageFactory = zope.i18nmessageid.MessageFactory('zeit.seo')
+from zeit.cms.i18n import MessageFactory
