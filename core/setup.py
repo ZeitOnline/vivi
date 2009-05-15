77cd1d4909d3c35d40d628d0ab8ea6706fdcb4f4
Christian Zagrodnick <cz@gocept.com>
2009-05-15 09:14:33 +0000

    zeit locales base

---
+++ core/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.locales',
+    version='0.1dev',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages = ['zeit'],
+    install_requires=[
+        'setuptools',
+    ],
+)
