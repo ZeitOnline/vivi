1c566c372a6e4b6e7a1b3165de2bce1841c46f6a
Christian Zagrodnick <cz@gocept.com>
2009-05-15 13:26:23 +0000

    - Keine eigene Translationdomain mehr, Übersetzungen via zeit.locales.

--- core/src/zeit/content/cp/i18n.py
+++ core/src/zeit/content/cp/i18n.py
@@ -1,9 +1,4 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import zope.i18n
-
-class Dummy(object):
-    pass
-
-MessageFactory = zope.i18n.MessageFactory(Dummy.__module__)
+from zeit.cms.i18n import MessageFactory
