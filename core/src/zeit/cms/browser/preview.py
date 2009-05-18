8e31952bd6076e739977f85554d83e4ef0df3241
Martijn Faassen <faassen@startifact.com>
2009-05-18 15:01:50 +0000

    Factor out a function so we can more easily create preview URLs elsewhere (such as in zeit.find).

--- core/src/zeit/cms/browser/preview.py
+++ core/src/zeit/cms/browser/preview.py
@@ -11,18 +11,23 @@ import zeit.cms.browser.interfaces
 import zeit.cms.browser.view
 
 
+def get_preview_url(prefix, unique_id):
+    """Given a prefix and a unique id, return preview URL.
+    """
+    path = unique_id[len(zeit.cms.interfaces.ID_NAMESPACE):]
+    cms_config = zope.app.appsetup.product.getProductConfiguration(
+        'zeit.cms')
+    prefix = cms_config[prefix]
+    if not prefix.endswith('/'):
+        prefix = prefix + '/'
+    return urlparse.urljoin(prefix, path)
+    
 class PreviewBase(zeit.cms.browser.view.Base):
     """Base class for preview."""
 
     def get_preview_url_for(self, preview_context):
         unique_id = preview_context.uniqueId
-        path = unique_id[len(zeit.cms.interfaces.ID_NAMESPACE):]
-        cms_config = zope.app.appsetup.product.getProductConfiguration(
-            'zeit.cms')
-        prefix = cms_config[self.prefix]
-        if not prefix.endswith('/'):
-            prefix = prefix + '/'
-        return urlparse.urljoin(prefix, path)
+        return get_preview_url(self.prefix, unique_id)
 
     def __call__(self):
         preview_object = zeit.cms.browser.interfaces.IPreviewObject(
