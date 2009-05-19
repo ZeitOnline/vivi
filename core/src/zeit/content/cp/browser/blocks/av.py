5875b0fa348798165b1c41561c35197c2342d324
Sebastian Wehrmann <sw@gocept.com>
2009-05-19 12:48:26 +0000

    Re #5279: Use EditCommon-Form for quiz and video block.

--- core/src/zeit/content/cp/browser/blocks/av.py
+++ core/src/zeit/content/cp/browser/blocks/av.py
@@ -1,29 +1,13 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from zeit.content.cp.i18n import MessageFactory as _
-import zeit.content.cp.interfaces
-import zope.app.pagetemplate
 import zope.formlib.form
+import zeit.content.cp.interfaces
+import zeit.content.cp.browser.blocks.block
 
-
-class EditProperties(zope.formlib.form.SubPageEditForm):
-
-    template = zope.app.pagetemplate.ViewPageTemplateFile(
-        'av.edit-properties.pt')
+class EditProperties(zeit.content.cp.browser.blocks.block.EditCommon):
 
     form_fields = zope.formlib.form.Fields(
         zeit.content.cp.interfaces.IAVBlock).omit(
             'media_type', *list(zeit.content.cp.interfaces.IBlock))
-
-    close = False
-
-    @property
-    def form(self):
-        return super(EditProperties, self).template
-
-    @zope.formlib.form.action(_('Apply'))
-    def handle_edit_action(self, action, data):
-        self.close = True
-        # XXX: dear zope.formlib, are you serious?!
-        return super(EditProperties, self).handle_edit_action.success(data)
--- core/src/zeit/content/cp/browser/blocks/quiz.py
+++ core/src/zeit/content/cp/browser/blocks/quiz.py
@@ -2,27 +2,12 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from zeit.content.cp.i18n import MessageFactory as _
 import zope.formlib.form
 import zeit.content.cp.interfaces
+import zeit.content.cp.browser.blocks.block
 
-class EditProperties(zope.formlib.form.SubPageEditForm):
-
-    template = zope.app.pagetemplate.ViewPageTemplateFile(
-        'quiz.edit-properties.pt')
+class EditProperties(zeit.content.cp.browser.blocks.block.EditCommon):
 
     form_fields = zope.formlib.form.Fields(
         zeit.content.cp.interfaces.IQuizBlock).omit(
             *list(zeit.content.cp.interfaces.IBlock))
-
-    close = False
-
-    @property
-    def form(self):
-        return super(EditProperties, self).template
-
-    @zope.formlib.form.action(_('Apply'))
-    def handle_edit_action(self, action, data):
-        self.close = True
-        # XXX: dear zope.formlib, are you serious?!
-        return super(EditProperties, self).handle_edit_action.success(data)
