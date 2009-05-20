815b894ae5e4b743a884b5abdc98f3b8ecfb4860
Christian Zagrodnick <cz@gocept.com>
2009-05-20 12:27:04 +0000

    JSONView changes: need to pass in the full url now.

--- core/src/zeit/find/browser/resources/find.js
+++ core/src/zeit/find/browser/resources/find.js
@@ -4,11 +4,11 @@ zeit.find = {};
 
     var init_search_form = function() {
         MochiKit.Signal.connect('search_button', 'onclick', function(e) {
-            search_result.render();
+            zeit.find.search_result.render();
         });
         MochiKit.Signal.connect('fulltext', 'onkeydown', function(e) {
             if (e.key()['string'] == 'KEY_ENTER') {
-                search_result.render();
+                zeit.find.search_result.render();
                 e.stop();
             };
         });
@@ -17,14 +17,14 @@ zeit.find = {};
             if ($('extended_search')) {
                 $('extended_search_form').innerHTML = '';
             } else {
-                extended_search_form.render();
+                zeit.find.extended_search_form.render();
             }
         });
         MochiKit.Signal.connect('result_filters_button', 'onclick', function(e) {
             if ($('filter_Zeit')) {
                 $('result_filters').innerHTML = '';
             } else {
-                result_filters.render();
+                zeit.find.result_filters.render();
             }
         });
     };
@@ -56,7 +56,8 @@ zeit.find = {};
                     related_info.innerHTML = '';
                 } else {
                     MochiKit.DOM.addElementClass(related_links, 'expanded');
-                    expanded_search_result.render(related_info, related_url);
+                    zeit.find.expanded_search_result.render(
+                        related_info, related_url);
                 }
             });
         });
@@ -72,7 +73,8 @@ zeit.find = {};
             var toggle_favorited = MochiKit.Selector.findChildElements(
                 entry, ['.toggle_favorited'])[0];
             MochiKit.Signal.connect(toggle_favorited, 'onclick', function(e) {
-                favorited.render(toggle_favorited, favorite_url);
+                zeit.find.favorited.render(
+                    toggle_favorited, favorite_url);
             });
         });
     }
@@ -84,54 +86,62 @@ zeit.find = {};
     }
 
     var init = function() {
-        search_form.render();
+
+        var base_url = application_url + '/@@';
+        zeit.find.search_form = new zeit.cms.JSONView(
+            base_url + 'search_form', 'search_form');
+        zeit.find.search_result = new zeit.cms.JSONView(
+            base_url + 'search_result', 'search_result',
+            search_form_parameters);
+        zeit.find.extended_search_form = new zeit.cms.JSONView(
+            base_url + 'extended_search_form', 'extended_search_form');
+        zeit.find.result_filters = new zeit.cms.JSONView(
+            base_url + 'result_filters', 'result_filters',
+            search_form_parameters);
+        zeit.find.expanded_search_result = new zeit.cms.JSONView(
+            base_url + 'expanded_search_result')
+        zeit.find.favorited = new zeit.cms.JSONView(
+            base_url + 'toggle_favorited')
+        zeit.find.favorites = new zeit.cms.JSONView(
+            base_url + 'favorites', 'favorites');
+
+        MochiKit.Signal.connect(zeit.find.search_form, 'load', init_search_form);
+
+        MochiKit.Signal.connect(zeit.find.search_result, 'load',
+                                connect_related);
+        
+        MochiKit.Signal.connect(zeit.find.search_result, 'load',
+                                connect_toggle_favorited);
+
+        MochiKit.Signal.connect(zeit.find.search_result, 'load',
+                                connect_draggables);
+        MochiKit.Signal.connect(zeit.find.search_result, 'before-load',
+                                disconnect_draggables);
+      
+        MochiKit.Signal.connect(zeit.find.favorites, 'load',
+                                connect_related);
+        MochiKit.Signal.connect(zeit.find.favorites, 'load',
+                                connect_toggle_favorited);
+
+        MochiKit.Signal.connect(zeit.find.favorites, 'load',
+                                connect_draggables);
+        MochiKit.Signal.connect(zeit.find.favorites, 'before-load',
+                                disconnect_draggables);
+
+        zeit.find.search_form.render();
         zeit.find.tabs = new zeit.cms.Tabs('cp-search');
-        zeit.find.tabs.add(new zeit.cms.ViewTab('search_form', 'Suche', search_result));
-        zeit.find.tabs.add(new zeit.cms.ViewTab('favorites', 'Favoriten', favorites));
-        zeit.find.tabs.add(new zeit.cms.Tab('for-this-page', 'Für diese Seite'));
+        zeit.find.tabs.add(new zeit.cms.ViewTab(
+            'search_form', 'Suche', zeit.find.search_result));
+        zeit.find.tabs.add(new zeit.cms.ViewTab(
+            'favorites', 'Favoriten', zeit.find.favorites));
+        zeit.find.tabs.add(new zeit.cms.Tab(
+            'for-this-page', 'Für diese Seite'));
     };
 
     var search_form_parameters = function() {
         return MochiKit.Base.queryString($('search_form'));
     };
 
-    var search_form = new zeit.cms.JSONView(
-        'search_form', 'search_form');
-    var search_result = new zeit.cms.JSONView(
-        'search_result', 'search_result', search_form_parameters);
-    var extended_search_form = new zeit.cms.JSONView(
-        'extended_search_form', 'extended_search_form');
-    var result_filters = new zeit.cms.JSONView(
-        'result_filters', 'result_filters', search_form_parameters);
-    var expanded_search_result = new zeit.cms.JSONView(
-        'expanded_search_result')
-    var favorited = new zeit.cms.JSONView(
-        'toggle_favorited')
-    var favorites = new zeit.cms.JSONView(
-        'favorites', 'favorites');
-
     MochiKit.Signal.connect(window, 'onload', init);
-    MochiKit.Signal.connect(search_form, 'load', init_search_form);
-
-    MochiKit.Signal.connect(search_result, 'load',
-                            connect_related);
-    
-    MochiKit.Signal.connect(search_result, 'load',
-                            connect_toggle_favorited);
-
-    MochiKit.Signal.connect(search_result, 'load',
-                            connect_draggables);
-    MochiKit.Signal.connect(search_result, 'before-load',
-                            disconnect_draggables);
-  
-    MochiKit.Signal.connect(favorites, 'load',
-                            connect_related);
-    MochiKit.Signal.connect(favorites, 'load',
-                            connect_toggle_favorited);
-
-    MochiKit.Signal.connect(favorites, 'load',
-                            connect_draggables);
-    MochiKit.Signal.connect(favorites, 'before-load',
-                            disconnect_draggables);
 
 })();
