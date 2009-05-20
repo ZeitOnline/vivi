51d1e54344a70cbcaa2e693518b178cb28d4c3c1
Christian Zagrodnick <cz@gocept.com>
2009-05-20 14:07:07 +0000

    made elements draggable -- not droppable anywhere, yet re #5352

--- core/src/zeit/content/cp/browser/resources/library.js
+++ core/src/zeit/content/cp/browser/resources/library.js
@@ -1,17 +1,60 @@
+zeit.content.cp.library = {}
+
 MochiKit.Signal.connect(
     window, 'cp-editor-loaded', function() {
 
+    var connect_draggables = function(view, draggables) {
+        var modules = MochiKit.DOM.getElementsByTagAndClassName(
+            'div', 'module', view.target_id);
+        forEach(modules, function(module) {
+            draggables.push(
+                new MochiKit.DragAndDrop.Draggable(module, {
+                    zindex: null,
+            }));
+        });
+    };
+
+    var disconnect_draggables = function(draggables) {
+        while(draggables.length) {
+            draggables.pop().destroy();
+        }
+    };
+
+    var create_library = function(element_id, title) {
+        var url = $(element_id).getAttribute('cms:url') +
+            '/@@block-factories.json';
+        var library_id = 'library-' + element_id;
+        var view = new zeit.cms.JSONView(url, library_id);
+        tabs.add(new zeit.cms.ViewTab(library_id, title, view));
+        var draggables = []
+
+        MochiKit.Signal.connect(view, 'before-load', function() {
+            disconnect_draggables(draggables);
+        });
+        MochiKit.Signal.connect(view, 'load', function() {
+            connect_draggables(view, draggables);
+        });
+    }
+
     var tabs = new zeit.cms.Tabs('cp-library');
+    create_library('cp-informatives-inner', 'Informatives');
+    create_library('cp-teasermosaic', 'Mosaic');
 
-    var url = $('cp-informatives-inner').getAttribute('cms:url') +
-        '/@@block-factories.json';
-    tabs.add(new zeit.cms.ViewTab(
-        'library-informatives', 'Informatives',
-        new zeit.cms.JSONView(url, 'library-informatives')));
+    MochiKit.Signal.connect(MochiKit.DragAndDrop.Draggables, 'start',
+        function(draggable)  {
+            if (!MochiKit.DOM.hasElementClass(draggable.element, 'module')) {
+                return
+            }
+            var dim = MochiKit.Style.getElementDimensions(draggable.element);
+            draggable.element = draggable.element.cloneNode(true);
+            MochiKit.Style.setElementDimensions(draggable.element, dim);
+            draggable.offset = [-10, -10];
+            $('body').appendChild(draggable.element);
+    });
 
-    var url = $('cp-teasermosaic').getAttribute('cms:url') + 
-        '/@@block-factories.json';
-    tabs.add(new zeit.cms.ViewTab(
-        'library-mosaic', 'Mosaic',
-        new zeit.cms.JSONView(url, 'library-mosaic')));
+    MochiKit.Signal.connect(MochiKit.DragAndDrop.Draggables, 'end',
+        function(draggable) {
+            MochiKit.DOM.removeElement(draggable.element);
+    });
+
 });
