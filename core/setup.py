11fd88afef43c511cad70e9b15dde623308110d1
Christian Zagrodnick <cz@gocept.com>
2009-05-26 08:16:29 +0000

    vb

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version='0.7dev',
+    version='0.8dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
