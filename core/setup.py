23eff86e367d145c470a1a6d84e16abdd629ad31
Christian Zagrodnick <cz@gocept.com>
2009-06-05 13:32:20 +0000

    2.0 wg. divisions

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.article',
-    version='1.8dev',
+    version='2.0dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms/zeit.content.article',
