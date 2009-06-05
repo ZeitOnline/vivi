27ef6cf0f2944f31950368b2f1cb03471cf5d086
Christian Zagrodnick <cz@gocept.com>
2009-06-05 13:34:23 +0000

    es ist 2009 ...

--- core/src/zeit/content/article/comment.py
+++ core/src/zeit/content/article/comment.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2008 gocept gmbh & co. kg
+# Copyright (c) 2008-2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 """Aggregated comments."""
 
--- core/src/zeit/content/article/interfaces.py
+++ core/src/zeit/content/article/interfaces.py
@@ -1,5 +1,5 @@
 # vim:fileencoding=utf-8 encoding=utf-8
-# Copyright (c) 2007-2008 gocept gmbh & co. kg
+# Copyright (c) 2007-2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 # $Id$
 
