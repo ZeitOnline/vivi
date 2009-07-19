1a9a9fe05f92a3dfcdde2bd4350d542c93fb410a
Christian Zagrodnick <cz@gocept.com>
2009-07-19 09:31:20 +0000

    return the same xml tree instance for the same state during one request. using weakrefs would be better, but lxml trees cannot be weakref'ed, yet.
    fixes #5669
    fixes #5707

--- core/src/zeit/cms/content/lxmlpickle.py
+++ core/src/zeit/cms/content/lxmlpickle.py
@@ -2,14 +2,39 @@
 # See also LICENSE.txt
 
 import copy_reg
-
+import hashlib
 import lxml.etree
 import lxml.objectify
+import threading
+import zope.app.publication.interfaces
+import zope.testing.cleanup
+
+class Refs(threading.local):
+
+    def __init__(self):
+        self.refs = {}
+
+_refs = Refs()
+zope.testing.cleanup.addCleanUp(_refs.refs.clear)
 
 
-def treeFactory(state):
+def hash_state(state):
+    return hashlib.sha224(state).digest()
+
+
+@zope.component.adapter(zope.app.publication.interfaces.IEndRequestEvent)
+def clean_refs(event, _refs=_refs):
+    _refs.refs.clear()
+
+
+def treeFactory(state, _refs=_refs):
     """Un-Pickle factory."""
-    return lxml.objectify.fromstring(state)
+    hash_ = hash_state(state)
+    tree = _refs.refs.get(hash_)
+    if tree is None:
+        tree = lxml.objectify.fromstring(state)
+        _refs.refs[hash_] = tree
+    return tree
 
 copy_reg.constructor(treeFactory)
 
@@ -19,8 +44,12 @@ def reduceObjectifiedElement(object):
 
     See http://docs.python.org/lib/pickle-protocol.html for details.
     """
-    return (treeFactory,
-            (lxml.etree.tostring(object.getroottree()), ))
+
+    root_tree = object.getroottree()
+    state = lxml.etree.tostring(root_tree)
+    _refs.refs[hash_state(state)] = root_tree.getroot()
+    return (treeFactory, (state, ))
+
 
 copy_reg.pickle(lxml.objectify.ObjectifiedElement,
                 reduceObjectifiedElement,
--- core/src/zeit/cms/content/tests.py
+++ core/src/zeit/cms/content/tests.py
@@ -145,7 +145,6 @@ def test_suite():
     suite.addTest(doctest.DocFileSuite(
         'adapter.txt',
         'keyword.txt',
-        'lxmlpickle.txt',
         'property.txt',
         optionflags=(doctest.REPORT_NDIFF + doctest.NORMALIZE_WHITESPACE +
                      doctest.ELLIPSIS),
@@ -154,6 +153,7 @@ def test_suite():
         'dav.txt',
         'field.txt',
         'liveproperty.txt',
+        'lxmlpickle.txt',
         'metadata.txt',
         'semanticchange.txt',
         'sources.txt',
