b83cc7b4ebc12a55c0317bbbcb321fb147290f94
Christian Zagrodnick <cz@gocept.com>
2009-07-22 12:00:42 +0000

    trunk to 2.0dev

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.gallery',
-    version='1.23dev',
+    version='2.0dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
