76b01d4d1de1c2a75d02e135e8ad64aa72439a6c
Christian Zagrodnick <cz@gocept.com>
2009-07-28 11:11:48 +0000

    vb

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.find',
-    version='0.8dev',
+    version='0.9dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.find',
