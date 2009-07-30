685b681ebd2d6dd5513a9b68d4116368e93a9c57
Christian Zagrodnick <cz@gocept.com>
2009-07-30 11:53:58 +0000

    sorted
    removed unused import

--- core/src/zeit/content/cp/blocks/tests.py
+++ core/src/zeit/content/cp/blocks/tests.py
@@ -2,12 +2,12 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import zeit.content.cp.blocks
 import zeit.content.cp.testing
 
 
 def test_suite():
     return zeit.content.cp.testing.FunctionalDocFileSuite(
-        'teaser.txt',
         'cpextra.txt',
-        'xml.txt')
+        'teaser.txt',
+        'xml.txt',
+    )
