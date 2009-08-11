06959215bc77cc89df53e12821f471c7ca24ea64
Wolfgang Schnerring <ws@gocept.com>
2009-08-11 13:01:03 +0000

    Re #5937: pass the redirect from the ajaxed form to the browser

--- src/zeit/addcentral/sidebar.py
+++ src/zeit/addcentral/sidebar.py
@@ -2,26 +2,33 @@
 # See also LICENSE.txt
 
 from zeit.cms.i18n import MessageFactory as _
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 import zc.resourcelibrary
 import zeit.addcentral.add
 import zeit.addcentral.interfaces
-import zeit.cms.content.browser.form
 import zeit.cms.content.sources
-import zope.cachedescriptors.property
 import zope.formlib.form
+
+
+class Sidebar(object):
+
+    def __init__(self, *args, **kw):
+        zc.resourcelibrary.need('zeit.cms.content.dropdown')
+        super(Sidebar, self).__init__(*args, **kw)
 
 
 class Form(zope.formlib.form.SubPageForm):
 
-    field_groups = None
+    template = ViewPageTemplateFile('form.pt')
     form_fields = zope.formlib.form.FormFields(
         zeit.addcentral.interfaces.IContentAdder).omit('year', 'month')
 
-    @property
-    def template(self):
-        zc.resourcelibrary.need('zeit.cms.content.dropdown')
-        return super(Form, self).template
+    next_url = None
 
     @zope.formlib.form.action(_('Add'))
     def add(self, action, data):
-        import pdb; pdb.set_trace();
+        adder = zeit.addcentral.add.ContentAdder(self.request, **data)
+        self.next_url = adder()
+
+    def nextURL(self):
+        return self.next_url
