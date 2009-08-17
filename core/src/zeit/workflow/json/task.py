f258d98aca85a6c9a88293dc7cd42a6c83270f79
Wolfgang Schnerring <ws@gocept.com>
2009-08-17 08:50:54 +0000

    Re #5661: implemented JSON-views for querying the remotetask utility

---
+++ core/src/zeit/workflow/json/task.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2009 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import cjson
+import lovely.remotetask.interfaces
+import zope.component
+
+
+class Status(object):
+
+    def __init__(self, context, request):
+        self.context = zope.component.getUtility(
+            lovely.remotetask.interfaces.ITaskService, 'general')
+        self.request = request
+
+    def getStatus(self, job):
+        return cjson.encode(self.context.getStatus(int(job)))
+
+    def getError(self, job):
+        return cjson.encode(self.context.getError(int(job)))
+
+    def getResult(self, job):
+        return cjson.encode(self.context.getResult(int(job)))
--- core/src/zeit/workflow/json/tests.py
+++ core/src/zeit/workflow/json/tests.py
@@ -2,6 +2,7 @@
 # See also LICENSE.txt
 
 import cjson
+import lovely.remotetask.interfaces
 import transaction
 import zeit.cms.testing
 import zeit.workflow.interfaces
@@ -10,37 +11,67 @@ import zope.security.management
 import zope.testbrowser.testing
 
 
-class PublishJSONTest(zeit.cms.testing.FunctionalTestCase):
+class JSONTestCase(zeit.cms.testing.FunctionalTestCase):
 
     layer = zeit.workflow.tests.WorkflowLayer
     product_config = zeit.workflow.tests.product_config
 
+
     def setUp(self):
-        super(PublishJSONTest, self).setUp()
-        zope.security.management.endInteraction()
+        super(JSONTestCase, self).setUp()
         self.browser = zope.testbrowser.testing.Browser()
         self.browser.addHeader('Authorization', 'Basic user:userpw')
+        self.browser.handleErrors = False
 
-    def test_negative_can_publish_should_return_false(self):
-        b = self.browser
-        b.open('http://localhost/repository/online/2007/01/Somalia/@@publish')
-        result = cjson.decode(b.contents)
-        self.assertEqual(False, result)
+    def call_json(self, url):
+        zope.security.management.endInteraction()
+        self.browser.open(url)
+        return cjson.decode(self.browser.contents)
 
-    def test_publish_should_return_job_id(self):
+    def enable_publish(self, unique_id):
+        zope.security.management.endInteraction()
         zeit.cms.testing.create_interaction()
-        somalia = zeit.cms.interfaces.ICMSContent(
-            'http://xml.zeit.de/online/2007/01/Somalia')
+        somalia = zeit.cms.interfaces.ICMSContent(unique_id)
         workflow = zeit.workflow.interfaces.IContentWorkflow(somalia)
         workflow.urgent = True
         transaction.commit()
-        zope.security.management.endInteraction()
 
-        b = self.browser
-        b.open('http://localhost/repository/online/2007/01/Somalia/@@publish')
-        result = cjson.decode(b.contents)
+    def process(self):
+        tasks = zope.component.getUtility(
+            lovely.remotetask.interfaces.ITaskService, 'general')
+        zeit.cms.testing.create_interaction()
+        tasks.process()
+
+
+class PublishJSONTest(JSONTestCase):
+
+    def test_negative_can_publish_should_return_false(self):
+        result = self.call_json(
+            'http://localhost/repository/online/2007/01/Somalia/@@publish')
+        self.assertEqual(False, result)
+
+    def test_publish_should_return_job_id(self):
+        self.enable_publish('http://xml.zeit.de/online/2007/01/Somalia')
+        result = self.call_json(
+            'http://localhost/repository/online/2007/01/Somalia/@@publish')
         self.assertNotEqual(False, result)
         try:
             int(result)
         except ValueError:
             self.fail('a job id should be returned')
+
+
+class RemoteTaskTest(JSONTestCase):
+
+    def test_get_status(self):
+        self.enable_publish('http://xml.zeit.de/online/2007/01/Somalia')
+        job = self.call_json(
+            'http://localhost/repository/online/2007/01/Somalia/@@publish')
+
+        status = self.call_json(
+            'http://localhost/@@publish-status?job=%s' % job)
+        self.assertEqual('queued', status)
+        self.process()
+        status = self.call_json(
+            'http://localhost/@@publish-status?job=%s' % job)
+        self.assertEqual('completed', status)
