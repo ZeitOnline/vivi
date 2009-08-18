bbec82428f6cecec0b643db01dbc11a0f765086a
Christian Zagrodnick <cz@gocept.com>
2009-08-18 10:10:57 +0000

    use pkg_resources

--- core/src/zeit/wysiwyg/tests.py
+++ core/src/zeit/wysiwyg/tests.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2007-2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os.path
+import pkg_resources
 import unittest
 import zeit.cms.testing
 import zope.app.testing.functional
 
 
 WYSIWYGLayer = zope.app.testing.functional.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
+    pkg_resources.resource_filename(__name__, 'ftesting.zcml'),
     __name__, 'WYSIWYGLayer', allow_teardown=True)
 
 
