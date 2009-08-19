515a7ad6cdac6fc3f33a1f8f092a64bdae55bb3d
Wolfgang Schnerring <ws@gocept.com>
2009-08-19 09:22:12 +0000

    Re #5902: create FCKeditor via javascript (to make it usable from a lightbox)

--- core/src/zeit/wysiwyg/browser/widget.py
+++ core/src/zeit/wysiwyg/browser/widget.py
@@ -2,37 +2,40 @@
 # See also LICENSE.txt
 
 import urllib
+import zc.resourcelibrary
 import zope.app.form.browser.textwidgets
 import zope.component
 
 
-TEMPLATE = '''\
-<input type="hidden" id="%(field_name)s" name="%(field_name)s"
-    value="%(html)s" />
-<input type="hidden" id="%(field_name)s___Config"
-    value="%(config)s" />
-<iframe id="%(field_name)s___Frame" class="fckeditor"
-    src="%(editor-path)s/editor/fckeditor.html?InstanceName=%(field_name)s&amp;Toolbar=Zeit"
-    frameborder="0" scolling="no"></iframe>
-'''
+TEMPLATE = """
+<script type="text/javascript">
+var fckeditor = new FCKeditor('%(field_name)s');
+fckeditor.BasePath = '/%(gocept_fckeditor)s/';
+fckeditor.Height = 400;
+fckeditor.ToolbarSet = 'Zeit';
+fckeditor.Config['CustomConfigurationsPath'] = '%(zeit_wysiwyg)s/config.js';
+fckeditor.Config['EditorAreaCSS'] = '%(zeit_wysiwyg)s/editor.css';
+fckeditor.Config['ZeitResources'] = '%(zeit_wysiwyg)s';
+fckeditor.Config['ApplicationURL'] = '%(application_url)s';
+
+fckeditor.ReplaceTextarea();
+</script>
+"""
 
 
 class FckEditorWidget(zope.app.form.browser.textwidgets.TextAreaWidget):
 
     def __call__(self, *args, **kw):
+        zc.resourcelibrary.need('zeit.wysiwyg')
+
         wysiwyg_resource = zope.component.getAdapter(
             self.request, name='zeit.wysiwyg')()
         fck_resource = zope.component.getAdapter(
             self.request, name='gocept.fckeditor')()
-        config = dict(
-            EditorAreaCSS='%s/editor.css' % wysiwyg_resource,
-            CustomConfigurationsPath='%s/config.js' % wysiwyg_resource,
-            ZeitResources=wysiwyg_resource,
-            ApplicationURL=self.request.getApplicationURL())
-        data = {
-            'field_name': self.name,
-            'config': urllib.urlencode(config),
-            'editor-path': fck_resource,
-            'html': zope.app.form.browser.textwidgets.escape(
-                self._getFormValue()).replace('"', '&quot;')}
-        return TEMPLATE % data
+        data = dict(
+            field_name=self.name,
+            gocept_fckeditor=fck_resource,
+            zeit_wysiwyg=wysiwyg_resource,
+            application_url=self.request.getApplicationURL()
+        )
+        return super(FckEditorWidget, self).__call__() + TEMPLATE % data
