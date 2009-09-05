ae0322b416f5ede6ca17689c14716daf15cbf57f
Christian Zagrodnick <cz@gocept.com>
2009-09-05 10:46:16 +0000

    Back to development: 0.10dev

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.objectlog',
-    version = '0.9',
+    version = '0.10dev',
     author='Christian Zagrodnick',
     author_email='cz@gocept.com',
     description="""\
