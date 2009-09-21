e0adec7ddb9ef0645180f5289d1eecd725044475
Christian Zagrodnick <cz@gocept.com>
2009-09-21 10:00:32 +0000

    Back to development: 0.11dev

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version = '0.10',
+    version = '0.11dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
