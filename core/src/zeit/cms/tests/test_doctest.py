3a30d5c40006639f289e988f979b47a9cf01a265
Christian Zagrodnick <cz@gocept.com>
2009-10-27 09:24:45 +0000

    moving tests to tests package (re #6385)

--- core/src/zeit/cms/tests/test_doctest.py
+++ core/src/zeit/cms/tests/test_doctest.py
@@ -1,18 +1,16 @@
 # Copyright (c) 2008-2009 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
-
-import unittest
 
 from zope.testing import doctest
-
+import unittest
 import zeit.cms.testing
+
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(doctest.DocFileSuite(
-        'content.txt'))
+        '../content.txt'))
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
-        'async.txt',
-        'cleanup.txt'))
+        '../async.txt',
+        '../cleanup.txt'))
     return suite
