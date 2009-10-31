928f772041e4c7bbf2b14a0d7c8a8f4835e1e0e9
Christian Zagrodnick <cz@gocept.com>
2009-10-31 20:19:39 +0000

    - Es gibt jetzt einen einfachen Weg das Artikel-Bild aus der einem Artikel   zugewiesenen Bildergruppe einzubinden (fixes #6108).

---
+++ core/src/zeit/wysiwyg/browser/image.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2009 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zeit.content.image.interfaces
+import zeit.cms.browser.view
+
+
+class Images(zeit.cms.browser.view.JSON):
+
+    def json(self):
+        images = zeit.content.image.interfaces.IImages(self.context)
+        result = []
+        for image in images.images:
+            if zeit.content.image.interfaces.IImageGroup.providedBy(image):
+                for i in image.values():
+                    result.append(i)
+            else:
+                result.append(image)
+        return dict(
+            images=[image.uniqueId for image in result])
