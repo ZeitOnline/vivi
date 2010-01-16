05b2218369d16b071952715f29fbb54c4e1bd01c
Christian Zagrodnick <cz@gocept.com>
2010-01-16 15:10:52 +0000

    - ITZInfo-Adapter für alle Requests registriert, nicht nur für BrowserRequest.

--- core/src/zeit/cms/browser/timezone.py
+++ core/src/zeit/cms/browser/timezone.py
@@ -6,10 +6,10 @@ import pytz
 import zope.component
 import zope.interface
 import zope.interface.common.idatetime
-import zope.publisher.interfaces.browser
+import zope.publisher.interfaces
 
 
-@zope.component.adapter(zope.publisher.interfaces.browser.IBrowserRequest)
+@zope.component.adapter(zope.publisher.interfaces.IRequest)
 @zope.interface.implementer(zope.interface.common.idatetime.ITZInfo)
 def tzinfo(request):
     return pytz.timezone('Europe/Berlin').localize(
