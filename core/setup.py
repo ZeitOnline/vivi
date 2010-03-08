60fa4f04fcab928924981e68ab7e5a6828c63e9c
Christian Zagrodnick <cz@gocept.com>
2010-03-08 10:37:40 +0000

    added package skeleton (re #6943)

---
+++ core/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'zeit.brightcove',
+    version = '0.1.0dev',
+    author = 'Christian Zagrodnick',
+    author_email = 'cz@gocept.com',
+    description = '',
+    packages = find_packages('src'),
+    package_dir = {'' : 'src'},
+    include_package_data = True,
+    zip_safe = False,
+    namespace_packages = ['zeit'],
+    install_requires = [
+        'zeit.cms',
+        'pybrightcove',
+    ],
+    entry_points = """
+        """
+)
