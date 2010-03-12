d0bf12367cd35588eda2120c3dfaf5de98f70955
Christian Zagrodnick <cz@gocept.com>
2010-03-12 16:24:10 +0000

    coding style

--- core/src/zeit/brightcove/solr.py
+++ core/src/zeit/brightcove/solr.py
@@ -6,28 +6,25 @@ import gocept.runner
 import pytz
 import zeit.brightcove.content
 import zeit.solr.interfaces
-import zope
 import zeit.solr.query
+import zope
+
 
 def _index_changed_videos_and_playlists():
     from_date = datetime.datetime.now(pytz.UTC) - datetime.timedelta(days=1)
-    videos = zeit.brightcove.content.Video.find_modified(
-        from_date=from_date)
-    
+    videos = zeit.brightcove.content.Video.find_modified(from_date=from_date)
     for content in videos:
         _update_single_content(content)
-    
     playlists = list(zeit.brightcove.content.Playlist.find_all())
-    
-    # we don't get the item_state on playlists, so this is necessary
+    # we don't get the item_state on playlists, so this is necessary:
     _empty_playlists()
-
     for content in playlists:
         _update_single_content(content)
 
-def _update_single_content(content):       
+
+def _update_single_content(content):
     deleter = zope.component.getAdapter(
-         content.uniqueId, zeit.solr.interfaces.IUpdater, 
+         content.uniqueId, zeit.solr.interfaces.IUpdater,
          name='delete')
     updater = zeit.solr.interfaces.IUpdater(content)
 
@@ -42,14 +39,15 @@ def _update_single_content(content):
         deleter.update(solr='public')
 
 def _empty_playlists():
-    query = zeit.solr.query.field('type', 'zeit.brightcove.interfaces.IPlaylist')
+    query = zeit.solr.query.field(
+        'type', 'zeit.brightcove.interfaces.IPlaylist')
     query = query.encode('UTF-8')
-    
-    conn = zope.component.getUtility(zeit.solr.interfaces.ISolr)
-    conn.delete(q=query, commit=False)
-    
-    conn = zope.component.getUtility(zeit.solr.interfaces.ISolr, name='public')
-    conn.delete(q=query, commit=False)
+    solr = zope.component.getUtility(zeit.solr.interfaces.ISolr)
+    public_solr= zope.component.getUtility(
+        zeit.solr.interfaces.ISolr, name='public')
+    solr.delete(q=query, commit=False)
+    public_solr.delete(q=query, commit=False)
+
 
 @gocept.runner.once(principal=gocept.runner.from_config(
     'zeit.brightcove', 'index-principal'))
