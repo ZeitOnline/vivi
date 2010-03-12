25d520adbab8cbd4cbab381eb81cda27d085e449
Christian Zagrodnick <cz@gocept.com>
2010-03-12 16:57:30 +0000

    styled form
    fixes #6884

--- core/src/zeit/brightcove/browser.py
+++ core/src/zeit/brightcove/browser.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+from zeit.cms.i18n import MessageFactory as _
+import gocept.form.grouped
 import zeit.brightcove.interfaces
 import zeit.cms.browser.form
 import zope.formlib.form
@@ -8,11 +10,34 @@ import zope.formlib.form
 
 class VideoEditForm(zeit.cms.browser.form.EditForm):
 
-    form_fields = zope.formlib.form.FormFields(
-        zeit.brightcove.interfaces.IVideo)
+    title = _('Edit video')
 
+    form_fields = zope.formlib.form.FormFields(
+        zeit.brightcove.interfaces.IVideo).omit('__name__', 'thumbnail')
+
+    field_groups = (
+        gocept.form.grouped.Fields(
+            _("Texts"),
+            ('supertitle', 'title', 'subtitle', 'teaserText'),
+            css_class='wide-widgets column-left'),
+        gocept.form.grouped.Fields(
+            _("Navigation"),
+            ('id', 'ressort', 'keywords', 'serie'),
+            css_class='column-right'),
+        gocept.form.grouped.Fields(
+            _("Options"),
+            ('item_state', 'dailyNewsletter', 'banner', 'banner_id',
+             'breaking_news', 'has_recensions', 'product_id'),
+            css_class='column-right checkboxes'),
+        gocept.form.grouped.Fields(
+            _('Teaser elements'),
+            ('related',),
+            'wide-widgets full-width'),
+    )
 
 class PlaylistDisplayForm(zeit.cms.browser.form.DisplayForm):
+
+    title = _('View playlist')
 
     form_fields = zope.formlib.form.FormFields(
         zeit.brightcove.interfaces.IPlaylist)
