69605735976351ec325367e56bedd70dd25324c9
Christian Zagrodnick <cz@gocept.com>
2010-03-31 10:44:07 +0000

    update next version to 0.34.0

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version = '0.33.1dev',
+    version = '0.34.0dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
