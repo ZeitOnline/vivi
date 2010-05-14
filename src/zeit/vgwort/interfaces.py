f6aec2bbe27a970f71c04e31073a1a38711983b0
Christian Zagrodnick <cz@gocept.com>
2010-05-14 15:55:00 +0000

    assign token on publish
    fixes #7213

--- src/zeit/vgwort/interfaces.py
+++ src/zeit/vgwort/interfaces.py
@@ -2,7 +2,9 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+from zeit.cms.i18n import MessageFactory as _
 import zope.interface
+import zope.schema
 
 
 class ITokens(zope.interface.Interface):
@@ -17,3 +19,26 @@ class ITokens(zope.interface.Interface):
 
     def load(csv_file):
         """Load tokens from csv file (file like object)."""
+
+    def add(public_token, private_token):
+        """Add tokens."""
+
+
+class IToken(zope.interface.Interface):
+
+    public_token = zope.schema.TextLine(
+        title=_('Public VGWort token'),
+        required=False)
+    private_token = zope.schema.TextLine(
+        title=_('Private VGWort token'),
+        required=False)
+
+
+class IGenerallyReportableContent(zope.interface.Interface):
+    """Marker for types which are generally reportable to vgwort.
+
+    This marker is used to determine types which content types contain content
+    which is in general reportable to vgwort (meldefähig). There are more
+    constraints which are not supported yet (≥1800 characters etc.)
+
+    """
