c30176f453b5476ab6039cc69892fe142bf3ea03
Wolfgang Schnerring <ws@gocept.com>
2010-06-09 09:15:13 +0000

    Re #7333: implemented list representation for author

---
+++ src/zeit/content/author/browser/author.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import grokcore
+import zeit.cms.browser.listing
+import zeit.content.image.interfaces
+import zope.publisher.interfaces
+
+
+class AuthorListRepresentation(
+    grokcore.component.MultiAdapter,
+    zeit.cms.browser.listing.BaseListRepresentation):
+
+    grokcore.component.adapts(
+        zeit.content.author.interfaces.IAuthor,
+        zope.publisher.interfaces.IPublicationRequest)
+    grokcore.component.implements(
+        zeit.cms.browser.interfaces.IListRepresentation)
+
+    ressort = page = volume = year = u''
+
+    @property
+    def title(self):
+        return self.context.display_name
+
+    @property
+    def searchableText(self):
+        return self.context.display_name
