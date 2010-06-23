6303ed32b36f78b22b9567b3cba44c86841a4714
Wolfgang Schnerring <ws@gocept.com>
2010-06-23 09:30:51 +0000

    Re #6415: enable flashmessages to be updated via javascript

--- core/src/zeit/cms/browser/js/messages.js
+++ core/src/zeit/cms/browser/js/messages.js
@@ -1,40 +1,66 @@
 // Copyright (c) 2007-2010 gocept gmbh & co. kg
 // See also LICENSE.txt
-// $Id$
 
-connect(window, 'onload', function(event) {
-    // Connect messages hide/show
-    var showtoggle = $('messages_toggle')
-    var messages = $('messages')
+zeit.cms.MessageView = zeit.cms.View.extend({
 
-    if (messages == null) {
-        // no messages, nothing do to
-        return
-    }
+    construct: function(url, target_id) {
+        var self = this;
+        arguments.callee.$.construct.call(self, url, target_id);
+        self.initialize();
+        MochiKit.Signal.connect(
+            self, 'load', self, self.initialize);
+        // XXX disconnect signals set up by connect_toggle_button before
+        // unloading old DOM?
+    },
 
-    var errors = getElementsByTagAndClassName('li', 'error', messages)
-    var timeout = 0.5;
-    if (errors.length > 0) {
-        timeout = 5;
-    }
+    initialize: function() {
+        var self = this;
+        var messages = $('messages');
+        if (messages == null) {
+            return;
+        }
+        self.connect_toggle_button();
+        self.fadeout();
+    },
 
-    var d = callLater(timeout, function() {
-        fade(messages, {
-            afterFinish: function() {
-                addElementClass(messages, 'hiddenMessages');
-                // remove opacity and display from fade
-                messages.setAttribute('style', '');
-                },
-            });
-    });
+    fadeout: function() {
+        var self = this;
+        var messages = $('messages');
+        var errors = MochiKit.DOM.getElementsByTagAndClassName(
+            'li', 'error', messages);
+        var timeout = 0.5;
+        if (errors.length > 0) {
+            timeout = 5;
+        }
 
-    connect(messages, 'onclick', function(event) {
-        d.cancel();
-        addElementClass(messages, 'hiddenMessages');
-    });
-    connect(showtoggle, 'onclick', function(event) {
-        d.cancel();
-        toggleElementClass('hiddenMessages', messages);
-    });
+        self.fade = callLater(timeout, function() {
+            MochiKit.Visual.fade(messages, {
+                afterFinish: function() {
+                    MochiKit.DOM.addElementClass(messages, 'hiddenMessages');
+                    // remove opacity and display from fade
+                    messages.setAttribute('style', '');
+                    },
+                });
+        });
+    },
 
+    connect_toggle_button: function() {
+        var self = this;
+        var messages = $('messages');
+        var showtoggle = $('messages_toggle');
+        MochiKit.Signal.connect(messages, 'onclick', function(event) {
+            self.fade.cancel();
+            MochiKit.DOM.addElementClass(messages, 'hiddenMessages');
+        });
+        MochiKit.Signal.connect(showtoggle, 'onclick', function(event) {
+            self.fade.cancel();
+            MochiKit.DOM.toggleElementClass('hiddenMessages', messages);
+        });
+    },
+});
+
+
+MochiKit.Signal.connect(window, 'onload', function(event) {
+    zeit.cms.messages = new zeit.cms.MessageView(
+        application_url + '/@@messages', 'messages_container');
 });
