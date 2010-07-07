811160152895724f9dad0b19c477cb730b238946
Christian Zagrodnick <cz@gocept.com>
2010-07-07 06:04:10 +0000

    Preparing release 0.39.0

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version = '0.38.1dev',
+    version = '0.39.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
