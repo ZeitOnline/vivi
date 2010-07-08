cb1cf94d820a57a3b39446e9e4fe1f2ea787e064
Christian Zagrodnick <cz@gocept.com>
2010-07-08 11:26:11 +0000

    Preparing release 0.20.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version = '0.19.1dev',
+    version = '0.20.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
