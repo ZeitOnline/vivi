77c5f987a1ec824ffa8b7b2ffbd8b715981d94e4
Christian Zagrodnick <cz@gocept.com>
2010-08-05 08:08:56 +0000

    update major version
    re #7549

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version = '1.45.1dev',
+    version = '1.46.0dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
