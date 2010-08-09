3ff75a13d4ddf04658247c4083dfd800aac7d3c3
Christian Zagrodnick <cz@gocept.com>
2010-08-09 12:01:24 +0000

    Preparing release 0.7.1

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.securitypolicy',
-    version = '0.7.1dev',
+    version='0.7.1',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
