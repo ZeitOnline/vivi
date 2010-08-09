7a6ed4d5d2861dfe497bfe336b9888aea04b8168
Christian Zagrodnick <cz@gocept.com>
2010-08-09 11:52:16 +0000

    Preparing release 0.20.3

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.find',
-    version='0.20.3dev',
+    version='0.20.3',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.find',
