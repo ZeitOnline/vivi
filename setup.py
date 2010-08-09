d2a447b54ae41d862ede51c72984f9fc5fa7d26e
Christian Zagrodnick <cz@gocept.com>
2010-08-09 12:02:17 +0000

    Back to development: 0.7.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.securitypolicy',
-    version='0.7.1',
+    version='0.7.2dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
