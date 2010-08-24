8f1a15ed8b5cb9b307d7d4bb8f830473d0c6a6b7
Christian Zagrodnick <cz@gocept.com>
2010-08-24 13:04:41 +0200

    move tests to separate directory

--- core/src/zeit/cms/workingcopy/tests/test_doctest.py
+++ core/src/zeit/cms/workingcopy/tests/test_doctest.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2007-2010 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
 
-from zope.testing import doctest
+import doctest
 
 
 def test_suite():
     return doctest.DocFileSuite(
         'README.txt',
+        package='zeit.cms.workingcopy',
         optionflags=(doctest.REPORT_NDIFF + doctest.NORMALIZE_WHITESPACE +
                      doctest.ELLIPSIS))
