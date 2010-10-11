2178fb4f41271aa002d29d206885eabc38f63efb
Christian Zagrodnick <cz@gocept.com>
2010-10-11 18:00:55 +0200

    added some basic contentEditable, nodemerge, restore expermiments
    * merging sort of works
    * doesn't save anything yet
    * re-load of to-be-saved data works

--- core/src/zeit/content/article/browser/resources/editor.js
+++ core/src/zeit/content/article/browser/resources/editor.js
@@ -12,4 +12,69 @@ var ident = MochiKit.Signal.connect(
         'editable-body');
     });
 
+
+zeit.content.article.Editable = gocept.Class.extend({
+    // Inline editing module
+
+    construct: function(context_element) {
+        var self = this;
+        self.context = context_element;
+        self.editable = self.merge();
+        self.editable.contentEditable = true;
+        self.editable.focus();
+        MochiKit.Signal.connect(self.editable, 'onblur', self, self.save);
+    },
+
+    merge: function() {
+        var self = this;
+        var block = MochiKit.DOM.getFirstParentByTagAndClassName(
+            self.context, null, 'block');
+        var blocks = MochiKit.DOM.getElementsByTagAndClassName(
+            null, 'block', block.parentNode);
+        var i = blocks.indexOf(block);
+        var paragraphs = [];
+        while (i > 0) {
+            i -= 1;
+            if (MochiKit.DOM.hasElementClass(blocks[i], 'type-paragraph')) {
+                paragraphs.push(blocks[i]);
+            } else {
+                break
+            }
+        }
+        paragraphs.reverse();
+        paragraphs.push(block);
+        var i = blocks.indexOf(block);
+        while (i < blocks.length) {
+            i += 1;
+            if (MochiKit.DOM.hasElementClass(blocks[i], 'type-paragraph')) {
+                paragraphs.push(blocks[i]);
+            } else {
+                break
+            }
+        }
+        var editable = MochiKit.DOM.getFirstElementByTagAndClassName(
+            null, 'editable', paragraphs[0]);
+        forEach(paragraphs.slice(1), function(paragraph) {
+            forEach(MochiKit.Selector.findChildElements(
+                paragraph, ['.editable p']), function(p) {
+                editable.appendChild(p);
+            });
+            // We might need to store the id of the removed paragraph here; not
+            // sure, yet.
+            MochiKit.DOM.removeElement(paragraph);
+        });
+        return editable
+    },
+
+    save: function() {
+        var self = this;
+        log('I should save', self.editable.innerHTML);
+        MochiKit.Signal.disconnectAll(self.editable, 'onblur');
+        // until now, the editor can only be contained in an editable-body.
+        var url = $('editable-body').getAttribute('cms:url') + '/@@contents';
+        zeit.edit.editor.reload('editable-body', url);
+    }
+
+});
+
 })();
