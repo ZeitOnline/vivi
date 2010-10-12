7e0b24e7dbe29e463966909aac3cb7ab1fd09b10
Christian Zagrodnick <cz@gocept.com>
2010-10-12 09:30:04 +0200

    move generic add/delete views from zeit.content.cp

--- core/src/zeit/edit/browser/block.py
+++ core/src/zeit/edit/browser/block.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import zeit.edit.browser.view
+import zeit.edit.interfaces
+import zope.component
 import zope.viewlet.manager
 
 
@@ -18,3 +21,27 @@ class BlockViewletManager(zope.viewlet.manager.WeightOrderedViewletManager):
         #if self.validation_class:
         #    classes.append(self.validation_class)
         return ' '.join(classes)
+
+
+class Add(zeit.edit.browser.view.Action):
+
+    type = zeit.edit.browser.view.Form('type')
+
+    def update(self):
+        factory = zope.component.getAdapter(
+            self.context, zeit.edit.interfaces.IElementFactory,
+            name=self.type)
+        created = factory()
+        self.signal('after-reload', 'added', created.__name__)
+
+
+class Delete(zeit.edit.browser.view.Action):
+
+    key = zeit.edit.browser.view.Form('key')
+
+    def update(self):
+        del self.context[self.key]
+        self.signal('before-reload', 'deleted', self.key)
+        self.signal(
+            None, 'reload', self.context.__name__, self.url(self.context,
+                                                            '@@contents'))
