20c428687877a6d871265901a79393721e0e2881
Christian Zagrodnick <cz@gocept.com>
2010-11-23 15:56:52 +0100

    re #8080: implemented raw functionality to rename on checkin

--- core/src/zeit/cms/repository/checkout.py
+++ core/src/zeit/cms/repository/checkout.py
@@ -6,6 +6,7 @@ import zeit.cms.checkout.interfaces
 import zeit.cms.interfaces
 import zeit.cms.repository.interfaces
 import zope.component
+import zope.copypastemove.interfaces
 import zope.interface
 
 
@@ -37,8 +38,23 @@ def default_local_content_adapter(context):
 def add_to_repository(context, ignore_conflicts):
     repository = zope.component.getUtility(
         zeit.cms.repository.interfaces.IRepository)
+    renamable = zeit.cms.repository.interfaces.IAutomaticallyRenameable(
+        context)
+    if renamable.renamable and renamable.rename_to:
+        rename_to = renamable.rename_to
+        # Remove the attributes so we don't clutter the dav.
+        renamable.renamable = None
+        renamable.rename_to = None
+    else:
+        rename_to = None
+
     repository.addContent(context, ignore_conflicts)
     added = repository.getContent(context.uniqueId)
+    if rename_to:
+        mover = zope.copypastemove.interfaces.IObjectMover(added)
+        rename_to = mover.moveTo(added.__parent__, rename_to)
+        added = added.__parent__[rename_to]
+
     return added
 
 
@@ -59,3 +75,15 @@ def default_repository_content_adapter_non_conflicting(context):
     # Default adapter to adapt local content to repository content: add to
     # repository and return
     return add_to_repository(context, True)
+
+
+class AutomaticallyRenamable(zeit.cms.content.dav.DAVPropertiesAdapter):
+
+    grokcore.component.adapts(zeit.cms.repository.interfaces.IDAVContent)
+    grokcore.component.implements(
+        zeit.cms.repository.interfaces.IAutomaticallyRenameable)
+
+    zeit.cms.content.dav.mapProperties(
+        zeit.cms.repository.interfaces.IAutomaticallyRenameable,
+        'http//namespaces.zeit.de/CMS/meta',
+        ('renamable', 'rename_to'))
--- core/src/zeit/cms/repository/tests/test_checkout.py
+++ core/src/zeit/cms/repository/tests/test_checkout.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import unittest2
 import zeit.cms.testing
 
 
@@ -35,3 +36,75 @@ class DefaultAdapterTests(zeit.cms.testing.FunctionalTestCase):
             self.assertEqual('Could not adapt', e.args[0])
         else:
             self.fail('TypeError not raised')
+
+
+class TestRenameOnCheckin(zeit.cms.testing.FunctionalTestCase,
+                          unittest2.TestCase):
+
+    def get_content(self, name='testcontent'):
+        import zeit.cms.interfaces
+        return zeit.cms.interfaces.ICMSContent(
+            'http://xml.zeit.de/{0}'.format(name))
+
+    def test_content_should_not_be_automatically_renamable_by_default(self):
+        from zeit.cms.repository.interfaces import IAutomaticallyRenameable
+        self.assertFalse(
+            IAutomaticallyRenameable(self.get_content()).renamable)
+
+    def test_renamable_and_new_name_should_rename(self):
+        from zeit.cms.repository.interfaces import IAutomaticallyRenameable
+        content = self.get_content()
+        from zeit.cms.checkout.helper import checked_out
+        with checked_out(content) as co:
+            renamable = IAutomaticallyRenameable(co)
+            renamable.renamable = True
+            renamable.rename_to = u'new-name'
+        self.assertIn('new-name', content.__parent__.keys())
+        self.assertNotIn('testcontent', content.__parent__.keys())
+
+    def test_renamable_and_no_new_name_should_not_rename(self):
+        from zeit.cms.repository.interfaces import IAutomaticallyRenameable
+        content = self.get_content()
+        from zeit.cms.checkout.helper import checked_out
+        self.assertIn('testcontent', content.__parent__.keys())
+        with checked_out(content) as co:
+            renamable = IAutomaticallyRenameable(co)
+            renamable.renamable = True
+            self.assertIsNone(renamable.rename_to)
+        self.assertIn('testcontent', content.__parent__.keys())
+
+    def test_not_renamable_and_new_name_should_not_rename(self):
+        from zeit.cms.repository.interfaces import IAutomaticallyRenameable
+        content = self.get_content()
+        from zeit.cms.checkout.helper import checked_out
+        with checked_out(content) as co:
+            renamable = IAutomaticallyRenameable(co)
+            renamable.renamable = False
+            renamable.rename_to = u'new-name'
+        self.assertNotIn('new-name', content.__parent__.keys())
+        self.assertIn('testcontent', content.__parent__.keys())
+
+    def test_not_renamable_and_no_new_name_should_not_rename(self):
+        from zeit.cms.repository.interfaces import IAutomaticallyRenameable
+        content = self.get_content()
+        from zeit.cms.checkout.helper import checked_out
+        with checked_out(content) as co:
+            renamable = IAutomaticallyRenameable(co)
+            renamable.renamable = False
+        self.assertNotIn('new-name', content.__parent__.keys())
+        self.assertIn('testcontent', content.__parent__.keys())
+
+    def test_renamable_should_be_none_after_rename(self):
+        from zeit.cms.repository.interfaces import IAutomaticallyRenameable
+        content = self.get_content()
+        from zeit.cms.checkout.helper import checked_out
+        with checked_out(content) as co:
+            renamable = IAutomaticallyRenameable(co)
+            renamable.renamable = True
+            renamable.rename_to = u'new-name'
+        content = self.get_content('new-name')
+        renamable = IAutomaticallyRenameable(content)
+        # Test for None as this is the value which we get when the DAV property
+        # does not exist. And thats what we really want.
+        self.assertIsNone(renamable.renamable)
+        self.assertIsNone(renamable.rename_to)
