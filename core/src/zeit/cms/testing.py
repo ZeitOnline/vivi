bfadd91f5952ff76744327e366ecbd5933eddd7c
Christian Zagrodnick <cz@gocept.com>
2010-12-09 14:18:30 +0100

    clear the session storage prior a test: we're now using it and need to be clean for a new test

--- core/src/zeit/cms/testing.py
+++ core/src/zeit/cms/testing.py
@@ -265,6 +265,7 @@ class SeleniumTestCase(gocept.selenium.ztk.TestCase,
             self.old_log_level = logging.root.level
             logging.root.setLevel(logging.ERROR)
             transaction.commit()
+        self.selenium.getEval('window.sessionStorage.clear()')
 
     def tearDown(self):
         zeit.cms.testing.tearDown(self)
