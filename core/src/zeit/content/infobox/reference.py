91631113e81ce95afa63757410f349b0409a9047
Christian Zagrodnick <cz@gocept.com>
2010-12-13 13:20:59 +0000

    - Define security for InfoboxReference (re #8412).

--- core/src/zeit/content/infobox/reference.py
+++ core/src/zeit/content/infobox/reference.py
@@ -1,29 +1,16 @@
 # Copyright (c) 2007-2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import grokcore.component
 import zeit.cms.content.dav
-import zeit.cms.interfaces
-import zeit.connector.interfaces
 import zeit.content.infobox.interfaces
-import zope.component
-import zope.interface
 
 
-class InfoboxReference(object):
+class InfoboxReference(zeit.cms.content.dav.DAVPropertiesAdapter):
 
-    zope.component.adapts(zeit.cms.interfaces.ICMSContent)
-    zope.interface.implements(
+    grokcore.component.implements(
         zeit.content.infobox.interfaces.IInfoboxReference)
 
     infobox = zeit.cms.content.dav.DAVProperty(
         zeit.content.infobox.interfaces.IInfoboxReference['infobox'],
         'http://namespaces.zeit.de/CMS/document', 'artbox_info')
-
-    def __init__(self, context):
-        self.context = context
-
-
-@zope.interface.implementer(zeit.connector.interfaces.IWebDAVProperties)
-@zope.component.adapter(InfoboxReference)
-def infobox_reference_to_webdav_properties(context):
-    return zeit.connector.interfaces.IWebDAVProperties(context.context)
--- core/src/zeit/content/infobox/tests.py
+++ core/src/zeit/content/infobox/tests.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2007-2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import mock
 import pkg_resources
 import unittest
 import zeit.cms.content.tests.test_contentsource
@@ -23,10 +24,28 @@ class InfoboxSourceTest(
     expected_types = ['infobox']
 
 
+class ReferenceTest(zeit.cms.testing.FunctionalTestCase):
+
+    layer = InfoboxLayer
+
+    def test_should_have_security_desclarations(self):
+        from zeit.content.infobox.reference import InfoboxReference
+        import zope.security
+        import zope.security.proxy
+        ref = InfoboxReference(mock.Mock)
+        ref_proxied = zope.security.proxy.ProxyFactory(ref)
+        # A False value indicates that currently there is no access but that
+        # there is a way to get access with the right permission. Otherwise
+        # ForbiddenAttribute is raised.
+        self.assertFalse(zope.security.canAccess(ref_proxied, 'infobox'))
+        self.assertFalse(zope.security.canWrite(ref_proxied, 'infobox'))
+
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
         layer=InfoboxLayer))
     suite.addTest(unittest.makeSuite(InfoboxSourceTest))
+    suite.addTest(unittest.makeSuite(ReferenceTest))
     return suite
