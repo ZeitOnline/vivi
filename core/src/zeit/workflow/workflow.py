b651390de30da9f0e8cce2c885955ecb06119a63
Christian Zagrodnick <cz@gocept.com>
2010-12-16 15:03:38 +0100

    future import no longer required

--- core/src/zeit/workflow/workflow.py
+++ core/src/zeit/workflow/workflow.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2007-2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from __future__ import with_statement
 from zeit.cms.i18n import MessageFactory as _
 import logging
 import zeit.cms.checkout.interfaces
