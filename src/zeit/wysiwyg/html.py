27715f1e117cf99a3277fc3eddc9d17b6f52a0fd
Christian Zagrodnick <cz@gocept.com>
2011-01-17 11:27:30 +0100

    2011

--- src/zeit/wysiwyg/html.py
+++ src/zeit/wysiwyg/html.py
@@ -1,5 +1,5 @@
 # coding: utf8
-# Copyright (c) 2008-2010 gocept gmbh & co. kg
+# Copyright (c) 2008-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
 from zeit.cms.content.util import objectify_soup_fromstring
