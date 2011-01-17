ff59938c15de59a3539cc36c6d8f44a1609c0e2f
Christian Zagrodnick <cz@gocept.com>
2011-01-17 11:27:30 +0100

    2011

--- core/src/zeit/__init__.py
+++ core/src/zeit/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2010 gocept gmbh & co. kg
+# Copyright (c) 2009-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 #namespace package boilerplate
 __import__('pkg_resources').declare_namespace(__name__)
