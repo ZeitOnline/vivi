5b5e1dd9694c7f64418f7bcfab4b394eedc3f26b
Christian Zagrodnick <cz@gocept.com>
2011-01-20 16:27:43 +0100

    make default 0.9.0dev

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'zeit.brightcove',
-    version='0.8.1dev',
+    version='0.9.0dev',
     author = 'Christian Zagrodnick',
     author_email = 'cz@gocept.com',
     description = '',
