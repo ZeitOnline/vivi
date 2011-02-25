a64b1c683cdad3c8bad1c883346fe39f2ffc5b1f
Wolfgang Schnerring <ws@gocept.com>
2011-02-25 12:51:16 +0100

    Re #8108: Add rule globs for article

---
+++ core/src/zeit/content/article/edit/rule.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from zeit.edit.rule import glob
+import zeit.content.article.edit.interfaces
+import zope.interface
+
+
+@glob(zeit.content.article.edit.interfaces.IArticle)
+def article(context):
+    return True
+
+
+@glob(zope.interface.Interface)
+def article(context):
+    return False
+
+
+@glob(zeit.content.article.edit.interfaces.IVideo)
+def content(context):
+    return filter(None, [context.video, context.video_2])
+
+
+@glob(zeit.content.article.edit.interfaces.IReference)
+def content(context):
+    return [context.references]
+
+
+@glob(zeit.content.article.edit.interfaces.ILayoutable)
+def layout(context):
+    return context.layout
+
+# layout(Interface) is defined in zeit.content.cp.rule
