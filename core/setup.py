e7ceb918c7035354e8f427f49a57703e537c252d
Christian Zagrodnick <cz@gocept.com>
2011-03-23 22:30:22 +0100

    Preparing release 0.9.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'zeit.brightcove',
-    version='0.9.0dev',
+    version='0.9.0',
     author = 'Christian Zagrodnick',
     author_email = 'cz@gocept.com',
     description = '',
