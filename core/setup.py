1e974c595c1141b0d3c07fd19fb4d26aab2eb183
Wolfgang Schnerring <ws@gocept.com>
2011-06-21 12:08:55 +0200

    Back to development: 1.49.2

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='1.49.1',
+    version='1.49.2dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
