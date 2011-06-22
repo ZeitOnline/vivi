93511c7e81bb1008f5e27a450a3d3d71c5820532
Christian Zagrodnick <cz@gocept.com>
2011-06-22 16:43:44 +0200

    Back to development: 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.vgwort',
-    version='0.3.1',
+    version='0.3.2dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
