b80bb886e1afa32441ee0a59ff6f86ac030f4aae
Wolfgang Schnerring <ws@gocept.com>
2011-06-27 11:08:38 +0200

    Create skeleton

---
+++ core/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.newsletter',
+    version='0.1dev',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages=['zeit'],
+    install_requires=[
+        'zeit.cms',
+        'zeit.edit',
+    ],
+)
