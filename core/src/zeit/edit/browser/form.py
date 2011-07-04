2de7ae30c233b4737b0568a497b586a5f87a5675
Wolfgang Schnerring <ws@gocept.com>
2011-07-04 15:12:24 +0200

    Extract base class InlineForm and the collecting view (used to be @@article-forms, now @@edit-forms) from zeit.content.article

---
+++ core/src/zeit/edit/browser/form.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zeit.edit.browser.view
+import zope.app.pagetemplate
+import zope.formlib.form
+
+
+class Forms(object):
+    """View that collects all inline forms."""
+
+
+class InlineForm(zope.formlib.form.SubPageEditForm,
+                 zeit.edit.browser.view.UndoableMixin):
+
+    template = zope.app.pagetemplate.ViewPageTemplateFile('inlineform.pt')
+
+    def __call__(self):
+        self.mark_transaction_undoable()
+        return super(InlineForm, self).__call__()
+
+    @property
+    def widget_data(self):
+        result = []
+        for widget in self.widgets:
+            css_class = ['widget-outer']
+            if widget.error():
+                css_class.append('error')
+            result.append(dict(
+                css_class=' '.join(css_class),
+                widget=widget,
+            ))
+        return result
