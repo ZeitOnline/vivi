e8411f14568ab0aa152a4e3899ddfcfbeb295b58
Wolfgang Schnerring <ws@gocept.com>
2011-07-05 13:35:23 +0200

    Re #9132: Show some kind of image for teasers (very rough draft)

--- core/src/zeit/newsletter/browser/edit.py
+++ core/src/zeit/newsletter/browser/edit.py
@@ -2,6 +2,8 @@
 # See also LICENSE.txt
 
 from zeit.cms.i18n import MessageFactory as _
+from zope.cachedescriptors.property import Lazy as cachedproperty
+import zeit.cms.browser.view
 import zeit.cms.content.interfaces
 import zeit.cms.interfaces
 import zeit.edit.browser.form
@@ -33,12 +35,35 @@ class TeaserLandingZone(LandingZoneBase):
     order = 'after-context'
 
 
-class Teaser(object):
+class Teaser(zeit.cms.browser.view.Base):
 
-    @property
+    @cachedproperty
     def metadata(self):
         return zeit.cms.content.interfaces.ICommonMetadata(
             self.context.reference, None)
+
+    @cachedproperty
+    def image(self):
+        # XXX copy&paste&tweak of zeit.content.cp.browser.blocks.teaser.Display
+        content = self.context.reference
+        if content is None:
+            return
+        images = zeit.content.image.interfaces.IImages(content, None)
+        if images is None:
+            preview = zope.component.queryMultiAdapter(
+                (content, self.request), name='preview')
+            if preview:
+                return self.url(preview)
+            return
+        if not images.images:
+            return
+        image = images.images[0]
+        if zeit.content.image.interfaces.IImageGroup.providedBy(image):
+            img = image.get('184x84')
+            if img:
+                return self.url(img)
+        else:
+            return self.url(image)
 
 
 class GroupTitle(zeit.edit.browser.form.InlineForm):
