25f10e46c92739d2ca6899d1b9370093c19f9ef4
Wolfgang Schnerring <ws@gocept.com>
2011-07-05 15:18:54 +0200

    Re #9138: Add a workflow and form specific for newsletters

---
+++ core/src/zeit/newsletter/workflow.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import grokcore.component as grok
+import zeit.newsletter.interfaces
+import zeit.workflow.interfaces
+import zeit.workflow.publishinfo
+
+
+class Workflow(zeit.workflow.publishinfo.NotPublishablePublishInfo,
+               grok.Adapter):
+
+    grok.context(zeit.newsletter.interfaces.INewsletter)
+    grok.implements(zeit.newsletter.interfaces.INewsletterWorkflow)
+    grok.provides(zeit.newsletter.interfaces.INewsletterWorkflow)
+
+    zeit.cms.content.dav.mapProperties(
+        zeit.newsletter.interfaces.INewsletterWorkflow,
+        zeit.workflow.interfaces.WORKFLOW_NS,
+        ('sent',),
+        use_default=True, live=True)
+
+    def can_publish(self):
+        return True
