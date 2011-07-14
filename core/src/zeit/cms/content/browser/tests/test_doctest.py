21dc4a860dd53d4e1d56a8724b1a1d62c5226dcb
Dominik Hoppe <dominik.hoppe@zeit.de>
2011-07-14 17:28:07 +0200

    This is a merge from r2464.
    Fixes #9231: Fix product source so it works in the face of SecurityProxies
    
    The plain instanceof in the equality check always returned False, so the
    Drop-Down always displayed the default value instead of the value the content
    object actually has

--- core/src/zeit/cms/content/browser/tests/test_doctest.py
+++ core/src/zeit/cms/content/browser/tests/test_doctest.py
@@ -10,6 +10,7 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'form.txt',
+        'sources.txt',
         'template.txt',
         'typechange.txt',
         'widget-subnav.txt',
