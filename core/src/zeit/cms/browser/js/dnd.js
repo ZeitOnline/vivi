c210d05ae49fe95ed3a866b5afc594b8b43b535d
Wolfgang Schnerring <ws@gocept.com>
2011-08-05 14:02:31 +0200

    Fixes #9404: The draggable source element might not have a css_class

--- core/src/zeit/cms/browser/js/dnd.js
+++ core/src/zeit/cms/browser/js/dnd.js
@@ -55,8 +55,9 @@ MochiKit.Signal.connect(
     // the type-* class is used as an accept filter by ObjectSequenceWidget and
     // DropObjectWidget, so the drag-pane needs to have those too to be
     // droppable there
+    var class_ = source_element.getAttribute('class') || '';
     forEach(
-        source_element.getAttribute('class').split(' '), function(class_) {
+        class_.split(' '), function(class_) {
             if (class_.indexOf('type-') == 0) {
                 MochiKit.DOM.addElementClass(div, class_);
             }
