aa47dd230b528807aa3ad89fa5e5389b03d6d19d
Wolfgang Schnerring <ws@gocept.com>
2011-08-08 12:56:48 +0200

    Make room for unit tests

---
+++ core/src/zeit/cms/checkout/tests/test_doctests.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2007-2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zeit.cms.checkout
+import zeit.cms.testing
+
+
+def test_suite():
+    return zeit.cms.testing.FunctionalDocFileSuite(
+        'README.txt',
+        'manager.txt',
+        package=zeit.cms.checkout)
