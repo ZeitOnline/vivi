e081d41c3445f8371b963ece530c454ae5ef27ed
Wolfgang Schnerring <ws@gocept.com>
2011-08-24 13:18:00 +0200

    Re #9242: Extract common settings into base class

---
+++ core/src/zeit/cms/browser/tests/test_jslint.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zeit.cms.testing
+
+
+class JSLintTest(zeit.cms.testing.JSLintTestCase):
+
+    include = ('zeit.cms.browser:js',)
+    exclude = (
+        'MochiKit.js',
+        'json-template.js',
+        )
