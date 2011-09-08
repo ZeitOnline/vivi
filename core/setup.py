31f933d798ee78f0913895d29c9360a89270d8e3
Thomas Lotze <tl@gocept.com>
2011-09-08 10:52:39 +0200

    initial check-in of boilerplate for the 2.0 rewrite

---
+++ core/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.content.video',
+    version = '2.0dev',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='https://svn.gocept.com/repos/gocept-int/zeit.content.video',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages = ['zeit', 'zeit.content'],
+    install_requires=[
+        'setuptools',
+    ],
+)
