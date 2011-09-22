86d2a12b028b7a7707da67afcfbbdd7c88876604
Christian Zagrodnick <cz@gocept.com>
2011-09-22 17:34:59 +0200

    remove browser packge and use a more lightweight browser module

--- core/src/zeit/content/video/tests/test_doctest.py
+++ core/src/zeit/content/video/tests/test_doctest.py
@@ -8,4 +8,5 @@ import zeit.content.video.testing
 def test_suite():
     return zeit.content.video.testing.FunctionalDocFileSuite(
         'asset.txt',
+        'browser.txt',
         package='zeit.content.video')
