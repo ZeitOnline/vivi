b7c3f224e6bed74338a35fd46a14099a0fd79d4c
Christian Zagrodnick <cz@gocept.com>
2011-10-06 09:07:30 +0000

    Back to development: 0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.objectlog',
-    version='0.10',
+    version='0.11dev',
     author='Christian Zagrodnick',
     author_email='cz@gocept.com',
     description="""\
