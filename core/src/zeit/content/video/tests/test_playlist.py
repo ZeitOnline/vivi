239d6947a776f8563b89dabc49fa054dc066063b
Christian Zagrodnick <cz@gocept.com>
2011-10-06 14:36:54 +0200

    add references adapter so videos can know in which playlists they're in.

--- core/src/zeit/content/video/tests/test_playlist.py
+++ core/src/zeit/content/video/tests/test_playlist.py
@@ -16,3 +16,21 @@ class TestPlaylist(zeit.content.video.testing.TestCase):
         pls.videos = (video,)
         self.assertEqual(
             u'Bla bla', pls.xml['body']['videos']['video']['text'])
+
+
+class TestReferencesAdapter(zeit.content.video.testing.TestCase):
+
+    def test_should_contains(self):
+        factory = zeit.content.video.testing.video_factory(self)
+        video = factory.next()
+        video.teaserText = u'Bla bla'
+        video = factory.next()  # in repository
+        factory = zeit.content.video.testing.playlist_factory(self)
+        pls = factory.next()
+        pls.videos = (video,)
+
+        videos = zeit.cms.relation.interfaces.IReferences(pls)
+        self.assertEqual(1, len(videos))
+        self.assertEquals(
+            'http://xml.zeit.de/video', videos[0].uniqueId)
+
