1e659f159d6df873d15c2da2f4508d592d729b2a
Christian Zagrodnick <cz@gocept.com>
2011-11-13 18:19:16 +0000

    Preparing release 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.addcentral',
-    version='1.0.1dev',
+    version='1.0.1',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
