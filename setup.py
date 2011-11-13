b669b960955c995a83857a357a17bec8940cf5c6
Christian Zagrodnick <cz@gocept.com>
2011-11-13 12:20:43 +0000

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.addcentral',
-    version='1.0.0',
+    version='1.0.1dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
