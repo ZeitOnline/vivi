69b976555d6e272a787c63b09ff69851f9570b75
Christian Zagrodnick <cz@gocept.com>
2011-11-13 19:10:21 +0100

    Back to development: 1.40.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.wysiwyg',
-    version='1.40.0',
+    version='1.40.1dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
