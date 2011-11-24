99ca352fe464d635ab2dbdc93de02a9c7491fb59
Christian Zagrodnick <cz@gocept.com>
2011-11-24 14:05:30 +0000

    Back to development: 1.22.9

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.portraitbox',
-    version='1.22.8',
+    version='1.22.9dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
