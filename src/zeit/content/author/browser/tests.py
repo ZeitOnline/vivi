de0a56fabb2ad3f63edbde9e1122815a9a4d8259
Thomas Lotze <tl@gocept.com>
2011-12-06 11:34:09 +0100

    turned browser tests from doc tests to test class

--- src/zeit/content/author/browser/tests.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2010 gocept gmbh & co. kg
-# See also LICENSE.txt
-
-import zeit.content.author.testing
-
-
-def test_suite():
-    return zeit.content.author.testing.FunctionalDocFileSuite(
-        'README.txt')
