c39aaf3f5dfd667784c38bfe43ab24d85ff8bd37
Wolfgang Schnerring <ws@gocept.com>
2011-12-08 15:40:45 +0100

    Re #10068: Use BeforeCheckin(while_publishing) instead of BeforePublish+cycle for update_feed_items to avoid the extra checkin/checkout-cycle

--- core/src/zeit/content/cp/centerpage.py
+++ core/src/zeit/content/cp/centerpage.py
@@ -217,43 +217,41 @@ class Feed(zeit.cms.related.related.RelatedBase):
 
 @zope.component.adapter(
     zeit.content.cp.interfaces.ICenterPage,
-    zeit.cms.workflow.interfaces.IBeforePublishEvent)
+    zeit.cms.checkout.interfaces.IBeforeCheckinEvent)
 def update_feed_items(context, event):
-    # The CP is cycled during publish but we need to do that again :(
-    # Ignore conflicts during checkin here. It's almost impossible that
-    # somebody else has changed the CP. Unfortunately there is no explicit test
-    # for ignoring conflicts.
-    with zeit.cms.checkout.helper.checked_out(
-        context, events=False, ignore_conflicts=True) as co:
-        if co is None:
-            return
-        feed = zeit.content.cp.interfaces.ICPFeed(co)
-        items = []
-        check_items = []
-        for item in feed.items:
-            if zeit.content.cp.interfaces.IXMLTeaser.providedBy(item):
-                check_item = item.original_content
-            else:
-                check_item = item
-            items.append(item)
-            check_items.append(check_item)
+    if not event.publishing:
+        # This event should only happen while publishing. It used to be on
+        # BeforePublish, but since that requires another checkin/checkout
+        # cycle, and the centerpage is cycled during publish anyway, we can
+        # register it like this and avoid the additional cycle.
+        return
+    feed = zeit.content.cp.interfaces.ICPFeed(context)
+    items = []
+    check_items = []
+    for item in feed.items:
+        if zeit.content.cp.interfaces.IXMLTeaser.providedBy(item):
+            check_item = item.original_content
+        else:
+            check_item = item
+        items.append(item)
+        check_items.append(check_item)
 
-        for item in zeit.cms.syndication.interfaces.IReadFeed(context):
-            if zeit.content.cp.interfaces.IXMLTeaser.providedBy(item):
-                if item.original_content in check_items:
-                    continue
-            elif item in check_items:
+    for item in zeit.cms.syndication.interfaces.IReadFeed(context):
+        if zeit.content.cp.interfaces.IXMLTeaser.providedBy(item):
+            if item.original_content in check_items:
                 continue
-            items.insert(0, item)
+        elif item in check_items:
+            continue
+        items.insert(0, item)
 
-        items_in_lead = len(zeit.cms.syndication.interfaces.IReadFeed(context))
-        config = zope.app.appsetup.product.getProductConfiguration(
-            'zeit.content.cp')
-        max_items = max(items_in_lead, int(config['cp-feed-max-items']))
-        while len(items) > max_items:
-            del items[-1]
+    items_in_lead = len(zeit.cms.syndication.interfaces.IReadFeed(context))
+    config = zope.app.appsetup.product.getProductConfiguration(
+        'zeit.content.cp')
+    max_items = max(items_in_lead, int(config['cp-feed-max-items']))
+    while len(items) > max_items:
+        del items[-1]
 
-        feed.items = items
+    feed.items = items
 
 
 def has_changed(context):
