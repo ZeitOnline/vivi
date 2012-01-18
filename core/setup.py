c8a6f06fc997ad703dcb94d49287a6c9635d3a69
Christian Zagrodnick <cz@gocept.com>
2012-01-18 14:54:43 +0100

    Preparing release 2.1.1

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.video',
-    version='2.1.1dev',
+    version='2.1.1',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.content.video',
