260d588aefb31f16282b36de3e852a77c4f778b0
Thomas Lotze <tl@gocept.com>
2012-02-06 13:53:10 +0000

    re #10363: Fixed the existence test for security declarations

--- core/src/zeit/content/infobox/tests.py
+++ core/src/zeit/content/infobox/tests.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2007-2009 gocept gmbh & co. kg
+# Copyright (c) 2007-2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
 import mock
@@ -28,17 +28,20 @@ class ReferenceTest(zeit.cms.testing.FunctionalTestCase):
 
     layer = InfoboxLayer
 
-    def test_should_have_security_desclarations(self):
+    def test_should_have_security_declarations(self):
         from zeit.content.infobox.reference import InfoboxReference
         import zope.security
         import zope.security.proxy
         ref = InfoboxReference(mock.Mock)
         ref_proxied = zope.security.proxy.ProxyFactory(ref)
-        # A False value indicates that currently there is no access but that
-        # there is a way to get access with the right permission. Otherwise
-        # ForbiddenAttribute is raised.
-        self.assertFalse(zope.security.canAccess(ref_proxied, 'infobox'))
-        self.assertFalse(zope.security.canWrite(ref_proxied, 'infobox'))
+        # If a security declaration exists, read or write access can be
+        # decided even though we don't care about the precise outcome of that
+        # (which is a matter of test user, security policy etc). Without a
+        # security declaration, ForbiddenAttribute would be raised.
+        self.assertNothingRaised(
+            zope.security.canAccess, ref_proxied, 'infobox')
+        self.assertNothingRaised(
+            zope.security.canWrite, ref_proxied, 'infobox')
 
 
 def test_suite():
