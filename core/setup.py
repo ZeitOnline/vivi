66073aea159223e9f9adbbe876048195c0b15e48
Thomas Lotze <tl@gocept.com>
2012-02-20 13:00:01 +0100

    re #10456: bumped version due to addition to testing API

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='1.52.1dev',
+    version='1.53.0dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
