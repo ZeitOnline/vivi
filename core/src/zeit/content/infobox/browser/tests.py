4f38fa08893e2c6c71c499501ef719868c106cfc
Thomas Lotze <tl@gocept.com>
2012-02-20 12:13:03 +0000

    imports

--- core/src/zeit/content/infobox/browser/tests.py
+++ core/src/zeit/content/infobox/browser/tests.py
@@ -1,12 +1,9 @@
-# Copyright (c) 2007-2009 gocept gmbh & co. kg
+# Copyright (c) 2007-2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from zope.testing import doctest
-import os
 import unittest
 import zeit.cms.testing
 import zeit.content.infobox.tests
-import zope.app.testing.functional
 
 
 def test_suite():
