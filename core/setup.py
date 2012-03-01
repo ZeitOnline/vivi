7c2923c3df1d2dc34ec19557f635dca825450acd
Wolfgang Schnerring <ws@gocept.com>
2012-03-01 15:36:16 +0100

    Preparing release 1.24.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.connector',
-    version='1.24.0dev',
+    version='1.24.0',
     author='Tomas Zerolo, Christian Zagrodnick',
     author_email='tomas@tuxteam.de, cz@gocept.com',
     url='http://trac.gocept.com/zeit',
