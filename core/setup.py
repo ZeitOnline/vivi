9163f3df0eb010431fdc04b91e3f70ca2db475b5
Christian Zagrodnick <cz@gocept.com>
2012-03-06 15:13:06 +0100

    Back to development: 1.53.3

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='1.53.2',
+    version='1.53.3.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
