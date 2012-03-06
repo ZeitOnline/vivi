ce2d262d39bdcae258091163df5008d055a220ee
Christian Zagrodnick <cz@gocept.com>
2012-03-06 19:37:31 +0000

    Back to development: 1.23.7

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.infobox',
-    version='1.23.6',
+    version='1.23.7.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
