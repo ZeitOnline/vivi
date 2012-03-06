37d0a2794a7da1b313c576cb7cd4b14a6d04a588
Christian Zagrodnick <cz@gocept.com>
2012-03-06 12:34:27 +0100

    Preparing release 1.41.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.wysiwyg',
-    version='1.41.0dev',
+    version='1.41.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
