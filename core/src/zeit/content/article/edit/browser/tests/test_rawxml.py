9c45340ba9a6d400a4f7097963d114f53354e1e9
Thomas Lotze <tl@gocept.com>
2012-03-12 13:47:05 +0100

    fixes #8426: fixed test for the raw-xml block

---
+++ core/src/zeit/content/article/edit/browser/tests/test_rawxml.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zeit.content.article.edit.browser.testing
+
+
+class Form(zeit.content.article.edit.browser.testing.BrowserTestCase):
+
+    block_type = 'raw'
+
+    def test_inline_form_saves_values(self):
+        self.get_article(with_empty_block=True)
+        b = self.browser
+        b.open('editable-body/blockname/@@edit-rawxml?show_form=1')
+        b.getControl('XML source').value = """\
+<raw xmlns:ns0="http://namespaces.zeit.de/CMS/cp" ns0:__name__="blockname">
+  <foo> </foo>
+</raw>
+"""
+        b.getControl('Apply').click()
+        b.open('@@edit-rawxml?show_form=1')
+        self.assertEllipsis("""\
+<raw...xmlns:ns0="http://namespaces.zeit.de/CMS/cp"...ns0:__name__="blockname"...>
+  <foo> </foo>
+</raw>
+""", b.getControl('XML source').value)
+
+    def test_xml_is_validated_root_must_be_raw_element(self):
+        self.get_article(with_empty_block=True)
+        b = self.browser
+        b.open('editable-body/blockname/@@edit-rawxml?show_form=1')
+        b.getControl('XML source').value = '<foo />'
+        b.getControl('Apply').click()
+        self.assertIn(
+            '<span class="error">The root element must be &lt;raw&gt;.</span>',
+            b.contents)
+
+
+class FormLoader(zeit.content.article.edit.browser.testing.EditorTestCase):
+
+    def test_rawxml_form_is_loaded(self):
+        s = self.selenium
+        self.add_article()
+        self.create_block('raw')
+        s.assertElementPresent('css=.block.type-raw .inline-form '
+                               '.widget-outer.fieldname-xml')
