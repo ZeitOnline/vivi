11fd59d130dcdbb22fae3180ec63a9d932b5fad0
Wolfgang Schnerring <ws@gocept.com>
2012-06-11 13:45:00 +0200

    Fixes #10850: Shut down remotetasks properly

--- core/src/zeit/workflow/browser/tests/test_publish.py
+++ core/src/zeit/workflow/browser/tests/test_publish.py
@@ -1,25 +1,49 @@
 # Copyright (c) 2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import lovely.remotetask.interfaces
+import threading
 import zeit.cms.testing
 import zeit.workflow.testing
+import zope.component
 
 
-class TestPublish(zeit.cms.testing.SeleniumTestCase):
+class RemoteTaskHelper(object):
+
+    def start_tasks(self):
+        self.tasks = []
+        with zeit.cms.testing.site(self.getRootFolder()):
+            for name, task in zope.component.getUtilitiesFor(
+                lovely.remotetask.interfaces.ITaskService):
+                task.startProcessing()
+                self.tasks.append(task)
+
+    def stop_tasks(self):
+        for task in self.tasks:
+            task.stopProcessing()
+            self._join_thread(task)
+
+    def _join_thread(self, task):
+        # XXX it would be nice if TaskService offered an API to do this
+        for thread in threading.enumerate():
+            if thread.getName() == task._threadName():
+                thread.join()
+
+
+class TestPublish(zeit.cms.testing.SeleniumTestCase,
+                  RemoteTaskHelper):
 
     layer = zeit.workflow.testing.selenium_layer
     skin = 'vivi'
 
     def setUp(self):
-        import zope.component
-        import lovely.remotetask.interfaces
         super(TestPublish, self).setUp()
-        with zeit.cms.testing.site(self.getRootFolder()):
-            for name, task in zope.component.getUtilitiesFor(
-                lovely.remotetask.interfaces.ITaskService):
-                task.startProcessing()
-                self.addCleanup(task.stopProcessing)
+        self.start_tasks()
         self.open('/repository/testcontent')
+
+    def tearDown(self):
+        self.stop_tasks()
+        super(TestPublish, self).tearDown()
 
     def prepare_content(self):
         from zeit.workflow.interfaces import IContentWorkflow
@@ -70,23 +94,21 @@ class TestPublish(zeit.cms.testing.SeleniumTestCase):
             config['zeit.workflow']['publish-script'] = old_script
 
 
-class TestRetract(zeit.cms.testing.SeleniumTestCase):
+class TestRetract(zeit.cms.testing.SeleniumTestCase,
+                  RemoteTaskHelper):
 
     layer = zeit.workflow.testing.selenium_layer
     skin = 'vivi'
 
     def setUp(self):
-        import zope.component
-        import lovely.remotetask.interfaces
         super(TestRetract, self).setUp()
-        with zeit.cms.testing.site(self.getRootFolder()):
-            for name, task in zope.component.getUtilitiesFor(
-                lovely.remotetask.interfaces.ITaskService):
-                task.startProcessing()
-                self.addCleanup(task.stopProcessing)
-
+        self.start_tasks()
         self.publish_info.published = True
         self.open('/repository/testcontent')
+
+    def tearDown(self):
+        self.stop_tasks()
+        super(TestRetract, self).tearDown()
 
     @property
     def publish_info(self):
