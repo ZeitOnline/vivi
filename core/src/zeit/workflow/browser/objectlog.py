f6710f98df1a1626c90c6638bf50c2a800797897
Thomas Lotze <tl@gocept.com>
2012-06-12 14:22:11 +0200

    re #10774: added an objectlog view

--- core/src/zeit/workflow/browser/objectlog.py
+++ core/src/zeit/workflow/browser/objectlog.py
@@ -1,10 +1,10 @@
-# Copyright (c) 2008-2011 gocept gmbh & co. kg
+# Copyright (c) 2008-2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import zc.resourcelibrary
+import zeit.objectlog.interfaces
 import zope.component
 import zope.interface
-
-import zeit.objectlog.interfaces
 
 
 class ProcessForDisplay(object):
@@ -19,3 +19,24 @@ class ProcessForDisplay(object):
 
     def __call__(self, entries):
         return tuple(entries)[-self.max_entries:]
+
+
+class ObjectLog(object):
+
+    def groups(self):
+        entries = zeit.objectlog.interfaces.ILog(self.context).get_log()
+        groups = {}
+        for entry in entries:
+            groups.setdefault(entry.time.date(), []).append(entry)
+        for date, entries in sorted(groups.items()):
+            yield dict(
+                display_date=date.strftime('%d.%m.%Y'),
+                entries=[dict(
+                        display_time=entry.time.strftime('%H:%M'),
+                        entry=entry,
+                        ) for entry in entries],
+                )
+
+    def __call__(self):
+        zc.resourcelibrary.need('zeit.workflow')
+        return super(ObjectLog, self).__call__()
---
+++ core/src/zeit/workflow/browser/tests/test_objectlog.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from datetime import datetime
+import zeit.cms.testing
+import zeit.objectlog.interfaces
+import zeit.workflow.testing
+
+
+class ObjectLog(zeit.cms.testing.BrowserTestCase):
+
+    layer = zeit.workflow.testing.WorkflowLayer
+
+    def test_log_entries_are_grouped_by_date(self):
+        testcontent = self.repository['testcontent']
+        object_log = zeit.objectlog.interfaces.ILog(testcontent)
+        with zeit.cms.testing.interaction():
+            with zeit.cms.testing.site(self.getRootFolder()):
+                object_log.log('one', timestamp=datetime(2012, 6, 12, 9, 14))
+                object_log.log('two', timestamp=datetime(2012, 6, 12, 10, 25))
+                object_log.log('three', timestamp=datetime(2012, 6, 13, 12, 8))
+        self.browser.handleErrors = False
+        self.browser.open(
+            'http://localhost/++skin++vivi/repository/testcontent/@@objectlog')
+        self.assertEllipsis("""...
+            <th colspan="3">12.06.2012</th>...
+            <td>one</td>
+            <td>09:14</td>
+            <td class="principal">Zope.User</td>...
+            <td>two</td>
+            <td>10:25</td>
+            <td class="principal">Zope.User</td>...
+            <th colspan="3">13.06.2012</th>...
+            <td>three</td>
+            <td>12:08</td>
+            <td class="principal">Zope.User</td>...
+            """, self.browser.contents)
