5b3ab7e4f1a5ff545171c10f2f68d648e96853c1
Wolfgang Schnerring <ws@gocept.com>
2012-06-14 11:39:58 +0200

    Make room for unit tests

--- core/src/zeit/cms/workflow/tests/test_doctest.py
+++ core/src/zeit/cms/workflow/tests/test_doctest.py
@@ -1,10 +1,8 @@
 # Copyright (c) 2007-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import unittest
-
 from zope.testing import doctest
-
+import unittest
 import zeit.cms.testing
 
 
@@ -12,9 +10,12 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(doctest.DocFileSuite(
         'mock.txt',
+        package='zeit.cms.workflow',
         optionflags=(doctest.REPORT_NDIFF + doctest.NORMALIZE_WHITESPACE +
                      doctest.ELLIPSIS)))
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'modified.txt',
-        'status.txt'))
+        'status.txt',
+        package='zeit.cms.workflow',
+        ))
     return suite
