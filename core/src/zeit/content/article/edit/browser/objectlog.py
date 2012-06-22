1a154bd4bfe7912ce9daf8a5aac091b05e62942a
Wolfgang Schnerring <ws@gocept.com>
2012-06-22 10:59:55 +0200

    Fixes #11002: Load objectlog via JS

--- core/src/zeit/content/article/edit/browser/objectlog.py
+++ core/src/zeit/content/article/edit/browser/objectlog.py
@@ -2,10 +2,9 @@
 # See also LICENSE.txt
 
 import zeit.cms.interfaces
-import zope.viewlet.viewlet
 
 
-class ObjectLog(zope.viewlet.viewlet.ViewletBase):
+class ObjectLog(object):
 
     @property
     def repository_content(self):
