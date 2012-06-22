6b968fc4fb88fe9a53d36396288e8248c28f27eb
Wolfgang Schnerring <ws@gocept.com>
2012-06-22 11:17:00 +0200

    Fixes #11050: Remove options diver

--- core/src/zeit/edit/browser/form.py
+++ core/src/zeit/edit/browser/form.py
@@ -72,22 +72,3 @@ class DiverForm(InlineForm):
     template = zope.app.pagetemplate.ViewPageTemplateFile('diver.pt')
 
     css_class = 'diver-form'
-
-
-class OptionsDiver(DiverForm):
-
-    legend = _('Options')
-    prefix = 'options-diver'
-    undo_description = _('edit options')
-    template = zope.app.pagetemplate.ViewPageTemplateFile('options-diver.pt')
-
-    form_fields = ()
-
-    def render(self):
-        result = super(OptionsDiver, self).render()
-        if result:
-            result += (
-                '<script type="text/javascript">'
-                '    jQuery("#options-diver").createFoldBoxes()'
-                '</script>')
-        return result
