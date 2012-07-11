2e944e793f0923755e847486b57d093f165fc547
Thomas Lotze <tl@gocept.com>
2012-07-11 13:51:57 +0200

    re #10677: removed diver as a concept

--- core/src/zeit/edit/browser/form.py
+++ core/src/zeit/edit/browser/form.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.browser.view
 import zeit.edit.browser.view
 import zope.app.pagetemplate
@@ -18,40 +17,6 @@ FoldableFormGroup = zope.viewlet.viewlet.SimpleViewletClass('layout.foldable-for
 
 FormLoader = zope.viewlet.viewlet.SimpleViewletClass('layout.form-loader.pt')
 
-class DiverFormGroup(zope.viewlet.viewlet.SimpleViewletClass('layout.diver-forms.pt')):
-    """ Contains all diver forms."""
-
-    def forms(self):
-        """Returns all actual views that belong to this form group."""
-        # XXX the wohle code here is rather icky, but there doesn't seem to be
-        # a cleaner way
-        from zeit.content.article.interfaces import IArticle
-        from zeit.cms.browser.interfaces import ICMSLayer
-        from zeit.edit.interfaces import IContentViewletManager
-
-        # resolve wrapper class that ZCML creates for views and viewlets to get
-        # to the original class
-        form_group_class = self.__class__.__bases__[0]
-
-        result = []
-        for reg in zope.component.getGlobalSiteManager().registeredAdapters():
-            # step 1: get all viewlets which have ourselves as the view
-            required = (
-                IArticle,
-                ICMSLayer,
-                zope.interface.implementedBy(form_group_class),
-                IContentViewletManager
-                )
-            if reg.required != required:
-                continue
-
-            # step 2: these viewlets are form-loaders that load an actual view
-            # of the same name, so we resolve that to get to the actual form
-            form = zope.component.getMultiAdapter(
-                (self.context, self.request), name=reg.name)
-            result.append(form)
-        return result
-
 
 class InlineForm(zeit.cms.browser.form.WidgetCSSMixin,
                  zope.formlib.form.SubPageEditForm,
@@ -65,10 +30,3 @@ class InlineForm(zeit.cms.browser.form.WidgetCSSMixin,
     def __call__(self):
         self.mark_transaction_undoable()
         return super(InlineForm, self).__call__()
-
-
-class DiverForm(InlineForm):
-
-    template = zope.app.pagetemplate.ViewPageTemplateFile('diver.pt')
-
-    css_class = 'diver-form'
