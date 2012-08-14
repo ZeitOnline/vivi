c9b81261bff4526268c3abc0657f972bd7c92791
Wolfgang Schnerring <ws@gocept.com>
2012-08-14 13:30:11 +0200

    whitespace

--- core/src/zeit/content/gallery/browser/details.py
+++ core/src/zeit/content/gallery/browser/details.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.browser.view
 import json
+
 
 class Details(zeit.cms.browser.view.Base):
     def entries(self):
@@ -15,4 +15,3 @@ class Details(zeit.cms.browser.view.Base):
                 url=self.url(entry.image)+"/preview",
                 copyrights=copyrights))
         return json.dumps(entries)
-         
