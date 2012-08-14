08764b232049959fc82a542c0aea3d6d17f371ce
Wolfgang Schnerring <ws@gocept.com>
2012-08-14 13:33:45 +0200

    Re #10688: Restructure JS code
    * Move code to separate file and bind to HTML via fragment-ready
    * Use classes instead of IDs for identifying elements, since there might be more than one gallery details per page

---
+++ core/src/zeit/content/gallery/browser/resources/details.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2012 gocept gmbh & co. kg
+// See also LICENSE.txt
+
+(function($) {
+
+zeit.cms.GalleryDetails = gocept.Class.extend({
+
+    construct: function(element) {
+        var self = this;
+        self.element = $(element);
+        self.pos = 0;
+        self.entries = $.parseJSON(self.element.attr('cms:data'));
+        self.element.bind('click', MochiKit.Base.bind(self.handle_click, self));
+        self.change_image(0);
+    },
+
+    change_image: function(index) {
+        var self = this;
+        if (index < 0 || index > self.entries.length - 1) {
+            return;
+        }
+        self.pos = index;
+        var url = self.entries[self.pos].url;
+        var img = $('img', self.element);
+        img.attr('src', url);
+
+        var copy = self.entries[self.pos].copyrights;
+        var copyright = $('.gallery_details_display p', self.element);
+        copyright.children().remove();
+        copy = $(copy).each(function(i, val){
+            var html = $('<span></span>');
+            if (val[1] != null){
+                html = $('<a></a>');
+                html.attr('href', val[1]);
+            }
+            html.append(val[0]);
+            copyright.append(html);
+        });
+
+        $('.gallery_details_pos', self.element).text(self.pos+1);
+        $('.gallery_details_max', self.element).text(self.entries.length);
+    },
+
+    handle_click: function(event) {
+        var self = this;
+        var target = $(event.target);
+        if (target.hasClass('gallery_details_prev')) {
+            self.change_image(self.pos - 1);
+        } else if (target.hasClass('gallery_details_next')) {
+            self.change_image(self.pos + 1);
+        }
+    }
+
+});
+
+
+$(document).bind('fragment-ready', function(event) {
+    $('.gallery_details', event.__target).each(function(i, element) {
+        element.controller = new zeit.cms.GalleryDetails(element);
+    });
+});
+
+}(jQuery));
