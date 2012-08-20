0dd4b10647c777ef8c093bc9aff53a8b10e1eaf0
Wolfgang Schnerring <ws@gocept.com>
2012-08-20 14:58:01 +0200

    Re #11048: Add "edit" button in the top area of the article view

--- core/src/zeit/content/article/edit/browser/workflow.py
+++ core/src/zeit/content/article/edit/browser/workflow.py
@@ -115,3 +115,11 @@ class Checkin(zeit.cms.browser.view.Base):
             zope.interface.Interface,
             name='checkin')
         return checkin(semantic_change=(semantic_change == 'true'))
+
+
+class Foo(object):
+
+    @cachedproperty
+    def can_checkout(self):
+        manager = zeit.cms.checkout.interfaces.ICheckoutManager(self.context)
+        return manager.canCheckout
