eed8dd382e17844111b09f1224f9392ae6e4cf84
Thomas Lotze <tl@gocept.com>
2012-09-11 14:44:54 +0200

    made room for unit tests

--- core/src/zeit/cms/related/tests/test_doctest.py
+++ core/src/zeit/cms/related/tests/test_doctest.py
@@ -1,14 +1,11 @@
 # Copyright (c) 2007-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import unittest
-
 import zeit.cms.testing
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
+    return zeit.cms.testing.FunctionalDocFileSuite(
         'related.txt',
-    ))
-    return suite
+        package='zeit.cms.related',
+    )
