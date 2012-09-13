d6a287130fb38ff178185941e58380aca121a712
Wolfgang Schnerring <ws@gocept.com>
2012-09-13 08:46:49 +0200

    Remove superfluous TestSuite

--- core/src/zeit/cms/checkout/browser/tests.py
+++ core/src/zeit/cms/checkout/browser/tests.py
@@ -1,14 +1,10 @@
 # Copyright (c) 2007-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-from zope.testing import doctest
-import unittest
 import zeit.cms.testing
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
+    return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
-        'conflict.txt'))
-    return suite
+        'conflict.txt')
