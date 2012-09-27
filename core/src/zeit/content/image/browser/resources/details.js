f763ec8dc31f7a144964782041e2ae97dc8a47b5
Thomas Baumann <thomas.baumann@zeit.de>
2012-09-27 15:18:33 +0200

    Re #10717: Some styles and js for image details

--- core/src/zeit/content/image/browser/resources/details.js
+++ core/src/zeit/content/image/browser/resources/details.js
@@ -2,13 +2,13 @@
 // See also LICENSE.txt
 
 (function($) {
-
 $(document).bind('fragment-ready', function(event) {
     $('.image_details button.toggle_infos', event.__target).bind(
         'click', function(event) {
         var button = $(event.target);
         var pane = $('.picture_information', button.closest('.image_details'));
         pane.slideToggle('slow');
+        button.toggleClass('folded');
         return false;
     });
 });
