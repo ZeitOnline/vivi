69bb51c0e7e784e3ce8dc70f5b8e330e83be7401
Thomas Baumann <thomas.baumann@zeit.de>
2012-10-09 18:59:26 +0200

    Re #10712: Sorry, obviously code wasn't unnecessary. Corrected, test doesn't fail.

--- core/src/zeit/content/article/edit/browser/resources/editor.js
+++ core/src/zeit/content/article/edit/browser/resources/editor.js
@@ -289,11 +289,11 @@ zeit.content.article.Editable = gocept.Class.extend({
                 'a', null, self.toolbar), function(action) {
                 if (action.innerHTML == element.nodeName) {
                     MochiKit.DOM.addElementClass(action, 'active');
-                    if (element.nodeName == 'H3') {
+                    if (element.nodeName == 'H3' && action.innerHTML == 'H3') {
                       MochiKit.DOM.updateNodeAttributes(action, {'href': 'formatBlock/p'});
                       action.innerHTML = 'P';
                     }
-                    if (element.nodeName != 'H3') {
+                    if (element.nodeName != 'H3' && MochiKit.DOM.getNodeAttribute(action, 'href') == 'formatBlock/p') {
                       MochiKit.DOM.updateNodeAttributes(action, {'href': 'formatBlock/h3'});
                       action.innerHTML = 'H3';
                     }
