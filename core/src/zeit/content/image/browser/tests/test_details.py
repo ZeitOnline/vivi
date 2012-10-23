ea76a74950f84b107f27bfb1ca4675a93f73c188
Wolfgang Schnerring <ws@gocept.com>
2012-10-23 10:12:28 +0200

    Re #10717: Fix test setup so that we have a caption so that the toggle-button is displayed

--- core/src/zeit/content/image/browser/tests/test_details.py
+++ core/src/zeit/content/image/browser/tests/test_details.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import zeit.cms.checkout.helper
+import zeit.cms.interfaces
 import zeit.cms.testing
+import zeit.content.image.interfaces
 import zeit.content.image.tests
 
 
@@ -10,6 +13,14 @@ class ImageDetails(zeit.cms.testing.SeleniumTestCase):
     layer = zeit.content.image.tests.selenium_layer
 
     def test_clicking_button_shows_details_pane(self):
+        with zeit.cms.testing.site(self.getRootFolder()):
+            with zeit.cms.testing.interaction():
+                image = zeit.cms.interfaces.ICMSContent(
+                    'http://xml.zeit.de/2006/DSC00109_2.JPG')
+                with zeit.cms.checkout.helper.checked_out(image) as co:
+                    meta = zeit.content.image.interfaces.IImageMetadata(co)
+                    meta.caption = 'foo'
+
         s = self.selenium
         self.open('/repository/2006/DSC00109_2.JPG/@@wrap?view=object-details')
         s.assertNotVisible('css=.picture_information')
