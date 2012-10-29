53eb63266b8e1422aeebf2567543d53cd9a1cf3a
Ron Drongowski <ron.drongowski@zeit.de>
2012-10-29 17:05:12 +0100

    Fixes 11315: Needed postMessage from CMS-Backend to post height. Now works without proxy.

--- core/src/zeit/content/article/edit/browser/resources/preview.js
+++ core/src/zeit/content/article/edit/browser/resources/preview.js
@@ -42,8 +42,41 @@ zeit.content.article.PreviewTabs = gocept.Class.extend({
 
 MochiKit.Signal.connect(window, 'cp-editor-loaded', function() {
     if ($('#preview-tabs').length) {
+        
         new zeit.content.article.PreviewTabs('#preview-tabs');
+        
+        window.addEventListener("message", function(e){
+            if (e.data.tablet){
+                $('#preview-tablet').css('height',(e.data.height_obj.offsetHeight+20)+"px");
+            } 
+            else if (!e.data.tablet){
+                $('#preview-full').css('height',(e.data.height_obj.offsetHeight+20)+"px");
+            }
+        }, false)
+
+
+        /* We need this observer since an iframe with display:none
+         * cannot determine the height of its viewport correctly.
+         *
+         * The observer checks if a tab is activated an therefore the
+         * iframe neds to be reloaded.
+         */
+        
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.attributeName=="class" && 
+                    $(mutation.target).attr("class").indexOf("ui-tabs-hide") == -1 &&
+                    Number($(mutation.target).css('height').replace("px","")) <= 500){
+                    mutation.target.src = mutation.target.src; 
+                } 
+            });   
+        });
+
+        var config = { attributes: true, childList: false, characterData: false }
+        observer.observe(document.getElementById("preview-full"),config);        
+        observer.observe(document.getElementById("preview-tablet"),config);        
     }
+
 });
 
 })(jQuery);
