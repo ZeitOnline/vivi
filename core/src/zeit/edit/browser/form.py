2607c491ffdf1f62537ebe7f620edaa9fdafc05a
Wolfgang Schnerring <ws@gocept.com>
2012-11-09 09:42:45 +0100

    Fixes #11575: Determine those types of widgets that we want to style differently in view-mode

--- core/src/zeit/edit/browser/form.py
+++ core/src/zeit/edit/browser/form.py
@@ -8,6 +8,9 @@ import zeit.cms.checkout.interfaces
 import zeit.edit.browser.view
 import zope.app.pagetemplate
 import zope.formlib.form
+import zope.formlib.itemswidgets
+import zope.formlib.source
+import zope.formlib.widget
 
 
 class Forms(object):
@@ -48,3 +51,17 @@ class InlineForm(zeit.cms.browser.form.WidgetCSSMixin,
     def handle_edit_action(self, action, data):
         self.mark_transaction_undoable()
         return super(InlineForm, self).handle_edit_action.success(data)
+
+    def is_basic_display_widget(self, widget):
+        # XXX kludgy. We want to express "is this a base widget out of
+        # zope.formlib?" (since those are the ones we want to style differently
+        # in readonly-mode).
+        # We can't use IDisplayWidget, since a) some formlib
+        # widgets don't provide it while b) some widgets we don't want to
+        # include (like ObjectSequenceDisplayWidget) do provide it.
+        return type(widget) in [
+            zope.formlib.widget.DisplayWidget,
+            zope.formlib.widget.UnicodeDisplayWidget,
+            zope.formlib.source.SourceDisplayWidget,
+            zope.formlib.itemswidgets.ItemDisplayWidget,
+        ]
