4033a44a888c7ccd7bdc3d650282b2864b0ce053
Wolfgang Schnerring <ws@gocept.com>
2012-11-14 13:44:05 +0100

    Update test to work with current jquery-ui

--- core/src/zeit/content/article/edit/browser/tests/test_preview.py
+++ core/src/zeit/content/article/edit/browser/tests/test_preview.py
@@ -9,12 +9,12 @@ class Preview(zeit.content.article.testing.SeleniumTestCase):
     def test_selected_tab_is_stored_across_reload(self):
         self.open('/repository/online/2007/01/Somalia')
         s = self.selenium
-        selected_tab = 'css=#preview-tabs .ui-tabs-selected'
+        selected_tab = 'css=#preview-tabs .ui-tabs-active'
         s.waitForElementPresent(selected_tab)
-        s.assertText(selected_tab, 'iPad')
+        s.waitForText(selected_tab, 'iPad')
         s.click('css=#preview-tabs a:contains("Desktop")')
         s.waitForText(selected_tab, 'Desktop')
 
         s.refresh()
         s.waitForElementPresent(selected_tab)
-        s.assertText(selected_tab, 'Desktop')
+        s.waitForText(selected_tab, 'Desktop')
