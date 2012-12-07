9a4de5b68eb7e26426ef47759e4dec8e1f7ec2e6
Wolfgang Schnerring <ws@gocept.com>
2012-12-07 10:44:13 +0100

    Fixes #11737: Copy teaser title and -text to article area, too; don't overwrite existing values

--- core/src/zeit/content/article/edit/browser/resources/sync.js
+++ core/src/zeit/content/article/edit/browser/resources/sync.js
@@ -1,11 +1,24 @@
 (function($){
 
+var copy_to = function(target) {
+    return function() {
+        var src = $(this).val();
+        if (! $(target).val()) {
+            $(target).val(src).focusout();
+        }
+    };
+};
+
+
 $(document).bind('fragment-ready', function(event) {
     $('#teaser-supertitle\\.teaserSupertitle', event.__target).bind(
-        'change', function(){
-            var value = $(this).val();
-            $('#article-content-head\\.supertitle').val(value).focusout();
-        });
+        'change', copy_to('#article-content-head\\.supertitle'));
+
+    $('#teaser-title\\.teaserTitle', event.__target).bind(
+        'change', copy_to('#article-content-head\\.title'));
+
+    $('#teaser-text\\.teaserText', event.__target).bind(
+        'change', copy_to('#article-content-head\\.subtitle'));
 });
 
 }(jQuery));
