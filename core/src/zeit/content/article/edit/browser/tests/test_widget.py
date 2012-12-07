c0430d59a37de96bc9c03286323d01fa9b90886f
Wolfgang Schnerring <ws@gocept.com>
2012-12-07 11:47:13 +0100

    Fixes #11708, #11715: Register custom widget for the badges that shows tooltips and works in display mode

---
+++ core/src/zeit/content/article/edit/browser/tests/test_widget.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from zeit.content.article.article import Article
+import zeit.cms.testing
+import zeit.content.article.testing
+
+
+class BadgeWidget(zeit.cms.testing.BrowserTestCase):
+
+    layer = zeit.content.article.testing.ArticleLayer
+
+    def test_shows_labels_in_display_mode(self):
+        with zeit.cms.testing.site(self.getRootFolder()):
+            with zeit.cms.testing.interaction():
+                self.repository['article'] = Article()
+        b = self.browser
+        b.open('http://localhost/++skin++vivi/repository'
+               '/article/@@edit.form.asset-badges?show_form=1')
+        # just looking for any label is not enough, since each widget begins
+        # with one
+        self.assertEllipsis('...<label cms:tooltip="Video"...', b.contents)
+
+    def test_labels_have_tooltip_in_input_mode(self):
+        with zeit.cms.testing.site(self.getRootFolder()):
+            with zeit.cms.testing.interaction():
+                self.repository['article'] = Article()
+        b = self.browser
+        b.open('http://localhost/++skin++vivi/repository'
+               '/article/@@checkout')
+        b.open('@@edit.form.asset-badges?show_form=1')
+        self.assertEllipsis('...<label cms:tooltip="Video"...', b.contents)
---
+++ core/src/zeit/content/article/edit/browser/widget.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from xml.sax.saxutils import escape
+import re
+import zope.formlib.source
+
+
+class BadgeInputWidget(zope.formlib.source.SourceMultiCheckBoxWidget):
+
+    def _renderItem(
+        self, index, text, value, name, cssClass, checked=False):
+        result = super(BadgeInputWidget, self)._renderItem(
+            index, text, value, name, cssClass, checked=False)
+        result = result.replace(
+            '<label', '<label cms:tooltip="%s"' % escape(text))
+        return result
+
+
+class BadgeDisplayWidget(BadgeInputWidget):
+
+    INPUT = re.compile('<input.*?/>')
+
+    def _renderItem(self, *args, **kw):
+        result = super(BadgeDisplayWidget, self)._renderItem(*args, **kw)
+        return self.INPUT.sub('', result)
