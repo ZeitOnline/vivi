0b5df9bc01286cf7f02b18b5e17228e5ab584764
Wolfgang Schnerring <ws@gocept.com>
2012-12-07 11:48:14 +0100

    Re #11715: Make badge source identifyable so we can register a custom widget for it

--- core/src/zeit/cms/asset/interfaces.py
+++ core/src/zeit/cms/asset/interfaces.py
@@ -2,16 +2,25 @@
 # See also LICENSE.txt
 
 from zeit.cms.i18n import MessageFactory as _
+import zc.sourcefactory.source
 import zeit.cms.content.sources
 import zope.interface
 import zope.interface.interfaces
 import zope.schema
 
 
+# XXX kludge to be able to identify the source later on (to register widgets
+# for it), we should create a better mechanism for this purpose.
+class FactoredBadgeSource(
+    zc.sourcefactory.source.FactoredContextualSource):
+    pass
+
+
 class BadgeSource(zeit.cms.content.sources.XMLSource):
 
     config_url = 'source-badges'
     attribute = 'name'
+    source_class = FactoredBadgeSource
 
 
 class IBadges(zope.interface.Interface):
