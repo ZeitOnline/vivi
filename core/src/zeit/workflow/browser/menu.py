d8ba2ff928463040bb2eea64dc04bc712926ab95
Wolfgang Schnerring <ws@gocept.com>
2012-12-12 15:18:11 +0100

    Fixes #11773: Allow hiding the workflow tab for articles

--- core/src/zeit/workflow/browser/menu.py
+++ core/src/zeit/workflow/browser/menu.py
@@ -2,6 +2,7 @@
 # See also LICENSE.txt
 
 from zeit.cms.i18n import MessageFactory as _
+from zeit.cms.repository.interfaces import IRepositoryContent
 import transaction
 import urllib
 import zeit.cms.browser.menu
@@ -30,3 +31,13 @@ class WorkflowMenuItem(zeit.cms.browser.menu.ContextViewsMenu):
     def selected(self):
         """We are selected when no other item is selected."""
         return self.request.getURL().endswith('@@workflow.html')
+
+    def render(self):
+        # XXX kludgy: we don't want to show this menu item for IArticle.
+        # To do that, we want to override the viewlet for IArticle,
+        # but we can't do that if we register this for IRepositoryContent,
+        # since that is orthogonal to the type, while ICMSContent is
+        # a superclass of IArticle.
+        if not IRepositoryContent.providedBy(self.context):
+            return ''
+        return super(WorkflowMenuItem, self).render()
