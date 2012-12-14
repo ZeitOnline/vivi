39ad9dd319cb784682ffecfd79e956f3d2fb7fd6
Wolfgang Schnerring <ws@gocept.com>
2012-12-14 10:12:53 +0100

    Fixes #11733: Make binding event handler idempotent

--- core/src/zeit/content/image/browser/resources/details.js
+++ core/src/zeit/content/image/browser/resources/details.js
@@ -2,9 +2,12 @@
 // See also LICENSE.txt
 
 (function($) {
+
+// idempotent bind, see http://stackoverflow.com/a/12322655/1885340
 $(document).bind('fragment-ready', function(event) {
-    $('.image_details button.toggle_infos', event.__target).bind(
-        'click', function(event) {
+    $('.image_details button.toggle_infos', event.__target).unbind(
+    'click.zeit.content.image.details').bind(
+        'click.zeit.content.image.details', function(event) {
         var button = $(event.target);
         var pane = $('.picture_information', button.closest('.image_details'));
         pane.slideToggle('slow');
