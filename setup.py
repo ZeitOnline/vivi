9c98c4ad3178543f594e84e08cc7561f5d8668f7
Christian Zagrodnick <cz@gocept.com>
2012-12-17 10:50:26 +0000

    Preparing release 0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.objectlog',
-    version='0.11dev',
+    version='0.11',
     author='Christian Zagrodnick',
     author_email='cz@gocept.com',
     description="""\
