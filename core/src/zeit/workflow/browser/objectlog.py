c80f77a4c04c3355f2e7175eed2fcbd889b5c1e1
Wolfgang Schnerring <ws@gocept.com>
2013-02-07 13:24:07 +0100

    Display principal title, not its id

--- core/src/zeit/workflow/browser/objectlog.py
+++ core/src/zeit/workflow/browser/objectlog.py
@@ -3,6 +3,7 @@
 
 import zc.resourcelibrary
 import zeit.objectlog.interfaces
+import zope.authentication.interfaces
 import zope.component
 import zope.interface
 
@@ -24,19 +25,24 @@ class ProcessForDisplay(object):
 class ObjectLog(object):
 
     def groups(self):
+        auth = zope.component.getUtility(
+            zope.authentication.interfaces.IAuthentication)
         entries = zeit.objectlog.interfaces.ILog(self.context).get_log()
         groups = {}
         for entry in entries:
             groups.setdefault(entry.time.date(), []).append(entry)
         for date, entries in reversed(sorted(groups.items())):
-            yield dict(
-                display_date=date.strftime('%d.%m.%Y'),
-                entries=[dict(
-                        display_time=entry.time.strftime('%H:%M'),
-                        entry=entry,
-                        ) for entry in reversed(sorted(
-                            entries, key=lambda x: x.time))],
-                )
+            items = []
+            for entry in reversed(sorted(entries, key=lambda x: x.time)):
+                try:
+                    principal = auth.getPrincipal(entry.principal)
+                except zope.authentication.interfaces.PrincipalLookupError:
+                    principal = None
+                items.append(dict(
+                    display_time=entry.time.strftime('%H:%M'),
+                    entry=entry,
+                    principal=principal.title if principal else ''))
+            yield dict(entries=items, display_date=date.strftime('%d.%m.%Y'))
 
     def __call__(self):
         zc.resourcelibrary.need('zeit.workflow')
