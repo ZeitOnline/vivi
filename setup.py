d75acb70ce6f9377e184eb999df9252248293401
Sebastian Wehrmann <sw@gocept.com>
2013-04-23 10:18:08 +0200

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.author',
-    version='2.0',
+    version='2.0.1.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://code.gocept.com/svn/gocept-int/zeit.cms',
