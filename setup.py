ef3647b42931ced6610f8ec493dfe6e78ec0d0c5
Sebastian Wehrmann <sw@gocept.com>
2013-04-23 11:49:32 +0200

    Preparing release 2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.securitypolicy',
-    version='0.7.2dev',
+    version='2.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://code.gocept.com/hg/public/zeit.securitypolicy',
