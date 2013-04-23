01d2eae2138ab35c7a6c112c346f50992a3d2661
Sebastian Wehrmann <sw@gocept.com>
2013-04-23 11:28:02 +0200

    Preparing release 2.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='0.22.1dev',
+    version='2.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
