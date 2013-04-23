38614621f35104ae5f3061ca627cc4cb3b7f5e8e
Sebastian Wehrmann <sw@gocept.com>
2013-04-23 10:09:55 +0200

    Preparing release 2.6.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.gallery',
-    version='2.4.3.dev0',
+    version='2.6.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
