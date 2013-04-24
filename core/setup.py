f92edaa5b3e20ab454f1ad07114720c80cda029a
Christian Zagrodnick <cz@gocept.com>
2013-04-24 17:25:18 +0200

    Preparing release 2.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.vgwort',
-    version='0.4.5.dev0',
+    version='2.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
