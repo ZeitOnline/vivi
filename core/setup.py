ca75ca5548eaf8fc70419e7e597be306479ff8c1
Christian Zagrodnick <cz@gocept.com>
2013-05-22 14:55:07 +0200

    Back to development: 2.6.3

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'zeit.brightcove',
-    version='2.6.2',
+    version='2.6.3.dev0',
     author = 'Christian Zagrodnick',
     author_email = 'cz@gocept.com',
     description = '',
