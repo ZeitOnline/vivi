b92e4a68d5ba8d399601f6be98eb513cb8d59a7d
Christian Zagrodnick <cz@gocept.com>
2013-05-29 09:46:15 +0200

    Preparing release 2.1.0

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 
-version = '2.0.1.dev0'
+version = '2.1.0'
 
 install_requires = [
     'setuptools',
