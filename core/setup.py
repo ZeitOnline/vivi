5eb03eea6da8fb33d4fd12ea617273c1bfd9b357
Christian Zagrodnick <cz@gocept.com>
2013-05-29 09:46:34 +0200

    Back to development: 2.2.0

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 
-version = '2.1.0'
+version = '2.2.0.dev0'
 
 install_requires = [
     'setuptools',
