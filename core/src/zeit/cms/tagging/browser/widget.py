1c14bc6b2692ccdd18f5b79af5612b7ff74bef28
Wolfgang Schnerring <ws@gocept.com>
2013-06-28 10:05:37 +0200

    Remove superfluous guard
    This was introduced in commit:85bd5a01e4b2 because
    ICommonMetadata.keywords had changed from "required=False, default=()"
    to "required=True, default=None", but since no tests fail this seems overzealous now.

--- core/src/zeit/cms/tagging/browser/widget.py
+++ core/src/zeit/cms/tagging/browser/widget.py
@@ -65,7 +65,7 @@ class Widget(grokcore.component.MultiAdapter,
         return json.dumps([{
             'code': zeit.cms.tagging.interfaces.ID_NAMESPACE + x.code,
             'label': x.label,
-            'pinned': x.pinned} for x in value or ()])
+            'pinned': x.pinned} for x in value])
 
     def _toFieldValue(self, value):
         tags = json.loads(value)
