5ad8180581c11ad9d6b7810fbdcbaea8a9640fd4
Wolfgang Schnerring <ws@gocept.com>
2013-07-01 14:59:15 +0200

    Bump dependency version

--- core/setup.py
+++ core/setup.py
@@ -33,7 +33,7 @@ setup(
         'unittest2',
         'zc.sourcefactory',
         'zeit.content.video',
-        'zeit.cms>1.44.0',
+        'zeit.cms>=2.2.0.dev0',
         'zeit.content.quiz>=0.4.2',
         'zeit.edit',
         'zeit.find >= 0.4',
