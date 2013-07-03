e1d6c733031e81da7fac79396ccd3d5e200b199b
Wolfgang Schnerring <ws@gocept.com>
2013-07-03 09:16:13 +0200

    Preparing release 2.0.1

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='2.0.1.dev0',
+    version='2.0.1',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
