3381f5cb4dd88f00ef42c37fdfc7d1ced33118b5
Wolfgang Schnerring <ws@gocept.com>
2013-08-14 13:12:36 +0200

    Back to development: 2.6.4

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'zeit.brightcove',
-    version='2.6.3',
+    version='2.6.4.dev0',
     author = 'Christian Zagrodnick',
     author_email = 'cz@gocept.com',
     description = '',
