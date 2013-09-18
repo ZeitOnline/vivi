2a4acc9acc3e83c39f9ccc268b4a3f1b5e89e8b2
Wolfgang Schnerring <ws@gocept.com>
2013-09-18 09:29:20 +0200

    whitespace

--- core/src/zeit/content/article/edit/browser/resources/preview.js
+++ core/src/zeit/content/article/edit/browser/resources/preview.js
@@ -25,16 +25,16 @@ zeit.content.article.PreviewTabs = gocept.Class.extend({
             self.save_selection(id);
             self.reload_frame(id);
         });
-        
+
         $('#edit-form-article-content').bind('fold',function(e,folded){
             if (!folded){
                 var id = window.sessionStorage.getItem(self.SESSION_KEY);
                 if ($(id).css('height')=="500px"){
                     $(id).data('reloaded',false);
                     self.reload_frame(id);
-                }    
+                }
             }
-        }); 
+        });
     },
 
     reload_frame: function(id) {
@@ -61,12 +61,12 @@ zeit.content.article.PreviewTabs = gocept.Class.extend({
 
 MochiKit.Signal.connect(window, 'cp-editor-loaded', function() {
     if ($('#preview-tabs').length) {
-        
+
         var preview_tabs = new zeit.content.article.PreviewTabs('#preview-tabs');
-        
+
         window.addEventListener("message", function(e){
             var id =  e.data.tablet ? "#preview-tablet" : "#preview-full";
-            var height = e.data.height_obj.offsetHeight; 
+            var height = e.data.height_obj.offsetHeight;
             if (height >= 500){
                $(id).css('height',(height+20)+"px");
             }
