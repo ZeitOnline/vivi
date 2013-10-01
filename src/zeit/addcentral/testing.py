17938eeb473bf8edec9865d6b5f66f2564dc6047
Wolfgang Schnerring <ws@gocept.com>
2013-10-01 11:39:35 +0200

    Re #12573: Use webdriver

--- src/zeit/addcentral/testing.py
+++ src/zeit/addcentral/testing.py
@@ -1,18 +1,15 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import gocept.httpserverlayer.zopeapptesting
+import gocept.selenium
 import zeit.cms.testing
-import gocept.selenium.ztk
 
 
-layer = zeit.cms.testing.ZCMLLayer('ftesting.zcml', product_config=True)
-selenium_layer  = gocept.selenium.ztk.Layer(layer)
-
-
-def FunctionalDocFileSuite(*args, **kw):
-    kw.setdefault('layer', layer)
-    return zeit.cms.testing.FunctionalDocFileSuite(*args, **kw)
-
-
-class FunctionalTestCase(zeit.cms.testing.FunctionalTestCase):
-    layer = layer
+ZCML_LAYER = zeit.cms.testing.ZCMLLayer('ftesting.zcml', product_config=True)
+HTTP_LAYER = gocept.httpserverlayer.zopeapptesting.Layer(
+    name='HTTPLayer', bases=(ZCML_LAYER,))
+WD_LAYER = gocept.selenium.WebdriverLayer(
+    name='WebdriverLayer', bases=(HTTP_LAYER,))
+SELENIUM_LAYER = gocept.selenium.WebdriverSeleneseLayer(
+    name='SeleniumLayer', bases=(WD_LAYER,))
