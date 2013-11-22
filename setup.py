82f1ecd7d4e7704ea8f8ae317d19b7d5eaddbba1
Wolfgang Schnerring <ws@gocept.com>
2013-11-22 11:21:22 +0100

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.newsletter',
-    version='0.1',
+    version='0.2.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
