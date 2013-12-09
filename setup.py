274439bf4682a98416247588b35c05b2f611161b
Wolfgang Schnerring <ws@gocept.com>
2013-12-09 08:21:14 +0100

    Create skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.magazin',
+    version='0.1.dev0',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='',
+    description="""\
+""",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages=['zeit'],
+    install_requires=[
+        'setuptools',
+        'zeit.cms>=2.12.0.dev',
+        'zope.interface',
+        'zope.component',
+    ],
+)
