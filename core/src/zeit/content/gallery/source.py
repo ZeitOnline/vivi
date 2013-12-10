6eef24866cd247775234efbb5e80807ca7f39582
Wolfgang Schnerring <ws@gocept.com>
2013-12-10 11:52:37 +0100

    Re VIV-247: Use default XMLSource implementation so we get ``available`` behaviour

--- core/src/zeit/content/gallery/source.py
+++ core/src/zeit/content/gallery/source.py
@@ -6,37 +6,22 @@ import zc.sourcefactory.basic
 import zeit.cms.content.sources
 
 
-# XXX This was inspired b^W^Wcopied from zeit.content.cp, so some refactoring
-# to extract a generic type source might be in order.
-class GalleryTypeSource(zeit.cms.content.sources.SimpleContextualXMLSource):
-    # we are contextual so we can set a default value, but have it not
-    # validated at import time, since we don't have our product config then,
-    # yet.
+class GalleryTypeSource(zeit.cms.content.sources.XMLSource):
 
     product_configuration = 'zeit.content.gallery'
     config_url = 'gallery-types-url'
-
-    def getValues(self, context):
-        tree = self._get_tree()
-        return [unicode(item.get('name'))
-                for item in tree.iterchildren()]
-
-    def getTitle(self, context, value):
-        __traceback_info__ = (value, )
-        tree = self._get_tree()
-        return unicode(tree.xpath('/gallery-types/type[@name = "%s"]' %
-                                  value)[0])
+    attribute = 'name'
 
 
 class LayoutSource(zc.sourcefactory.basic.BasicSourceFactory):
 
-    values = {u'image-only': _('Image only'),
-              u'hidden': _('Hidden'),
-             }
+    values = {
+        u'image-only': _('Image only'),
+        u'hidden': _('Hidden'),
+    }
 
     def getValues(self):
         return self.values.keys()
 
     def getTitle(self, value):
         return self.values[value]
-
