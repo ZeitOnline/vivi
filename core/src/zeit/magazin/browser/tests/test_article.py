f358be90587b24b18c22b01f012d9b5588362a8b
Wolfgang Schnerring <ws@gocept.com>
2013-12-12 13:43:12 +0100

    Re VIV-241: Add selenium test for template/header dropdown

---
+++ core/src/zeit/magazin/browser/tests/test_article.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+# Copyright (c) 2013 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zeit.content.article.edit.browser.testing
+import zeit.magazin.testing
+
+
+class ArticleTemplateTest(
+        zeit.content.article.edit.browser.testing.EditorTestCase):
+
+    layer = zeit.magazin.testing.SELENIUM_LAYER
+
+    def setUp(self):
+        super(ArticleTemplateTest, self).setUp()
+        self.add_article('/repository/magazin')
+        self.selenium.waitForElementPresent('id=options-zmo.template')
+
+    def test_changing_template_should_update_header_layout_list(self):
+        s = self.selenium
+        s.click('css=#edit-form-misc .edit-bar .fold-link')
+
+        s.assertSelectedLabel('id=options-zmo.template', '(nothing selected)')
+        s.assertNotVisible('css=.fieldname-header_layout')
+        s.select('id=options-zmo.template', 'Kolumne')
+        s.pause(100)
+
+        kolumne_layouts = [
+            u'(nothing selected)',
+            u'Heiter bis glücklich',
+            u'Ich habe einen Traum',
+            u'Martenstein',
+            u'Standard',
+            u'Von A nach B',
+        ]
+
+        s.assertVisible('css=.fieldname-header_layout')
+        self.assertEqual(
+            kolumne_layouts,
+            s.getSelectOptions('id=options-zmo.header_layout'))
+        s.click('options-zmo.actions.apply')
+        s.pause(250)
+        self.assertEqual(
+            kolumne_layouts,
+            s.getSelectOptions('id=options-zmo.header_layout'))
