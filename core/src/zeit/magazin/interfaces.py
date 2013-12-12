c87763652d17a55fc45edbd6de7161217385f19d
Wolfgang Schnerring <ws@gocept.com>
2013-12-12 13:02:18 +0100

    Fixes VIV-246: Add field longtext for ZMO portrait boxes

--- core/src/zeit/magazin/interfaces.py
+++ core/src/zeit/magazin/interfaces.py
@@ -2,9 +2,11 @@
 # See also LICENSE.txt
 
 from zeit.cms.i18n import MessageFactory as _
+import zc.form.field
 import zeit.cms.interfaces
 import zeit.cms.section.interfaces
 import zeit.content.article.interfaces
+import zeit.content.portraitbox.interfaces
 import zeit.magazin.sources
 import zope.interface
 import zope.schema
@@ -20,8 +22,20 @@ class IZMOContent(
     pass
 
 
+class IZMOFolder(
+        zeit.cms.repository.interfaces.IFolder,
+        zeit.cms.section.interfaces.ISectionMarker):
+    pass
+
+
 class IZMOArticle(
         zeit.content.article.interfaces.IArticle,
+        zeit.cms.section.interfaces.ISectionMarker):
+    pass
+
+
+class IZMOPortraitbox(
+        zeit.content.portraitbox.interfaces.IPortraitbox,
         zeit.cms.section.interfaces.ISectionMarker):
     pass
 
@@ -36,4 +50,11 @@ class IArticleTemplateSettings(zope.interface.Interface):
     header_layout = zope.schema.Choice(
         title=_("Header layout"),
         source=zeit.magazin.sources.ArticleHeaderSource(),
+        required=False)
+
+
+class IPortraitboxLongtext(zope.interface.Interface):
+
+    longtext = zc.form.field.HTMLSnippet(
+        title=_("long text (ZMO)"),
         required=False)
