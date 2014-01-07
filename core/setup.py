d621867eac657eada80f12e4804d80ecc1b293eb
Wolfgang Schnerring <ws@gocept.com>
2014-01-07 14:23:09 +0100

    Preparing release 2.1.6

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 
-version = '2.1.6.dev0'
+version = '2.1.6'
 
 install_requires = [
     'setuptools',
