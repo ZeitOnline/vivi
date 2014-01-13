8ede1bcad0b6ae5eeef9f7309c064e848b354173
Wolfgang Schnerring <ws@gocept.com>
2014-01-13 11:03:24 +0100

    Re VIV-274: Implement reference object for authors that stores location information, provide InlineForm for editing it

--- core/src/zeit/content/author/reference.py
+++ core/src/zeit/content/author/reference.py
@@ -1,15 +1,14 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import grokcore
+import grokcore.component as grok
 import lxml.objectify
 import zeit.cms.content.interfaces
 import zeit.content.author.interfaces
 
 
-@grokcore.component.adapter(
-    zeit.content.author.interfaces.IAuthor, name='author')
-@grokcore.component.implementer(zeit.cms.content.interfaces.IXMLReference)
+@grok.adapter(zeit.content.author.interfaces.IAuthor, name='author')
+@grok.implementer(zeit.cms.content.interfaces.IXMLReference)
 def XMLReference(context):
     node = lxml.objectify.E.author(href=context.uniqueId)
     updater = zeit.cms.content.interfaces.IXMLReferenceUpdater(context)
@@ -23,3 +22,12 @@ class XMLReferenceUpdater(zeit.cms.content.xmlsupport.XMLReferenceUpdater):
 
     def update_with_context(self, node, context):
         node.display_name = context.display_name
+
+
+class Reference(zeit.cms.content.reference.Reference):
+
+    grok.implements(zeit.content.author.interfaces.IAuthorReference)
+    grok.provides(zeit.content.author.interfaces.IAuthorReference)
+    grok.name('author')
+
+    location = zeit.cms.content.property.ObjectPathProperty('.location')
