29d0a03a231fd0a5cb6ad1464450408c8a65d5d7
Wolfgang Schnerring <ws@gocept.com>
2014-02-06 11:29:29 +0100

    Simplify spelling

--- core/src/zeit/cms/cmscontent.py
+++ core/src/zeit/cms/cmscontent.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2009-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import grokcore.component
+import grokcore.component as grok
 import urlparse
 import zeit.cms.interfaces
 import zeit.cms.workingcopy.interfaces
 import zope.component
 
 
-@grokcore.component.adapter(basestring)
-@grokcore.component.implementer(zeit.cms.interfaces.ICMSContent)
+@grok.adapter(basestring)
+@grok.implementer(zeit.cms.interfaces.ICMSContent)
 def unique_id_to_cms_content(unique_id):
     parsed = urlparse.urlparse(unique_id)
     name = '%s://' % (parsed.scheme or '<no-scheme>')
@@ -17,8 +17,8 @@ def unique_id_to_cms_content(unique_id):
         unique_id, zeit.cms.interfaces.ICMSContent, name=name)
 
 
-@grokcore.component.adapter(basestring, name='http://')
-@grokcore.component.implementer(zeit.cms.interfaces.ICMSContent)
+@grok.adapter(basestring, name='http://')
+@grok.implementer(zeit.cms.interfaces.ICMSContent)
 def http_scheme_unique_id_to_cms_content(unique_id):
     parsed = urlparse.urlparse(unique_id)
     assert parsed.scheme == 'http'
@@ -27,8 +27,8 @@ def http_scheme_unique_id_to_cms_content(unique_id):
         unique_id, zeit.cms.interfaces.ICMSContent, name=name)
 
 
-@grokcore.component.adapter(basestring, name='<no-scheme>://')
-@grokcore.component.implementer(zeit.cms.interfaces.ICMSContent)
+@grok.adapter(basestring, name='<no-scheme>://')
+@grok.implementer(zeit.cms.interfaces.ICMSContent)
 def no_scheme_unique_id_to_cms_content(unique_id):
     parsed = urlparse.urlparse(unique_id)
     name = '<no-scheme>://%s/' % (parsed.netloc or '<no-netloc>')
