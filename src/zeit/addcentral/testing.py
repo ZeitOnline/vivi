d4bd129c92908597649d7277aa07d62c58316073
Wolfgang Schnerring <ws@gocept.com>
2014-02-06 08:34:46 +0100

    Re VIV-295: Switch selenium tests to use WSGI application

--- src/zeit/addcentral/testing.py
+++ src/zeit/addcentral/testing.py
@@ -1,14 +1,15 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import gocept.httpserverlayer.zopeapptesting
+import gocept.httpserverlayer.wsgi
 import gocept.selenium
 import zeit.cms.testing
 
 
 ZCML_LAYER = zeit.cms.testing.ZCMLLayer('ftesting.zcml', product_config=True)
-HTTP_LAYER = gocept.httpserverlayer.zopeapptesting.Layer(
-    name='HTTPLayer', bases=(ZCML_LAYER,))
+WSGI_LAYER = zeit.cms.testing.WSGILayer(name='WSGILayer', bases=(ZCML_LAYER,))
+HTTP_LAYER = gocept.httpserverlayer.wsgi.Layer(
+    name='HTTPLayer', bases=(WSGI_LAYER,))
 WD_LAYER = gocept.selenium.WebdriverLayer(
     name='WebdriverLayer', bases=(HTTP_LAYER,))
 SELENIUM_LAYER = gocept.selenium.WebdriverSeleneseLayer(
