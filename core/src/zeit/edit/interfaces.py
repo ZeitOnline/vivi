696a7e990575e60a8c2d18f260b9db4d2588afa9
Wolfgang Schnerring <ws@gocept.com>
2014-02-10 13:13:01 +0100

    Re VIV-305: Back out commit:84654533973b, it causes test failures in z.c.article
    Apparently giving blocks uniqueIds has more side-effects than expected

--- core/src/zeit/edit/interfaces.py
+++ core/src/zeit/edit/interfaces.py
@@ -3,14 +3,10 @@
 
 from zeit.content.cp.i18n import MessageFactory as _
 import zeit.cms.content.interfaces
-import zeit.cms.interfaces
 import zope.container.interfaces
 import zope.interface
 import zope.schema
 import zope.viewlet.interfaces
-
-
-BLOCK_NAMESPACE = 'http://block.vivi.zeit.de/'
 
 
 class IEditable(zope.interface.Interface):
@@ -56,10 +52,6 @@ class IReadContainer(zeit.cms.content.interfaces.IXMLRepresentation,
     This references a <region> or <cluster>
 
     """
-
-    def index(value):
-        """Determine position of the given value among self.values,
-        even when value does not have a __name__ yet."""
 
 
 class IWriteContainer(zope.container.interfaces.IOrdered):
