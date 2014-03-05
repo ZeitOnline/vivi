d9b788cef8750acae668b36060311e8788cdfc9f
Wolfgang Schnerring <ws@gocept.com>
2014-03-05 11:22:47 +0100

    zeit.content.image has its own egg now

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         'pysolr',
         'setuptools',
         'zeit.cms>=2.15.0.dev0',
+        'zeit.content.image',
         'zeit.edit',
         'zeit.find',
         'zope.annotation',
