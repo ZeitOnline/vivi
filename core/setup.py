c755cd4f12240019a727f0b8165cf7e6e4db49d1
Wolfgang Schnerring <ws@gocept.com>
2014-03-05 11:23:56 +0100

    zeit.content.image has its own egg now

--- core/setup.py
+++ core/setup.py
@@ -34,6 +34,7 @@ setup(
         'setuptools',
         'zc.sourcefactory',
         'zeit.cms>=2.15.0.dev0',
+        'zeit.content.image',
         'zeit.content.quiz>=0.4.2',
         'zeit.content.video',
         'zeit.edit >= 2.1.3',
