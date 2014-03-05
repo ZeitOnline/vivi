412e6ef3cc95dcafc050c76632f9d99b54198b63
Wolfgang Schnerring <ws@gocept.com>
2014-03-05 11:16:42 +0100

    Re VIV-305: Provide ImageReference that allows overriding caption, alt and title locally

--- src/zeit/content/image/imagereference.py
+++ src/zeit/content/image/imagereference.py
@@ -39,6 +39,18 @@ class ImagesAdapter(zeit.cms.related.related.RelatedBase):
 class ImageReference(zeit.cms.content.reference.Reference):
 
     grok.name('image')
+    grok.implements(zeit.content.image.interfaces.IImageReference)
+    grok.provides(zeit.cms.content.interfaces.IReference)
+
+    _title = zeit.cms.content.property.ObjectPathAttributeProperty(
+        '.', 'local_title',
+        zeit.content.image.interfaces.IImageReference['title'])
+    _alt = zeit.cms.content.property.ObjectPathAttributeProperty(
+        '.', 'local_alt',
+        zeit.content.image.interfaces.IImageReference['alt'])
+    _caption = zeit.cms.content.property.ObjectPathAttributeProperty(
+        '.', 'local_caption',
+        zeit.content.image.interfaces.IImageReference['caption'])
 
     @property
     def target_unique_id(self):
@@ -46,6 +58,46 @@ class ImageReference(zeit.cms.content.reference.Reference):
         if unique_id is None:
             unique_id = self.xml.get('src')
         return unique_id
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+        # XXX Maybe fall back to original value (from self.target) when
+        # value is None? Or is it enough that we wait until checkin,
+        # when update_metadata will do that anyway?
+        self.xml.set('title', value)
+
+    # XXX We probably want a more general solution for this than copy&paste.
+    @property
+    def alt(self):
+        return self._alt
+
+    @alt.setter
+    def alt(self, value):
+        self._alt = value
+        self.xml.set('alt', value)
+
+    @property
+    def caption(self):
+        return self._caption
+
+    @caption.setter
+    def caption(self, value):
+        self._caption = value
+        self.xml['bu'] = value
+
+    def update_metadata(self):
+        super(ImageReference, self).update_metadata()
+        for name in zope.schema.getFieldNames(
+                zeit.content.image.interfaces.IImageMetadata):
+            if hasattr(self, name):
+                value = getattr(self, name, None)
+                if value is not None:
+                    setattr(self, name, value)
 
 
 @zope.component.adapter(zeit.cms.content.interfaces.ITemplate)
