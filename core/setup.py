037f62cacf207e14465ff380529ae68f1863a5f4
Wolfgang Schnerring <ws@gocept.com>
2014-03-05 16:47:57 +0100

    Create skeleton

---
+++ core/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zeit.content.image',
+    version='2.0.0.dev0',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
+    description="ZEIT Image",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='gocept proprietary',
+    namespace_packages=['zeit', 'zeit.content'],
+    install_requires=[
+        'cssselect',
+        'Pillow',
+        'gocept.form',
+        'setuptools',
+        'zeit.cms>=2.15.0.dev0',
+        'zeit.connector',
+        'zope.app.appsetup',
+        'zope.app.testing',
+        'zope.component',
+        'zope.formlib',
+        'zope.interface',
+        'zope.publisher',
+        'zope.security',
+        'zope.testing',
+    ],
+)
