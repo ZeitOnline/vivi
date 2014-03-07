36063ec975b32716c08634c21e2fc46247c8722c
Wolfgang Schnerring <ws@gocept.com>
2014-03-07 11:15:09 +0100

    Re VIV-285: Remove special-case tests, we have a general one in zeit.edit now

--- core/src/zeit/content/article/edit/browser/tests/test_audio.py
+++ core/src/zeit/content/article/edit/browser/tests/test_audio.py
@@ -26,21 +26,3 @@ class FormLoader(zeit.content.article.edit.browser.testing.EditorTestCase):
         self.create_block('audio')
         s.assertElementPresent('css=.block.type-audio .inline-form '
                                '.field.fieldname-audio_id')
-
-
-class AudioEditTest(zeit.content.article.edit.browser.testing.EditorTestCase):
-
-    def test_audio_id_should_be_editable(self):
-        s = self.selenium
-        self.add_article()
-        self.create_block('audio')
-        # XXX a label-selector would be nice (gocept.selenium #6492)
-        input = 'css=.block.type-audio form.wired input'
-        s.waitForElementPresent(input)
-        s.type(input, 'asdf')
-        s.fireEvent(input, 'blur')
-        s.waitForElementNotPresent('css=.field.dirty')
-        # Re-open the page and verify that the data is still there
-        s.clickAndWait('link=Edit contents')
-        s.waitForElementPresent(input)
-        s.assertValue(input, 'asdf')
