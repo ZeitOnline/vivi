9380b1aad8e4f0e1ad3324c57e47a8101ac6e20c
Wolfgang Schnerring <ws@gocept.com>
2014-03-10 13:02:10 +0100

    Preparing release 0.3.1

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.newsletter',
-    version='0.4.dev0',
+    version='0.3.1',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
