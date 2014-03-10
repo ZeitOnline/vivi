cfacde7a2eec89021f145a68b8ab896d2f26a599
Wolfgang Schnerring <ws@gocept.com>
2014-03-10 12:58:32 +0100

    Back to development: 2.0.1

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.text',
-    version='2.0.0',
+    version='2.0.1.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://bitbucket.org/gocept/zeit.content.text',
