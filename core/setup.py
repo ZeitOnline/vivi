d03d2e818329dd10b3aeb0471964a32a89a7bf3e
Wolfgang Schnerring <ws@gocept.com>
2014-03-24 13:08:39 +0100

    Preparing release 2.3.3

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version='2.3.3.dev0',
+    version='2.3.3',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
