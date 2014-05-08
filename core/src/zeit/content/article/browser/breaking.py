04c5a572703eebd235555bd256083320f6fdf10a
Wolfgang Schnerring <ws@gocept.com>
2014-05-08 14:32:40 +0200

    Re VIV-367: Allow editing the article body (or rather, one paragraph in it)

--- core/src/zeit/content/article/browser/breaking.py
+++ core/src/zeit/content/article/browser/breaking.py
@@ -2,12 +2,53 @@ from zeit.cms.checkout.interfaces import ICheckinManager
 from zeit.cms.i18n import MessageFactory as _
 from zeit.cms.workflow.interfaces import IPublishInfo
 from zeit.content.article.edit.interfaces import IEditableBody
+from zeit.content.article.edit.interfaces import IParagraph
+import grokcore.component as grok
 import zeit.cms.browser.form
 import zeit.cms.settings.interfaces
 import zeit.content.article.article
 import zeit.edit.interfaces
 import zope.component
 import zope.formlib.form
+import zope.i18n
+import zope.interface
+
+
+class IBreakingNewsBody(zope.interface.Interface):
+
+    text = zope.schema.Text(
+        title=_('Article body'),
+        default=_('breaking-news-more-shortly'),
+        required=False)
+
+
+class BreakingNewsBody(grok.Adapter):
+
+    grok.context(zeit.content.article.interfaces.IArticle)
+    grok.implements(IBreakingNewsBody)
+
+    @property
+    def text(self):
+        return self._paragraph and self._paragraph.text
+
+    @text.setter
+    def text(self, value):
+        if not self._paragraph:
+            factory = zope.component.getAdapter(
+                self._body, zeit.edit.interfaces.IElementFactory,
+                name='p')
+            factory()
+        self._paragraph.text = value
+
+    @property
+    def _paragraph(self):
+        for block in self._body.values():
+            if IParagraph.providedBy(block):
+                return block
+
+    @property
+    def _body(self):
+        return IEditableBody(self.context)
 
 
 class Add(zeit.cms.browser.form.AddForm,
@@ -16,13 +57,20 @@ class Add(zeit.cms.browser.form.AddForm,
     factory = zeit.content.article.article.Article
     next_view = 'do-publish'
 
-    form_fields = zope.formlib.form.FormFields(
-        zeit.content.article.interfaces.IArticle).select(
-            'ressort', 'sub_ressort', 'title', '__name__')
+    form_fields = (
+        zope.formlib.form.FormFields(
+            zeit.content.article.interfaces.IArticle).select(
+                'ressort', 'sub_ressort', 'title', '__name__')
+        + zope.formlib.form.FormFields(IBreakingNewsBody))
 
     def setUpWidgets(self, *args, **kw):
         super(Add, self).setUpWidgets(*args, **kw)
         self.set_charlimit('title')
+        if not self.widgets['text'].hasInput():
+            self.widgets['text'].setRenderedValue(
+                zope.i18n.translate(
+                    self.form_fields['text'].field.default,
+                    context=self.request))
 
     @zope.formlib.form.action(
         _('Publish and push'), condition=zope.formlib.form.haveInputWidgets)
@@ -36,10 +84,19 @@ class Add(zeit.cms.browser.form.AddForm,
             self.context)
         article.year = settings.default_year
         article.volume = settings.default_volume
+
+        body = IEditableBody(article)
         image_factory = zope.component.getAdapter(
-            IEditableBody(article), zeit.edit.interfaces.IElementFactory,
+            body, zeit.edit.interfaces.IElementFactory,
             name='image')
         image_factory()
+        if len(body) == 2:
+            # The business rule is that each article should start with an image
+            # block. Now IBreakingNewsBody caused a paragraph to be created
+            # (which already happened in super), but since factories only
+            # append at the end, we need to swap the blocks.
+            body.updateOrder(tuple(reversed(body.keys())))
+
         zope.event.notify(zope.lifecycleevent.ObjectCreatedEvent(article))
 
         return article
--- core/src/zeit/content/article/browser/tests/test_breaking.py
+++ core/src/zeit/content/article/browser/tests/test_breaking.py
@@ -1,7 +1,9 @@
 from zeit.cms.interfaces import ICMSContent
 from zeit.cms.workflow.interfaces import IPublishInfo
+from zeit.content.article.edit.interfaces import IEditableBody
 import zeit.cms.testing
 import zeit.content.article.testing
+import zope.i18n.translationdomain
 
 
 class TestAdding(zeit.cms.testing.BrowserTestCase):
@@ -10,18 +12,30 @@ class TestAdding(zeit.cms.testing.BrowserTestCase):
 
     def setUp(self):
         super(TestAdding, self).setUp()
+        domain = zope.i18n.translationdomain.TranslationDomain('zeit.cms')
+        self.zca.patch_utility(domain, name='zeit.cms')
+        self.catalog = zeit.cms.testing.TestCatalog()
+        domain.addCatalog(self.catalog)
+
         self.browser.open(
             'http://localhost:8080/++skin++vivi/repository/online/2007/01/')
 
-    def test_default_values_should_be_set(self):
+    def create_breakingnews(self):
         b = self.browser
         menu = b.getControl(name='add_menu')
-        menu.displayValue = ['Breaking news']
+        menu.displayValue = ['Add breaking news']
         b.open(menu.value[0])
+
+    def fill_in_required_values(self):
+        b = self.browser
         b.getControl('Ressort').displayValue = ['International']
         b.getControl('Title').value = 'Mytitle'
         b.getControl('File name').value = 'foo'
-        b.getControl('Publish and push').click()
+
+    def test_default_values_should_be_set(self):
+        self.create_breakingnews()
+        self.fill_in_required_values()
+        self.browser.getControl('Publish and push').click()
 
         with zeit.cms.testing.site(self.getRootFolder()):
             article = ICMSContent('http://xml.zeit.de/online/2007/01/foo')
@@ -33,3 +47,33 @@ class TestAdding(zeit.cms.testing.BrowserTestCase):
 
             # XXX Split into separate test?
             self.assertEqual(True, IPublishInfo(article).urgent)
+
+    def test_setting_body_text_creates_paragraph(self):
+        self.create_breakingnews()
+        self.fill_in_required_values()
+        self.browser.getControl('Article body').value = 'mytext'
+        self.browser.getControl('Publish and push').click()
+
+        with zeit.cms.testing.site(self.getRootFolder()):
+            article = ICMSContent('http://xml.zeit.de/online/2007/01/foo')
+            body = IEditableBody(article)
+            para = body.values()[1]  # 0 is image
+            self.assertEqual('mytext', para.text)
+
+    def test_body_text_not_given_creates_no_paragraph(self):
+        self.create_breakingnews()
+        self.fill_in_required_values()
+        self.browser.getControl('Article body').value = ''
+        self.browser.getControl('Publish and push').click()
+
+        with zeit.cms.testing.site(self.getRootFolder()):
+            article = ICMSContent('http://xml.zeit.de/online/2007/01/foo')
+            body = IEditableBody(article)
+            self.assertEqual(1, len(body))
+
+    def test_body_text_default_value_is_translated(self):
+        b = self.browser
+        b.addHeader('Accept-Language', 'tt')
+        self.catalog.messages['breaking-news-more-shortly'] = 'foo'
+        self.create_breakingnews()
+        self.assertEqual('foo', b.getControl('Article body').value)
