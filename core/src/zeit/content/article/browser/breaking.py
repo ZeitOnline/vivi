6c0d6635a1a5afb00c984113fc7107cfedae9617
Wolfgang Schnerring <ws@gocept.com>
2014-05-08 11:24:15 +0200

    Re VIV-367: Implement separate, minimal add form for Eilmeldung

---
+++ core/src/zeit/content/article/browser/breaking.py
@@ -0,0 +1,48 @@
+from zeit.cms.checkout.interfaces import ICheckinManager
+from zeit.cms.i18n import MessageFactory as _
+from zeit.content.article.edit.interfaces import IEditableBody
+import zeit.cms.browser.form
+import zeit.cms.settings.interfaces
+import zeit.content.article.article
+import zeit.edit.interfaces
+import zope.component
+import zope.formlib.form
+
+
+class Add(zeit.cms.browser.form.AddForm,
+          zeit.cms.browser.form.CharlimitMixin):
+
+    factory = zeit.content.article.article.Article
+
+    form_fields = zope.formlib.form.FormFields(
+        zeit.content.article.interfaces.IArticle).select(
+            'ressort', 'sub_ressort', 'title', '__name__')
+
+    def setUpWidgets(self, *args, **kw):
+        super(Add, self).setUpWidgets(*args, **kw)
+        self.set_charlimit('title')
+
+    @zope.formlib.form.action(
+        _('Publish and push'), condition=zope.formlib.form.haveInputWidgets)
+    def handle_add(self, action, data):
+        self.createAndAdd(data)
+
+    def create(self, data):
+        article = super(Add, self).create(data)
+        # XXX Duplicated from .form.AddAndCheckout
+        settings = zeit.cms.settings.interfaces.IGlobalSettings(
+            self.context)
+        article.year = settings.default_year
+        article.volume = settings.default_volume
+        image_factory = zope.component.getAdapter(
+            IEditableBody(article), zeit.edit.interfaces.IElementFactory,
+            name='image')
+        image_factory()
+        zope.event.notify(zope.lifecycleevent.ObjectCreatedEvent(article))
+        return article
+
+    def add(self, object, container=None):
+        super(Add, self).add(object, container)
+        self._created_object = ICheckinManager(
+            self._created_object).checkin()
+        self._checked_out = False
---
+++ core/src/zeit/content/article/browser/tests/test_breaking.py
@@ -0,0 +1,31 @@
+from zeit.cms.interfaces import ICMSContent
+import zeit.cms.testing
+import zeit.content.article.testing
+
+
+class TestAdding(zeit.cms.testing.BrowserTestCase):
+
+    layer = zeit.content.article.testing.TestBrowserLayer
+
+    def setUp(self):
+        super(TestAdding, self).setUp()
+        self.browser.open(
+            'http://localhost:8080/++skin++vivi/repository/online/2007/01/')
+
+    def test_default_values_should_be_set(self):
+        b = self.browser
+        menu = b.getControl(name='add_menu')
+        menu.displayValue = ['Breaking news']
+        b.open(menu.value[0])
+        b.getControl('Ressort').displayValue = ['International']
+        b.getControl('Title').value = 'Mytitle'
+        b.getControl('File name').value = 'foo'
+        b.getControl('Publish and push').click()
+
+        with zeit.cms.testing.site(self.getRootFolder()):
+            article = ICMSContent('http://xml.zeit.de/online/2007/01/foo')
+            # XXX Kind of duplicate from .test_form.TestAdding
+            self.assertEqual(2008, article.year)
+            self.assertEqual(26, article.volume)
+            self.assertEqual('ZEDE', article.product.id)
+            self.assertEqual(True, article.commentsAllowed)
