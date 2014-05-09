e65f8df7d762e0d1bb8a06b79dbb26397d45f466
Wolfgang Schnerring <ws@gocept.com>
2014-05-09 14:31:36 +0200

    Preparing release 1.1.0

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.push',
-    version='1.1.0.dev0',
+    version='1.1.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://bitbucket.org/gocept/zeit.push',
