28983d87d4d6240f4254b1194a3d1a65db81df1b
thomas <thomas@krusty.ws.whq.gocept.com>
2014-05-26 16:09:37 +0200

    re #25: fixed unicode handling for breaking news published to a centerpage

--- src/zeit/push/tests/test_cp.py
+++ src/zeit/push/tests/test_cp.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from zeit.cms.content.interfaces import ISemanticChange
 from zeit.cms.workflow.interfaces import IPublishInfo
 from zeit.content.article.edit.interfaces import IEditableBody
@@ -24,4 +25,17 @@ class StaticArticlePublisherTest(zeit.push.testing.TestCase):
             None, ISemanticChange(article).last_semantic_change)
         self.assertEllipsis(
             '<p...><a href="http://zeit.de/foo">mytext</a></p>',
+            lxml.etree.tostring(IEditableBody(article).values()[0].xml))
+
+    def test_regression_handles_unicode(self):
+        self.repository['foo'] = zeit.content.article.testing.create_article()
+        self.assertEqual(
+            None, ISemanticChange(self.repository['foo']).last_semantic_change)
+        publisher = zeit.push.cp.StaticArticlePublisher(
+            'http://xml.zeit.de/foo')
+        publisher.send(u'mütext', 'http://zeit.de/foo')
+        zeit.workflow.testing.run_publish()
+        article = self.repository['foo']
+        self.assertEllipsis(
+            '...m&#252;text...',
             lxml.etree.tostring(IEditableBody(article).values()[0].xml))
