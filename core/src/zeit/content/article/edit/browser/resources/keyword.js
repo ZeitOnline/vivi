7d302039466867b0e05e44baf939dc146c10f504
Wolfgang Schnerring <ws@gocept.com>
2014-06-03 08:20:03 +0200

    Re VIV-359: The actual visibility toggle is done by zeit.cms:details.js now, we only need to toggle the button caption

--- core/src/zeit/content/article/edit/browser/resources/keyword.js
+++ core/src/zeit/content/article/edit/browser/resources/keyword.js
@@ -1,13 +1,12 @@
 (function($) {
 
+// The actual visibility toggle is done by zeit.cms:details.js
 // idempotent bind, see http://stackoverflow.com/a/12322655/1885340
 $(document).bind('fragment-ready', function(event) {
     $('button.toggle_infos', event.__target).unbind(
     'click.zeit.article.keywords').bind(
         'click.zeit.article.keywords', function(event) {
         var button = $(event.target);
-        $('.folded_information', button.parent()).slideToggle('slow');
-        button.toggleClass('folded');
         var current_caption = button.text();
         button.text(button.data('caption'));
         button.data('caption', current_caption);
