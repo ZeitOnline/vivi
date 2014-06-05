35e1d721d9587fda9aa96289c97a6c0724f7fec8
Wolfgang Schnerring <ws@gocept.com>
2014-06-05 11:42:26 +0200

    Simplify spelling

--- core/src/zeit/content/rawxml/tests.py
+++ core/src/zeit/content/rawxml/tests.py
@@ -1,19 +1,13 @@
 # Copyright (c) 2008-2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os
-import unittest
 import zeit.cms.testing
 
 
-RawXMLLayer = zeit.cms.testing.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
-    __name__, 'RawXMLLayer', allow_teardown=True)
+RawXMLLayer = zeit.cms.testing.ZCMLLayer('ftesting.zcml')
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
+    return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
-        layer=RawXMLLayer))
-    return suite
+        layer=RawXMLLayer)
