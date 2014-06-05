c0c1f29a927fb2ff7540d7482a99bede7f82e9d6
Wolfgang Schnerring <ws@gocept.com>
2014-06-05 16:08:53 +0200

    Back to development: 1.1.2

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.magazin',
-    version='1.1.1',
+    version='1.1.2.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
