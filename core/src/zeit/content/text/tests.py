8e4c7eafbec33ddc098ff28774cafc7987295959
Wolfgang Schnerring <ws@gocept.com>
2014-06-05 11:42:44 +0200

    Simplify spelling

--- core/src/zeit/content/text/tests.py
+++ core/src/zeit/content/text/tests.py
@@ -1,19 +1,13 @@
 # Copyright (c) 2007-2012 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import os
-import unittest
 import zeit.cms.testing
 
 
-TextLayer = zeit.cms.testing.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftesting.zcml'),
-    __name__, 'TextLayer', allow_teardown=True)
+TextLayer = zeit.cms.testing.ZCMLLayer('ftesting.zcml')
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
+    return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
-        layer=TextLayer))
-    return suite
+        layer=TextLayer)
