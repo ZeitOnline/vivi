b421c574a13cec32990614a399568de6604e4a0c
Wolfgang Schnerring <ws@gocept.com>
2014-06-19 13:25:19 +0200

    Re VIV-370: Truncating too long twitter messages happens in UI now (for better user feedback)
    Backs out commit:6addec39a172

--- src/zeit/push/tests/test_message.py
+++ src/zeit/push/tests/test_message.py
@@ -16,28 +16,3 @@ class MessageTest(zeit.push.testing.TestCase):
             zeit.push.interfaces.IPushNotifier, name='parse')
         self.assertEqual(
             [('mytext', u'http://www.zeit.de/testcontent', {})], parse.calls)
-
-    def test_short_text_is_truncated_with_ellipsis(self):
-        content = self.repository['testcontent']
-        push = zeit.push.interfaces.IPushMessages(content)
-        push.short_text = 'a' * 106 + ' This is too long'
-        message = zope.component.getAdapter(
-            content, zeit.push.interfaces.IMessage, name='parse')
-        self.assertEqual(117, len(message.text))
-        self.assertTrue(message.text.endswith('This is...'))
-
-    def test_short_text_is_left_alone_if_below_limit(self):
-        content = self.repository['testcontent']
-        push = zeit.push.interfaces.IPushMessages(content)
-        push.short_text = 'a' * 100 + ' This is not long'
-        message = zope.component.getAdapter(
-            content, zeit.push.interfaces.IMessage, name='parse')
-        self.assertEqual(push.short_text, message.text)
-
-    def test_long_text_is_left_alone(self):
-        content = self.repository['testcontent']
-        push = zeit.push.interfaces.IPushMessages(content)
-        push.long_text = 'a' * 200
-        message = zope.component.getAdapter(
-            content, zeit.push.interfaces.IMessage, name='facebook')
-        self.assertEqual(push.long_text, message.text)
