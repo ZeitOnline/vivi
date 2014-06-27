62da04caeb44c799a348be865c9e8c427b7f1373
Wolfgang Schnerring <ws@gocept.com>
2014-06-27 11:50:21 +0200

    Don't send a push when no text is configured

--- core/src/zeit/push/tests/test_message.py
+++ core/src/zeit/push/tests/test_message.py
@@ -16,3 +16,13 @@ class MessageTest(zeit.push.testing.TestCase):
             zeit.push.interfaces.IPushNotifier, name='parse')
         self.assertEqual(
             [('mytext', u'http://www.zeit.de/testcontent', {})], parse.calls)
+
+    def test_no_text_configured_should_not_send(self):
+        content = self.repository['testcontent']
+        message = zope.component.getAdapter(
+            content, zeit.push.interfaces.IMessage, name='parse')
+        with self.assertRaises(ValueError):
+            message.send()
+        parse = zope.component.getUtility(
+            zeit.push.interfaces.IPushNotifier, name='parse')
+        self.assertEqual([], parse.calls)
