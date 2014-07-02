d3f5a0fe67ff1511953be5eefab768176ad67969
Thomas Lotze <tl@gocept.com>
2014-07-02 08:59:23 +0200

    Preparing release 2.0.20

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='2.0.20.dev0',
+    version='2.0.20',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
