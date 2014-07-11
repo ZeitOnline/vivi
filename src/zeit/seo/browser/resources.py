9ca54d92defed27ffbc2598403855915891de3c0
Wolfgang Schnerring <ws@gocept.com>
2014-07-11 15:35:20 +0200

    Add character counter to form fields

---
+++ src/zeit/seo/browser/resources.py
@@ -0,0 +1,7 @@
+from zeit.cms.browser.resources import Resource, Library
+import zeit.cms.browser.resources
+
+
+lib = Library('zeit.seo', 'resources')
+Resource('counter.js', depends=[zeit.cms.browser.resources.base,
+                                zeit.cms.browser.resources.counter_js])
