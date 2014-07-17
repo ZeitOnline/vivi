b9a3d75efc650756ebc7ec2929c172453962622a
Wolfgang Schnerring <ws@gocept.com>
2014-07-17 11:49:35 +0200

    Re VIV-415: Add form groups for category metadata

--- src/zeit/newsletter/browser/category.py
+++ src/zeit/newsletter/browser/category.py
@@ -2,6 +2,7 @@
 # See also LICENSE.txt
 
 from zeit.cms.i18n import MessageFactory as _
+import gocept.form.grouped
 import zeit.cms.browser.form
 import zeit.cms.browser.view
 import zeit.newsletter.interfaces
@@ -19,6 +20,23 @@ class FormBase(object):
 
     form_fields = zope.formlib.form.FormFields(
         zeit.newsletter.interfaces.INewsletterCategory)
+
+    field_groups = (
+        gocept.form.grouped.Fields(_('Middle ad'), (
+            'ad_middle_title', 'ad_middle_text', 'ad_middle_href',
+            'ad_middle_image', 'ad_middle_groups_above'
+        )),
+        gocept.form.grouped.Fields(_('Bottom ad'), (
+            'ad_bottom_title', 'ad_bottom_text', 'ad_bottom_href',
+            'ad_bottom_image',
+        )),
+        gocept.form.grouped.Fields(_('General'), (
+            'subject', 'ressorts', 'video_playlist',
+        )),
+        gocept.form.grouped.Fields(_('Optivo'), (
+            'mandant', 'recipientlist', 'recipientlist_test',
+        )),
+    )
 
 
 class DisplayForm(FormBase, zeit.cms.browser.form.DisplayForm):
