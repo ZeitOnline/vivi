cbf2a116a9fed7dfb5abecb9ee1585b14ae94196
Wolfgang Schnerring <ws@gocept.com>
2014-07-26 14:48:40 +0200

    pep8

--- src/zeit/content/author/testing.py
+++ src/zeit/content/author/testing.py
@@ -13,8 +13,7 @@ product_config = """
 
 ZCMLLayer = zeit.cms.testing.ZCMLLayer(
     'ftesting.zcml',
-    product_config=
-    zeit.cms.testing.cms_product_config +
+    product_config=zeit.cms.testing.cms_product_config +
     zeit.workflow.testing.product_config +
     product_config)
 
