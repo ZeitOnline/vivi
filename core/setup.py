429e860b5cc5fbc98cc51d623d4b4f2dfd78a878
Wolfgang Schnerring <ws@gocept.com>
2014-08-29 07:53:45 +0200

    Back to development: 2.3.5

--- core/setup.py
+++ core/setup.py
@@ -7,7 +7,7 @@ NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(
     name='zeit.edit',
-    version='2.3.4',
+    version='2.3.5.dev0',
     description="Vivi Editor",
     long_description=README + '\n\n' + NEWS,
     keywords='',
