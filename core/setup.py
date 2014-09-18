0613091a7faf7a07fa952b1058a8f1d1178968c3
Wolfgang Schnerring <ws@gocept.com>
2014-09-18 10:57:22 +0200

    Back to development: 2.6.4

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version='2.6.3',
+    version='2.6.4.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/zeit-cms',
