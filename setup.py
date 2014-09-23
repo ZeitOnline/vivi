73edd8bf7d36974466c6e3a05fcb346ae7c17804
Thomas Lotze <tl@gocept.com>
2014-09-23 09:39:23 +0200

    Preparing release 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.newsletter',
-    version='1.1.1.dev0',
+    version='1.2',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
