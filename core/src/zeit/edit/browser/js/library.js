f68d648e7b0ed5f0d7f64541518425d13d1c171d
Wolfgang Schnerring <ws@gocept.com>
2014-10-09 10:51:36 +0200

    Re VIV-405: Add event when dragging modules, so the content-editable can save itself and restore its landing zones
    Note that the content-drag start handler in zeit.cms.browser:js/dnd.js
    calls Droppables.prepare() synchronously instead of in an
    after-reload handler, which is the proper way we use here.
    It seems that the timing is more favorable in the content case than in
    the module case, so that it works out accidentally -- kludgy! And
    since dnd.js is in zeit.cms, we can't actually depend on
    zeit.edit.editor, so we're *lucky* it works, otherwise we'd have to do
    something clever and/or ugly to resolve the dependency.

--- core/src/zeit/edit/browser/js/library.js
+++ core/src/zeit/edit/browser/js/library.js
@@ -57,11 +57,20 @@ MochiKit.Signal.connect(MochiKit.DragAndDrop.Draggables, 'start',
         if (!MochiKit.DOM.hasElementClass(draggable.element, 'module')) {
             return;
         }
+        // XXX It's unclear which semantics we want here: A generic before-drag
+        // event, or specific before-content-drag, before-module-drag etc.?
+        // Since there is only one subscriber at the moment, we leave it as is.
+        MochiKit.Signal.signal(window, 'before-content-drag');
         draggable.element = draggable.element.cloneNode(true);
         MochiKit.DOM.addElementClass(
             draggable.element, 'module-drag-pane');
         draggable.offset = [-10, -10];
         jQuery('body').append(draggable.element);
+        var ident = MochiKit.Signal.connect(
+            zeit.edit.editor, 'after-reload', function() {
+                MochiKit.Signal.disconnect(ident);
+                MochiKit.DragAndDrop.Droppables.prepare(draggable.element);
+        });
 });
 
 MochiKit.Signal.connect(MochiKit.DragAndDrop.Draggables, 'end',
