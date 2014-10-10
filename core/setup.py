2096db32ac0ec48ebf8e335461c9d2eb870c71c3
Wolfgang Schnerring <ws@gocept.com>
2014-10-10 10:24:37 +0200

    Re VIV-516: Add social media section to edit form

--- core/setup.py
+++ core/setup.py
@@ -20,6 +20,7 @@ setup(
         'zeit.cms>=2.15.0.dev0',
         'zeit.content.image',
         'zeit.connector',
+        'zeit.push>=1.6.0.dev0',
         'zope.cachedescriptors',
         'zope.component',
         'zope.formlib',
--- core/src/zeit/content/link/browser/form.py
+++ core/src/zeit/content/link/browser/form.py
@@ -3,16 +3,19 @@
 """Link forms."""
 
 from zeit.cms.i18n import MessageFactory as _
+import gocept.form.grouped
 import zeit.cms.content.browser.form
 import zeit.content.link.interfaces
 import zeit.content.link.link
+import zeit.push.browser.form
 import zope.formlib.form
 
 
 class Base(object):
 
     form_fields = zope.formlib.form.FormFields(
-        zeit.content.link.interfaces.ILink).omit('xml', 'authors')
+        zeit.content.link.interfaces.ILink).omit(
+            'xml', 'authors')
 
 
 class Add(Base, zeit.cms.content.browser.form.CommonMetadataAddForm):
@@ -23,9 +26,31 @@ class Add(Base, zeit.cms.content.browser.form.CommonMetadataAddForm):
         'automaticMetadataUpdateDisabled')
 
 
-class Edit(Base, zeit.cms.content.browser.form.CommonMetadataEditForm):
+class Edit(Base,
+           zeit.push.browser.form.SocialBase,
+           zeit.cms.content.browser.form.CommonMetadataEditForm):
 
     title = _('Edit link')
+
+    def __init__(self, *args, **kw):
+        social_fields = gocept.form.grouped.Fields(
+            _("Social media"),
+            ('long_text', 'facebook', 'facebook_magazin',
+             'short_text', 'twitter', 'twitter_ressort'),
+            css_class='wide-widgets column-left')
+        if zope.app.appsetup.appsetup.getConfigContext().hasFeature(
+                'zeit.content.article.social-push-mobile'):
+            social_fields.fields += ('mobile',)
+        social_fields.fields += ('enabled',)
+        self.field_groups = self.field_groups[:3] + (
+            social_fields,) + self.field_groups[3:]
+        super(Edit, self).__init__(*args, **kw)
+
+    @zope.formlib.form.action(
+        _('Apply'), condition=zope.formlib.form.haveInputWidgets)
+    def handle_edit_action(self, action, data):
+        self.applyAccountData(data)
+        return super(Edit, self).handle_edit_action.success(data)
 
 
 class Display(Base, zeit.cms.content.browser.form.CommonMetadataDisplayForm):
