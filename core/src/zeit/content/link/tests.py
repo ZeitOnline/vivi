f19959a723568ca648e4eb8f00b682511e1ae158
Wolfgang Schnerring <ws@gocept.com>
2014-10-13 08:29:13 +0200

    Re VIV-516: Wire up zeit.push in tests (belongs to commit:896e5683552d)

--- core/src/zeit/content/link/tests.py
+++ core/src/zeit/content/link/tests.py
@@ -2,9 +2,13 @@
 # See also LICENSE.txt
 
 import zeit.cms.testing
+import zeit.push
 
 
-layer = zeit.cms.testing.ZCMLLayer('ftesting.zcml', product_config=True)
+layer = zeit.cms.testing.ZCMLLayer(
+    'ftesting.zcml',
+    product_config=(zeit.cms.testing.cms_product_config
+                    + zeit.push.product_config))
 
 
 def test_suite():
