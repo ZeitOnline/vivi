9ac2b65d1651d3d950dc0f69b514fd576b91c347
Thomas Lotze <tl@gocept.com>
2014-10-15 11:42:47 +0200

    re #VIV-513: fixed tests setting video creation dates where necessary

--- src/zeit/newsletter/tests/test_category.py
+++ src/zeit/newsletter/tests/test_category.py
@@ -9,9 +9,9 @@ from zeit.newsletter.newsletter import Newsletter
 import datetime
 import mock
 import pytz
-import unittest
 import zeit.cms.repository.folder
 import zeit.newsletter.testing
+import zope.dublincore.interfaces
 import zope.schema
 
 
@@ -231,19 +231,29 @@ class BuilderTest(zeit.newsletter.testing.TestCase):
         self.assertEqual('Video', video_group.title)
         self.assertEqual(0, len(video_group))
 
-    @unittest.skip('need to find out how to control video creation date')
     def test_should_populate_video_group_from_playlist(self):
-        playlist = zeit.content.video.playlist.Playlist()
+        created = datetime.datetime(2014, 9, 22, 8, 0, tzinfo=pytz.UTC)
+
         video1 = zeit.content.video.video.Video()
         video2 = zeit.content.video.video.Video()
         video1.title = 'Video 1'
         self.repository['video1'] = video1
+        video1 = self.repository['video1']
+        with checked_out(video1) as co:
+            zope.dublincore.interfaces.IDCTimes(co).created = created
         video2.title = 'Video 2'
         self.repository['video2'] = video2
+        video2 = self.repository['video2']
+        with checked_out(video2) as co:
+            zope.dublincore.interfaces.IDCTimes(co).created = created
+
+        playlist = zeit.content.video.playlist.Playlist()
         playlist.videos = (video1, video2)
         self.repository['playlist'] = playlist
 
         self.category.video_playlist = playlist.uniqueId
+        self.category.last_created = created - datetime.timedelta(1)
+
         self.builder(())
         body = self.newsletter['newsletter_body']
         video_group = body.values()[self.VIDEO_GROUP_POSITION]
@@ -251,15 +261,7 @@ class BuilderTest(zeit.newsletter.testing.TestCase):
         self.assertEqual('Video 1', video_group.values()[0].reference.title)
         self.assertEqual('Video 2', video_group.values()[1].reference.title)
 
-    @unittest.skip('need to find out how to control video creation date')
     def test_should_populate_video_group_using_last_created_date(self):
-        # XXX This test is meant to express an intention but it was skipped
-        # from the start.
-        from datetime import datetime
-        dt = mock.Mock()
-        dt.side_effect = lambda *args, **kw: datetime(*args, **kw)
-
-        playlist = zeit.content.video.playlist.Playlist()
         video1 = zeit.content.video.video.Video()
         video2 = zeit.content.video.video.Video()
         video3 = zeit.content.video.video.Video()
@@ -267,25 +269,37 @@ class BuilderTest(zeit.newsletter.testing.TestCase):
         video2.title = 'Video 2'
         video3.title = 'Video 3'
         self.repository['video1'] = video1
-        with mock.patch('datetime.datetime', dt):
-            dt.now.return_value = datetime(2014, 9, 22, 8, 0, tzinfo=pytz.UTC)
-            self.repository['video2'] = video2
-            dt.now.return_value = datetime(2014, 9, 23, 8, 0, tzinfo=pytz.UTC)
-            self.repository['video3'] = video3
+        self.repository['video2'] = video2
+        self.repository['video3'] = video3
+        video1 = self.repository['video1']
         video2 = self.repository['video2']
         video3 = self.repository['video3']
+
+        with checked_out(video1) as co:
+            co.title = 'Video 1'
+            # Keep created date value None to make sure this can be handled.
+        with checked_out(video2) as co:
+            co.title = 'Video 2'
+            zope.dublincore.interfaces.IDCTimes(co).created = \
+                datetime.datetime(2014, 9, 22, 8, 0, tzinfo=pytz.UTC)
+        with checked_out(video3) as co:
+            co.title = 'Video 3'
+            zope.dublincore.interfaces.IDCTimes(co).created = \
+                datetime.datetime(2014, 9, 23, 8, 0, tzinfo=pytz.UTC)
+
+        playlist = zeit.content.video.playlist.Playlist()
         playlist.videos = (video1, video2, video3)
         self.repository['playlist'] = playlist
 
         self.category.video_playlist = playlist.uniqueId
-        self.category.last_created = datetime(
+        self.category.last_created = datetime.datetime(
             2014, 9, 22, 18, 0, tzinfo=pytz.UTC)
         self.builder(())
+
         body = self.newsletter['newsletter_body']
         video_group = body.values()[self.VIDEO_GROUP_POSITION]
-        self.assertEqual(2, len(video_group))
-        self.assertEqual('Video 1', video_group.values()[0].reference.title)
-        self.assertEqual('Video 3', video_group.values()[1].reference.title)
+        self.assertEqual(1, len(video_group))
+        self.assertEqual('Video 3', video_group.values()[0].reference.title)
 
     def test_middle_advertisement_should_be_inserted(self):
         with checked_out(self.category) as co:
