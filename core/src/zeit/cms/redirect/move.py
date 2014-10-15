fea08421784b502e39538cf99823c4b6a7b32c5f
Wolfgang Schnerring <ws@gocept.com>
2014-10-15 13:40:38 +0200

    Re WEB-298: Update referencing objects after an object has been moved

--- core/src/zeit/cms/redirect/move.py
+++ core/src/zeit/cms/redirect/move.py
@@ -1,8 +1,14 @@
 import grokcore.component as grok
 import zeit.cms.redirect.interfaces
 import zeit.cms.repository.interfaces
+import zeit.cms.relation.corehandlers
 import zope.component
 import zope.lifecycleevent.interfaces
+
+
+class Dummy(object):
+
+    uniqueId = None
 
 
 @grok.subscribe(
@@ -13,5 +19,11 @@ def store_redirect(context, event):
                 event.oldName, event.newName]):
         return
     lookup = zope.component.getUtility(zeit.cms.redirect.interfaces.ILookup)
-    lookup.rename(event.oldParent.uniqueId + event.oldName,
-                  event.newParent.uniqueId + event.newName)
+    old_id = event.oldParent.uniqueId + event.oldName
+    new_id = event.newParent.uniqueId + event.newName
+    lookup.rename(old_id, new_id)
+    # We need to update objects referencing the old name. But context already
+    # has the new name, so we need to substitute a dummy with the old name.
+    dummy = Dummy()
+    dummy.uniqueId = old_id
+    zeit.cms.relation.corehandlers.update_referencing_objects(dummy)
--- core/src/zeit/cms/redirect/tests/test_move.py
+++ core/src/zeit/cms/redirect/tests/test_move.py
@@ -1,5 +1,9 @@
+from zeit.cms.checkout.helper import checked_out
 from zeit.cms.related.interfaces import IRelatedContent
 from zeit.cms.testcontenttype.testcontenttype import TestContentType
+import gocept.async.tests
+import lxml.etree
+import mock
 import zeit.cms.testing
 import zope.copypastemove.interfaces
 
@@ -9,12 +13,19 @@ class MoveReferencesTest(zeit.cms.testing.ZeitCmsTestCase):
     def test_renaming_referenced_obj_updates_uniqueId_in_referencing_obj(self):
         self.repository['article'] = TestContentType()
         article = self.repository['article']
-        referencing = TestContentType()
-        IRelatedContent(referencing).related = (article,)
-        self.repository['referencing'] = referencing
+        self.repository['referencing'] = TestContentType()
+        with checked_out(self.repository['referencing']) as co:
+            IRelatedContent(co).related = (article,)
 
         zope.copypastemove.interfaces.IObjectMover(article).moveTo(
             self.repository, 'changed')
-        self.assertEqual(
-            ['http://xml.zeit.de/changed'],
-            [x.uniqueId for x in IRelatedContent(referencing).related])
+        gocept.async.tests.process()
+        referencing = self.repository['referencing']
+        with mock.patch('zeit.cms.redirect.interfaces.ILookup') as lookup:
+            self.assertEqual(
+                ['http://xml.zeit.de/changed'],
+                [x.uniqueId for x in IRelatedContent(referencing).related])
+            self.assertFalse(lookup().find.called)
+        self.assertIn(
+            'http://xml.zeit.de/changed',
+            lxml.etree.tostring(referencing.xml, pretty_print=True))
