efc94125a6f90acd60e99e885d388e9671d6d434
Wolfgang Schnerring <ws@gocept.com>
2014-11-03 12:48:15 +0100

    Make room for unit tests

--- core/src/zeit/content/link/testing.py
+++ core/src/zeit/content/link/testing.py
@@ -5,13 +5,7 @@ import zeit.cms.testing
 import zeit.push
 
 
-layer = zeit.cms.testing.ZCMLLayer(
+ZCML_LAYER = zeit.cms.testing.ZCMLLayer(
     'ftesting.zcml',
     product_config=(zeit.cms.testing.cms_product_config
                     + zeit.push.product_config))
-
-
-def test_suite():
-    return zeit.cms.testing.FunctionalDocFileSuite(
-        'README.txt',
-        layer=layer)
--- core/src/zeit/content/link/tests/test_doctest.py
+++ core/src/zeit/content/link/tests/test_doctest.py
@@ -1,11 +1,8 @@
-# Copyright (c) 2007-2011 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 import zeit.cms.testing
-import zeit.content.link.testing
 
 
 def test_suite():
     return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
+        package='zeit.content.link',
         layer=zeit.content.link.testing.ZCML_LAYER)
