c2be9d8ac9275d39807533ceadc8e1faccadd3b2
Wolfgang Schnerring <ws@gocept.com>
2014-11-05 15:22:41 +0100

    Sort add menu items alphabetically

--- core/src/zeit/cms/repository/browser/menu.py
+++ core/src/zeit/cms/repository/browser/menu.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2008-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import zeit.cms.browser.menu
 from zeit.cms.i18n import MessageFactory as _
+import zeit.cms.browser.menu
+import zope.browsermenu.menu
+import zope.i18n
+
+
+class SortingMenu(zope.browsermenu.menu.BrowserMenu):
+
+    def getMenuItems(self, object, request):
+        result = super(SortingMenu, self).getMenuItems(object, request)
+        for item in result:
+            item['title-translated'] = zope.i18n.translate(
+                item['title'], context=request)
+        return sorted(result, key=lambda x: x['title-translated'])
 
 
 class Rename(zeit.cms.browser.menu.LightboxActionMenuItem):
