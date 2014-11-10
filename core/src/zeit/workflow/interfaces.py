bb2babeb0e11cb3db9050a6938e1423776eef61f
Florian Pilz <fp@gocept.com>
2014-11-10 15:32:54 +0100

    Typo.

--- core/src/zeit/workflow/interfaces.py
+++ core/src/zeit/workflow/interfaces.py
@@ -108,7 +108,7 @@ class IPublicationDependencies(zope.interface.Interface):
     """Adapter to find the publication dependencies of an object."""
 
     def get_dependencies():
-        """Return a sequence of all depdnent objects.
+        """Return a sequence of all dependent objects.
 
     The sequence contains all objects which need to be published along with the
     adapted object. Dependent containers will be published recursively.
