1a3d4c9584531df0057f051a65972f2ad1e23e96
Wolfgang Schnerring <ws@gocept.com>
2014-12-16 13:03:48 +0100

    Remove unused view

--- core/src/zeit/content/cp/browser/region.py
+++ core/src/zeit/content/cp/browser/region.py
@@ -3,12 +3,6 @@ import zeit.edit.interfaces
 import zope.formlib.form
 
 
-class EditProperties(zeit.edit.browser.view.EditBox):
-
-    interface = zeit.content.cp.interfaces.IRegion
-    form_fields = zope.formlib.form.Fields()  # XXX implement me
-
-
 class EditCommon(zeit.edit.browser.view.EditBox):
 
     form_fields = zope.formlib.form.Fields(
