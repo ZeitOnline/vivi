6d695c938eef1ee12cfd41b97d0fc30e566d3011
Wolfgang Schnerring <ws@gocept.com>
2014-12-17 10:41:02 +0100

    Re FRIED-37: Update tests since mock connector now yields trailing slashes for folder ids

--- core/setup.py
+++ core/setup.py
@@ -34,7 +34,7 @@ setup(
         'setuptools',
         'xml-compare',
         'zc.sourcefactory',
-        'zeit.cms>=2.31.0.dev0',
+        'zeit.cms>=2.32.0.dev0',
         'zeit.content.image',
         'zeit.content.quiz>=0.4.2',
         'zeit.content.video',
