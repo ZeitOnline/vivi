80146a8fb5e7367a7da663e1b21b166cec24a76b
Wolfgang Schnerring <ws@gocept.com>
2014-12-17 08:18:02 +0100

    Back to development: 3.6.7

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.article',
-    version='3.6.6',
+    version='3.6.7.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
