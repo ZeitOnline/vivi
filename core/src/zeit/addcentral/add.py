66b0c72706b0e2828e5fa961089e8d772f2943db
Wolfgang Schnerring <ws@gocept.com>
2015-01-08 16:05:37 +0100

    Update copyright

--- core/src/zeit/addcentral/add.py
+++ core/src/zeit/addcentral/add.py
@@ -1,5 +1,2 @@
-# Copyright (c) 2009 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 # BBB
 from zeit.cms.content.add import ContentAdder, find_or_create_folder
--- core/src/zeit/addcentral/interfaces.py
+++ core/src/zeit/addcentral/interfaces.py
@@ -1,5 +1,2 @@
-# Copyright (c) 2009 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 # BBB
 from zeit.cms.content.interfaces import IContentAdder, IAddLocation
