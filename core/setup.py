83305c126ca353ec52d99cb7afa4584d5e879a03
Wolfgang Schnerring <ws@gocept.com>
2015-01-08 17:05:24 +0100

    Update copyright

--- core/setup.py
+++ core/setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='zeit.locales',
     version='2.0.29.dev0',
-    author='gocept',
-    author_email='mail@gocept.com',
-    url='https://svn.gocept.com/repos/gocept-int/zeit.cms',
-    description="""\
-""",
+    author='gocept, Zeit Online',
+    author_email='zon-backend@zeit.de',
+    url='http://www.zeit.de/',
+    description="vivi i18n",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    license='gocept proprietary',
+    license='BSD',
     namespace_packages=['zeit'],
     install_requires=[
         'setuptools',
