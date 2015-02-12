26c1ff933e9285654213d3a53d563048c1c14e8f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-02-12 08:34:27 +0100

    Add missing import
    (cherry picked from commit 88ca270a40d0e2e1d61332913380f1570cc30d25)

--- core/src/zeit/content/cp/browser/blocks/block.py
+++ core/src/zeit/content/cp/browser/blocks/block.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2009-2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import zeit.cms.browser.widget
 import zeit.content.cp.interfaces
 import zeit.edit.browser.view
 import zope.component
