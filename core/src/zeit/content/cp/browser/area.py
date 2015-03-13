bbba43be640f476860ebc4e476c999bf2fd9f6ac
Florian Pilz <fp@gocept.com>
2015-03-13 11:42:48 +0100

    re DEV-681: Remove form to toggle whether teaser layouts should be applied automatically (partially removes 2c0757b and 6dd1b1d).

--- core/src/zeit/content/cp/browser/area.py
+++ core/src/zeit/content/cp/browser/area.py
@@ -36,14 +36,6 @@ class EditOverflow(zeit.edit.browser.view.EditBox):
             'block_max', 'overflow_into')
 
 
-class EditTeaserLayout(zeit.edit.browser.view.EditBox):
-
-    form_fields = zope.formlib.form.Fields(
-        zeit.content.cp.interfaces.IArea).select(
-            'apply_teaser_layouts_automatically',
-            'first_teaser_layout', 'remaining_teaser_layout')
-
-
 class EditAutomatic(zeit.content.cp.browser.blocks.teaser.EditCommon):
 
     form_fields = zope.formlib.form.FormFields(
