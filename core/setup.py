cf5539c9e7682f9f09f8111a4bc66fdc4435cc03
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-04-17 13:39:52 +0200

    Back to development: 2.6.13

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.brightcove',
-    version='2.6.12',
+    version='2.6.13.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
