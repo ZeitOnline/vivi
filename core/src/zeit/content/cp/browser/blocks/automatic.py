86f390eda7088bcb7df4bc3c46d433c46a3ee2ca
Florian Pilz <fp@gocept.com>
2015-04-22 11:23:51 +0200

    re DEV-745: Make it possible to adjust layout and common properties of automatic teasers and reload area instead of the single teaser.

--- core/src/zeit/content/cp/browser/blocks/automatic.py
+++ core/src/zeit/content/cp/browser/blocks/automatic.py
@@ -1,4 +1,5 @@
 import zeit.content.cp.browser.blocks.teaser
+import zeit.content.cp.browser.editor
 
 
 class Empty(object):
@@ -10,3 +11,17 @@ class Empty(object):
 class Display(zeit.content.cp.browser.blocks.teaser.Display):
 
     base_css_classes = ['teaser-contents']
+
+
+class ChangeLayout(zeit.content.cp.browser.blocks.teaser.ChangeLayout):
+    """Always reload surrounding area when changing layout."""
+
+    def reload(self):
+        super(ChangeLayout, self).reload(self.context.__parent__)
+
+
+class ToggleVisible(zeit.content.cp.browser.editor.ToggleVisible):
+    """Always reload surrounding area when toggling visibility."""
+
+    def reload(self):
+        super(ToggleVisible, self).reload(self.context.__parent__)
---
+++ core/src/zeit/content/cp/browser/blocks/tests/test_automatic.py
@@ -0,0 +1,72 @@
+from zeit.cms.testcontenttype.testcontenttype import TestContentType
+import transaction
+import zeit.cms.checkout.interfaces
+import zeit.content.cp.centerpage
+import zeit.content.cp.interfaces
+import zeit.content.cp.testing
+
+
+class TestAutomaticTeaserBlock(zeit.content.cp.testing.SeleniumTestCase):
+
+    def setUp(self):
+        super(TestAutomaticTeaserBlock, self).setUp()
+        self.auto_teaser_title = 'Teaser Title Foo'
+        teaser = TestContentType()
+        teaser.teaserTitle = self.auto_teaser_title
+        self.repository['t1'] = teaser
+
+        cp_with_teaser = self.create_and_checkout_centerpage('cp_with_teaser')
+        cp_with_teaser['lead'].create_item('teaser').insert(
+            0, self.repository['t1'])
+        zeit.cms.checkout.interfaces.ICheckinManager(cp_with_teaser).checkin()
+
+        self.cp = self.create_and_checkout_centerpage('cp')
+        self.area = zeit.content.cp.interfaces.IAutomaticArea(
+            self.cp['feature'].create_item('area'))
+        self.area.referenced_cp = self.repository['cp_with_teaser']
+        self.area.count = 1
+        self.area.automatic = True
+        self.area.automatic_type = 'centerpage'
+
+        transaction.commit()
+        self.open_centerpage(create_cp=False)
+
+    def test_fill_automatic_areas_with_teaser(self):
+        self.selenium.waitForTextPresent(self.auto_teaser_title)
+
+    def test_toggle_visible_reloads_teaser(self):
+        sel = self.selenium
+        sel.assertCssCount('css=.block-visible-off', 0)
+        sel.assertTextPresent(self.auto_teaser_title)
+
+        sel.click('css=.block.type-auto-teaser .toggle-visible-link')
+        sel.waitForCssCount('css=.block-visible-off', 1)
+        sel.assertTextPresent(self.auto_teaser_title)
+
+        sel.click('css=.block.type-auto-teaser .toggle-visible-link')
+        sel.waitForCssCount('css=.block-visible-off', 0)
+        sel.assertTextPresent(self.auto_teaser_title)
+
+    def test_change_layout_reloads_teaser(self):
+        sel = self.selenium
+        sel.assertTextPresent(self.auto_teaser_title)
+        sel.click('css=.block.type-auto-teaser .edit-link')
+        sel.waitForElementPresent('css=.lightbox')
+        sel.click('css=.two-side-by-side')
+        sel.waitForElementPresent('css=.teaser-contents.two-side-by-side')
+        sel.assertTextPresent(self.auto_teaser_title)
+
+    def test_change_common_property_reloads_teaser(self):
+        sel = self.selenium
+        sel.assertTextPresent(self.auto_teaser_title)
+        sel.assertCssCount('css=.block-visible-off', 0)
+
+        sel.click('css=.block.type-auto-teaser .edit-link')
+        sel.waitForElementPresent('css=.lightbox')
+        sel.click('//a[@href="tab-1"]')
+        sel.waitForElementPresent('id=form.visible')
+        sel.check('id=form.visible')
+        sel.click(r'css=#tab-1 #form\.actions\.apply')
+
+        sel.waitForCssCount('css=.block-visible-off', 1)
+        sel.assertTextPresent(self.auto_teaser_title)
