c75103f438ec5e2869510f83e3a843b2d9a6b20d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-04-22 07:59:57 +0200

    Re DEV-704: Global ``enabled`` setting has been removed

--- core/src/zeit/content/article/edit/browser/tests/test_social.py
+++ core/src/zeit/content/article/edit/browser/tests/test_social.py
@@ -24,23 +24,6 @@ class SocialFormTest(zeit.cms.testing.BrowserTestCase):
             'http://localhost/++skin++vivi/workingcopy/zope.user/'
             'Somalia/@@edit.form.social?show_form=1')
 
-    def test_checkbox_is_only_editable_while_checked_out(self):
-        self.open_form()
-        b = self.browser
-        # Assert nothing raised
-        b.getControl('Push after next publish?').selected = True
-
-        b.open('@@checkin')
-        b.open('@@edit.form.social?show_form=1')
-        with self.assertRaises(AttributeError) as err:
-            b.getControl('Push after next publish?').selected = True
-        self.assertEqual('item is disabled', err.exception.message)
-
-        b.open('@@checkout')
-        b.open('@@edit.form.social?show_form=1')
-        # Assert nothing raised
-        b.getControl('Push after next publish?').selected = True
-
     def test_smoke_form_submit_stores_values(self):
         self.open_form()
         b = self.browser
