9578ea1035bb151930d195ce9e8b6fefcb6a6f4d
Florian Pilz <fp@gocept.com>
2015-04-28 10:23:12 +0200

    re DEV-746: Bump version of zeit.edit for removal of deduplication.

--- core/setup.py
+++ core/setup.py
@@ -34,7 +34,7 @@ setup(
         'zeit.content.image>=2.2.7.dev0',
         'zeit.content.quiz>=0.4.2',
         'zeit.content.video',
-        'zeit.edit >= 2.11.0.dev0',
+        'zeit.edit >= 2.11.1.dev0',
         'zeit.find >= 2.4.0.dev0',
         'zope.app.appsetup',
         'zope.app.generations',
