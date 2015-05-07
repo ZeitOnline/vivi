64e7e5fd792cc138bc8a12124f3b3e95dc862efd
Florian Pilz <fp@gocept.com>
2015-05-07 08:00:29 +0200

    re DEV-765: Initial commit.

---
+++ core/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zeit.content.dynamicfolder',
+    version='0.1.0.dev0',
+    author='gocept, Zeit Online',
+    author_email='zon-backend@zeit.de',
+    url='http://www.zeit.de/',
+    description="vivi Content-Type DynamicFolder",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='BSD',
+    namespace_packages=['zeit', 'zeit.content'],
+    install_requires=[
+    ],
+    entry_points={
+    },
+)
