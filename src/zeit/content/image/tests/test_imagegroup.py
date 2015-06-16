fc762e8dd368e1578ee12d7ec7ab7f52117761f6
Florian Pilz <fp@gocept.com>
2015-06-16 10:30:48 +0200

    re DEV-796: Scale down images to size given in URL.
    - rename method `crop` to `create_variant_image` since it does cropping and
      resizing
    
    - add size argument to method signature to resize the image

--- src/zeit/content/image/tests/test_imagegroup.py
+++ src/zeit/content/image/tests/test_imagegroup.py
@@ -38,8 +38,8 @@ class ImageGroupTest(zeit.cms.testing.FunctionalTestCase):
         self.assertEqual('/group/square', self.group.variant_url('square'))
 
     def test_returns_image_for_variant_with_size(self):
-        self.assertEqual(  # XXX TODO scale down to given size
-            (1536, 1536), self.group['square__200x200'].getImageSize())
+        self.assertEqual(
+            (200, 200), self.group['square__200x200'].getImageSize())
 
     def test_dav_content_with_same_name_is_preferred(self):
         self.assertEqual((1536, 1536), self.group['square'].getImageSize())
