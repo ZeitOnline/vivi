829dcc5e4045f47658aa0e48a3b3a95a4158f056
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-06-18 08:08:12 +0200

    Re DEV-797: Create thumbnail source image only while group is in repository
    Apache's mod_dav doesn't seem to allow adding a member to a locked
    collection, unfortunately, even though the WebDAV spec reads like that
    should be possible. Specifically: calling LOCK on a member inside a
    collection checks the parent collection with the flag "acquire
    exclusive lock", which is of course denied since the collection
    already is locked; however my understanding is that the client wants
    the lock only for the member, not the parent, so the check about the
    state of the parent should reflect that.

--- core/src/zeit/content/image/tests/test_imagegroup.py
+++ core/src/zeit/content/image/tests/test_imagegroup.py
@@ -1,8 +1,6 @@
 from zeit.content.image.testing import create_image_group_with_master_image
 import zeit.cms.testing
 import zeit.content.image.testing
-import zope.event
-import zope.lifecycleevent
 import zope.traversing.api
 
 
@@ -64,15 +62,6 @@ class ImageGroupTest(zeit.cms.testing.FunctionalTestCase):
         self.assertEqual((750, 750), thumbnails['square'].getImageSize())
 
     def test_thumbnail_source_is_created_on_add(self):
-        self.assertNotIn('thumbnail-source-master-image.jpg', self.group)
-        zope.event.notify(zope.lifecycleevent.ObjectAddedEvent(self.group))
-        self.assertIn('thumbnail-source-master-image.jpg', self.group)
-
-    def test_thumbnail_source_is_created_on_modified(self):
-        self.assertNotIn('thumbnail-source-master-image.jpg', self.group)
-        zope.event.notify(zope.lifecycleevent.ObjectModifiedEvent(
-            self.group, zope.lifecycleevent.Attributes(
-                zeit.content.image.interfaces.IImageGroup, 'master_image')))
         self.assertIn('thumbnail-source-master-image.jpg', self.group)
 
 
