03cf34652ccdc8a9bb407f7ea18223877a147fed
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-08-04 08:58:59 +0200

    Preparing release 2.12.2

--- core/setup.py
+++ core/setup.py
@@ -8,7 +8,7 @@ NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(
     name='zeit.edit',
-    version='2.12.2.dev0',
+    version='2.12.2',
     description="Vivi Editor",
     long_description=README + '\n\n' + NEWS,
     keywords='',
