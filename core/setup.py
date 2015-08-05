6bd10cf2cc3c7004b8c9c1eb26f35b691cafa602
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-08-05 16:59:43 +0200

    Preparing release 2.53.2

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='2.53.2.dev0',
+    version='2.53.2',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
