c523f39cdc7c8eb49c77e00e6bdc3b07b028c337
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-08-25 09:37:02 +0200

    Use simplified folder view that does not instantiate all child objects.

--- core/src/zeit/content/dynamicfolder/browser/folder.py
+++ core/src/zeit/content/dynamicfolder/browser/folder.py
@@ -1,5 +1,6 @@
 from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.browser.form
+import zeit.cms.browser.listing
 import zeit.content.dynamicfolder.interfaces
 import zope.formlib.form
 
@@ -18,3 +19,34 @@ class EditForm(FormBase, zeit.cms.browser.form.EditForm):
 
     title = _('Edit metadata')
     form_fields = FormBase.form_fields.omit('__name__')
+
+
+class View(zeit.cms.browser.listing.Listing,
+           zeit.cms.browser.view.Base):
+
+    columns = (
+        zeit.cms.browser.listing.GetterColumn(
+            title=_("File name"),
+            # zc.table can't deal with spaces in colum names
+            name='filename',
+            getter=lambda i, f: i.__name__),
+        zeit.cms.browser.listing.MetadataColumn(
+            u'Metadaten', name='metadata', searchable_text=False),
+    )
+
+    @property
+    def content(self):
+        result = []
+        for name in sorted(self.context.keys()):
+            result.append(ContentPlaceholder(
+                name, self.context, self.url(self.context, name)))
+        return result
+
+
+class ContentPlaceholder(object):
+
+    def __init__(self, name, parent, url):
+        self.__name__ = name
+        self.__parent__ = parent
+        self.uniqueId = parent.uniqueId + name
+        self.url = url
