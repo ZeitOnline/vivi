a3d3b144249f23add731c9acf001d53173080318
Florian Pilz <fp@gocept.com>
2015-10-14 09:18:31 +0200

    re DEV-951: Change last_semantic_change and date_last_published_semantic on apply.

--- core/src/zeit/cms/admin/date_back.py
+++ core/src/zeit/cms/admin/date_back.py
@@ -1,4 +1,5 @@
 import zeit.cms.admin.interfaces
+import zeit.cms.content.interfaces
 import zeit.cms.workflow.interfaces
 import zope.component
 import zope.interface
@@ -18,10 +19,17 @@ class DateBackSemanticPublish(object):
             self.context).date_last_published_semantic
 
     @date_back_last_publish_semantic.setter
-    def date_back_last_publish_semantic(self, value):
-        # TODO actually change all fields that require changing
-        # CAUTION: Cannot just write on IPublishInfo, since fields are readonly
-        pass
+    def date_back_last_publish_semantic(self, datetime):
+        # Remove security, since last_semantic_change and
+        # date_last_published_semantic are defined as readonly.
+        context = zope.security.proxy.removeSecurityProxy(self.context)
+
+        semantic = zeit.cms.content.interfaces.ISemanticChange(context)
+        semantic.has_semantic_change = False
+        semantic.last_semantic_change = datetime
+
+        publish_info = zeit.cms.workflow.interfaces.IPublishInfo(context)
+        publish_info.date_last_published_semantic = datetime
 
 
 @zope.component.adapter(zope.interface.Interface)
