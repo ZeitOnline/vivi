8300cb91debf6de6a3bee206d7a07067d3d2d89e
Florian Pilz <fp@gocept.com>
2015-10-23 15:30:13 +0200

    re DEV-951: Test behaviour of adapter and admin menu.

---
+++ core/src/zeit/cms/admin/browser/tests/test_admin.py
@@ -0,0 +1,15 @@
+import zeit.cms.testing
+
+
+class TestAdminMenu(zeit.cms.testing.ZeitCmsBrowserTestCase):
+
+    def test_admin_menu_is_displayed_for_repository_objects(self):
+        self.browser.open(
+            'http://localhost:8080/++skin++vivi/repository/testcontent')
+        self.assertIn('Admin Menu', self.browser.contents)
+
+    def test_admin_menu_is_hidden_for_checked_out_objects(self):
+        self.browser.open(
+            'http://localhost:8080/++skin++vivi/repository/testcontent')
+        self.browser.getLink('Checkout').click()
+        self.assertNotIn('Admin Menu', self.browser.contents)
---
+++ core/src/zeit/cms/admin/tests/test_semantic.py
@@ -0,0 +1,46 @@
+from zeit.cms.admin.interfaces import IAdjustSemanticPublish
+from zeit.cms.testcontenttype.testcontenttype import TestContentType
+import datetime
+import pytz
+import zeit.cms.content.interfaces
+import zeit.cms.testing
+import zeit.cms.workflow.interfaces
+
+
+class TestSemantic(zeit.cms.testing.ZeitCmsTestCase):
+
+    def setUp(self):
+        super(TestSemantic, self).setUp()
+        self.content = TestContentType()
+
+    def test_adjust_semantic_publish_displays_date_last_published_semantic(
+            self):
+        lsc = datetime.datetime(2013, 10, 30, tzinfo=pytz.UTC)
+        zeit.cms.workflow.interfaces.IPublishInfo(
+            self.content).date_last_published_semantic = lsc
+        self.assertEqual(
+            lsc, IAdjustSemanticPublish(self.content).adjust_semantic_publish)
+
+    def test_setting_adjust_semantic_publish_overwrites_lsc_and_dlps(self):
+        lsc = datetime.datetime(2013, 10, 30, tzinfo=pytz.UTC)
+        semantic = IAdjustSemanticPublish(self.content)
+        semantic.adjust_semantic_publish = lsc
+
+        self.assertEqual(
+            lsc, zeit.cms.workflow.interfaces.IPublishInfo(
+                self.content).date_last_published_semantic)
+        self.assertEqual(
+            lsc, zeit.cms.content.interfaces.ISemanticChange(
+                self.content).last_semantic_change)
+
+    def test_setting_adjust_semantic_publish_overwrites_has_semantic(self):
+        zeit.cms.content.interfaces.ISemanticChange(
+            self.content).has_semantic_change = True
+
+        semantic = IAdjustSemanticPublish(self.content)
+        semantic.adjust_semantic_publish = datetime.datetime(
+            2013, 10, 30, tzinfo=pytz.UTC)
+
+        self.assertEqual(
+            False, zeit.cms.content.interfaces.ISemanticChange(
+                self.content).has_semantic_change)
