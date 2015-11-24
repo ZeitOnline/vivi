6977668163b8de8a4ea93d478bb5da307b74d28e
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-11-24 10:41:32 +0100

    Back to development: 1.1.3

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.dynamicfolder',
-    version='1.1.2',
+    version='1.1.3.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
