97184e225f81ef65aa0412bb827895a5a24e3ff8
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-01-22 09:05:37 +0100

    Back to development: 2.13.4

--- core/setup.py
+++ core/setup.py
@@ -8,7 +8,7 @@ NEWS = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(
     name='zeit.edit',
-    version='2.13.3',
+    version='2.13.4.dev0',
     description="Vivi Editor",
     long_description=README + '\n\n' + NEWS,
     keywords='',
