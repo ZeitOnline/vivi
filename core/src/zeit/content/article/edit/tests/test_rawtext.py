1aba618fb8f8bf688b1d2b8e20bc67656375890d
Ron Drongowski <ron.drongowski@gmail.com>
2016-03-01 18:15:45 +0100

    Add testing

---
+++ core/src/zeit/content/article/edit/tests/test_rawtext.py
@@ -0,0 +1,34 @@
+import zeit.content.article.testing
+
+
+class RawTextTest(zeit.content.article.testing.FunctionalTestCase):
+
+    def get_rawtext(self):
+        from zeit.content.article.edit.rawtext import RawText
+        import lxml.objectify
+        rawtext = RawText(None, lxml.objectify.E.rawtext())
+        return rawtext
+
+    def test_rawtext_should_be_set(self):
+        rawtext = self.get_rawtext()
+        rawtext.text = u'my_text'
+        self.assertEqual(u'my_text', rawtext.xml.xpath('text')[0])
+
+
+class TestFactory(zeit.content.article.testing.FunctionalTestCase):
+
+    def test_factory_should_create_rawtext_node(self):
+        import zeit.content.article.article
+        import zeit.content.article.edit.interfaces
+        import zeit.edit.interfaces
+        import zope.component
+        article = zeit.content.article.article.Article()
+        body = zeit.content.article.edit.body.EditableBody(
+            article, article.xml.body)
+        factory = zope.component.getAdapter(
+            body, zeit.edit.interfaces.IElementFactory, 'rawtext')
+        self.assertEqual('Raw text', factory.title)
+        div = factory()
+        self.assertTrue(
+            zeit.content.article.edit.interfaces.IRawText.providedBy(div))
+        self.assertEqual('rawtext', div.xml.tag)
