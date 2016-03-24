5fe8260dffc27ff99214906e7f75244bec95857b
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-03-24 15:02:40 +0100

    Update changelog

--- core/setup.py
+++ core/setup.py
@@ -31,7 +31,7 @@ setup(
         'setuptools',
         'xml-compare',
         'zc.sourcefactory',
-        'zeit.cms>=2.64.0.dev0',
+        'zeit.cms>=2.67.0.dev0',
         'zeit.content.image>=2.5.1.dev0',
         'zeit.content.quiz>=0.4.2',
         'zeit.content.text>=2.0.2.dev0',
