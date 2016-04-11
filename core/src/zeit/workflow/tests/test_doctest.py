b5290066e66aa29d6ffcbb1f85a3e605d0fd2080
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-04-11 09:58:16 +0200

    Re ZON-2750: Don't automatically publish RelatedContent anymore

--- core/src/zeit/workflow/tests/test_doctest.py
+++ core/src/zeit/workflow/tests/test_doctest.py
@@ -5,7 +5,6 @@ import zeit.workflow.testing
 def test_suite():
     return zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
-        'dependency.txt',
         'syndication.txt',
         layer=zeit.workflow.testing.LAYER,
         package='zeit.workflow')
