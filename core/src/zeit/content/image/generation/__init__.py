a9a1685e3ccea96925c6e66571a6672653478609
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-04-18 14:33:24 +0200

    Update deprecated import name to one that actually works

--- core/src/zeit/content/image/generation/__init__.py
+++ core/src/zeit/content/image/generation/__init__.py
@@ -1,8 +1,8 @@
-import zope.app.generations
+import zope.generations.generations
 
 
 minimum_generation = 1
 generation = 1
 
-manager = zope.app.generations.generations.SchemaManager(
+manager = zope.generations.generations.SchemaManager(
     minimum_generation, generation, "zeit.content.image.generation")
