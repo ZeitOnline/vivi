87a143d75eedd4525fa6dd13af53356da3be6899
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-04-18 14:36:31 +0200

    Move browser code into the browser subpackage

--- src/zeit/push/banner.py
+++ src/zeit/push/banner.py
@@ -8,7 +8,6 @@ from zeit.content.article.edit.interfaces import IBreakingNewsBody
 import grokcore.component as grok
 import logging
 import pytz
-import zeit.content.article.interfaces
 import zeit.push.interfaces
 import zeit.push.message
 import zope.app.appsetup.product
@@ -98,32 +97,3 @@ class WrapperMessage(zeit.push.message.Message):
     def url(self):
         return zeit.push.interfaces.IPushURL(self.context).replace(
             zeit.cms.interfaces.ID_NAMESPACE, 'http://wrapper.zeit.de/')
-
-
-class Retract(object):
-
-    @property
-    def homepage(self):
-        notifier = zope.component.getUtility(
-            zeit.push.interfaces.IPushNotifier, name='homepage')
-        return ICMSContent(notifier.uniqueId)
-
-    @property
-    def ios_legacy(self):
-        notifier = zope.component.getUtility(
-            zeit.push.interfaces.IPushNotifier, name='ios-legacy')
-        return ICMSContent(notifier.uniqueId)
-
-    @property
-    def wrapper(self):
-        notifier = zope.component.getUtility(
-            zeit.push.interfaces.IPushNotifier, name='wrapper')
-        return ICMSContent(notifier.uniqueId)
-
-    @property
-    def banner_matches(self):
-        breaking = zeit.content.article.interfaces.IBreakingNews(
-            self.context, None)
-        if breaking is None:
-            return False
-        return breaking.banner_matches(self.homepage)
---
+++ src/zeit/push/browser/banner.py
@@ -0,0 +1,33 @@
+from zeit.cms.interfaces import ICMSContent
+import zeit.content.article.interfaces
+import zeit.push.interfaces
+import zope.component
+
+
+class Retract(object):
+
+    @property
+    def homepage(self):
+        notifier = zope.component.getUtility(
+            zeit.push.interfaces.IPushNotifier, name='homepage')
+        return ICMSContent(notifier.uniqueId)
+
+    @property
+    def ios_legacy(self):
+        notifier = zope.component.getUtility(
+            zeit.push.interfaces.IPushNotifier, name='ios-legacy')
+        return ICMSContent(notifier.uniqueId)
+
+    @property
+    def wrapper(self):
+        notifier = zope.component.getUtility(
+            zeit.push.interfaces.IPushNotifier, name='wrapper')
+        return ICMSContent(notifier.uniqueId)
+
+    @property
+    def banner_matches(self):
+        breaking = zeit.content.article.interfaces.IBreakingNews(
+            self.context, None)
+        if breaking is None:
+            return False
+        return breaking.banner_matches(self.homepage)
