1a2ca6e940f7457bee16a39b9bc7c50c4f21ad08
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-04-21 13:06:39 +0200

    Back to development: 1.24.1

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.infobox',
-    version='1.24.0',
+    version='1.24.1.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
