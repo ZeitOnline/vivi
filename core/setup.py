306f72f13d592a39a33a7853718c51c24e8a3744
Florian Pilz <fp@gocept.com>
2016-07-19 10:10:54 +0200

    re ZON-3171: Remove dependency to `zeit.imp`.

--- core/setup.py
+++ core/setup.py
@@ -41,7 +41,6 @@ setup(
         'zeit.cms>=2.46.0.dev0',
         'zeit.connector>=2.4.0.dev0',
         'zeit.edit>=2.11.3.dev0',
-        'zeit.imp>=0.15.0.dev0',
         'zeit.wysiwyg',
         'zope.app.appsetup',
         'zope.app.container',
