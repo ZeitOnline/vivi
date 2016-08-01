d1254467fdc42a99148b9b100e1f3a84be0a73f0
Florian Pilz <fp@gocept.com>
2016-08-01 13:15:57 +0200

    re ZON-3213: Reuse logic for Parse push notifications to send similar payloads.
    - solution is incomplete, e.g. payload for iOS is missing, imageUrl is required etc

--- src/zeit/push/urbanairship.py
+++ src/zeit/push/urbanairship.py
@@ -4,37 +4,41 @@ import urbanairship
 import zeit.cms.content.interfaces
 import zeit.push.interfaces
 import zeit.push.message
+import zeit.push.parse
 import zope.app.appsetup.product
 import zope.interface
 
 
-class Connection(object):
+class Connection(zeit.push.parse.Connection):
 
-    zope.interface.implements(zeit.push.interfaces.IPushNotifier)
-
-    def __init__(self, application_key, master_secret, expire_interval):
-        self.application_key = application_key
-        self.master_secret = master_secret
-        self.expire_interval = expire_interval
-
-    def send(self, text, link, **kw):
-        airship = urbanairship.Airship(
-            self.application_key, self.master_secret)
+    def push(self, data):
+        config = zope.app.appsetup.product.getProductConfiguration(
+            'zeit.push') or {}
+        channel = config.get(zeit.push.interfaces.PARSE_NEWS_CHANNEL)
+        airship = urbanairship.Airship(self.application_id, self.rest_api_key)
         push = airship.create_push()
-        push.audience = 'all'
-        push.notification = urbanairship.notification(alert='Hello')
-        push.device_types = urbanairship.device_types('all')
-        return push.send()
+        push.audience = {
+            'tag': 'News' if channel in data['where']['channels']['$in'] else 'Eilmeldung',
+            'group': 'device'
+        }
+        if data['where']['deviceType'] == 'android':
+            push.notification = urbanairship.notification(android=urbanairship.android(extra={
+                    'headline': data['data']['headline'],
+                    'text': data['data']['text'],
+                    'url': data['data']['url'],
+                    'imageUrl': data['data']['imageUrl'],
+                    'teaser': data['data']['teaser'],
+                    'tag': 'News' if channel in data['where']['channels']['$in'] else 'Eilmeldung',
+            }))
+        else:
+            return
+        push.device_types = urbanairship.device_types(data['where']['deviceType'])
+        push.send()  # might raise unauthorized
 
 
-class Message(zeit.push.message.Message):
+class Message(zeit.push.parse.Message):
 
     grok.name('urbanairship')
-    grok.context(zeit.cms.content.interfaces.ICommonMetadata)
-
-    @property
-    def text(self):
-        return self.context.title
 
 
 @zope.interface.implementer(zeit.push.interfaces.IPushNotifier)
