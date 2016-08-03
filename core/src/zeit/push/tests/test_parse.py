8dd0624d564b794d2e0cad097b2a802385dfa830
Florian Pilz <fp@gocept.com>
2016-08-03 10:45:18 +0200

    re ZON-3213: Extend and clean up tests for `zeit.push.parse.Connection`.
    - the test `test_friedbert_version_links_to_app_content` could be removed,
      since the payload does not depend on versions anymore
    - add test to make sure channels are present, not only absent
    - add test for raising a `TechnicalError`
    - test formatting of expiration time, since is different for Urban Airship
    - smoke test that payloads are send for android and ios

--- core/src/zeit/push/tests/test_parse.py
+++ core/src/zeit/push/tests/test_parse.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from zeit.push.interfaces import PARSE_NEWS_CHANNEL
 import mock
 import os
+import pytz
 import unittest
 import zeit.push.parse
 import zeit.push.testing
@@ -19,8 +21,7 @@ class ParseTest(unittest.TestCase):
     def test_push_works(self):
         # Parse offers no REST API to retrieve push messages,
         # so this is just a smoke test.
-        api = zeit.push.parse.Connection(
-            self.app_id, self.api_key, 1)
+        api = zeit.push.parse.Connection(self.app_id, self.api_key, 1)
         api.send('Being pushy.', 'http://example.com', skip_ios=True)
 
     def test_push_works_with_channels(self):
@@ -34,18 +35,38 @@ class ParseTest(unittest.TestCase):
         with self.assertRaises(zeit.push.interfaces.WebServiceError):
             api.send('Being pushy.', 'http://example.com')
 
-    def test_friedbert_version_links_to_app_content(self):
+    def test_server_error_should_raise(self):
+        response = mock.Mock()
+        response.status_code = 500
+        api = zeit.push.parse.Connection(self.app_id, self.api_key, 1)
+        with mock.patch('requests.post') as post:
+            post.return_value = response
+            with self.assertRaises(zeit.push.interfaces.TechnicalError):
+                api.send('Being pushy.', 'http://example.com')
+
+    def test_pushes_to_android_and_ios(self):
         api = zeit.push.parse.Connection(None, None, 1)
         with mock.patch.object(api, 'push') as push:
-            api.send('', 'http://www.zeit.de/bar', channels=PARSE_NEWS_CHANNEL)
-            android = push.call_args_list[0][0][0]
+            api.send('foo', 'any')
+            self.assertEqual({
+                'deviceType': 'android',
+                'appVersion': {'$gte': '1.4'},
+            }, push.call_args_list[0][0][0]['where'])
+            self.assertEqual({
+                'deviceType': 'ios',
+                'appVersion': {'$gte': '20150914'},
+            }, push.call_args_list[1][0][0]['where'])
+
+    def test_channels_can_be_set_via_parameter(self):
+        api = zeit.push.parse.Connection(None, None, 1)
+        with mock.patch.object(api, 'push') as push:
+            api.send('foo', 'any', channels=PARSE_NEWS_CHANNEL)
             self.assertEqual(
-                'http://www.zeit.de/bar',
-                android['data']['url'].split('?')[0])
-            ios = push.call_args_list[1][0][0]
+                ['News'],
+                push.call_args_list[0][0][0]['where']['channels']['$in'])
             self.assertEqual(
-                'http://www.zeit.de/bar',
-                ios['data']['aps']['url'].split('?')[0])
+                ['News'],
+                push.call_args_list[1][0][0]['where']['channels']['$in'])
 
     def test_no_channels_given_omits_channels_parameter(self):
         api = zeit.push.parse.Connection(None, None, 1)
@@ -63,3 +84,17 @@ class ParseTest(unittest.TestCase):
             api.send('foo', 'any', channels='foo')
             self.assertNotIn('channels', push.call_args_list[0][0][0]['where'])
             self.assertNotIn('channels', push.call_args_list[1][0][0]['where'])
+
+    def test_sets_expiration_time_in_payload(self):
+        api = zeit.push.parse.Connection('any', 'any', 3600)
+        with mock.patch('zeit.push.mobile.datetime') as mock_datetime:
+            mock_datetime.now.return_value = (
+                datetime(2014, 07, 1, 10, 15, 7, 38, tzinfo=pytz.UTC))
+            with mock.patch.object(api, 'push') as push:
+                api.send('foo', 'any')
+                self.assertEqual(
+                    '2014-07-01T11:15:07+00:00',
+                    push.call_args_list[0][0][0]['expiration_time'])
+                self.assertEqual(
+                    '2014-07-01T11:15:07+00:00',
+                    push.call_args_list[1][0][0]['expiration_time'])
