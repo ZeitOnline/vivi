9ee14b868ecb10eff105165d32f3695a7499281d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-08-04 16:27:43 +0200

    Preparing release 3.20.4

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.article',
-    version='3.20.4.dev0',
+    version='3.20.4',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
