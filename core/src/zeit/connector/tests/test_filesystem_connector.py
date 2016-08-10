37941d23dedd2f98f88303b5055cfdb0c480ba55
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-08-10 09:27:02 +0200

    Update test to new testcontent (belongs to commit:a26ecba)

--- core/src/zeit/connector/tests/test_filesystem_connector.py
+++ core/src/zeit/connector/tests/test_filesystem_connector.py
@@ -9,6 +9,7 @@ class FilesystemConnectorTest(zeit.connector.testing.FilesystemConnectorTest):
             [
                 (u'2006', u'http://xml.zeit.de/2006/'),
                 (u'2007', u'http://xml.zeit.de/2007/'),
+                (u'2016', u'http://xml.zeit.de/2016/'),
                 (u'online', u'http://xml.zeit.de/online/'),
                 (u'politik.feed', u'http://xml.zeit.de/politik.feed'),
                 (u'testcontent', u'http://xml.zeit.de/testcontent'),
