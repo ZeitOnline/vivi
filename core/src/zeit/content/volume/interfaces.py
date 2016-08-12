838e54f824cd4d1e6e21304a5abb899b8ecb3bad
Florian Pilz <fp@gocept.com>
2016-08-12 10:53:58 +0200

    re ZON-3252: Add content-type `Volume`.

--- core/src/zeit/content/volume/interfaces.py
+++ core/src/zeit/content/volume/interfaces.py
@@ -1,1 +1,22 @@
 # coding: utf8
+from zeit.cms.i18n import MessageFactory as _
+import zeit.cms.content.interfaces
+import zope.schema
+
+
+class IVolume(zeit.cms.content.interfaces.IXMLContent):
+
+    year = zope.schema.Int(
+        title=_("Year"),
+        min=1900,
+        max=2100)
+
+    volume = zope.schema.Int(
+        title=_("Volume"),
+        min=1,
+        max=53)
+
+    teaserText = zope.schema.Text(
+        title=_("Teaser text"),
+        required=False,
+        max_length=170)
