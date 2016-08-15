629606ef3c615dad19fd97dbc022e4502d395fa3
Florian Pilz <fp@gocept.com>
2016-08-15 08:11:38 +0200

    Create basic infrastructure.

---
+++ core/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zeit.content.volume',
+    version='1.0.0.dev0',
+    author='gocept, Zeit Online',
+    author_email='zon-backend@zeit.de',
+    url='http://www.zeit.de/',
+    description="vivi Content-Type Volume",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='BSD',
+    namespace_packages=['zeit', 'zeit.content'],
+    install_requires=[
+        'grokcore.component',
+        'setuptools',
+        'zeit.cms',
+        'zope.component',
+        'zope.interface',
+        'zope.schema',
+        'zope.security',
+    ],
+    entry_points={
+    },
+)
