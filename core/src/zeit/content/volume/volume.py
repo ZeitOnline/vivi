467ddadcb588a9eae688a8221fbce392d8d389f6
Florian Pilz <fp@gocept.com>
2016-08-16 11:55:00 +0200

    re ZON-3252: Save chosen product in DAV properties of ``Volume``.
    - belongs to commit efcac8f31876b204f361437ac8710a30e5c8026c

--- core/src/zeit/content/volume/volume.py
+++ core/src/zeit/content/volume/volume.py
@@ -4,6 +4,7 @@ import zeit.cms.content.xmlsupport
 import zeit.cms.interfaces
 import zeit.cms.type
 import zeit.content.volume.interfaces
+import zeit.workflow
 import zope.interface
 
 
@@ -18,7 +19,25 @@ class Volume(zeit.cms.content.xmlsupport.XMLContentBase):
     zeit.cms.content.dav.mapProperties(
         zeit.content.volume.interfaces.IVolume,
         zeit.cms.interfaces.DOCUMENT_SCHEMA_NS,
-        ('year', 'volume', 'teaserText',))
+        ('product', 'year', 'volume', 'teaserText',))
+
+    _product_id = zeit.cms.content.dav.DAVProperty(
+        zope.schema.TextLine(),
+        zeit.workflow.interfaces.WORKFLOW_NS,
+        'product-id')
+
+    @property
+    def product(self):
+        source = zeit.content.volume.interfaces.IVolume['product'].source(self)
+        for value in source:
+            if value.id == self._product_id:
+                return value
+
+    @product.setter
+    def product(self, value):
+        if self._product_id == value.id:
+            return
+        self._product_id = value.id if value is not None else None
 
 
 class VolumeType(zeit.cms.type.XMLContentTypeDeclaration):
