611c51a5b3d285bd4d3b0ccc230ebceec8d5b792
Steffen Allner <sa@gocept.com>
2016-08-19 09:09:23 +0200

    re ZON-3252: Use common products.xml also here.

--- core/src/zeit/content/volume/testing.py
+++ core/src/zeit/content/volume/testing.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import pkg_resources
-import re
 import zeit.cms.testing
 import zeit.content.image.testing
 
@@ -12,18 +11,10 @@ product_config = """
 """.format(here=pkg_resources.resource_filename(__name__, '.'))
 
 
-# We need a `products.xml` config with `volume="true"` attributes for filtering
-cms_product_config = re.sub(
-    'source-products file://.*/products.xml',
-    'source-products file://{here}/tests/fixtures/products.xml'.format(
-        here=pkg_resources.resource_filename(__name__, '.')),
-    zeit.cms.testing.cms_product_config)
-
-
 ZCML_LAYER = zeit.cms.testing.ZCMLLayer(
     'ftesting.zcml', product_config=(
         product_config +
-        cms_product_config +
+        zeit.cms.testing.cms_product_config +
         zeit.content.image.testing.product_config))
 
 
