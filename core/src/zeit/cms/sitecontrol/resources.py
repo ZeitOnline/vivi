0b27d1ed336b122f2949ff8c531b331bf6d07af4
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-08-23 16:23:04 +0200

    Remove sitecontrol sidebar, it's a resource hog and not reallly used anymore.

--- core/src/zeit/cms/sitecontrol/resources.py
+++
@@ -1,6 +0,0 @@
-from zeit.cms.browser.resources import Resource, Library
-import zeit.cms.browser.resources
-
-
-lib = Library('zeit.cms.sitecontrol', 'resources')
-Resource('sidebar.js', depends=[zeit.cms.browser.resources.base])
