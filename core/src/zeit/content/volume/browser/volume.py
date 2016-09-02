20e850d24a77999baae65e65344c3c4bb00302f5
Steffen Allner <sa@gocept.com>
2016-09-02 17:03:28 +0200

    re ZON-3304: Show meaningful title for ``Volume`` in search results.

---
+++ core/src/zeit/content/volume/browser/volume.py
@@ -0,0 +1,34 @@
+import grokcore.component as grok
+import zeit.cms.browser.listing
+import zope.publisher.interfaces
+
+
+class VolumeListRepresentation(
+        grok.MultiAdapter,
+        zeit.cms.browser.listing.BaseListRepresentation):
+
+    grok.adapts(
+        zeit.content.volume.interfaces.IVolume,
+        zope.publisher.interfaces.IPublicationRequest)
+    grok.implements(
+        zeit.cms.browser.interfaces.IListRepresentation)
+
+    ressort = page = author = u''
+
+    @property
+    def volume(self):
+        return self.context.volume
+
+    @property
+    def year(self):
+        return self.context.year
+
+    @property
+    def title(self):
+        volume = self.context
+        return '{} {}/{}'.format(
+            volume.product.title, volume.year, volume.volume)
+
+    @property
+    def searchableText(self):
+        return self.title
