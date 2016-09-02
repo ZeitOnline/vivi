0d8114f73b7f23b4aa39508fe1b7d58e5247813b
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-09-02 11:45:14 +0200

    ZON-2409: Bump version (new API)

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.video',
-    version='2.5.4.dev0',
+    version='2.6.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
