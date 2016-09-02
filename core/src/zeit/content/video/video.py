189b44173f5f374b771a88813127343a26ec17db
Florian Pilz <fp@gocept.com>
2016-09-02 13:36:39 +0200

    Add docstring

--- core/src/zeit/content/video/video.py
+++ core/src/zeit/content/video/video.py
@@ -47,6 +47,7 @@ class AuthorshipsProperty(zeit.cms.content.reference.ReferenceProperty):
     # Similar to zeit.cms.content.reference.MultiResource, but only the setter.
 
     def references(self, instance):
+        """Returns a ``References`` object to easen creation of references."""
         return super(AuthorshipsProperty, self).__get__(instance, None)
 
     def __set__(self, instance, value):
