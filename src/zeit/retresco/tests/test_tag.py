52ddec80be8e2c90807a7b07425a6f260c9e86e0
Florian Pilz <fp@gocept.com>
2016-09-14 10:42:15 +0200

    ZON-3199: Test that adapter in `zeit.cms` handles unicode escaped uniqueId correctly.

--- src/zeit/retresco/tests/test_tag.py
+++ src/zeit/retresco/tests/test_tag.py
@@ -1,11 +1,17 @@
 # coding: utf8
-import unittest
+import zeit.cms.interfaces
+import zeit.retresco.testing
 
 
-class TagTest(unittest.TestCase):
+class TagTest(zeit.retresco.testing.FunctionalTestCase):
     """Testing ..tag.Tag."""
 
     def test_from_code_generates_a_tag_object_equal_to_its_source(self):
         from ..tag import Tag
         tag = Tag(u'Vipraschül', 'Person')
         self.assertEqual(tag, Tag.from_code(tag.code))
+
+    def test_uniqueId_from_tag_can_be_adapted_to_tag(self):
+        from ..tag import Tag
+        tag = Tag(u'Vipraschül', 'Person')
+        self.assertEqual(tag, zeit.cms.interfaces.ICMSContent(tag.uniqueId))
