13c972c55af71d789142383c52b5dcd4addcd657
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-09-15 18:21:12 +0200

    ZON-3318: Remove bigshare buttons from link form.

--- src/zeit/magazin/browser/link.py
+++ src/zeit/magazin/browser/link.py
@@ -1,11 +1,16 @@
+import copy
 import zeit.content.link.browser.form
 import zeit.magazin.browser.social
 
 
 base = zeit.content.link.browser.form.Base
+social_fields = copy.copy(zeit.push.browser.form.SocialBase.social_fields)
+social_fields_list = list(social_fields.fields)
+social_fields_list.remove('bigshare_buttons')
+social_fields.fields = tuple(social_fields_list)
 field_groups = (
     base.field_groups[:4]
-    + (zeit.magazin.browser.social.SocialBase.social_fields,)
+    + (social_fields,)
     + base.field_groups[5:]
 )
 
@@ -14,15 +19,18 @@ class Add(zeit.magazin.browser.social.SocialBase,
           zeit.content.link.browser.form.Add):
 
     field_groups = field_groups
+    social_fields = social_fields
 
 
 class Edit(zeit.magazin.browser.social.SocialBase,
            zeit.content.link.browser.form.Edit):
 
     field_groups = field_groups
+    social_fields = social_fields
 
 
 class Display(zeit.magazin.browser.social.SocialBase,
               zeit.content.link.browser.form.Display):
 
     field_groups = field_groups
+    social_fields = social_fields
