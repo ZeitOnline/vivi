613884b99eb0d6415210c4642d2496a291597ee4
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-09-15 12:01:43 +0200

    ZON-3199: Declare dependency

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'plone.testing',
         'requests',
         'setuptools',
-        'zeit.cms>=2.87.0.dev0',
+        'zeit.cms>=2.89.0.dev0',
         'zeit.content.author',
         'zeit.content.article',
         'zeit.content.image',
