1b2569ec3ca872803ff38f6a62eb44440a8d5923
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-09-26 16:26:28 +0200

    Preparing release 1.16.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.push',
-    version='1.15.6.dev0',
+    version='1.16.0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
