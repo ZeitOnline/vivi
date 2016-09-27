f524bb89b25ee8fbc37444de16379252a83d2a5d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-09-27 16:03:55 +0200

    Update changelog

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.cp',
-    version='3.11.2.dev0',
+    version='3.12.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
