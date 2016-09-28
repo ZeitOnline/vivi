7db18fa4a5e90deaa2d949ad2b223a7474c84d46
Nicolas Drebenstedt <cutoffthetop@me.com>
2016-09-28 11:57:48 +0200

    VIV-263: Deprecate automatic image link rewriting
    It was originally meant for sevenval, but never supported by the apps
    and deactivated ever since.

--- core/src/zeit/content/image/browser/tests/test_image.py
+++ core/src/zeit/content/image/browser/tests/test_image.py
@@ -1,6 +1,3 @@
-import ZODB.utils
-import os.path
-import unittest
 import zeit.cms.testing
 import zeit.content.image.testing
 
@@ -30,36 +27,3 @@ class TestDelete(zeit.cms.testing.BrowserTestCase):
         self.assertEllipsis(
             '...Do you really want to delete your workingcopy?...',
             self.browser.contents)
-
-
-class ImageEdit(zeit.cms.testing.BrowserTestCase):
-
-    layer = zeit.content.image.testing.ZCML_LAYER
-
-    def setUp(self):
-        super(ImageEdit, self).setUp()
-        b = self.browser
-        b.open(
-            'http://localhost/++skin++vivi/repository'
-            '/2006/DSC00109_2.JPG/@@checkout')
-        b.getControl(
-            name='form.copyrights.0..combination_00').value = 'required'
-
-    @unittest.skip(
-        'Disabled because the frontend does not interpret rewritten links '
-        'correctly yet.')
-    def test_rewrites_links_from_www_zeit_de_to_xml_zeit_de(self):
-        b = self.browser
-        b.getControl('Links to').value = 'http://www.zeit.de/foo/bar'
-        b.getControl('Apply').click()
-        self.assertEllipsis('...Updated on...', b.contents)
-        self.assertEqual(
-            'http://xml.zeit.de/foo/bar', b.getControl('Links to').value)
-
-    def test_leaves_other_links_alone(self):
-        b = self.browser
-        b.getControl('Links to').value = 'http://example.de/foo/bar'
-        b.getControl('Apply').click()
-        self.assertEllipsis('...Updated on...', b.contents)
-        self.assertEqual(
-            'http://example.de/foo/bar', b.getControl('Links to').value)
