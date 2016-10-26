4489711612eb15392bc1884a4a4bb1b47043400e
Steffen Allner <sa@gocept.com>
2016-10-26 09:46:43 +0200

    ZON-3409: Move tests to separate folder.

--- core/src/zeit/cms/relation/tests/test_doctests.py
+++ core/src/zeit/cms/relation/tests/test_doctests.py
@@ -9,5 +9,5 @@ import zeit.cms.testing
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite('README.txt'))
+    suite.addTest(zeit.cms.testing.FunctionalDocFileSuite('../README.txt'))
     return suite
