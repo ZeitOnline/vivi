1a90e4fb6f9d8c36ebe564b84cd636c2542bb61d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-11-07 15:05:13 +0100

    pep8

--- src/zeit/content/author/browser/form.py
+++ src/zeit/content/author/browser/form.py
@@ -111,8 +111,8 @@ class AddContextfree(zeit.cms.browser.form.AddForm):
     """
 
     title = _('Add author')
-    form_fields = (FormBase._form_fields.omit('__name__')
-                   + zope.formlib.form.FormFields(IDuplicateConfirmation))
+    form_fields = (FormBase._form_fields.omit('__name__') +
+                   zope.formlib.form.FormFields(IDuplicateConfirmation))
     factory = zeit.content.author.author.Author
     next_view = 'view.html'
 
