c3fd986f07dfe2f4aef356adde10f85e1add3bcf
Andreas Zeidler <andi@pyfidelity.com>
2016-11-15 16:24:18 +0100

    ZON-2996: Don't display the 'Delete' menu item for published content.

--- core/src/zeit/cms/repository/browser/menu.py
+++ core/src/zeit/cms/repository/browser/menu.py
@@ -1,4 +1,6 @@
 from zeit.cms.i18n import MessageFactory as _
+from zeit.cms.repository.interfaces import IRepositoryContent
+from zeit.cms.workflow.interfaces import IPublishInfo
 import zeit.cms.browser.menu
 import zope.browsermenu.menu
 import zope.i18n
@@ -24,3 +26,14 @@ class Delete(zeit.cms.browser.menu.LightboxActionMenuItem):
     """Delete menu item."""
 
     title = _('Delete')
+
+    @property
+    def visible(self):
+        return IRepositoryContent.providedBy(self.context) and \
+            not IPublishInfo(self.context).published
+
+    def render(self):
+        if self.visible:
+            return super(Delete, self).render()
+        else:
+            return ''
---
+++ core/src/zeit/cms/repository/browser/tests/test_delete.py
@@ -0,0 +1,23 @@
+from mechanize import LinkNotFoundError
+from zeit.cms.testing import ZeitCmsBrowserTestCase
+from zeit.cms.workflow.interfaces import IPublishInfo, IPublish
+from zeit.cms.workflow.interfaces import CAN_PUBLISH_SUCCESS
+
+
+class TestDeleteMenuItem(ZeitCmsBrowserTestCase):
+
+    def test_delete_menu_item_is_displayed_for_unpublished_objects(self):
+        content = self.repository['testcontent']
+        self.assertFalse(IPublishInfo(content).published)
+        self.browser.open(
+            'http://localhost:8080/++skin++vivi/repository/testcontent')
+        self.browser.getLink(url='@@delete.html')   # assert link exists
+
+    def test_delete_menu_item_is_not_displayed_for_published_objects(self):
+        content = self.repository['testcontent']
+        IPublishInfo(content).set_can_publish(CAN_PUBLISH_SUCCESS)
+        IPublish(content).publish()
+        self.browser.open(
+            'http://localhost:8080/++skin++vivi/repository/testcontent')
+        with self.assertRaises(LinkNotFoundError):
+            self.browser.getLink(url='@@delete.html')
