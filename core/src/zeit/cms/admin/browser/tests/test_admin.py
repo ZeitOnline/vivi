a00a76213626f831f0711e6f014a6afbb138b9e2
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-11-30 16:20:33 +0100

    Fix test setup for admin views. Use a privileged user for tests, don't give privileges to the normal user

--- core/src/zeit/cms/admin/browser/tests/test_admin.py
+++ core/src/zeit/cms/admin/browser/tests/test_admin.py
@@ -1,7 +1,13 @@
 import zeit.cms.testing
+import zope.testbrowser.testing
 
 
 class TestAdminMenu(zeit.cms.testing.ZeitCmsBrowserTestCase):
+
+    def setUp(self):
+        super(TestAdminMenu, self).setUp()
+        self.browser = zope.testbrowser.testing.Browser()
+        self.browser.addHeader('Authorization', 'Basic zmgr:mgrpw')
 
     def test_admin_menu_is_displayed_for_repository_objects(self):
         self.browser.open(
