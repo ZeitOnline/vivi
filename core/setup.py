59745fd08a88b3a6b5ce795014544c0c5089f60d
Michael Howitz <mh@gocept.com>
2016-12-09 12:02:48 +0100

    ZON-3453: Remove calls to no longer existing function.
    As these calls are no longer needed.

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.video',
-    version='2.7.2.dev0',
+    version='3.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
@@ -22,7 +22,7 @@ setup(
         'plone.testing',
         'pytz',
         'setuptools',
-        'zeit.cms>=2.43.1.dev0',
+        'zeit.cms >= 3.0.dev0',
         'zeit.connector',
         'zeit.push >= 1.17.0.dev0',
         'zeit.solr>=2.2.0.dev0',
