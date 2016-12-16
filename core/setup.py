100dcb920e25ff80fa1f30fd142779fcf2ead966
Michael Howitz <mh@gocept.com>
2016-12-16 15:10:27 +0100

    ZON-3409: Update to version with celery.

--- core/setup.py
+++ core/setup.py
@@ -22,7 +22,7 @@ setup(
         'setuptools',
         'zc.iso8601',
         'zc.resourcelibrary',
-        'zeit.cms>=2.93.dev0',
+        'zeit.cms >= 3.0.dev0',
         'zeit.content.image>=2.13.6.dev0',
         'zope.app.pagetemplate',
         'zope.app.testing',
