f9cbbe080f1fc1e6134447e748cb678b9c1bd388
Michael Howitz <mh@gocept.com>
2016-12-16 15:01:58 +0100

    No need for a new major version.

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.video',
-    version='3.0.dev0',
+    version='2.7.2.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
