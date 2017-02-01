5905a53a6e9a21b3ec81a1a5a5a6e9f0e8f1db87
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-02-01 12:45:37 +0100

    BUG-633: Rewrite test to how IReference actually works

--- core/src/zeit/content/volume/tests/test_reference.py
+++ core/src/zeit/content/volume/tests/test_reference.py
@@ -1,3 +1,4 @@
+import lxml.objectify
 import zeit.cms.content.interfaces
 import zeit.content.article.edit.volume
 import zeit.content.volume.testing
@@ -9,11 +10,8 @@ class VolumeReferenceTest(zeit.content.volume.testing.FunctionalTestCase):
     def setUp(self):
         from zeit.content.volume.volume import Volume
         super(VolumeReferenceTest, self).setUp()
-
         self.repository['testvolume'] = Volume()
         self.volume = self.repository['testvolume']
-        self.reference_container = zeit.content.article.edit.volume.Volume(
-            self.volume, self.volume.xml)
 
     def test_volume_can_be_adapted_to_IXMLReference(self):
         result = zope.component.getAdapter(
@@ -26,11 +24,16 @@ class VolumeReferenceTest(zeit.content.volume.testing.FunctionalTestCase):
     def test_volume_can_be_adapted_to_IReference(self):
         from zeit.content.volume.interfaces import IVolumeReference
 
-        result = zope.component.getMultiAdapter(
-            (self.reference_container, self.volume.xml),
+        node = zope.component.getAdapter(
+            self.volume, zeit.cms.content.interfaces.IXMLReference,
+            name='related')
+        source = zeit.content.article.edit.volume.Volume(
+            None, lxml.objectify.XML('<volume/>'))
+        reference = zope.component.getMultiAdapter(
+            (source, node),
             zeit.cms.content.interfaces.IReference, name='related')
 
-        result.teaserText = 'Test teaser'
+        reference.teaserText = 'Test teaser'
 
-        self.assertEqual(True, IVolumeReference.providedBy(result))
-        self.assertEqual('Test teaser', result.xml.teaserText.text)
+        self.assertEqual(True, IVolumeReference.providedBy(reference))
+        self.assertEqual('Test teaser', reference.xml.teaserText.text)
