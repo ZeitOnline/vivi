1a7c86bbbce28fe741ecc0827ec7c1f99a7b9c84
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-02-17 11:48:12 +0100

    PERF: Don't update TMS during publishing.

--- core/src/zeit/retresco/update.py
+++ core/src/zeit/retresco/update.py
@@ -39,6 +39,8 @@ def index_after_add(event):
     zeit.cms.interfaces.ICMSContent,
     zeit.cms.checkout.interfaces.IAfterCheckinEvent)
 def index_after_checkin(context, event):
+    if event.publishing:
+        return
     index_async(context.uniqueId, enrich=True)
 
 
