c50265af392ee19db1ccd5bd0555fe8a6b4bc9e3
Michael Howitz <mh@gocept.com>
2017-02-20 14:00:15 +0100

    ZON-3514: Remove services and jobs from ZODB, too.

--- core/src/zeit/cms/generation/evolve14.py
+++ core/src/zeit/cms/generation/evolve14.py
@@ -6,11 +6,13 @@ import zope.component
 
 def update(root):
     site_manager = zope.component.getSiteManager()
-    for name, _ in site_manager.getUtilitiesFor(ITaskService):
+    for name, service in site_manager.getUtilitiesFor(ITaskService):
         done = site_manager.unregisterUtility(provided=ITaskService, name=name)
         if not done:
             raise RuntimeError('unregisterUtility did not return True')
+        del service.__parent__[service.__name__]
 
 
 def evolve(context):
+    """Remove the lovely.remotetask services."""
     zeit.cms.generation.do_evolve(context, update)
