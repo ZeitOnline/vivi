24618a5369193cabb2f354d1a206469cc57b16fb
Knut Goetz <Knut.Goetz@zeit.de>
2017-02-22 16:58:16 +0100

    Configure Volume Admin View

--- core/src/zeit/content/volume/browser/admin.py
+++ core/src/zeit/content/volume/browser/admin.py
@@ -2,9 +2,11 @@
 from zeit.solr import query
 import zeit.content.article.article
 import zeit.content.infobox.infobox
+import zeit.cms.admin.browser.admin
 
 
-class Admin():
+class VolumeAdminForm(zeit.cms.admin.browser.admin.EditFormCI):
+
     """
     Admin View with the publish button.
     Check out the normal Admin View.
@@ -12,6 +14,10 @@ class Admin():
     """
 
     def _get_result_from_solr(self):
+        # TODO: Think about infoboxes. They should be published as well,
+        # if they never got checked out they wont be in SOLR.
+        # One Dirty Way would be to checkout all infoboxes after they got
+        # imported.
         additional_queries = [
             query.field('published', 'not-published'),
             query.or_(query.field_raw(
