f39fadec90ab812ff94f0638003e42b7654497fd
Knut Goetz <Knut.Goetz@zeit.de>
2017-02-23 13:42:22 +0100

    Add additional Form action for publish

--- core/src/zeit/content/volume/browser/admin.py
+++ core/src/zeit/content/volume/browser/admin.py
@@ -3,6 +3,7 @@ from zeit.solr import query
 import zeit.content.article.article
 import zeit.content.infobox.infobox
 import zeit.cms.admin.browser.admin
+import zope.formlib.form as form
 
 
 class VolumeAdminForm(zeit.cms.admin.browser.admin.EditFormCI):
@@ -13,19 +14,33 @@ class VolumeAdminForm(zeit.cms.admin.browser.admin.EditFormCI):
     /zeit.cms/src/zeit/cms/admin/browser/admin.py.
     """
 
+    extra_actions = form.Actions()
+
+    @property
+    def actions(self):
+        return list(super(VolumeAdminForm, self).actions) + \
+               list(self.extra_actions)
+
+    @form.action("Publish content of this volume", extra_actions)
+    def publish_all(self, action, data):
+        to_publish = self._get_result_from_solr(self)
+
+
     def _get_result_from_solr(self):
-        # TODO: Think about infoboxes. They should be published as well,
-        # if they never got checked out they wont be in SOLR.
-        # One Dirty Way would be to checkout all infoboxes after they got
-        # imported.
+        # TODO: Think about infoboxes. They should be published as well.
+        # Check if they are in Solr
         additional_queries = [
             query.field('published', 'not-published'),
-            query.or_(query.field_raw(
-                'type', zeit.content.article.article.ArticleType.type),
-                  query.field_raw(
-                'type', zeit.content.infobox.infobox.InfoboxType.type),
+            query.or_(
+                query.and_(
+                    query.bool_field('urgent', True),
+                    query.field_raw(
+                        'type',
+                        zeit.content.article.article.ArticleType.type)),
+                query.field_raw('type',
+                                zeit.content.infobox.infobox.InfoboxType.type),
             ),
-            query.bool_field('urgent', True)
+
         ]
         return self.context.all_content_via_solr(
             additional_query_contstraints=additional_queries)
