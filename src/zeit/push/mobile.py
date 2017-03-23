03a2bf03dc26490356d90b77c77e11a9ed5e4a8d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-03-23 16:37:23 +0100

    MAINT: Revert "Remove headline for channel "news""
    This reverts commit 3c42de4dc5db7f1272f46ebc9fa668b214c604e5.
    
    UA doesn't like title=None, and we don't have time to investigate this right now

--- src/zeit/push/mobile.py
+++ src/zeit/push/mobile.py
@@ -61,7 +61,7 @@ class ConnectionBase(object):
 
         """
         if self.config.get(CONFIG_CHANNEL_NEWS) in channels:
-            return None
+            headline = _('push-news-title')
         else:
             headline = _('push-breaking-title')
 
@@ -93,12 +93,11 @@ class ConnectionBase(object):
         full_link = '/'.join((push_target, path))
         deep_link = '://'.join((self.APP_IDENTIFIER, path))
 
-        headline = self.get_headline(channels)
         return {
             'ios': {
                 'alert': title,
                 'deep_link': self.add_tracking(deep_link, channels, 'ios'),
-                'headline': headline,
+                'headline': self.get_headline(channels),
                 'sound': 'chime.aiff' if is_breaking else '',
                 'tag': extra_tag,
                 'url': self.add_tracking(full_link, channels, 'ios')
@@ -107,7 +106,7 @@ class ConnectionBase(object):
                 'alert': title,
                 'deep_link': self.add_tracking(deep_link, channels, 'android'),
                 'headline': 'ZEIT ONLINE {}'.format(
-                    headline) if headline else None,
+                    self.get_headline(channels)),
                 'priority': 2 if is_breaking else 0,
                 'tag': extra_tag,
                 'url': self.add_tracking(full_link, channels, 'android')
