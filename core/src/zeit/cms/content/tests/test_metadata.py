050095185bce10d88d820c179e463c9a5c63998e
Tobias Kabbeck <tobias.kabbeck@zeit.de>
2017-04-06 12:06:25 +0200

    ZON-3851: Test that access changes are logged

--- core/src/zeit/cms/content/tests/test_metadata.py
+++ core/src/zeit/cms/content/tests/test_metadata.py
@@ -1,6 +1,6 @@
 from zeit.cms.checkout.helper import checked_out
-import zeit.cms.testcontenttype.interfaces
 from zeit.cms.testcontenttype.testcontenttype import ExampleContentType
+import zeit.cms.testcontenttype.interfaces
 import zeit.cms.testing
 import zeit.content.article.testing
 import zope.lifecycleevent
@@ -49,3 +49,18 @@ class ChannelCopying(zeit.cms.testing.ZeitCmsTestCase):
                     zeit.cms.testcontenttype.interfaces.IExampleContentType,
                     'ressort'))
             self.assertEqual((), co.channels)
+
+
+class AccessChangeEvent(zeit.content.article.testing.FunctionalTestCase):
+
+    def test_change_access_value_is_logged(self):
+        article = zeit.cms.interfaces.ICMSContent(
+            'http://xml.zeit.de/online/2007/01/Somalia')
+        log = zeit.objectlog.interfaces.ILog(article)
+        with zeit.cms.checkout.helper.checked_out(article) as co:
+            zope.lifecycleevent.modified(
+                co, zope.lifecycleevent.Attributes(
+                    zeit.cms.content.interfaces.ICommonMetadata,
+                    'access'))
+            entries = list(log.get_log())
+            assert entries[-1].message == 'Access changed'
