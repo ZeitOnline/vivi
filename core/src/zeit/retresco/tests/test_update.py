34245ceeedc7c77a125da293cd8547ea3da4cdf0
Tobias Kabbeck <tobias.kabbeck@zeit.de>
2017-05-05 14:54:35 +0200

    ZON-3173: Apply requested changes

--- core/src/zeit/retresco/tests/test_update.py
+++ core/src/zeit/retresco/tests/test_update.py
@@ -14,7 +14,6 @@ import zeit.workflow.testing
 import zope.component
 import zope.event
 import zope.lifecycleevent
-import mechanize
 
 
 @gocept.async.function(service='events')
@@ -167,41 +166,3 @@ class UpdateTest(zeit.retresco.testing.FunctionalTestCase):
         zeit.cms.workflow.interfaces.IPublishInfo(content).urgent = True
         zeit.cms.workflow.interfaces.IPublish(content).publish()
         zeit.workflow.testing.run_publish()
-
-
-class TMSUpdateRequestTest(zeit.cms.testing.BrowserTestCase):
-
-    layer = zeit.retresco.testing.ZCML_LAYER
-
-    def test_endpoint_avoids_get(self):
-        b = zope.testbrowser.testing.Browser()
-        with self.assertRaisesRegexp(mechanize.HTTPError,
-                                     'HTTP Error 400: Bad Request'):
-            b.open('http://localhost/@@update_keywords_by_list')
-
-    def test_endpoint_rejects_post_without_doc_ids(self):
-        b = zope.testbrowser.testing.Browser()
-        with self.assertRaisesRegexp(mechanize.HTTPError,
-                                     'HTTP Error 400: Bad Request'):
-            b.post('http://localhost/@@update_keywords_by_list', '')
-        with self.assertRaisesRegexp(mechanize.HTTPError,
-                                     'HTTP Error 400: Bad Request'):
-            b.post('http://localhost/@@update_keywords_by_list',
-                   '{"foo" : "bar"}', 'application/x-javascript')
-
-    def test_endpoint_rejects_invalid_doc_ids(self):
-        b = zope.testbrowser.testing.Browser()
-        with self.assertRaisesRegexp(mechanize.HTTPError,
-                                     'HTTP Error 422: Unprocessable Entity'):
-            b.post('http://localhost/@@update_keywords_by_list',
-                   '{"doc_ids" : [1, 2, 3]}', 'application/x-javascript')
-
-    def test_endpoint_successful_update_vaild_article(self):
-        b = zope.testbrowser.testing.Browser()
-        b.post('http://localhost/@@update_keywords_by_list',
-               '{"doc_ids" : ['
-                '"{urn:uuid:9cb93717-2467-4af5-9521-25110e1a7ed8}", '
-                '"{urn:uuid:0da8cb59-1a72-4ae2-bbe2-006e6b1ff621}"]}',
-                'application/x-javascript')
-        self.assertEquals('{"update": true}', b.contents)
-        self.assertEquals('200 Ok', b.headers.getheader('status'))
