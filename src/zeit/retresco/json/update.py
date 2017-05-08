8b16f19ff27a89b732a16f94058f872035f16140
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-05-08 11:16:26 +0200

    Let's have some more guard-style

--- src/zeit/retresco/json/update.py
+++ src/zeit/retresco/json/update.py
@@ -21,28 +21,29 @@ class UpdateKeywords(zeit.cms.browser.view.Base):
             self.request.response.setStatus(405)
             return json.dumps(status)
 
-        body = self.request.bodyStream.read(
-            int(self.request['CONTENT_LENGTH']))
         try:
-            for doc_id in json.loads(body).get('doc_ids'):
-                try:
-                    content = zeit.cms.content.contentuuid.uuid_to_content(
-                        zeit.cms.content.interfaces.IUUID(doc_id))
-                    index_async(content.uniqueId, True, True)
-                    status['updated'].append(doc_id)
-                    status['updated_content'].append(content.uniqueId)
-                except (AttributeError, TypeError):
-                    status['invalid'].append(doc_id)
-                    continue
-                else:
-                    log.info('Scheduling %s for reindex', content.uniqueId)
-            status['message'] = 'Nothing updated'
-            if status['updated']:
-                status['message'] = 'Update successful'
-            return json.dumps(status)
-
-        except (TypeError, ValueError):
+            body = self.request.bodyStream.read(
+                int(self.request['CONTENT_LENGTH']))
+            doc_ids = json.loads(body)['doc_ids']
+        except:
             status['message'] = (
                 'JSON body with parameter doc_ids (list of uuids) required')
             self.request.response.setStatus(400)
             return json.dumps(status)
+
+        for doc_id in doc_ids:
+            try:
+                content = zeit.cms.content.contentuuid.uuid_to_content(
+                    zeit.cms.content.interfaces.IUUID(doc_id))
+                index_async(content.uniqueId, True, True)
+                status['updated'].append(doc_id)
+                status['updated_content'].append(content.uniqueId)
+            except (AttributeError, TypeError):
+                status['invalid'].append(doc_id)
+                continue
+            else:
+                log.info('Scheduling %s for reindex', content.uniqueId)
+        status['message'] = 'Nothing updated'
+        if status['updated']:
+            status['message'] = 'Update successful'
+        return json.dumps(status)
