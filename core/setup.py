4573584bcca7866faaac2fa0e79af3727783918f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-05-17 11:55:17 +0200

    BUG-669: Declare dependency; the IPortraitbox.text only works properly with the updated Structure descriptor

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.article',
-    version='3.21.14.dev0',
+    version='3.22.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
@@ -44,7 +44,7 @@ setup(
         'zc.sourcefactory',
         'zc.table',
         'zdaemon',
-        'zeit.cms>=2.95.0.dev0',
+        'zeit.cms>=2.99.0.dev0',
         'zeit.connector>=2.3.1.dev0',
         'zeit.content.author>=2.7.1.dev0',
         'zeit.content.cp>=0.33.0',
