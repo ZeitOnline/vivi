1adde4e098016a1663bfe08f95460227839d85c8
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-05-26 11:59:45 +0200

    ZON-3948: Implement configurable path blacklist to prevent publishing

--- core/src/zeit/workflow/asset.py
+++ core/src/zeit/workflow/asset.py
@@ -1,9 +1,9 @@
-import zope.component
-import zope.interface
-
+from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.interfaces
 import zeit.workflow.interfaces
 import zeit.workflow.timebased
+import zope.component
+import zope.interface
 
 
 class AssetWorkflow(zeit.workflow.timebased.TimeBasedWorkflow):
@@ -13,4 +13,9 @@ class AssetWorkflow(zeit.workflow.timebased.TimeBasedWorkflow):
     zope.component.adapts(zeit.cms.interfaces.IAsset)
 
     def can_publish(self):
+        if self.matches_blacklist():
+            self.error_messages = (
+                _('publish-preconditions-blacklist',
+                  mapping=self._error_mapping),)
+            return zeit.cms.workflow.interfaces.CAN_PUBLISH_ERROR
         return zeit.cms.workflow.interfaces.CAN_PUBLISH_SUCCESS
