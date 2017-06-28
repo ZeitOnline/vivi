37e3d8ac5cd0aee3cda7871aaf0a499d5fb14364
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-06-28 13:10:23 +0200

    ZON-3983: Re-introduce adapting to interface, it turns out we do need it after all
    This reverts commit be0089a66b45c36ff1d2777b53aaad0e5a7baaca.

--- core/src/zeit/brightcove/convert2.py
+++ core/src/zeit/brightcove/convert2.py
@@ -15,9 +15,14 @@ class dictproperty(object):
     # Video.properties set it for us instead.
     __name__ = None
 
-    def __init__(self, bc_name, field, converter=None):
+    def __init__(self, bc_name, iface=None, fieldname=None, field=None,
+                 converter=None):
         """The value is stored in the instance's data dict, under the key
         ``bc_name`` (may contain '/' to denote nested dicts).
+
+        Pass either iface and fieldname, or for special cases a
+        zope.schema.Field instance, so we can look up type conversion
+        accordingly, and access CMS values that reside in adapters.
 
         You can pass the class name of an ITypeConverter (to prevent source
         code ordering / import time issues), otherwise one will be looked up
@@ -27,7 +32,15 @@ class dictproperty(object):
         segments = self.bc_name.split('/')
         self.path = segments[:-1]
         self.name = segments[-1]
-        self.field = field
+
+        assert (iface and fieldname) or field
+        if field:
+            self.iface = None
+            self.field = field
+        else:
+            self.iface = iface
+            self.field = iface[fieldname]
+
         self._converter = converter
 
     def __get__(self, instance, cls):
@@ -80,33 +93,33 @@ class Video(object):
     Can be constructed from both a CMS object and a BC API result dict.
     """
 
-    id = dictproperty('id', zope.schema.TextLine(readonly=True))
-    title = dictproperty('name', IVideo['title'])
-    teaserText = dictproperty('description', IVideo['teaserText'])
+    id = dictproperty('id', field=zope.schema.TextLine(readonly=True))
+    title = dictproperty('name', IVideo, 'title')
+    teaserText = dictproperty('description', IVideo, 'teaserText')
 
-    authorships = dictproperty('custom_fields/authors', IVideo['authorships'],
-                               'AuthorshipsConverter')
+    authorships = dictproperty('custom_fields/authors', IVideo, 'authorships',
+                               converter='AuthorshipsConverter')
     commentsAllowed = dictproperty(
-        'custom_fields/allow_comments', IVideo['commentsAllowed'])
+        'custom_fields/allow_comments', IVideo, 'commentsAllowed')
     commentsPremoderate = dictproperty(
-        'custom_fields/premoderate_comments', IVideo['commentsPremoderate'])
-    banner = dictproperty('custom_fields/banner', IVideo['banner'])
-    banner_id = dictproperty('custom_fields/banner-id', IVideo['banner_id'])
+        'custom_fields/premoderate_comments', IVideo, 'commentsPremoderate')
+    banner = dictproperty('custom_fields/banner', IVideo, 'banner')
+    banner_id = dictproperty('custom_fields/banner-id', IVideo, 'banner_id')
     dailyNewsletter = dictproperty(
-        'custom_fields/newsletter', IVideo['dailyNewsletter'])
+        'custom_fields/newsletter', IVideo, 'dailyNewsletter')
     has_recensions = dictproperty(
-        'custom_fields/recensions', IVideo['has_recensions'])
-    keywords = dictproperty('custom_fields/cmskeywords', IVideo['keywords'],
-                            'KeywordsConverter')
-    product = ProductProperty('custom_fields/produkt-id', IVideo['product'],
-                              'ProductConverter')
-    ressort = dictproperty('custom_fields/ressort', IVideo['ressort'])
-    serie = dictproperty('custom_fields/serie', IVideo['serie'],
-                         'SeriesConverter')
-    subtitle = dictproperty('long_description', IVideo['subtitle'])
-    supertitle = dictproperty('custom_fields/supertitle', IVideo['supertitle'])
+        'custom_fields/recensions', IVideo, 'has_recensions')
+    keywords = dictproperty('custom_fields/cmskeywords', IVideo, 'keywords',
+                            converter='KeywordsConverter')
+    product = ProductProperty('custom_fields/produkt-id', IVideo, 'product',
+                              converter='ProductConverter')
+    ressort = dictproperty('custom_fields/ressort', IVideo, 'ressort')
+    serie = dictproperty('custom_fields/serie', IVideo, 'serie',
+                         converter='SeriesConverter')
+    subtitle = dictproperty('long_description', IVideo, 'subtitle')
+    supertitle = dictproperty('custom_fields/supertitle', IVideo, 'supertitle')
     video_still_copyright = dictproperty(
-        'custom_fields/credit', IVideo['video_still_copyright'])
+        'custom_fields/credit', IVideo, 'video_still_copyright')
 
     def __init__(self):
         self.data = {}
@@ -115,10 +128,17 @@ class Video(object):
     def from_cms(cls, video):
         instance = cls()
         instance.data['id'] = video.brightcove_id
+        adapters = {}
         for prop in instance._dictproperties:
             if prop.field.readonly:
                 continue
-            setattr(instance, prop.__name__, prop.field.get(video))
+            wrapped = video
+            if prop.iface:
+                wrapped = adapters.get(prop.iface)
+                if wrapped is None:
+                    wrapped = prop.iface(video)
+                    adapters[prop.iface] = wrapped
+            setattr(instance, prop.__name__, prop.field.get(wrapped))
         return instance
 
     @property
