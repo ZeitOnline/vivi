5b3ec47eceebedd10fd950c3830626faa7f166b4
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-07-06 10:32:36 +0200

    ZON-3983: Radically simplify the implementation

--- core/src/zeit/brightcove/convert2.py
+++ core/src/zeit/brightcove/convert2.py
@@ -1,199 +1,77 @@
-from zeit.content.video.interfaces import IVideo
 from zope.cachedescriptors.property import Lazy as cachedproperty
-import grokcore.component as grok
-import zeit.cms.interfaces
-import zope.interface
-import zope.schema
-
-
-class dictproperty(object):
-    """Maps a property from a CMS name to a BC name, and performs type
-    conversion, using an associated CMS schema field."""
-
-    # Might be cleaner if we figured this out by ourselves, but that would be
-    # mechanically finicky and doesn't seem worth the hassle. So we simply have
-    # Video.properties set it for us instead.
-    __name__ = None
-
-    def __init__(self, bc_name, iface=None, fieldname=None, field=None,
-                 converter=None):
-        """The value is stored in the instance's data dict, under the key
-        ``bc_name`` (may contain '/' to denote nested dicts).
-
-        Pass either iface and fieldname, or for special cases a
-        zope.schema.Field instance, so we can look up type conversion
-        accordingly, and access CMS values that reside in adapters.
-
-        You can pass the class name of an ITypeConverter (to prevent source
-        code ordering / import time issues), otherwise one will be looked up
-        for the given field.
-        """
-        self.bc_name = bc_name
-        segments = self.bc_name.split('/')
-        self.path = segments[:-1]
-        self.name = segments[-1]
-
-        assert (iface and fieldname) or field
-        if field:
-            self.iface = None
-            self.field = field
-        else:
-            self.iface = iface
-            self.field = iface[fieldname]
-
-        self._converter = converter
-
-    def __get__(self, instance, cls):
-        if instance is None:
-            return self
-        data = instance.data
-        for x in self.path:
-            data = data.get(x, {})
-        try:
-            value = data[self.name]
-        except KeyError:
-            return self.field.default
-        return self.converter.to_cms(value)
-
-    def __set__(self, instance, value):
-        if self.field.readonly:
-            raise AttributeError('Cannot set %s', self.bc_name)
-        data = instance.data
-        for x in self.path:
-            data = data.setdefault(x, {})
-        data[self.name] = self.converter.to_bc(value)
-
-    @cachedproperty
-    def converter(self):
-        if self._converter is None:
-            return ITypeConverter(self.field)
-        else:
-            return globals()[self._converter](self.field)
-
-
-class ProductProperty(dictproperty):
-
-    def __get__(self, instance, cls):
-        if instance is None:
-            return self
-        if (not instance.data.get('custom_fields', {}).get('produkt-id') and
-                instance.data.get('reference_id')):
-            return zeit.cms.content.sources.PRODUCT_SOURCE(None).find(
-                'Reuters')  # XXX Magic hard-coded defaults.
-        return super(ProductProperty, self).__get__(instance, cls)
-
-
-class RelatedProperty(dictproperty):
-
-    RELATED_COUNT = range(1, 6)
-
-    def __get__(self, instance, cls):
-        if instance is None:
-            return self
-        custom = instance.data.get('custom_fields', {})
-        if not custom:
-            return self.field.default
-        return self.converter.to_cms(
-            [custom.get('ref_link%s' % i) for i in self.RELATED_COUNT])
-
-    def __set__(self, instance, value):
-        if not value:
-            value = ()
-        custom = instance.data.setdefault('custom_fields', {})
-        for i in self.RELATED_COUNT:
-            try:
-                item = value[i - 1]
-            except IndexError:
-                custom['ref_link%s' % i] = ''
-                custom['ref_title%s' % i] = ''
-            else:
-                id, title = self.converter.to_bc(item)
-                custom['ref_link%s' % i] = id
-                custom['ref_title%s' % i] = title
+import zeit.cms.content.interfaces
+import zeit.cms.related.interfaces
 
 
 class Video(object):
-    """Converts video data between CMS and Brightcove.
-
-    * Stores json data received from the Brightcove API in a nested dict
-    * Presents it as properties with CMS names and with CMS types for reading
-    * On write, those properties convert to BC names and BC types
-
-    Can be constructed from both a CMS object and a BC API result dict.
-    """
-
-    id = dictproperty('id', field=zope.schema.TextLine(readonly=True))
-    title = dictproperty('name', IVideo, 'title')
-    teaserText = dictproperty('description', IVideo, 'teaserText')
-
-    authorships = dictproperty('custom_fields/authors', IVideo, 'authorships',
-                               converter='AuthorshipsConverter')
-    commentsAllowed = dictproperty(
-        'custom_fields/allow_comments', IVideo, 'commentsAllowed')
-    commentsPremoderate = dictproperty(
-        'custom_fields/premoderate_comments', IVideo, 'commentsPremoderate')
-    banner = dictproperty('custom_fields/banner', IVideo, 'banner')
-    banner_id = dictproperty('custom_fields/banner-id', IVideo, 'banner_id')
-    dailyNewsletter = dictproperty(
-        'custom_fields/newsletter', IVideo, 'dailyNewsletter')
-    has_recensions = dictproperty(
-        'custom_fields/recensions', IVideo, 'has_recensions')
-    keywords = dictproperty('custom_fields/cmskeywords', IVideo, 'keywords',
-                            converter='KeywordsConverter')
-    product = ProductProperty('custom_fields/produkt-id', IVideo, 'product',
-                              converter='ProductConverter')
-    related = RelatedProperty(
-        'custom_fields/ref_*',
-        zeit.cms.related.interfaces.IRelatedContent, 'related',
-        converter='RelatedConverter')
-    ressort = dictproperty('custom_fields/ressort', IVideo, 'ressort')
-    serie = dictproperty('custom_fields/serie', IVideo, 'serie',
-                         converter='SeriesConverter')
-    subtitle = dictproperty('long_description', IVideo, 'subtitle')
-    supertitle = dictproperty('custom_fields/supertitle', IVideo, 'supertitle')
-    video_still_copyright = dictproperty(
-        'custom_fields/credit', IVideo, 'video_still_copyright')
 
     def __init__(self):
         self.data = {}
 
     @classmethod
-    def from_cms(cls, video):
+    def from_cms(cls, cmsobj):
         instance = cls()
-        instance.data['id'] = video.brightcove_id
-        adapters = {}
-        for prop in instance._dictproperties:
-            if prop.field.readonly:
-                continue
-            wrapped = video
-            if prop.iface:
-                wrapped = adapters.get(prop.iface)
-                if wrapped is None:
-                    wrapped = prop.iface(video)
-                    adapters[prop.iface] = wrapped
-            setattr(instance, prop.__name__, prop.field.get(wrapped))
+        data = instance.data
+        data['custom_fields'] = custom = {}
+
+        # This is coupled to our IAddLocation and importing implementation.
+        data['id'] = cmsobj.__name__
+
+        data['name'] = cmsobj.title
+        data['description'] = cmsobj.teaserText
+        data['long_description'] = cmsobj.subtitle
+
+        custom['authors'] = ' '.join(
+            x.target.uniqueId for x in cmsobj.authorships)
+        custom['allow_comments'] = cls.bc_bool(cmsobj.commentsAllowed)
+        custom['premoderate_comments'] = cls.bc_bool(
+            cmsobj.commentsPremoderate)
+        custom['banner'] = cmsobj.banner
+        custom['banner_id'] = cmsobj.banner
+        custom['newsletter'] = cmsobj.dailyNewsletter
+        custom['recensions'] = cmsobj.has_recensions
+        custom['produkt-id'] = cmsobj.product.id if cmsobj.product else None
+        custom['cmskeywords'] = u';'.join(x.code for x in cmsobj.keywords)
+        custom['ressort'] = cmsobj.ressort
+        custom['serie'] = cmsobj.serie.serienname if cmsobj.serie else None
+        custom['supertitle'] = cmsobj.supertitle
+        custom['credit'] = cmsobj.video_still_copyright
+
+        related = zeit.cms.related.interfaces.IRelatedContent(cmsobj).related
+        for i in range(1, 6):
+            try:
+                item = related[i - 1]
+            except IndexError:
+                custom['ref_link%s' % i] = ''
+                custom['ref_title%s' % i] = ''
+            else:
+                metadata = zeit.cms.content.interfaces.ICommonMetadata(
+                    item, None)
+                if metadata and metadata.teaserTitle:
+                    title = metadata.teaserTitle
+                else:
+                    title = u'unknown'
+                custom['ref_link%s' % i] = item.uniqueId
+                custom['ref_title%s' % i] = title
+
         return instance
+
+    @staticmethod
+    def bc_bool(value):
+        return '1' if value else '0'
 
     @property
     def write_data(self):
-        """Copy of our data dict, with all readonly properties removed."""
-        data = self.data.copy()  # first-level copy is sufficient right now.
-        for prop in self._dictproperties:
-            if prop.field.readonly:
-                data.pop(prop.bc_name, None)
+        data = {}
+        for key in [
+                'custom_fields', 'name', 'description', 'long_description']:
+            if key in self.data:
+                data[key] = self.data[key]
         return data
 
-    @property
-    def _dictproperties(self):
-        cls = type(self)
-        result = []
-        for propname in dir(cls):
-            prop = getattr(cls, propname)
-            if not isinstance(prop, dictproperty):
-                continue
-            prop.__name__ = propname
-            result.append(prop)
-        return result
+    @cachedproperty
+    def id(self):
+        return str(self.data.get('id', ''))
 
     def __repr__(self):
         return '<%s.%s %s>' % (
@@ -212,123 +90,3 @@ def publish_on_checkin(context, event):
     # publishing (to update XML references etc.)
     if not event.publishing:
         zeit.cms.workflow.interfaces.IPublish(context).publish()
-
-
-class ITypeConverter(zope.interface.Interface):
-
-    def to_bc(value):
-        pass
-
-    def to_cms(value):
-        pass
-
-
-class Converter(grok.Adapter):
-
-    grok.baseclass()
-    grok.implements(ITypeConverter)
-
-    def to_bc(self, value):
-        return value
-
-    def to_cms(self, value):
-        return value
-
-
-class DefaultPassthroughConverter(Converter):
-
-    grok.context(zope.interface.Interface)
-
-
-class BoolConverter(Converter):
-
-    grok.context(zope.schema.Bool)
-
-    def to_bc(self, value):
-        return '1' if value else '0'
-
-    def to_cms(self, value):
-        return value == '1'
-
-
-class AuthorshipsConverter(Converter):
-
-    # used explicitly, since we cannot register an adapter for a field instance
-    grok.baseclass()
-
-    SEPARATOR = u' '
-
-    def to_bc(self, value):
-        return self.SEPARATOR.join(x.target.uniqueId for x in value)
-
-    def to_cms(self, value):
-        if not value:
-            return self.context.default
-        authors = [zeit.cms.interfaces.ICMSContent(x, None)
-                   for x in value.split(self.SEPARATOR)]
-        return tuple([x for x in authors if x is not None])
-
-
-class KeywordsConverter(Converter):
-
-    grok.baseclass()
-
-    SEPARATOR = u';'
-
-    def to_bc(self, value):
-        return self.SEPARATOR.join(x.code for x in value)
-
-    def to_cms(self, value):
-        if not value:
-            return self.context.default
-        whitelist = zope.component.getUtility(
-            zeit.cms.tagging.interfaces.IWhitelist)
-        keywords = [whitelist.get(code)
-                    for code in value.split(self.SEPARATOR)]
-        return tuple([x for x in keywords if x is not None])
-
-
-class ProductConverter(Converter):
-
-    grok.baseclass()
-
-    def to_bc(self, value):
-        return value.id if value else None
-
-    def to_cms(self, value):
-        return self.context.source(None).find(value)
-
-
-class SeriesConverter(Converter):
-
-    grok.baseclass()
-
-    def to_bc(self, value):
-        return value.serienname if value else None
-
-    def to_cms(self, value):
-        return self.context.source(None).find(value)
-
-
-class RelatedConverter(Converter):
-
-    grok.baseclass()
-
-    # Our method signatures are not symmetrical (single- vs multi-valued),
-    # but this way it's rather convenient to use in RelatedProperty.
-
-    def to_bc(self, value):
-        metadata = zeit.cms.content.interfaces.ICommonMetadata(value, None)
-        if metadata and metadata.teaserTitle:
-            title = metadata.teaserTitle
-        else:
-            title = u'unknown'
-        return value.uniqueId, title
-
-    def to_cms(self, value):
-        result = []
-        for item in value:
-            if not item:
-                continue  # Micro-optimization
-            result.append(zeit.cms.interfaces.ICMSContent(item, None))
-        return tuple(result)
--- core/src/zeit/brightcove/tests/test_convert2.py
+++ core/src/zeit/brightcove/tests/test_convert2.py
@@ -16,26 +16,8 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
         cms.title = u'title'
         cms.teaserText = u'teaser'
         bc = BCVideo.from_cms(cms)
-        self.assertEqual('title', bc.title)
         self.assertEqual('title', bc.data['name'])
-        self.assertEqual('teaser', bc.teaserText)
         self.assertEqual('teaser', bc.data['description'])
-
-    def test_setting_readonly_field_raises(self):
-        bc = BCVideo()
-        with self.assertRaises(AttributeError):
-            bc.id = 'id'
-
-    def test_missing_data_returns_field_default(self):
-        bc = BCVideo()
-        self.assertEqual(None, bc.ressort)
-
-    def test_bc_names_with_slash_denote_nested_dict(self):
-        cms = CMSVideo()
-        cms.ressort = u'Deutschland'
-        bc = BCVideo.from_cms(cms)
-        self.assertEqual('Deutschland', bc.ressort)
-        self.assertEqual('Deutschland', bc.data['custom_fields']['ressort'])
 
     def test_readonly_fields_are_removed_for_writing(self):
         bc = BCVideo()
@@ -46,7 +28,6 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
         cms = CMSVideo()
         cms.commentsAllowed = True
         bc = BCVideo.from_cms(cms)
-        self.assertEqual(True, bc.commentsAllowed)
         self.assertEqual('1', bc.data['custom_fields']['allow_comments'])
 
     def test_converts_authors(self):
@@ -63,8 +44,6 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
         self.assertEqual(
             'http://xml.zeit.de/a1 http://xml.zeit.de/a2',
             bc.data['custom_fields']['authors'])
-        self.assertEqual(
-            (self.repository['a1'], self.repository['a2']), bc.authorships)
 
     def test_converts_keywords(self):
         cms = CMSVideo()
@@ -73,7 +52,6 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
         self.assertEqual(
             'staatsanwaltschaft;parlament',
             bc.data['custom_fields']['cmskeywords'])
-        self.assertEqual(cms.keywords, bc.keywords)
 
     def test_converts_product(self):
         cms = CMSVideo()
@@ -81,12 +59,6 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
             'TEST')
         bc = BCVideo.from_cms(cms)
         self.assertEqual('TEST', bc.data['custom_fields']['produkt-id'])
-        self.assertEqual(cms.product, bc.product)
-
-    def test_product_defaults_to_reuters(self):
-        bc = BCVideo()
-        bc.data['reference_id'] = '1234'
-        self.assertEqual('Reuters', bc.product.id)
 
     def test_converts_serie(self):
         cms = CMSVideo()
@@ -94,7 +66,6 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
             None).find('erde/umwelt')
         bc = BCVideo.from_cms(cms)
         self.assertEqual('erde/umwelt', bc.data['custom_fields']['serie'])
-        self.assertEqual(cms.serie, bc.serie)
 
     def test_converts_related(self):
         cms = CMSVideo()
@@ -106,7 +77,6 @@ class VideoTest(zeit.cms.testing.FunctionalTestCase,
         self.assertEqual(
             'http://xml.zeit.de/online/2007/01/eta-zapatero',
             bc.data['custom_fields']['ref_link1'])
-        self.assertEqual(related.related, bc.related)
 
 
 class SaveTest(zeit.cms.testing.FunctionalTestCase):
