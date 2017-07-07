f33365d2653a002f0e9079642467a017f1eb6505
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-07-07 09:54:49 +0200

    MAINT: Add link to TMS to keyword widget

--- core/src/zeit/retresco/browser/widget.py
+++ core/src/zeit/retresco/browser/widget.py
@@ -1,4 +1,7 @@
+import urlparse
 import zeit.cms.tagging.browser.widget
+import zeit.retresco.interfaces
+import zope.component
 
 
 class RetrescoTagWidget(zeit.cms.tagging.browser.widget.Widget):
@@ -8,3 +11,8 @@ class RetrescoTagWidget(zeit.cms.tagging.browser.widget.Widget):
     again.
 
     """
+
+    @property
+    def tms_host(self):
+        tms = zope.component.getUtility(zeit.retresco.interfaces.ITMS)
+        return urlparse.urlparse(tms.url).netloc
