3bc19e8ad4d410868fdccf16e2cdd2d339352a3b
Knut Goetz <Knut.Goetz@zeit.de>
2017-07-10 14:34:20 +0200

    ARBEIT-1: Add skeleton for our new vivi-package

---
+++ core/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zeit.arbeit',
+    version='0.1dev0',
+    author='Zeit Online',
+    author_email='zon-backend@zeit.de',
+    url='http://www.zeit.de/',
+    description="vivi ZAR Content-Type extensions ",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='BSD',
+    namespace_packages=['zeit'],
+    install_requires=[
+        'setuptools',
+        'zope.component',
+        'zope.interface',
+    ],
+)
