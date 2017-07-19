c891595ec96521dab2fc9f1ab31f17b3339e0373
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-07-19 15:37:27 +0200

    ZON-3994: Bump version (new API)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def project_path(*names):
 
 setup(
     name='zeit.retresco',
-    version='1.10.6.dev0',
+    version='1.11.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
