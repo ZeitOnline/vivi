b8db8a1b2c1aaf22a2d1205e2c36c961db075ab8
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-07-26 13:27:32 +0200

    ZON-4007: Fix test setup

--- core/src/zeit/campus/testing.py
+++ core/src/zeit/campus/testing.py
@@ -8,7 +8,7 @@ import zeit.cms.testing
 import zeit.content.article.testing
 import zeit.content.gallery.testing
 import zeit.content.link.testing
-import zeit.push
+import zeit.push.testing
 import zope.component
 import zope.interface
 
@@ -24,16 +24,19 @@ ZCML_LAYER = zeit.cms.testing.ZCMLLayer(
     'ftesting.zcml', product_config=(
         product_config +
         zeit.cms.testing.cms_product_config +
-        zeit.push.product_config +
+        zeit.push.testing.product_config +
         zeit.content.article.testing.product_config +
         zeit.content.cp.testing.product_config +
         zeit.content.gallery.testing.product_config +
         zeit.content.link.testing.product_config))
 
+PUSH_LAYER = zeit.push.testing.UrbanairshipTemplateLayer(
+    name='UrbanairshipTemplateLayer', bases=(ZCML_LAYER,))
+
 
 class Layer(plone.testing.Layer):
 
-    defaultBases = (ZCML_LAYER,)
+    defaultBases = (PUSH_LAYER,)
 
     def testSetUp(self):
         with zeit.cms.testing.site(self['functional_setup'].getRootFolder()):
