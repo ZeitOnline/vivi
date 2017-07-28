a3cc0484ac40143f72f7b3ebf0aef73cdd147539
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-07-28 10:10:27 +0200

    ZON-4010: Render template before attempting to parse it as json (belongs to commit:ca57efc)
    Templates that use jinja control flow are not valid json until they are rendered

--- src/zeit/push/browser/mobile.py
+++ src/zeit/push/browser/mobile.py
@@ -1,5 +1,6 @@
-import logging
 import json
+import logging
+import zeit.content.text.jinja
 import zeit.push.interfaces
 
 log = logging.getLogger(__name__)
@@ -14,7 +15,8 @@ class FindTitle(object):
         template = zeit.push.interfaces.PAYLOAD_TEMPLATE_SOURCE.factory.find(
             name)
         try:
-            return json.loads(template.text)['default_title']
+            result = template(zeit.content.text.jinja.MockDict())
+            return json.loads(result)['default_title']
         except:
             log.debug('No default title for %s', name, exc_info=True)
             return ''
