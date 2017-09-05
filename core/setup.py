7b2af2dc94dba104b1169809a6b926a4cfd2b854
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-09-05 10:25:13 +0200

    Preparing release 1.2.2

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.arbeit',
-    version='1.2.2.dev0',
+    version='1.2.2',
     author='Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
