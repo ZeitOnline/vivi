7d9ca88d6aaac5ed5f6196607efa34b78a38d13a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-10-02 15:46:58 +0200

    ZON-3409: Make test spelling consistent with other browser tests

--- core/src/zeit/workflow/json/tests.py
+++ core/src/zeit/workflow/json/tests.py
@@ -1,14 +1,9 @@
-from zeit.cms.workflow.interfaces import PRIORITY_DEFAULT
-import celery.states
 import json
-import mock
 import transaction
 import uuid
 import zeit.cms.testing
 import zeit.workflow.interfaces
 import zeit.workflow.testing
-import zope.security.management
-import zope.testbrowser.testing
 
 
 class PublishJSONTest(zeit.cms.testing.BrowserTestCase):
@@ -20,17 +15,16 @@ class PublishJSONTest(zeit.cms.testing.BrowserTestCase):
         self.browser.handleErrors = False
 
     def call_json(self, url):
-        zope.security.management.endInteraction()
         self.browser.open(url)
         return json.loads(self.browser.contents)
 
     def enable_publish(self, unique_id):
-        zope.security.management.endInteraction()
-        zeit.cms.testing.create_interaction()
-        somalia = zeit.cms.interfaces.ICMSContent(unique_id)
-        workflow = zeit.workflow.interfaces.IContentWorkflow(somalia)
-        workflow.urgent = True
-        transaction.commit()
+        with zeit.cms.testing.site(self.getRootFolder()):
+            with zeit.cms.testing.interaction():
+                somalia = zeit.cms.interfaces.ICMSContent(unique_id)
+                workflow = zeit.workflow.interfaces.IContentWorkflow(somalia)
+                workflow.urgent = True
+                transaction.commit()
 
     def test_negative_can_publish_should_return_false(self):
         result = self.call_json(
