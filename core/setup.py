8db21929d75924baf456251559446fce8bdc15da
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-10-19 12:59:31 +0200

    Back to development: 2.16.2

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='zeit.edit',
-    version='2.16.1',
+    version='2.16.2.dev0',
     description="Vivi Editor",
     long_description='',
     keywords='',
