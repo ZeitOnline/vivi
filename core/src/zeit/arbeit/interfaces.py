9f076538a9e0052134def9db231b5b89e48ed919
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-10-19 11:27:39 +0200

    MAINT: Move jobbox module to z.c.modules and z.c.article

--- core/src/zeit/arbeit/interfaces.py
+++ core/src/zeit/arbeit/interfaces.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.interfaces
 import zeit.cms.section.interfaces
 import zeit.cms.repository.interfaces
-import zeit.cms.content.sources
 import zeit.content.article.interfaces
 import zeit.content.cp.interfaces
 import zeit.content.infobox.interfaces
-import zope.schema
 
 
 class IZARSection(zeit.cms.section.interfaces.ISection):
@@ -43,15 +40,3 @@ class IZARInfobox(
         zeit.content.infobox.interfaces.IInfobox,
         zeit.cms.section.interfaces.ISectionMarker):
     pass
-
-
-JOBBOX_TICKER_SOURCE = zeit.cms.content.sources.JobboxTickerSource(
-    zeit.content.article.interfaces.IArticle)
-
-
-class IJobboxTicker(zeit.edit.interfaces.IBlock):
-
-    jobbox_ticker = zope.schema.Choice(
-        title=_('Jobbox ticker'),
-        required=True,
-        source=JOBBOX_TICKER_SOURCE)
