840a1f98531bc2d1b8823007ba9fcb2fcc0f9fef
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-10-20 14:33:31 +0200

    FIX: Enable sending no-cache headers for the article/cp main edit view

--- core/src/zeit/edit/browser/editor.py
+++ core/src/zeit/edit/browser/editor.py
@@ -1,8 +1,9 @@
 from zeit.cms.i18n import MessageFactory as _
 from zope.browserpage import ViewPageTemplateFile
+import zeit.cms.browser.view
 
 
-class Editor(object):
+class Editor(zeit.cms.browser.view.Base):
 
     render = ViewPageTemplateFile('editor.pt')
     title = _('Edit')
