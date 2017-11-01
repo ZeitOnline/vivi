91b5258e05f2b89ed594d2a04a53448374551986
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-11-01 10:12:12 +0100

    Preparing release 2.17.0

--- core/setup.py
+++ core/setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='zeit.edit',
-    version='2.17.0.dev0',
+    version='2.17.0',
     description="Vivi Editor",
     long_description='',
     keywords='',
