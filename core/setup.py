99f94eadb133d8692a21574d7b5a421f49a249eb
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-11-01 14:49:10 +0100

    Back to development: 2.10.4

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.connector',
-    version='2.10.3',
+    version='2.10.4.dev0',
     author='Tomas Zerolo, gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
