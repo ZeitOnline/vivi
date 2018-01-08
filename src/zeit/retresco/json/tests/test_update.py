78f1516deff61987c9876a4cfc78ff156c4f3428
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-01-08 09:36:26 +0100

    MAINT: Remove superfluous test setup after zeit.cms got smarter

--- src/zeit/retresco/json/tests/test_update.py
+++ src/zeit/retresco/json/tests/test_update.py
@@ -2,22 +2,26 @@ import json
 import mock
 import urllib2
 import zeit.cms.interfaces
+import zeit.cms.testing
 import zeit.retresco.testing
-import zope.testbrowser.testing
 
 
 class TMSUpdateRequestTest(zeit.cms.testing.BrowserTestCase):
 
     layer = zeit.retresco.testing.ZCML_LAYER
 
+    def setUp(self):
+        super(TMSUpdateRequestTest, self).setUp()
+        self.browser = zeit.cms.testing.Browser()
+
     def test_endpoint_avoids_get(self):
-        b = zope.testbrowser.testing.Browser()
+        b = self.browser
         with self.assertRaisesRegexp(urllib2.HTTPError,
                                      'HTTP Error 405: Method Not Allowed'):
             b.open('http://localhost/@@update_keywords')
 
     def test_endpoint_rejects_post_without_doc_ids(self):
-        b = zope.testbrowser.testing.Browser()
+        b = self.browser
         with self.assertRaisesRegexp(urllib2.HTTPError,
                                      'HTTP Error 400: Bad Request'):
             b.post('http://localhost/@@update_keywords', '')
@@ -27,7 +31,7 @@ class TMSUpdateRequestTest(zeit.cms.testing.BrowserTestCase):
                    '{"foo" : "bar"}', 'application/x-javascript')
 
     def test_endpoint_creates_async_job(self):
-        b = zope.testbrowser.testing.Browser()
+        b = self.browser
         with mock.patch('zeit.retresco.update.index') as index:
             b.post('http://localhost/@@update_keywords',
                    '{"doc_ids" : ['
@@ -36,13 +40,11 @@ class TMSUpdateRequestTest(zeit.cms.testing.BrowserTestCase):
                    'application/x-javascript')
             self.assertEquals({'message': 'OK'}, json.loads(b.contents))
             self.assertEquals('200 Ok', b.headers.getheader('status'))
-
-            with zeit.cms.testing.site(self.getRootFolder()):
-                self.assertEqual(2, index.call_count)
-                self.assertEqual(
-                    zeit.cms.interfaces.ICMSContent(
-                        'http://xml.zeit.de/online/2007/01/Somalia'),
-                    index.call_args[0][0])
-                self.assertEqual(
-                    {'enrich': True, 'publish': True},
-                    index.call_args[1])
+            self.assertEqual(2, index.call_count)
+            self.assertEqual(
+                zeit.cms.interfaces.ICMSContent(
+                    'http://xml.zeit.de/online/2007/01/Somalia'),
+                index.call_args[0][0])
+            self.assertEqual(
+                {'enrich': True, 'publish': True},
+                index.call_args[1])
