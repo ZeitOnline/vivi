e8afb110cfff2fbb515f9d615c2e2d312e7dd569
Tom Lazar <tomster@pyfidelity.com>
2018-01-23 15:25:41 +0100

    Preparing release 1.15.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def project_path(*names):
 
 setup(
     name='zeit.retresco',
-    version='1.15.9.dev0',
+    version='1.15.9',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
