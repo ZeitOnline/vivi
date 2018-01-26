73d50f134570e08e6cc192d20e3a4c9cd38e1cd5
Knut Goetz <Knut.Goetz@zeit.de>
2018-01-26 15:55:14 +0100

    Preparing release 2.12.4

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.brightcove',
-    version='2.12.4.dev0',
+    version='2.12.4',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
