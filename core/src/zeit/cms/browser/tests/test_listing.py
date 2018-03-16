d8ef023ec9386e24ee921e6d1083f25bb7290f40
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-03-16 14:21:40 +0100

    BUG-717: Ignore any errors in listing columns

--- core/src/zeit/cms/browser/tests/test_listing.py
+++ core/src/zeit/cms/browser/tests/test_listing.py
@@ -1,3 +1,4 @@
+import mock
 import zeit.cms.browser.interfaces
 import zeit.cms.browser.listing
 import zeit.cms.content.interfaces
@@ -26,3 +27,21 @@ class HitColumnTest(zeit.cms.testing.ZeitCmsTestCase):
             zeit.cms.browser.interfaces.IListRepresentation)
         column = zeit.cms.browser.listing.HitColumn()
         self.assertEquals((19, 5), column.getSortKey(listrep, formatter=None))
+
+
+class ListingTest(zeit.cms.testing.ZeitCmsBrowserTestCase):
+
+    def test_columns_ignore_exceptions(self):
+        with mock.patch(
+                'zeit.cms.testcontenttype.testcontenttype.'
+                'ExampleContentType.authors', new=mock.PropertyMock) as author:
+            author.side_effect = RuntimeError('provoked')
+            b = self.browser
+            b.handleErrors = False
+            with self.assertNothingRaised():
+                b.open('http://localhost/++skin++vivi/repository')
+            # Check that the cells are present but empty.
+            self.assertEllipsis(
+                '...<td> <span class="filename">testcontent</span> </td>'
+                ' <td> 2008 ... </td> <td> </td> <td> </td> <td> </td>...',
+                b.contents)
