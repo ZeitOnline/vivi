1154e6099035881342e4ef21ca6712f71e862064
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-03-19 15:13:29 +0100

    MAINT: Use a hopefully less confusing error message placeholder

--- src/zeit/retresco/connection.py
+++ src/zeit/retresco/connection.py
@@ -207,7 +207,7 @@ class TMS(object):
             response.raise_for_status()
         except requests.exceptions.RequestException, e:
             status = getattr(e.response, 'status_code', 500)
-            body = getattr(e.response, 'text', '<body/>')
+            body = getattr(e.response, 'text', '(no error detail)')
             message = '{verb} {path} {error!r}\n{body}'.format(
                 verb=verb, path=path, error=e, body=body)
             if status < 500:
