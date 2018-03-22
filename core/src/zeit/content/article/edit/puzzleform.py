7fb678d721eb555c0f8a1ddec2eb9dda98f3e5ee
Knut Goetz <Knut.Goetz@zeit.de>
2018-03-22 10:11:08 +0100

    Use DAVConverterWrapper to determine the objects build from the source

--- core/src/zeit/content/article/edit/puzzleform.py
+++ core/src/zeit/content/article/edit/puzzleform.py
@@ -10,25 +10,12 @@ class PuzzleForm(zeit.content.article.edit.block.Block):
     grok.implements(zeit.content.article.edit.interfaces.IPuzzleForm)
     type = 'puzzleform'
 
-    _puzzle_type_id = zeit.cms.content.property.ObjectPathAttributeProperty(
-        '.', 'puzzle_type_id')
+    puzzle_type = zeit.cms.content.property.DAVConverterWrapper(
+        zeit.cms.content.property.ObjectPathAttributeProperty('.', 'id'),
+        zeit.content.article.edit.interfaces.IPuzzleForm['puzzle_type'])
 
     year = zeit.cms.content.property.ObjectPathAttributeProperty(
         '.', 'year', zeit.content.article.edit.interfaces.IPuzzleForm['year'])
-
-    @property
-    def puzzle_type(self):
-        source = zeit.content.article.edit.interfaces.IPuzzleForm[
-            'puzzle_type'].source(self)
-        for value in source:
-            if value.id == self._puzzle_type_id:
-                return value
-
-    @puzzle_type.setter
-    def puzzle_type(self, value):
-        if self._puzzle_type_id == value.id:
-            return
-        self._puzzle_type_id = value.id if value is not None else None
 
 
 class Factory(zeit.content.article.edit.block.BlockFactory):
