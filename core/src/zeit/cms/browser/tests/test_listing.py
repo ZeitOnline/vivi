96ba517995108465dd66d8b2532486ad060f95a4
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-03-22 08:54:12 +0100

    MAINT: Formulate test a little more realistically

--- core/src/zeit/cms/browser/tests/test_listing.py
+++ core/src/zeit/cms/browser/tests/test_listing.py
@@ -1,17 +1,28 @@
-import unittest
+import zeit.cms.browser.interfaces
+import zeit.cms.browser.listing
+import zeit.cms.content.interfaces
+import zeit.cms.interfaces
+import zeit.cms.testing
+import zope.component
+import zope.publisher.browser
 
 
-class HitColumnTest(unittest.TestCase):
+class HitColumnTest(zeit.cms.testing.ZeitCmsTestCase):
 
     def test_sort_key(self):
-        import zeit.cms.browser.listing
-
-        class TestAccessCounter(object):
+        class FakeAccessCounter(object):
             hits = 5
             total_hits = 19
 
-        column = zeit.cms.browser.listing.HitColumn(
-            getter=lambda i, f: i)
-        self.assertEquals((19, 5),
-                          column.getSortKey(TestAccessCounter(),
-                                            formatter=None))
+            def __init__(self, context):
+                pass
+
+        zope.component.getSiteManager().registerAdapter(
+            FakeAccessCounter, (zeit.cms.interfaces.ICMSContent,),
+            zeit.cms.content.interfaces.IAccessCounter)
+        listrep = zope.component.queryMultiAdapter(
+            (self.repository['testcontent'],
+             zope.publisher.browser.TestRequest()),
+            zeit.cms.browser.interfaces.IListRepresentation)
+        column = zeit.cms.browser.listing.HitColumn()
+        self.assertEquals((19, 5), column.getSortKey(listrep, formatter=None))
