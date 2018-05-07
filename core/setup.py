cb641701b902b3108eeeeca11c9bd57f2a6f7592
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-05-07 10:10:23 +0200

    Back to development: 2.1.9

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='2.1.8',
+    version='2.1.9.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
