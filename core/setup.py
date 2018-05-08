a729b5a1c4a95180bdea0f5a8f1f288607d38066
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-05-08 16:19:04 +0200

    Preparing release 2.12.6

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.brightcove',
-    version='2.12.6.dev0',
+    version='2.12.6',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
