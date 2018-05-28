879045856a3be9b7be234c054afdb2a975734744
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-05-28 13:45:12 +0200

    MAINT: Move autocomplete source query view here from zeit.find, generalize using ISourceQueryURL lookup mechanism

--- core/src/zeit/cms/tagging/browser/autocomplete.py
+++ core/src/zeit/cms/tagging/browser/autocomplete.py
@@ -1,41 +1,9 @@
-from zeit.cms.i18n import MessageFactory as _
 import grokcore.component as grok
-import xml.sax.saxutils
 import zeit.cms.browser.interfaces
 import zeit.cms.browser.view
 import zeit.cms.tagging.interfaces
 import zeit.cms.tagging.source
 import zope.component.hooks
-import zope.formlib.interfaces
-import zope.i18n
-
-
-class AutocompleteSourceQuery(grok.MultiAdapter,
-                              zeit.cms.browser.view.Base):
-
-    # XXX copy&paste&tweak from zeit.find.browser.autocomplete, can this be
-    # refactored?
-
-    grok.adapts(
-        zeit.cms.tagging.source.IWhitelistSource,
-        zeit.cms.browser.interfaces.ICMSLayer)
-    grok.provides(zope.formlib.interfaces.ISourceQueryView)
-
-    def __init__(self, source, request):
-        self.source = source
-        self.request = request
-
-    def __call__(self):
-        return (
-            u'<input type="text" class="autocomplete" '
-            u'placeholder={placeholder} '
-            u'cms:autocomplete-source="{url}" />').format(
-            # XXX make use of ISourceQueryURL mechanism
-            url=self.url(
-                zope.component.hooks.getSite(), '@@zeit.cms.tagging.search'),
-            placeholder=xml.sax.saxutils.quoteattr(
-                zope.i18n.translate(_('Type to find entries ...'),
-                                    context=self.request)))
 
 
 class WhitelistSearch(zeit.cms.browser.view.JSON):
@@ -54,6 +22,17 @@ class WhitelistSearch(zeit.cms.browser.view.JSON):
             results = []
         return [dict(label=result.label, value=result.uniqueId)
                 for result in results]
+
+
+@grok.adapter(
+    zeit.cms.tagging.source.IWhitelistSource,
+    zeit.cms.browser.interfaces.ICMSLayer)
+@grok.implementer(zeit.cms.browser.interfaces.ISourceQueryURL)
+def WhitelistSearchURL(context, request):
+    base = zope.traversing.browser.absoluteURL(
+        zope.component.hooks.getSite(), request)
+    return (
+        base + '/@@zeit.cms.tagging.search')
 
 
 class LocationSearch(zeit.cms.browser.view.JSON):
