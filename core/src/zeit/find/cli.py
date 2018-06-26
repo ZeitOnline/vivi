07507be3b9b1d665ca53f36e1fe66f8817daba66
Andreas Zeidler <andreas.zeidler@zeit.de>
2018-06-26 15:04:06 +0200

    TMS-240: Allow search script to report the query & results.
    In addition search conditions are now specified using a <field>:<term>
    notation:
    
      $ work/zope/bin/search-solr --verbose "authors:Klaus Mustermann"
      $ work/zope/bin/search-elastic --verbose "fulltext:Klaus Lage"

--- core/src/zeit/find/cli.py
+++ core/src/zeit/find/cli.py
@@ -1,28 +1,41 @@
 from argparse import ArgumentParser
 from gocept.runner import once
+from logging import getLogger
+from operator import itemgetter
+from zeit.cms.interfaces import ICMSContent
 from zeit.find import search, elastic
+
+
+log = getLogger(__name__)
 
 
 def parse():
     parser = ArgumentParser(description='Elasticsearch debug client')
-    parser.add_argument('-f', '--fulltext', help='Fulltext search term')
-    parser.add_argument('-a', '--authors', help='Search author name')
-    return vars(parser.parse_args())
+    parser.add_argument('conditions', nargs='+', help='Search conditions')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='Report query & results')
+    args = parser.parse_args()
+    conditions = dict([c.split(':', 1) for c in args.conditions])
+    return args, conditions
+
+
+def perform_search(module, get_id):
+    args, conditions = parse()
+    query = module.query(**conditions)
+    if args.verbose:
+        log.info('using query: {}'.format(query))
+    response = module.search(query)
+    log.info('got {} results'.format(response.hits))
+    if args.verbose:
+        for idx, item in enumerate(response):
+            log.info('#{}: {}'.format(idx, get_id(item)))
 
 
 @once(principal='zope.manager')
 def search_solr():
-    args = parse()
-    query = search.query(**args)
-    print('using query: {}'.format(query))
-    response = search.search(query)
-    print('got {} results'.format(len(response)))
+    perform_search(search, itemgetter('uniqueId'))
 
 
 @once(principal='zope.manager')
 def search_elastic():
-    args = parse()
-    query = elastic.query(**args)
-    print('using query: {}'.format(query))
-    response = elastic.search(query)
-    print('got {} results'.format(response.hits))
+    perform_search(elastic, lambda item: ICMSContent(item).uniqueId)
