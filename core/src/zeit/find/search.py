06ea849d52e1e9a761d611579d21c45273754485
Andreas Zeidler <andreas.zeidler@zeit.de>
2018-06-28 11:59:49 +0200

    TMS-240: Add BBB import for `z.c.cp.automatic.SolrContentQuery`.

--- core/src/zeit/find/search.py
+++ core/src/zeit/find/search.py
@@ -1,4 +1,8 @@
 from zeit.find.elastic import search, query
 
 
-__all__ = 'search', 'query'
+# BBB import for `z.c.cp.automatic.SolrContentQuery`
+from zeit.find.solr import DEFAULT_RESULT_FIELDS
+
+
+__all__ = 'search', 'query', 'DEFAULT_RESULT_FIELDS'
