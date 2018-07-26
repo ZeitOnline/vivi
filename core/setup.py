f28c49c84391992340e41e019e3829882486508e
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-07-26 13:44:01 +0200

    Preparing release 1.29.0

--- core/setup.py
+++ core/setup.py
@@ -8,7 +8,7 @@ def project_path(*names):
 
 setup(
     name='zeit.retresco',
-    version='1.29.0.dev0',
+    version='1.29.0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
