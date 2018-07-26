0eb268f57c9fe98998b5c32497e7d16d3aade2c6
Knut Goetz <Knut.Goetz@zeit.de>
2018-07-26 09:45:17 +0200

    Fix using wrong setting

--- src/zeit/retresco/tagger.py
+++ src/zeit/retresco/tagger.py
@@ -229,7 +229,7 @@ class Tagger(zeit.cms.content.dav.DAVPropertiesAdapter):
     def links(self):
         tms = zope.component.getUtility(zeit.retresco.interfaces.ITMS)
         config = zope.app.appsetup.product.getProductConfiguration('zeit.cms')
-        live_prefix = config.get('live_prefix', 'https://www.zeit.de/')
+        live_prefix = config['live-prefix']
         keywords_with_link = tms.get_article_keywords(
             self.context,
             published=False)
