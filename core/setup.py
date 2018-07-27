f90c900177bbd3cdc2f55ae01baa097b90e0b072
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-07-27 09:23:37 +0200

    Back to development: 1.30.2

--- core/setup.py
+++ core/setup.py
@@ -8,7 +8,7 @@ def project_path(*names):
 
 setup(
     name='zeit.retresco',
-    version='1.30.1',
+    version='1.30.2.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
