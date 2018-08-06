bf2973fd157362c184d4e9a3df6c682a508b3818
Knut Goetz <Knut.Goetz@zeit.de>
2018-08-06 11:56:16 +0200

    Preparing release 1.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.magazin',
-    version='1.5.1.dev0',
+    version='1.5.1',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
