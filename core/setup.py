8a9094c54d0827fdc4f3a758823078ee2d1f2bd7
Knut Goetz <Knut.Goetz@zeit.de>
2018-08-20 10:51:23 +0200

    Back to development: 3.14.6

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.cms',
-    version='3.14.5',
+    version='3.14.6.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
