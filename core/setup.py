c3136c7c9ee2c8ad569833ded42585fb770d1b38
Knut Goetz <Knut.Goetz@zeit.de>
2018-08-24 11:06:47 +0200

    Back to development: 2.1.15

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='2.1.14',
+    version='2.1.15.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
