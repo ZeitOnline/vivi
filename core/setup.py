686c363e26da5233b4bf850788269244317b5e42
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-09-24 09:25:53 +0200

    ZON-3482: Fix test setup

--- core/setup.py
+++ core/setup.py
@@ -30,6 +30,7 @@ setup(
         'zeit.content.text',
         'zeit.content.video',
         'zeit.invalidate',
+        'zeit.push',
         'zeit.retresco>=1.19.0.dev0',
         'zeit.seo>=1.6.0.dev0',
         'zeit.vgwort>=2.4.0.dev0',
