e5adb717a93aa7122afe0de7cbfa4d5fa6d629e6
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-10-04 10:35:26 +0200

    Preparing release 2.2.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.securitypolicy',
-    version='2.2.9.dev0',
+    version='2.2.9',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
