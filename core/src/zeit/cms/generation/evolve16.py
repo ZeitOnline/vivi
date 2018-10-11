8629c2e227bbbd459ca9e0292df18bbdd916e7b1
Knut Goetz <Knut.Goetz@zeit.de>
2018-10-11 08:46:00 +0200

    pep8

--- core/src/zeit/cms/generation/evolve16.py
+++ core/src/zeit/cms/generation/evolve16.py
@@ -5,6 +5,7 @@ import zeit.cms.retractlog.retractlog
 def update(root):
     root['retractlog'] = zeit.cms.retractlog.retractlog.RetractLog()
 
+
 def evolve(context):
     """Remove the lovely.remotetask services."""
     zeit.cms.generation.do_evolve(context, update)
