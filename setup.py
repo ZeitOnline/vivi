dc1c377a5b6011b587413c586b33000e867ab5bc
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-10-17 14:03:53 +0200

    ZON-2694: Declare dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'xlrd',
         'zeit.brightcove',
         'zeit.calendar',
-        'zeit.cms>=3.9.0.dev0',
+        'zeit.cms>=3.19.0.dev0',
         'zeit.content.article',
         'zeit.content.cp >= 2.10.0.dev0',
         'zeit.content.image>=2.6.0.dev0',
