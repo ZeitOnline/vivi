b9342deaddd34827d9953c52e09d2b22cd06adff
Knut Goetz <Knut.Goetz@zeit.de>
2018-10-25 16:14:45 +0200

    ZON-4106: Replace doctest with unit test for copyrights view

--- core/src/zeit/content/image/browser/tests/test_copyright.py
+++ core/src/zeit/content/image/browser/tests/test_copyright.py
@@ -15,6 +15,18 @@ class TestCopyrights(zeit.cms.testing.BrowserTestCase,
         self.upload_primary_image('new-hampshire-artikel.jpg')
         self.save_imagegroup()
 
+    def bulk_change(self):
+        b = self.browser
+        b.open('http://localhost/++skin++cms/repository/imagegroup/'
+               '@@set-image-copyright')
+        b.getControl(name='form.copyrights.0..combination_00').value = (
+            'gocept')
+        b.getControl(name='form.copyrights.0..combination_01').displayValue = \
+            ['dpa']
+        b.getControl(name='form.copyrights.0..combination_03').value = (
+            'http://www.gocept.com/')
+        b.getControl('Set copyrights').click()
+
     def test_image_group_has_bulk_copyrights_link(self):
         b = self.browser
         b.open('http://localhost/++skin++cms/repository/imagegroup')
@@ -25,18 +37,20 @@ class TestCopyrights(zeit.cms.testing.BrowserTestCase,
             "++skin++cms/repository/imagegroup/@@set-image-copyright')",
             link.url)
 
-    def test_image_group_bulk_copyrights_are_added(self):
-        # Das scheint hier nicht zu funktionieren
+    def test_image_group_bulk_copyright_change_shows_message(self):
+        self.bulk_change()
         b = self.browser
-        b.open('http://localhost/++skin++cms/repository/imagegroup/'
-               '@@set-image-copyright')
-        b.getControl(name='form.copyrights.0..combination_00').value = (
-            'gocept')
-        b.getControl(name='form.copyrights.0..combination_01').displayValue = \
-            ['dpa']
-        b.getControl(name='form.copyrights.0..combination_03').value = (
-            'http://www.gocept.com/')
-        b.getControl('Add Copyrights').click()
-        b.getControl('Set copyrights').click()
         b.open('http://localhost/++skin++cms/repository/'
                'imagegroup/@@view.html')
+        self.assertIn('Copyright changed for: new-hampshire-artikel.jpg',
+                      b.contents)
+
+    def test_image_group_bulk_copyrights_are_set(self):
+        self.bulk_change()
+        image = zeit.cms.interfaces.ICMSContent(
+            'http://xml.zeit.de/imagegroup/new-hampshire-artikel.jpg')
+        metadata = zeit.content.image.interfaces.IImageMetadata(image)
+        self.assertEqual(
+            (u'gocept', u'dpa', None, 'http://www.gocept.com/', False),
+            metadata.copyrights[0]
+        )
