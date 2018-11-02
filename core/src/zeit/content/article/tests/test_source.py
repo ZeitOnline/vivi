37588c6c19c3b77cc2166062a9ecba33ad8f5be8
Nico Bruenjes <nico.bruenjes@gmail.com>
2018-11-02 14:17:15 +0100

    ZON-4933: Add testcase

--- core/src/zeit/content/article/tests/test_source.py
+++ core/src/zeit/content/article/tests/test_source.py
@@ -8,3 +8,10 @@ class GenreTest(zeit.content.article.testing.FunctionalTestCase):
         self.assertEqual(None, source.byline('nonexistent'))
         self.assertEqual(None, source.byline('nachricht'))
         self.assertEqual('Eine Glosse von', source.byline('glosse'))
+
+   def test_find_feedback(self):
+       source = zeit.content.article.interfaces.IArticle['genre'].source(None)
+       self.assertEqual(None, source.feedback('nonexistent'))
+       self.assertEqual(None, source.feedback('nachricht'))
+       self.assertEqual(
+       	'Hat Ihnen diese Glosse gefallen?', source.feedback('glosse'))
