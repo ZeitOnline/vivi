8ea8b689cb7ea2b9db946125ace0bfc684efd5aa
rdrongow <ron.drongowski@zeit.de>
2018-11-02 14:50:36 +0100

    Correct indentation

--- core/src/zeit/content/article/tests/test_source.py
+++ core/src/zeit/content/article/tests/test_source.py
@@ -3,11 +3,11 @@ import zeit.content.article.testing
 
 class GenreTest(zeit.content.article.testing.FunctionalTestCase):
 
-    def test_find_byline(self):
-        source = zeit.content.article.interfaces.IArticle['genre'].source(None)
-        self.assertEqual(None, source.byline('nonexistent'))
-        self.assertEqual(None, source.byline('nachricht'))
-        self.assertEqual('Eine Glosse von', source.byline('glosse'))
+   def test_find_byline(self):
+       source = zeit.content.article.interfaces.IArticle['genre'].source(None)
+       self.assertEqual(None, source.byline('nonexistent'))
+       self.assertEqual(None, source.byline('nachricht'))
+       self.assertEqual('Eine Glosse von', source.byline('glosse'))
 
    def test_find_feedback(self):
        source = zeit.content.article.interfaces.IArticle['genre'].source(None)
