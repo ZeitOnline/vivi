5ec1cdf69f17f1262642e436c9b84b7bdf5b7840
Knut Goetz <Knut.Goetz@zeit.de>
2018-11-07 15:43:21 +0100

    ZON-4106: Add explicit bw compat test

--- core/src/zeit/content/image/tests/test_metadata.py
+++ core/src/zeit/content/image/tests/test_metadata.py
@@ -4,6 +4,7 @@ from zeit.content.image.interfaces import IImageMetadata
 import lxml.etree
 import zeit.cms.testing
 import zeit.content.image.testing
+import zeit.content.image.metadata
 import zope.component
 
 
@@ -11,11 +12,15 @@ class ImageMetadataTest(zeit.cms.testing.FunctionalTestCase):
 
     layer = zeit.content.image.testing.ZCML_LAYER
 
-    def test_nofollow_is_written_to_rel_attribute(self):
+    def set_copyrights(self, value):
         image = ICMSContent('http://xml.zeit.de/2006/DSC00109_2.JPG')
         with zeit.cms.checkout.helper.checked_out(image) as co:
             metadata = IImageMetadata(co)
-            metadata.copyrights = ('Foo', 'http://example.com', True)
+            metadata.copyrights = value
+        return image
+
+    def test_nofollow_is_written_to_rel_attribute(self):
+        image = self.set_copyrights((('Foo', 'http://example.com', True),))
         ref = zope.component.getAdapter(image, IXMLReference, name='image')
         self.assertEllipsis("""\
 <image...>
@@ -39,3 +44,23 @@ class ImageMetadataTest(zeit.cms.testing.FunctionalTestCase):
             image, zeit.cms.content.interfaces.IXMLReference,
             name='related')
         self.assertEqual(image.uniqueId, node.get('href'))
+
+    def test_bw_compat_copyrights(self):
+        # Tuple of pairs
+        image = self.set_copyrights((('foo', 'bar'),))
+        self.assertEqual(
+            ('foo', None, None, 'bar', False),
+            IImageMetadata(image).copyrights
+        )
+        # Tuple of triples
+        image = self.set_copyrights((('foo', 'bar', True),))
+        self.assertEqual(
+            ('foo', None, None, 'bar', True),
+            IImageMetadata(image).copyrights
+        )
+        # Tuple of quintuples
+        image = self.set_copyrights((('foo', 'bar', 'baz', 'qux', True),))
+        self.assertEqual(
+            ('foo', 'bar', 'baz', 'qux', True),
+            IImageMetadata(image).copyrights
+        )
