aec4358e727f6269c2b6e52251a8fc7e1e6ddd6a
Knut Goetz <Knut.Goetz@zeit.de>
2018-11-20 13:44:28 +0100

    Back to development: 3.1.4

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.video',
-    version='3.1.3',
+    version='3.1.4.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
