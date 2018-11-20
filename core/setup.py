27762e17b91ca87e95a1d2df6df8d2119fe68f79
Knut Goetz <Knut.Goetz@zeit.de>
2018-11-20 12:54:29 +0100

    Preparing release 3.41.2

--- core/setup.py
+++ core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.content.article',
-    version='3.41.2.dev0',
+    version='3.41.2',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
