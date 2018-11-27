0600657c252820146cf57570de1dbdd397cdeb18
Knut Goetz <Knut.Goetz@zeit.de>
2018-11-27 15:33:54 +0100

    ZON-4106: Adjust UI for copyrights

--- src/zeit/content/image/browser/resources/form.js
+++ src/zeit/content/image/browser/resources/form.js
@@ -27,21 +27,20 @@
         }
     };
 
-    var update_copyright_visibility = function() {
+    var update_copyright_visibility = function(selector) {
         // Toggle copyright fields, depending on image company choice.
         // Image company is determinded by select widget.
-        var photographer = $('#form\\.copyright\\.combination_00');
+        var photographer = $(selector + ' #form\\.copyright\\.combination_00');
         var photographer_tr = photographer.closest('tr');
-        var company = $('#form\\.copyright\\.combination_01 option:selected');
+        var company = $(selector + ' #form\\.copyright\\.combination_01 option:selected');
         var company_tr = company.closest('tr');
-        var custom_company = $('#form\\.copyright\\.combination_02');
+        var custom_company = $(selector + ' #form\\.copyright\\.combination_02');
         var custom_company_tr = custom_company.closest('tr');
-
+        var company_text = company.text();
         // We need to place company above photographer here, because it's not
         // viable to do so in the model layer.
         company_tr.insertBefore(photographer_tr);
-
-        if (company.text() == 'Andere') {
+        if (company_text == 'Andere') {
             // Hide photographer in case there is no company selected.
             photographer_tr.hide();
             // Clear to prevent values in both fields.
@@ -54,26 +53,43 @@
             custom_company.val('');
             photographer_tr.show();
         }
+        // Not really cool, buy hey, we are checking the field value anyway
+        if (company_text == '(keine Auswahl)') {
+            company.parent().css('color', 'red');
+        } else {
+             company.parent().css('color', 'black');
+        }
     };
 
     var bind_function_to_select_change = function (selector, func) {
         // set initial visibility on load
-        func();
+        func(selector);
         // update visibility on change, unless we are in read-only mode
-        if ($(selector).find('select').length) {
+        if ($(selector).find('select').length)
+        {
             $(selector).find('select').on('change', function() {
-                func();
+                func(selector);
             });
         }
     };
 
-    $(document).ready(function() {
-        if (!$('fieldset.image-form').length) {
+
+    $(document).ready(function()  {
+        if (!($('fieldset.image-form').length)) {
             return;
         }
         bind_function_to_select_change('.fieldname-copyright .combinationFieldWidget',
             update_copyright_visibility);
         bind_function_to_select_change('.fieldname-display_type .widget',
             update_origin_visibility);
-    });
+        });
+
+    $(document).bind('fragment-ready', function () {
+        if ( !$('#copyright-box').length){
+            return;
+        }
+        bind_function_to_select_change('#copyright-box .combinationFieldWidget',
+            update_copyright_visibility);
+
+        });
 })();
