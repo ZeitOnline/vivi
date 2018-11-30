60a513e504d95737a6bfc1dc85adeedc0244a518
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-11-30 09:27:51 +0100

    FIX: Be very explicit that `message_config` is declared as a tuple
    I don't know how c45bb65 can ever have worked

--- core/src/zeit/push/tests/test_message.py
+++ core/src/zeit/push/tests/test_message.py
@@ -43,65 +43,65 @@ class MessageTest(zeit.push.testing.TestCase):
     def test_enabled_flag_is_removed_from_service_after_send(self):
         content = self.create_content('mytext')
         push = zeit.push.interfaces.IPushMessages(content)
-        push.message_config = [{'type': 'twitter', 'enabled': True}]
+        push.message_config = ({'type': 'twitter', 'enabled': True},)
         self.publish(content)
         self.assertEqual(
-            [{'type': 'twitter', 'enabled': False}], push.message_config)
+            ({'type': 'twitter', 'enabled': False},), push.message_config)
 
     def test_twitter_ressort_bbb_compat_for_enabled(self):
         content = self.create_content('mytext')
         push = zeit.push.interfaces.IPushMessages(content)
-        push.message_config = [{
-            'type': 'twitter', 'account': 'twitter_ressort_wissen'}]
+        push.message_config = ({
+            'type': 'twitter', 'account': 'twitter_ressort_wissen'},)
         data = zeit.push.interfaces.IAccountData(content)
         data.twitter_ressort_enabled = True
-        self.assertEqual([{
-            'type': 'twitter', 'variant': 'ressort',
-            'account': 'twitter_ressort_wissen', 'enabled': True}
-        ], push.message_config)
+        self.assertEqual(
+            ({'type': 'twitter', 'variant': 'ressort',
+              'account': 'twitter_ressort_wissen', 'enabled': True},),
+            push.message_config)
 
     def test_twitter_ressort_bbb_compat_for_ressort(self):
         content = self.create_content('mytext')
         push = zeit.push.interfaces.IPushMessages(content)
-        push.message_config = [{
+        push.message_config = ({
             'type': 'twitter', 'account': 'twitter_ressort_wissen',
-            'enabled': True}]
+            'enabled': True},)
         data = zeit.push.interfaces.IAccountData(content)
         data.twitter_ressort = 'twitter_ressort_politik'
-        self.assertEqual([{
-            'type': 'twitter', 'variant': 'ressort',
-            'account': 'twitter_ressort_politik', 'enabled': True}
-        ], push.message_config)
+        self.assertEqual(
+            ({'type': 'twitter', 'variant': 'ressort',
+              'account': 'twitter_ressort_politik', 'enabled': True},),
+            push.message_config)
 
     def test_mobile_is_separate_from_author(self):
         content = self.create_content('mytext')
         push = zeit.push.interfaces.IPushMessages(content)
-        push.message_config = [{
+        push.message_config = ({
             'type': 'mobile', 'enabled': True, 'variant': 'automatic-author',
-            'payload_template': 'authors.json'}]
+            'payload_template': 'authors.json'},)
         data = zeit.push.interfaces.IAccountData(content)
         self.assertFalse(data.mobile_enabled)
 
     def test_mobile_bbb_compat_recognizes_without_variant(self):
         content = self.create_content('mytext')
         push = zeit.push.interfaces.IPushMessages(content)
-        push.message_config = [{
+        push.message_config = ({
             'type': 'mobile', 'enabled': True, 'override_text': 'mobile',
-            'payload_template': 'foo.json'}]
+            'payload_template': 'foo.json'},)
         data = zeit.push.interfaces.IAccountData(content)
         self.assertEqual('mobile', data.mobile_text)
 
     def test_mobile_bbb_compat_adds_variant_on_write(self):
         content = self.create_content('mytext')
         push = zeit.push.interfaces.IPushMessages(content)
-        push.message_config = [{
+        push.message_config = ({
             'type': 'mobile', 'enabled': True, 'override_text': 'mobile',
             'title': 'mobile title', 'uses_image': False,
-            'payload_template': 'foo.json'}]
+            'payload_template': 'foo.json'},)
         data = zeit.push.interfaces.IAccountData(content)
         data.mobile_payload_template = zeit.cms.interfaces.ICMSContent(
             'http://xml.zeit.de/data/urbanairship-templates/eilmeldung.json')
-        self.assertEqual([{
+        self.assertEqual(({
             'type': 'mobile', 'enabled': True, 'override_text': 'mobile',
             'title': 'mobile title', 'uses_image': False, 'variant': 'manual',
-            'payload_template': 'eilmeldung.json'}], push.message_config)
+            'payload_template': 'eilmeldung.json'},), push.message_config)
