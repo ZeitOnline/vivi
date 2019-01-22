3890a912517ba6b64cb9a4c916df13b828d431f9
Knut Goetz <Knut.Goetz@zeit.de>
2019-01-22 15:54:46 +0100

    Preparing release 2.1.20

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='2.1.20.dev0',
+    version='2.1.20',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
