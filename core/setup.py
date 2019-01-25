14737edf4ee2bf796a514f424a9a2fe09ed76b1f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-01-25 11:41:42 +0100

    Preparing release 2.1.21

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.locales',
-    version='2.1.21.dev0',
+    version='2.1.21',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
