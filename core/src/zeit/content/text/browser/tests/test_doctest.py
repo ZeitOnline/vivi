f78393f01dbc7b03ef404b0ea0d057fdb25942aa
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-02-26 15:24:53 +0100

    MAINT: Make room for unittests

--- core/src/zeit/content/text/browser/tests/test_doctest.py
+++ core/src/zeit/content/text/browser/tests/test_doctest.py
@@ -7,5 +7,6 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
         'README.txt',
+        package='zeit.content.text.browser',
         layer=zeit.content.text.testing.ZCML_LAYER))
     return suite
