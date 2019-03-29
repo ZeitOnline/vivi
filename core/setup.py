1ed5befced0dd62bf33036f13c2fdadc250dc31a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-03-29 14:19:23 +0100

    Preparing release 3.0.9

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.find',
-    version='3.0.9.dev0',
+    version='3.0.9',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
