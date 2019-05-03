0f54bcf565a1b00f72ecc9880eccafa7d4b48111
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-05-03 11:15:50 +0200

    IR-59: Allow configuring excludes for checkin webhook

--- core/src/zeit/cms/checkout/tests/test_webhook.py
+++ core/src/zeit/cms/checkout/tests/test_webhook.py
@@ -1,7 +1,10 @@
 from zeit.cms.checkout.helper import checked_out
+from zeit.cms.content.sources import Product
+import lxml.objectify
+import mock
 import plone.testing
+import zeit.cms.checkout.webhook
 import zeit.cms.testing
-import zope.app.appsetup.product
 
 
 HTTP_LAYER = zeit.cms.testing.HTTPLayer(
@@ -18,13 +21,58 @@ class WebhookTest(zeit.cms.testing.ZeitCmsTestCase):
 
     layer = WEBHOOK_LAYER
 
+    def setUp(self):
+        super(WebhookTest, self).setUp()
+        self.config = ''
+        self.patch = mock.patch(
+            'zeit.cms.checkout.webhook.HookSource._get_tree',
+            new=lambda x: lxml.objectify.fromstring(self.config))
+        self.patch.start()
+        source = zeit.cms.checkout.webhook.HOOKS.factory
+        # XXX Have to pass the instance because of zc.factory init shenanigans.
+        source.getValues.invalidate(source)
+
+    def tearDown(self):
+        self.patch.stop()
+        super(WebhookTest, self).tearDown()
+
     def test_calls_post_with_uniqueId_for_configured_urls(self):
-        config = zope.app.appsetup.product.getProductConfiguration('zeit.cms')
-        config['checkin-webhooks'] = (
-            'http://localhost:%s' % self.layer['http_port'])
+        self.config = (
+            '<webhooks><webhook url="http://localhost:%s"/></webhooks>' %
+            self.layer['http_port'])
         with checked_out(self.repository['testcontent']):
             pass
         self.assertEqual(
             [{'body': '["http://xml.zeit.de/testcontent"]',
               'path': '/', 'verb': 'POST'}],
             self.layer['request_handler'].requests)
+
+    def test_does_not_call_hook_when_exclude_matches(self):
+        self.config = """<webhooks>
+          <webhook url="http://localhost:%s">
+            <exclude>
+              <type>testcontenttype</type>
+            </exclude>
+          </webhook>
+        </webhooks>
+        """ % self.layer['http_port']
+        with checked_out(self.repository['testcontent']):
+            pass
+        self.assertEqual([], self.layer['request_handler'].requests)
+
+
+class WebhookExcludeTest(zeit.cms.testing.ZeitCmsTestCase):
+
+    def test_match_contenttype(self):
+        hook = zeit.cms.checkout.webhook.Hook(None)
+        hook.add_exclude('type', 'testcontenttype')
+        self.assertTrue(hook.should_exclude(self.repository['testcontent']))
+        self.assertFalse(hook.should_exclude(self.repository['politik.feed']))
+
+    def test_match_product(self):
+        hook = zeit.cms.checkout.webhook.Hook(None)
+        hook.add_exclude('product', 'ZEI')
+        self.assertFalse(hook.should_exclude(self.repository['testcontent']))
+        with checked_out(self.repository['testcontent']) as co:
+            co.product = Product('ZEI')
+        self.assertTrue(hook.should_exclude(self.repository['testcontent']))
--- core/src/zeit/cms/checkout/webhook.py
+++ core/src/zeit/cms/checkout/webhook.py
@@ -1,9 +1,15 @@
+from zeit.cms.application import CONFIG_CACHE
+from zeit.cms.content.interfaces import ICommonMetadata
+from zeit.cms.interfaces import ITypeDeclaration
 import grokcore.component as grok
+import logging
 import requests
+import zeit.cms.celery
 import zeit.cms.checkout.interfaces
 import zeit.cms.interfaces
-import zope.app.appsetup.product
-import zeit.cms.celery
+
+
+log = logging.getLogger(__name__)
 
 
 @grok.subscribe(
@@ -18,8 +24,69 @@ def notify_after_checkin(context, event):
 
 @zeit.cms.celery.task(queuename='webhook')
 def notify_webhooks(uniqueId):
-    config = zope.app.appsetup.product.getProductConfiguration('zeit.cms')
-    for url in config.get('checkin-webhooks', '').split(' '):
-        if not url:
-            continue
-        requests.post(url, json=[uniqueId])
+    content = zeit.cms.interfaces.ICMSContent(uniqueId, None)
+    if content is None:
+        log.warning('Could not resolve %s, ignoring.', uniqueId)
+        return
+    for hook in HOOKS:
+        hook(content)
+
+
+class Hook(object):
+
+    def __init__(self, url):
+        self.url = url
+        self.excludes = []
+
+    def __call__(self, content):
+        if self.should_exclude(content):
+            return
+        log.debug('Notifying %s about %s', self.url, content)
+        self.deliver(content)
+
+    def deliver(self, content):
+        requests.post(self.url, json=[content.uniqueId])
+
+    def add_exclude(self, key, value):
+        self.excludes.append((key, value))
+
+    def should_exclude(self, content):
+        for exclude in self.excludes:
+            if self._matches(exclude, content):
+                log.debug('Skipping %s, matched exclude %s', content, exclude)
+                return True
+        return False
+
+    def _matches(self, exclude, content):
+        key, value = exclude
+        func = getattr(self, '_match_%s' % key)
+        return func(content, value)
+
+    def _match_type(self, content, value):
+        typ = getattr(
+            ITypeDeclaration(content, None), 'type_identifier', 'unknown')
+        return typ == value
+
+    def _match_product(self, content, value):
+        if not ICommonMetadata.providedBy(content):
+            return False
+        return content.product and content.product.id == value
+
+
+class HookSource(zeit.cms.content.sources.SimpleXMLSource):
+
+    config_url = 'checkin-webhook-config'
+
+    @CONFIG_CACHE.cache_on_arguments()
+    def getValues(self):
+        result = []
+        tree = self._get_tree()
+        for node in tree.iterchildren('webhook'):
+            hook = Hook(node.get('url'))
+            for exclude in node.xpath('exclude/*'):
+                hook.add_exclude(exclude.tag, exclude.text)
+            result.append(hook)
+        return result
+
+
+HOOKS = HookSource()
