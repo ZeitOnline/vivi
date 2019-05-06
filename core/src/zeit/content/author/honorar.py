2a9d21c2b6776da7d051ea4a6275317d1fc3dbc3
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-05-06 14:58:36 +0200

    IR-73: Implement utility to talk to the HDok REST API

---
+++ core/src/zeit/content/author/honorar.py
@@ -0,0 +1,86 @@
+from zeit.cms.interfaces import CONFIG_CACHE
+import base64
+import json
+import pkg_resources
+import requests
+import requests.exceptions
+import requests.utils
+import zeit.content.author.interfaces
+import zope.interface
+import zope.security.management
+
+
+@zope.interface.implementer(zeit.content.author.interfaces.IHonorar)
+class Honorar(object):
+
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+
+    def search(self, query, count=10):
+        result = self._request('POST /layouts/RESTautorenStamm/_find', json={
+            'query': [{'nameGesamt': query}],
+            'sort': [{'fieldName': 'nameGesamt', 'sortOrder': 'ascend'}],
+            'limit': str(count),
+        })
+        return [x['fieldData'] for x in result['response']['data']]
+
+    def create(self, data):
+        interaction = zope.security.management.getInteraction()
+        principal = interaction.participations[0].principal
+        data['anlageAccount'] = 'vivi.%s' % principal.id
+        result = self._request('GET /layouts/leer/records/1', params={
+            'script': 'restNeuAutor',
+            'script.param': b64encode(json.dumps(data))
+        })
+        return result['response']['scriptResult']
+
+    def _request(self, request, retries=0, **kw):
+        if retries > 1:
+            raise ValueError('Request %s failed' % request)
+
+        verb, path = request.split(' ')
+        method = getattr(requests, verb.lower())
+        try:
+            r = method(self.url + path, headers={
+                'Authorization': 'Bearer %s' % self.auth_token(),
+                'User-Agent': requests.utils.default_user_agent(
+                    'zeit.content.author-%s/python-requests' % (
+                        pkg_resources.get_distribution('vivi.core').version))
+            }, **kw)
+            r.raise_for_status()
+            return r.json()
+        except requests.exceptions.HTTPError as err:
+            status = getattr(err.response, 'status_code', 599)
+            if status == 401:
+                self.auth_token.invalidate(self)
+                return self._request(request, retries=retries + 1)
+            if status == 500:
+                messages = err.response.json().get('messages', ())
+                if messages and messages[0].get('code') == '401':
+                    # "No records match the request", wonderful API. :-(
+                    return {'response': {'data': []}}
+            raise
+
+    @CONFIG_CACHE.cache_on_arguments()
+    def auth_token(self):
+        r = requests.post(self.url + '/sessions',
+                          auth=(self.username, self.password),
+                          headers={'Content-Type': 'application/json'})
+        r.raise_for_status()
+        return r.headers.get('X-FM-Data-Access-Token')
+
+
+def b64encode(text):
+    return base64.b64encode(text.encode('ascii')).decode('ascii')
+
+
+@zope.interface.implementer(zeit.content.author.interfaces.IHonorar)
+def from_product_config():
+    config = zope.app.appsetup.product.getProductConfiguration(
+        'zeit.content.author')
+    return Honorar(
+        config['honorar-url'],
+        config['honorar-username'],
+        config['honorar-password'])
--- core/src/zeit/content/author/testing.py
+++ core/src/zeit/content/author/testing.py
@@ -1,6 +1,9 @@
+import mock
 import pkg_resources
+import plone.testing
 import zeit.cms.testing
 import zeit.find.testing
+import zope.component
 
 
 product_config = """
@@ -11,13 +14,41 @@ product_config = """
   sso-api-url http://meine.fake/api/1
   sso-user vivi@zeit.de
   sso-password password
+  honorar-url
+  honorar-username
+  honorar-password
 </product-config>
 """.format(fixtures=pkg_resources.resource_filename(__name__, '.'))
 
 CONFIG_LAYER = zeit.cms.testing.ProductConfigLayer(product_config, bases=(
     zeit.find.testing.CONFIG_LAYER,))
+
+
+class HonorarMockLayer(plone.testing.Layer):
+
+    def setUp(self):
+        self['honorar_mock'] = mock.Mock()
+        self['honorar_mock'].search.return_value = []
+        self['honorar_mock'].create.return_value = 'mock-honorar-id'
+        self['honorar_orig'] = zope.component.getUtility(
+            zeit.content.author.interfaces.IHonorar)
+        zope.component.getSiteManager().registerUtility(
+            self['honorar_mock'], zeit.content.author.interfaces.IHonorar)
+
+    def tearDown(self):
+        zope.component.getSiteManager().registerUtility(
+            self['honorar_orig'], zeit.content.author.interfaces.IHonorar)
+        del self['honorar_orig']
+        del self['honorar_mock']
+
+    def testTearDown(self):
+        self['honorar_mock'].reset_mock()
+
+
+HONORAR_MOCK_LAYER = HonorarMockLayer()
+
 ZCML_LAYER = zeit.cms.testing.ZCMLLayer(bases=(CONFIG_LAYER,))
-ZOPE_LAYER = zeit.cms.testing.ZopeLayer(bases=(ZCML_LAYER,))
+ZOPE_LAYER = zeit.cms.testing.ZopeLayer(bases=(ZCML_LAYER, HONORAR_MOCK_LAYER))
 WSGI_LAYER = zeit.cms.testing.WSGILayer(bases=(ZOPE_LAYER,))
 
 
