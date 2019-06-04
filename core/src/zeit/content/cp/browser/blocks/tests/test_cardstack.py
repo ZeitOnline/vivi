cef9f5d6baefca7464684d4a1d5f5aed3ff28346
Knut Goetz <Knut.Goetz@zeit.de>
2019-06-04 14:26:26 +0200

    ZON-5260: Add basic test

---
+++ core/src/zeit/content/cp/browser/blocks/tests/test_cardstack.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import zeit.cms.testing
+import zeit.content.cp
+import zeit.content.cp.centerpage
+
+
+class TestCardstack(zeit.cms.testing.BrowserTestCase):
+
+    layer = zeit.content.cp.testing.ZCML_LAYER
+
+    def test_cardstack_props_can_be_set(self):
+        self.centerpage = zeit.content.cp.centerpage.CenterPage()
+        self.centerpage['lead'].create_item('cardstack')
+        self.repository['centerpage'] = self.centerpage
+        b = self.browser
+        b.open(
+            'http://localhost/++skin++vivi/repository/centerpage/@@checkout')
+        b.open('contents')
+        b.getLink('Edit block properties', index=0).click()
+        form_url = b.url
+        b.getControl('Cardstack id').value = '123'
+        b.getControl('Background color').displayValue = ['#D8D8D8']
+        b.getControl('Apply').click()
+        b.open(form_url)
+        self.assertFalse('There were errors' in b.contents)
+        self.assertEqual('123', b.getControl('Cardstack id').value)
+        self.assertEqual(['#D8D8D8'],
+                         b.getControl('Background color').displayValue)
