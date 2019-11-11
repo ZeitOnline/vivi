b39ea491ebc2a7697e96a9d864725dc7b20541a1
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-11-11 09:37:25 +0100

    OPS-1133: Satisfy dogpile configuration in tests (belongs to b8fa47e)
    We cheat a little and configure it as an in-memory region; since we
    don't actually use the DAV cache in tests, it's currently not all that
    relevant anyway.

--- core/src/zeit/cms/testing.py
+++ core/src/zeit/cms/testing.py
@@ -458,9 +458,10 @@ cms_product_config = """\
   trisolute-ressort-url file://{base}/tagging/tests/fixtures/tris-ressorts.xml
   breadcrumbs-use-common-metadata true
 
-  cache-regions config, feature
+  cache-regions config, feature, dav
   cache-expiration-config 600
   cache-expiration-feature 15
+  cache-expiration-dav 0
   feature-toggle-source file://{base}/content/feature-toggle.xml
 
   sso-cookie-name-prefix my_sso_
