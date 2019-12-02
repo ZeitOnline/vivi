c4969d8bfea4d264481a1865c52605fa3cb426f6
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-12-02 07:55:09 +0100

    ZON-5593: Declare test-only dependencies separately

--- core/src/zeit/cms/content/sources.py
+++ core/src/zeit/cms/content/sources.py
@@ -16,11 +16,15 @@ import zope.component
 import zope.dottedname
 import zope.i18n
 import zope.security.proxy
-import zope.testing.cleanup
 
 
 logger = logging.getLogger('zeit.cms.content.sources')
-zope.testing.cleanup.addCleanUp(pyramid_dogpile_cache2.clear)
+
+try:
+    import zope.testing.cleanup
+    zope.testing.cleanup.addCleanUp(pyramid_dogpile_cache2.clear)
+except ImportError:
+    pass
 
 
 class CachedXMLBase(object):
