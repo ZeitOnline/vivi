6daa40604390d358513556dfc5946990a87efc3a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-12-04 13:33:26 +0100

    MAINT: Silence gocept.selenium warnings about unset environment variables

--- core/src/zeit/conftest.py
+++ core/src/zeit/conftest.py
@@ -8,6 +8,8 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
+    os.environ.setdefault('GOCEPT_WEBDRIVER_BROWSER', 'firefox')
+
     if config.getoption('visible'):
         os.environ['GOCEPT_SELENIUM_HEADLESS'] = 'false'
     else:
