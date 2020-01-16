98b014ace3ab7946e0196e04df2c5a3b5372647a
Tom Lazar <tom.lazar@zeit.de>
2020-01-16 15:23:22 +0100

    ZON-5647: we need to handle three cases here
    data can either be a unicode string, a non-unicode string or already
    bytes

--- core/src/zeit/connector/filesystem.py
+++ core/src/zeit/connector/filesystem.py
@@ -138,6 +138,12 @@ class Connector(object):
         except Exception:
             data = ''
         self.body_cache[id] = data
+        try:
+            data = data.encode()
+        except UnicodeDecodeError:
+            data = bytes(data)
+        except AttributeError:
+            pass
         return BytesIO(data)
 
     def _get_content_type(self, id):
