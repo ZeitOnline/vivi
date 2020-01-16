07bb043f60d36f6ad64ba5f0428cbbd18e15d406
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2020-01-16 14:57:23 +0100

    ZON-5645: Add security declarations for collections.abc

--- core/src/zeit/cms/security_patch.py
+++ core/src/zeit/cms/security_patch.py
@@ -1,5 +1,6 @@
-# https://github.com/zopefoundation/zope.security/issues/26
 import abc
+import collections.abc
+import zope.security.checker
 import zope.security.proxy
 
 
@@ -14,5 +15,29 @@ def instancecheck_with_zope_proxy(cls, instance):
     return original_check(cls, instance)
 
 
+# <https://github.com/zopefoundation/zope.security/issues/26>
 original_check = abc.ABCMeta.__instancecheck__
 abc.ABCMeta.__instancecheck__ = instancecheck_with_zope_proxy
+
+
+class DummyMapping(collections.abc.Mapping):
+
+    def __getitem__(self, key):
+        raise KeyError(key)
+
+    def __iter__(self):
+        return iter(())
+
+    def __len__(self):
+        return 0
+
+
+mydict = DummyMapping()
+
+# <https://github.com/zopefoundation/zope.security/issues/66>
+zope.security.checker.BasicTypes[
+    type(mydict.keys())] = zope.security.checker.NoProxy
+zope.security.checker.BasicTypes[
+    type(mydict.values())] = zope.security.checker.NoProxy
+zope.security.checker.BasicTypes[
+    type(mydict.items())] = zope.security.checker.NoProxy
