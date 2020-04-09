420d4a13445447eab718c0a09d0edaef3e1aacf1
Dominik Hoppe <dominik.hoppe@zeit.de>
2020-04-09 16:28:13 +0200

    WOMA-39_module: Add simple test for recipelist module draft

---
+++ core/src/zeit/content/modules/tests/test_recipelist.py
@@ -0,0 +1,18 @@
+import lxml.objectify
+import mock
+import zeit.content.modules.embed
+import zeit.content.modules.testing
+
+
+class RecipeListTest(zeit.content.modules.testing.FunctionalTestCase):
+
+    def setUp(self):
+        super(RecipeListTest, self).setUp()
+        self.context = mock.Mock()
+        self.context.__parent__ = None
+        self.module = zeit.content.modules.recipelist.RecipeList(
+            self.context, lxml.objectify.XML('<container/>'))
+
+    def test_name_should_be_stored_in_xml(self):
+        self.module.name = 'banana'
+        self.assertEqual(self.module.xml.xpath('//name'), ['banana'])
