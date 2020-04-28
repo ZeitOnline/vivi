32a70aa9e4ef2312a31d053b6c485f2f1949bb08
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-04-28 12:14:32 +0200

    WOMA-68: Set on default, the attribute is_author to true

--- core/src/zeit/content/author/author.py
+++ core/src/zeit/content/author/author.py
@@ -92,7 +92,10 @@ class Author(zeit.cms.content.xmlsupport.XMLContentBase):
     def image_group(self):
         # BBB Deprecated in favor of a separate images adapter
         return zeit.content.image.interfaces.IImages(self).image
-
+    
+    is_author = zeit.cms.content.dav.DAVProperty(
+        zeit.content.author.interfaces.IAuthor['is_author'],
+        zeit.cms.interfaces.PRINT_NAMESPACE, 'is_author', use_default=True)
 
 class AuthorType(zeit.cms.type.XMLContentTypeDeclaration):
 
