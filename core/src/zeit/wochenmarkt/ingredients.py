4c2114cf3d6b0a650b72ccf6012dbac269ef44be
Dominik Hoppe <dominik.hoppe@zeit.de>
2020-05-14 16:23:51 +0200

    WOMA-65: Align config section with deployment

--- core/src/zeit/wochenmarkt/ingredients.py
+++ core/src/zeit/wochenmarkt/ingredients.py
@@ -60,7 +60,8 @@ class Ingredients(grok.GlobalUtility):
 
     @CONFIG_CACHE.cache_on_arguments()
     def _fetch(self):
-        config = zope.app.appsetup.product.getProductConfiguration('zeit.cms')
+        config = zope.app.appsetup.product.getProductConfiguration(
+            'zeit.wochenmarkt')
         url = config.get('ingredients-url')
         log.info('Loading ingredients from %s', url)
         data = six.moves.urllib.request.urlopen(url)
