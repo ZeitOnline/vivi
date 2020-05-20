2524ac9637dc8c3394cf4a3136b4ffbf553c053d
Dominik Hoppe <dominik.hoppe@zeit.de>
2020-05-20 15:41:25 +0200

    WOMA-65: Seperate browser components

--- core/src/zeit/wochenmarkt/ingredients.py
+++ core/src/zeit/wochenmarkt/ingredients.py
@@ -5,8 +5,6 @@ import grokcore.component as grok
 import logging
 import lxml.etree
 import six
-import zeit.cms.browser.interfaces
-import zeit.cms.browser.view
 import zeit.wochenmarkt.interfaces
 import zope.component
 import zope.component.hooks
@@ -108,26 +106,3 @@ class IngredientsSource(
 
 
 ingredientsSource = IngredientsSource()
-
-
-class IngredientsSearch(zeit.cms.browser.view.JSON):
-
-    def json(self):
-        term = self.request.form.get('term')
-        if term:
-            tags = ingredientsSource.factory.search(term)
-        else:
-            tags = []
-        return [dict(label=x.name, value=x.id)
-                for x in tags]
-
-
-@grok.adapter(
-    zeit.wochenmarkt.interfaces.IIngredientsSource,
-    zeit.cms.browser.interfaces.ICMSLayer)
-@grok.implementer(zeit.cms.browser.interfaces.ISourceQueryURL)
-def IngredientsSearchURL(context, request):
-    base = zope.traversing.browser.absoluteURL(
-        zope.component.hooks.getSite(), request)
-    return (
-        base + '/@@ingredients_find')
