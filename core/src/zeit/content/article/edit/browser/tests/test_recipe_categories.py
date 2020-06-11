edc0a07fb451e14d92caf969b23a8e9167b42f79
Dominik Hoppe <dominik.hoppe@zeit.de>
2020-06-11 12:59:55 +0200

    WOMA-66: Display recipe categories only on special genres

--- core/src/zeit/content/article/edit/browser/tests/test_recipe_categories.py
+++ core/src/zeit/content/article/edit/browser/tests/test_recipe_categories.py
@@ -2,6 +2,30 @@
 import zeit.content.article.edit.browser.testing
 
 
+class RecipeCategoriesTest(
+        zeit.content.article.edit.browser.testing.BrowserTestCase):
+
+    def test_genre_should_toggle_recipe_categories(self):
+        b = self.browser
+        genre = b.getControl(name='metadata-genre.genre')
+        # Select Leserartikel, which is an invalid recipe genre
+        genre.value = 'dfb855684dc34acd36a91fa51bdce6ca'
+        b.getControl(name='metadata-genre.actions.apply').click()
+        b.open('@@contents')
+        self.assertTrue(
+            '<div class="recipe-categories-widget"' not in (b.contents))
+        # We still expect an empty fieldset
+        self.assertTrue(
+            '<fieldset id="form-recipe-categories" /></form>' in (b.contents))
+
+        genre = b.getControl(name='metadata-genre.genre')
+        # Select Rezept, which is a valid recipe genre
+        genre.value = 'ad481072a560c999a9044b43f26ced28'
+        b.getControl(name='metadata-genre.actions.apply').click()
+        b.open('@@contents')
+        self.assertTrue('<div class="recipe-categories-widget"' in b.contents)
+
+
 class FormLoader(zeit.content.article.edit.browser.testing.EditorTestCase):
 
     def test_recipe_categories_should_be_organizable(self):
