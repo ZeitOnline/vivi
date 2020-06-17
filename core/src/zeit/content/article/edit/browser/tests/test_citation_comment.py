7ea5e18e9de1db6b027d3b5abddf4bdfb05f60dc
Thomas Baumann <thomas.baumann@zeit.de>
2020-06-17 07:53:21 +0200

    ZON-5643: Add browser test for citation comment

---
+++ core/src/zeit/content/article/edit/browser/tests/test_citation_comment.py
@@ -0,0 +1,32 @@
+import zeit.content.article.edit.browser.testing
+
+
+class Form(zeit.content.article.edit.browser.testing.BrowserTestCase):
+
+    block_type = 'citation_comment'
+
+    def test_inline_form_saves_values(self):
+        self.get_article(with_empty_block=True)
+        b = self.browser
+        b.open('editable-body/blockname/@@edit-citation-comment?show_form=1')
+        b.getControl('Citation Comment', index=0).value = 'Der beste Kommentar'
+        b.getControl('URL', index=0).value = 'http://foo.de'
+        b.getControl('Apply').click()
+        b.open('@@edit-citation-comment?show_form=1')  # XXX
+        self.assertEqual(
+            'http://foo.de', b.getControl('URL', index=0).value)
+        self.assertEqual(
+            'Der beste Kommentar', b.getControl('Citation Comment',
+            index=0).value)
+
+
+class FormLoader(zeit.content.article.edit.browser.testing.EditorTestCase):
+
+    def test_citation_comment_form_is_loaded(self):
+        s = self.selenium
+        self.add_article()
+        self.create_block('citation_comment')
+        s.assertElementPresent('css=.block.type-citation_comment .inline-form '
+                           '.field.fieldname-text')
+        s.assertElementPresent('css=.block.type-citation_comment .inline-form '
+                               '.field.fieldname-url')
