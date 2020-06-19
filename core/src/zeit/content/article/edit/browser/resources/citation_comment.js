445924b9bb3d1fe479d8cbed849e2c9f21abc627
Thomas Baumann <thomas.baumann@zeit.de>
2020-06-19 08:02:30 +0200

    ZON-5643: use  env depending comments backend in js

--- core/src/zeit/content/article/edit/browser/resources/citation_comment.js
+++ core/src/zeit/content/article/edit/browser/resources/citation_comment.js
@@ -2,16 +2,17 @@
   $(document).bind('fragment-ready', function() {
     $('.block.type-citation_comment input[name$="url"]').bind('paste', function(e) {
       var id = $(this).context.id.split('.')[1];
+      var env_url = $(this).context.dataset.environment;
       pasted_url = e.originalEvent.clipboardData.getData('text/plain');
       var cid = pasted_url.split('#')[1].replace('cid-', '');
-      var comments_api_endpoint = "https://comments.zeit.de/comments?id=eq."+cid;
+      var comments_api_endpoint = env_url+"/comments?id=eq."+cid;
       $.getJSON(comments_api_endpoint)
       .done(function(data){
-        var comment = data[0]['raw_content'];
+        var comment = $(data[0]['content']).text();
         $('#citationcomment\\.'+id+'\\.text').val(comment);
       })
       .fail(function(jqxhr, textStatus, error){
-        $('#citationcomment\\.'+id+'\\.text').val('[FEHLER: KOMMENTAR KONNTE NICHT GELADEN WERDEN!\nComments-Server nicht erreichbar oder falsche URL/cid.]');
+        $('#citationcomment\\.'+id+'\\.text').val('FEHLER: Kommentar konnte nicht geladen werden!\n'+comments_api_endpoint+' nicht erreichbar? Falsche cid?');
         var err = textStatus + ", " + error;
         console.log( "Request Failed: " + err );
       });
