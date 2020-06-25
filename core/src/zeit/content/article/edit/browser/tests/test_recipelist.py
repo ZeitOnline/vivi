4b5fa8e52fe6283e25759607aecd0579e0080e87
Dominik Hoppe <dominik.hoppe@zeit.de>
2020-06-25 14:07:37 +0200

    WOMA-99: Fix recipe tests

--- core/src/zeit/content/article/edit/browser/tests/test_recipelist.py
+++ core/src/zeit/content/article/edit/browser/tests/test_recipelist.py
@@ -153,4 +153,4 @@ class FormLoader(zeit.content.article.edit.browser.testing.EditorTestCase):
         s.assertAttribute(
             'css=.ingredients-widget input@value',
             '[{"code":"brathaehnchen","label":"Brathähnchen",'
-            '"amount":"2","unit":"Stück"}]')
+            '"amount":"2","unit":""}]')
--- core/src/zeit/wochenmarkt/tests/test_ingredients.py
+++ core/src/zeit/wochenmarkt/tests/test_ingredients.py
@@ -35,7 +35,7 @@ class TestIngredients(zeit.wochenmarkt.testing.FunctionalTestCase):
     def test_autocomplete_should_be_available_for_ingredients(self):
         result = zope.component.getUtility(
             zeit.wochenmarkt.interfaces.IIngredientsWhitelist).search('B')
-        assert 7 == len(result)
+        assert 8 == len(result)
         names = []
         for item in result:
             names.append(item.name)
@@ -51,5 +51,5 @@ class TestIngredients(zeit.wochenmarkt.testing.FunctionalTestCase):
     def test_ingredients_should_be_sorted_with_exact_match_leading(self):
         result = zope.component.getUtility(
             zeit.wochenmarkt.interfaces.IIngredientsWhitelist).search('ei')
-        assert ['Ei', 'Basmatireis', 'Brei', 'Eis'] == (
+        assert ['Ei', 'Eis', 'Basmatireis', 'Brei'] == (
             [r.name for r in result])
