89bcb2698f1c5bcce3bbe0224aac6472c95d445f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2020-07-28 13:14:10 +0200

    ZON-6037: Introduce zeit.zett.interfaces.IZTTContent

---
+++ core/src/zeit/zett/tests/test_interfaces.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import zeit.zett.interfaces
+import zeit.zett.testing
+from zeit.cms.testcontenttype.testcontenttype import ExampleContentType
+
+
+class SectionTest(zeit.zett.testing.FunctionalTestCase):
+
+    def test_zett_ressort_content_is_ztt_content(self):
+        content = ExampleContentType()
+        content.ressort = u'zett'
+        self.repository['zettartikel'] = content
+        content = self.repository['zettartikel']
+        self.assertTrue(zeit.zett.interfaces.IZTTContent.providedBy(content))
