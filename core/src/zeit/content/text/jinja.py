a701e768a7343c90f655110b3b529380ea1026cd
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-10-19 15:20:38 +0200

    ZON-6162: Overwrite jinja traceback stack

--- core/src/zeit/content/text/jinja.py
+++ core/src/zeit/content/text/jinja.py
@@ -32,7 +32,84 @@ class JinjaTemplateType(zeit.content.text.text.TextType):
     addform = zeit.cms.type.SKIP_ADD
 
 
+class Environment(jinja2.Environment):
+    def handle_exception(self, exc_info=None):
+        return rewrite_traceback_stack(exc_info)
+
+def rewrite_traceback_stack(source=None):
+    """Rewrite the current exception to replace any tracebacks from
+    within compiled template code with tracebacks that look like they
+    came from the template source.
+
+    This must be called within an ``except`` block.
+
+    :param exc_info: A :meth:`sys.exc_info` tuple. If not provided,
+        the current ``exc_info`` is used.
+    :param source: For ``TemplateSyntaxError``, the original source if
+        known.
+    :return: A :meth:`sys.exc_info` tuple that can be re-raised.
+    """
+
+    if source is None:
+        source = sys.exc_info()
+
+    exc_type, exc_value, tb = source
+
+    if isinstance(exc_value, jinja2.exceptions.TemplateSyntaxError) and (
+            not exc_value.translated):
+        exc_value.translated = True
+        exc_value.source = source
+
+        try:
+            # Remove the old traceback on Python 3, otherwise the frames
+            # from the compiler still show up.
+            exc_value.with_traceback(None)
+        except AttributeError:
+            pass
+
+        # Outside of runtime, so the frame isn't executing template
+        # code, but it still needs to point at the template.
+        tb = fake_traceback(
+            exc_value, None, exc_value.filename or "<unknown>", exc_value.lineno
+        )
+    else:
+        # Skip the frame for the render function.
+        tb = tb.tb_next
+
+    stack = []
+
+    # Build the stack of traceback object, replacing any in template
+    # code with the source file and line information.
+    while tb is not None:
+        # Skip frames decorated with @internalcode. These are internal
+        # calls that aren't useful in template debugging output.
+        if tb.tb_frame.f_code in internal_code:
+            tb = tb.tb_next
+            continue
+
+        template = tb.tb_frame.f_globals.get("__jinja_template__")
+
+        if template is not None:
+            lineno = template.get_corresponding_lineno(tb.tb_lineno)
+            fake_tb = fake_traceback(exc_value, tb, template.filename, lineno)
+            stack.append(fake_tb)
+        else:
+            stack.append(tb)
+
+        tb = tb.tb_next
+
+    tb_next = None
+
+    # Assign tb_next in reverse to avoid circular references.
+    for tb in reversed(stack):
+        tb_next = tb_set_next(tb, tb_next)
+
+    return exc_type, exc_value, tb_next
+
+
 class Template(jinja2.Template):
+
+    jinja2.Template.environment_class = Environment
 
     def render(self, variables):
         # Patched from upstream to remove any and all dict-wrapping, so we can
@@ -43,7 +120,7 @@ class Template(jinja2.Template):
                     self.new_context(variables, shared=True)))
         except Exception:
             exc_info = sys.exc_info()
-        return self.environment.handle_exception(exc_info, True)
+        return self.environment.handle_exception(exc_info)
 
 
 class MockDict(collections.defaultdict):
