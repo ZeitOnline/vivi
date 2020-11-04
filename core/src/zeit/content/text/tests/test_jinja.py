966839039c206d7c6b7ce4c7df53ad926a644028
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-11-04 13:50:21 +0100

    ZON-6162: Add jinja template tests

--- core/src/zeit/content/text/tests/test_jinja.py
+++ core/src/zeit/content/text/tests/test_jinja.py
@@ -1,3 +1,4 @@
+import jinja2.exceptions
 import zeit.content.text.jinja
 import zeit.content.text.testing
 
@@ -13,6 +14,14 @@ class PythonScriptTest(zeit.content.text.testing.FunctionalTestCase):
     def test_renders_template(self):
         tpl = self.create('{{foo}}')
         self.assertEqual('bar', tpl({'foo': 'bar'}))
+
+        tpl = self.create('{{fo}}')
+        self.assertEqual('', tpl({'foo': 'bar'}))
+
+    def test_error_template_should_raise(self):
+        tpl = self.create('{{foo}')
+        with self.assertRaises(jinja2.exceptions.TemplateSyntaxError):
+            tpl({'foo': 'bar'})
 
     def test_mockdict_responds_to_any_variable(self):
         tpl = self.create('{{foo.bar.baz}}')
