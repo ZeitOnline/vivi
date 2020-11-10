4f07938ef80db5f62c8fb7a5411c4eae846a2adf
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-11-10 15:02:02 +0100

    ZON-6162: Add test case for formatted exceptions on error

--- core/src/zeit/content/text/tests/test_jinja.py
+++ core/src/zeit/content/text/tests/test_jinja.py
@@ -1,4 +1,3 @@
-import jinja2.exceptions
 import zeit.content.text.jinja
 import zeit.content.text.testing
 
@@ -18,10 +17,23 @@ class PythonScriptTest(zeit.content.text.testing.FunctionalTestCase):
         tpl = self.create('{{fo}}')
         self.assertEqual('', tpl({'foo': 'bar'}))
 
-    def test_error_template_should_raise(self):
+    def test_template_syntax_error_should_return_formatted_exception(self):
         tpl = self.create('{{foo}')
-        with self.assertRaises(jinja2.exceptions.TemplateSyntaxError):
-            tpl({'foo': 'bar'})
+        self.assertEqual(
+            "Traceback (most recent call last):\n" +
+            "jinja2.exceptions.TemplateSyntaxError: unexpected '}'\n",
+            tpl({'foo': 'bar'}))
+
+    def test_not_callable_str_should_return_formatted_exception(self):
+        tpl = self.create('{{foo()}}')
+        self.assertEqual(
+            "Traceback (most recent call last):\n  " +
+            "Module zeit.content.text.jinja, line 49, in &lt;genexpr&gt;" +
+            "\n    return ''.join(str(value) for value in " +
+            "self.root_render_func(\n  Module &lt;template&gt;, " +
+            "line 1, in top-level template code\nTypeError: 'str' " +
+            "object is not callable\n",
+            tpl({'foo': 'bar'}))
 
     def test_mockdict_responds_to_any_variable(self):
         tpl = self.create('{{foo.bar.baz}}')
