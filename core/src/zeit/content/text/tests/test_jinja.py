a5ab4dd5b2d7ca7d4a1d03559155008b5a0864d7
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-11-18 16:16:21 +0100

    ZON-6162: Remove tests for local debugging

--- core/src/zeit/content/text/tests/test_jinja.py
+++ core/src/zeit/content/text/tests/test_jinja.py
@@ -17,24 +17,6 @@ class PythonScriptTest(zeit.content.text.testing.FunctionalTestCase):
         tpl = self.create('{{fo}}')
         self.assertEqual('', tpl({'foo': 'bar'}))
 
-    def test_template_syntax_error_should_return_formatted_exception(self):
-        tpl = self.create('{{foo}')
-        self.assertEqual(
-            "Traceback (most recent call last):\n" +
-            "jinja2.exceptions.TemplateSyntaxError: unexpected '}'\n",
-            tpl({'foo': 'bar'}))
-
-    def test_not_callable_str_should_return_formatted_exception(self):
-        tpl = self.create('{{foo()}}')
-        self.assertEqual(
-            "Traceback (most recent call last):\n  " +
-            "Module zeit.content.text.jinja, line 49, in &lt;genexpr&gt;" +
-            "\n    return ''.join(str(value) for value in " +
-            "self.root_render_func(\n  Module &lt;template&gt;, " +
-            "line 1, in top-level template code\nTypeError: 'str' " +
-            "object is not callable\n",
-            tpl({'foo': 'bar'}))
-
     def test_mockdict_responds_to_any_variable(self):
         tpl = self.create('{{foo.bar.baz}}')
         self.assertIn('<MagicMock', tpl(zeit.content.text.jinja.MockDict()))
