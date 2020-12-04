093f73bc38914f9889505657b3a441d29e408351
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2020-12-04 11:19:33 +0100

    ZON-6162: Add test for jinja exception behaviour

--- core/src/zeit/content/text/tests/test_jinja.py
+++ core/src/zeit/content/text/tests/test_jinja.py
@@ -1,3 +1,5 @@
+import sys
+import traceback
 import zeit.content.text.jinja
 import zeit.content.text.testing
 
@@ -17,3 +19,16 @@ class PythonScriptTest(zeit.content.text.testing.FunctionalTestCase):
     def test_mockdict_responds_to_any_variable(self):
         tpl = self.create('{{foo.bar.baz}}')
         self.assertIn('<MagicMock', tpl(zeit.content.text.jinja.MockDict()))
+
+    def test_runtime_error_rewrites_traceback(self):
+        # Since we copied Template.render() wholesale from upstream,
+        # we want to make sure we did not break anything by doing that.
+        tpl = self.create('{{foo()}}')
+        try:
+            tpl({'foo': lambda: [][0]})
+        except IndexError:
+            self.assertIn(
+                '  File "<template>", line 1, in top-level template code\n',
+                traceback.format_exception(*sys.exc_info()))
+        else:
+            self.fail('did not raise')
