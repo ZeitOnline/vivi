dcccbdb102ecf70d7935d38431898e38718270ca
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2020-12-17 16:24:39 +0100

    ZON-5656: Simplify spelling (belongs to 92e5e3e)

--- core/src/zeit/content/image/mdb.py
+++ core/src/zeit/content/image/mdb.py
@@ -6,7 +6,6 @@ import pendulum
 import pkg_resources
 import re
 import requests
-import six
 import zeit.content.image.interfaces
 import zope.interface
 
@@ -98,8 +97,8 @@ class FakeMDB(MDB):
             filename = 'mdb-meta.xml'
         return requests_mock.create_response(
             requests.Request(url='http://example.invalid'),
-            text=six.ensure_text(pkg_resources.resource_string(
-                __name__, 'tests/fixtures/%s' % filename)))
+            content=pkg_resources.resource_string(
+                __name__, 'tests/fixtures/%s' % filename))
 
 
 @zope.interface.implementer(zeit.content.image.interfaces.IMDB)
