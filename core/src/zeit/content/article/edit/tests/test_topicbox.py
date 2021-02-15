68a47aa2f610339bd9dfb16108bb0e9b93dd91f6
Tobias Kabbeck <tobias.kabbeck@zeit.de>
2021-02-15 21:58:59 +0100

    ZON-5576: Test that source type defaults to manuell

--- core/src/zeit/content/article/edit/tests/test_topicbox.py
+++ core/src/zeit/content/article/edit/tests/test_topicbox.py
@@ -88,6 +88,10 @@ class TestTopicbox(zeit.content.article.testing.FunctionalTestCase):
             "http://xml.zeit.de/online/2007/01/Somalia")
         self.assertEqual([self.repository['foo'], ], list(box.values()))
 
+    def test_topicbox_defaults_to_source_type_manuell(self):
+        box = self.get_topicbox()
+        self.assertEqual('manuell', box.source_type)
+
     def test_topicbox_source_centerpage(self):
         box = self.get_topicbox()
         box.source_type = 'centerpage'
