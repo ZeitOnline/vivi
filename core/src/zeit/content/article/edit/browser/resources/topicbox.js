a5701987e1449d80c79486ef62ddc7de056aa701
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2021-05-07 16:27:19 +0200

    ZON-6655: Refactor JS to fix hide/show errors on multiple boxes in article

--- core/src/zeit/content/article/edit/browser/resources/topicbox.js
+++ core/src/zeit/content/article/edit/browser/resources/topicbox.js
@@ -1,103 +1,146 @@
-function display_elements(selected_automatic_type) {
+function getFieldClassNames() {
+    return {
+        'automatic_type': 'field fieldname-automatic_type fieldtype-text',
+        'first_reference': 'field fieldname-first_reference fieldtype-text',
+        'second_reference': 'field fieldname-second_reference fieldtype-text',
+        'third_reference': 'field fieldname-third_reference fieldtype-text',
+        'referenced_cp': 'field fieldname-referenced_cp fieldtype-text',
+        'referenced_topicpage': 'field fieldname-referenced_topicpage fieldtype-text',
+        'topicpage_filter': 'field fieldname-topicpage_filter fieldtype-text',
+        'elasticsearch_raw_query': 'field fieldname-elasticsearch_raw_query fieldtype-text',
+        'elasticsearch_raw_order': 'field fieldname-elasticsearch_raw_order fieldtype-text',
+        'preconfigured_query': 'field fieldname-preconfigured_query fieldtype-text',
+    }
+}
 
-    var cssClass = '.type-article #edit-form-article-content .fieldname-';
-    var cssClasses = [];
+function getAllTopicboxIds() {
+    var topicbox_ids = [];
 
-    var classes = [
-        'referenced_cp',
-        'referenced_topicpage',
-        'topicpage_filter',
-        'elasticsearch_raw_query',
-        'elasticsearch_raw_order',
-        'preconfigured_query',
-    ];
+    document.querySelectorAll('.type-topicbox').forEach((topicbox) => {
+        topicbox_ids.push(topicbox.id);
+    });
 
-    selected_automatic_type = selected_automatic_type.toLowerCase();
+    return topicbox_ids;
+}
 
-    switch (selected_automatic_type) {
+function getAutomaticTypeTextByTopicboxId(topicboxId) {
+    var automaticTypeElement = document.getElementById(`topicbox.${topicboxId}.automatic_type`);
+
+    if (automaticTypeElement !== null) {
+        return automaticTypeElement.selectedOptions[0].text;
+    }
+    return null;
+}
+
+function displayElements(event) {
+    // 'fragment-ready' is called, if the document is reloading
+    // 'changed' is called, if a automatic_type value is changed
+    var topicboxIds = getAllTopicboxIds();
+    if (event.type === 'fragment-ready') {
+        topicboxIds.forEach((topicboxId) => {
+            hideShowElementsByAutomaticTypeValue(topicboxId);
+        });
+    } else if (event.type === 'change') {
+        var topicboxId = (event.target.id.replace('topicbox.', '').replace('.automatic_type', ''));
+        hideShowElementsByAutomaticTypeValue(topicboxId);
+    } else {
+        console.log(`unhandled event: ${event.type}`);
+    }
+}
+
+function getTopicboxFieldSetById(topicboxId) {
+    return document.getElementById(`form-topicbox.${topicboxId}`);
+}
+
+function hideShowElementsByAutomaticTypeValue(topicboxId) {
+    var topicboxFieldSet = getTopicboxFieldSetById(topicboxId)
+    var fieldClassNames = getFieldClassNames();
+    var currentAutomaticTypeValue = getAutomaticTypeTextByTopicboxId(topicboxId);
+
+    if (currentAutomaticTypeValue === null) {
+        return;
+    }
+
+    // Hide all except automatic_type
+    Object.keys(fieldClassNames).filter((element) => {
+        return element.indexOf('automatic_type') == -1;
+    }).forEach((element) => {
+        hideElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+    });
+
+    switch(currentAutomaticTypeValue.toLowerCase()) {
         case 'klassisch':
-            hide_references(false);
+            Object.keys(fieldClassNames).filter((element) => {
+                return [
+                    'first_reference',
+                    'second_reference',
+                    'third_reference'].includes(element);
+            }).forEach((element) => {
+                showElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+            });
             break;
         case 'centerpage':
-            cssClasses.push('referenced_cp');
+            Object.keys(fieldClassNames).filter((element) => {
+                return ['referenced_cp'].includes(element);
+            }).forEach((element) => {
+                showElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+            });
             break;
         case 'themenseite':
-            cssClasses.push('referenced_topicpage');
-            cssClasses.push('topicpage_filter');
+            Object.keys(fieldClassNames).filter((element) => {
+                return [
+                    'referenced_topicpage',
+                    'topicpage_filter'].includes(element);
+            }).forEach((element) => {
+                showElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+            });
             break;
         case 'es-query':
-            cssClasses.push('elasticsearch_raw_query');
-            cssClasses.push('elasticsearch_raw_order');
+            Object.keys(fieldClassNames).filter((element) => {
+                return [
+                    'elasticsearch_raw_query',
+                    'elasticsearch_raw_order'].includes(element);
+            }).forEach((element) => {
+                showElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+            });
             break;
         case 'related-api':
-            cssClasses.push('topicpage_filter');
+            Object.keys(fieldClassNames).filter((element) => {
+                return ['topicpage_filter'].includes(element);
+            }).forEach((element) => {
+                showElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+            });
             break;
         case 'konfigurationsdatei':
-            cssClasses.push('preconfigured_query');
+            Object.keys(fieldClassNames).filter((element) => {
+                return ['preconfigured_query'].includes(element);
+            }).forEach((element) => {
+                showElementByTopicboxFieldSet(topicboxFieldSet, fieldClassNames[element]);
+            });
             break;
         default:
             break;
     }
-
-    if (selected_automatic_type != 'klassisch') {
-        hide_references(true);
-    }
-
-    classes.forEach((cls) => {
-        var el = document.querySelectorAll(cssClass + cls);
-        if (el.length > 0) {
-            el[0].style.display = 'none';
-        }
-    });
-
-    cssClasses.forEach((cls) => {
-        var el = document.querySelectorAll(cssClass + cls);
-        if (el.length > 0) {
-            el[0].style.display = 'block';
-        }
-    });
 }
 
-function hide_references(hide) {
-    var cssClassReferences = "[class*='fieldname-NAME_reference']";
-    ['first', 'second', 'third'].forEach((ref) => {
-        var reference = document.querySelectorAll(cssClassReferences.replace('NAME', ref));
-        if (reference.length > 0) {
-            reference[0].style.display = hide ? 'none' : 'block';
-        }
-    });
+function hideElementByTopicboxFieldSet(fieldset, className) {
+    fieldset.querySelectorAll(`[class="${className}"]`)[0].style.display = 'none';
 }
 
-function eval_automatic_type(el) {
-    if (el.length > 0) {
-        var sel_val = el[0].selectedOptions[0].text;
-        display_elements(sel_val);
-    }
+function showElementByTopicboxFieldSet(fieldset, className) {
+    fieldset.querySelectorAll(`[class="${className}"]`)[0].style.display = 'block';
 }
 
 (function ($) {
-    $(document).bind('fragment-ready', function(e) {
-        var current_automatic_type_value = document.querySelectorAll('[id$=automatic_type]');
-            if (current_automatic_type_value.length > 0) {
-                current_automatic_type_value.forEach((el) => {
-                    if(el.id.startsWith('topicbox.id') && el.id.endsWith('automatic_type')) {
-                        var val = el.selectedOptions[0].text;
-                    display_elements(val);
-                    return;
-                }
-            });
-        }
-        return;
+    $(document).bind('fragment-ready', function(event) {
+        getAllTopicboxIds().forEach(() => {
+            displayElements(event);
+        });
     });
 
-    $(document).bind('change', function(e) {
-        if (e.target && e.target.id.indexOf('automatic_type') > -1) {
-            var val = document.getElementById(e.target.id);
-            if (val == undefined) {
-                return;
-            }
-            val = val.selectedOptions[0].text;
-            display_elements(val);
+    $(document).bind('change', function(event) {
+        if (event.target && event.target.id.indexOf('automatic_type') > -1) {
+            displayElements(event);
         }
     });
  }(jQuery));
