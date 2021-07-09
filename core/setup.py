5c01e1c551d12784e6c0e050d2ea94e39e52df07
louika <louisa.huelsen@zeit.de>
2021-07-09 15:10:06 +0200

    Preparing release 4.57.4

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='4.57.4.dev0',
+    version='4.57.4',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
