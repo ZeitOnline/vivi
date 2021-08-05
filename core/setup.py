95eb00422c0bca137125eb9592165530dd8e8b21
hennnnes <hroemmer@googlemail.com>
2021-08-05 16:16:49 +0200

    Back to development: 4.60.3

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='4.60.2',
+    version='4.60.3.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
