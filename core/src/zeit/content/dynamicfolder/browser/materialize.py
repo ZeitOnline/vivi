de388866217bf40a19dd4819d195a85dd7545392
louika <louisa.huelsen@zeit.de>
2021-08-27 11:49:31 +0200

    ZO-156: Add button

---
+++ core/src/zeit/content/dynamicfolder/browser/materialize.py
@@ -0,0 +1,20 @@
+from zeit.cms.i18n import MessageFactory as _
+import zeit.content.dynamicfolder.materialize
+import zeit.cms.browser.menu
+import zeit.cms.browser.view
+
+
+class Materialize(zeit.cms.browser.view.Base):
+    """Materialize contents of dynamic folder"""
+
+    def __call__(self):
+        # zope.event to materialize the folder?
+        zeit.content.dynamicfolder.materialize.materialize_content(
+            self.context)
+        self.redirect(self.url(self.context, '@@view.html'))
+        return ''
+
+
+class MenuItem(zeit.cms.browser.menu.ActionMenuItem):
+
+    title = _('Materialize dynamic folder')
