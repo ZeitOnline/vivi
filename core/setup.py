b6826636cb0e469bfe42489b355322850ba201d6
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2021-09-02 12:16:51 +0200

    Back to development: 4.63.1

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='4.63.0',
+    version='4.63.1.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
