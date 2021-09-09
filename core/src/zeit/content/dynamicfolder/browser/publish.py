523f174038c7115c0550bc59bab58ec7a33260fa
louika <louisa.huelsen@zeit.de>
2021-09-09 14:30:10 +0200

    ZO-163: Add publish button

---
+++ core/src/zeit/content/dynamicfolder/browser/publish.py
@@ -0,0 +1,19 @@
+from zeit.cms.i18n import MessageFactory as _
+import zeit.content.dynamicfolder.publish
+import zeit.cms.browser.menu
+import zeit.cms.browser.view
+
+
+class PublishMaterializedContent(zeit.cms.browser.view.Base):
+    """Materialize contents of dynamic folder"""
+
+    def __call__(self):
+        zeit.content.dynamicfolder.publish.publish_content(
+            self.context.uniqueId)
+        self.redirect(self.url(self.context, '@@view.html'))
+        return ''
+
+
+class MenuItem(zeit.cms.browser.menu.ActionMenuItem):
+
+    title = _('Publish dynamic folder')
---
+++ core/src/zeit/content/dynamicfolder/publish.py
@@ -0,0 +1,13 @@
+
+
+# Todo
+# - identify materialized content
+# - publish button gängig machen?
+# - eigene berechtigung
+# - sammelveröffentlichung publish multipe oder so
+# - was ist dieser komische xml error
+
+
+def publish_content(unique_id):
+    import pdb; pdb.set_trace()
+    pass
