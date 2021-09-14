f4f91ab9afb5d8891b0128387b73d7a8ab19659b
Tobias Kabbeck <tobias.kabbeck@zeit.de>
2021-09-14 14:14:21 +0200

    Preparing release 4.63.4

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='4.63.4.dev0',
+    version='4.63.4',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
