2d00e7ff50722dcaa8f01b8d44b993f251af31f7
louika <louisa.huelsen@zeit.de>
2021-09-16 17:53:42 +0200

    ZO-156: Add logging to publish workflow

--- core/src/zeit/content/dynamicfolder/publish.py
+++ core/src/zeit/content/dynamicfolder/publish.py
@@ -1,8 +1,11 @@
 import transaction
 
+from zeit.cms.i18n import MessageFactory as _
+
 import zeit.cms.celery
 import zeit.cms.interfaces
 import zeit.content.dynamicfolder.interfaces as DFinterfaces
+import zeit.objectlog.interfaces
 
 
 @zeit.cms.celery.task
@@ -14,4 +17,6 @@ def publish_content(unique_id):
             objects.append(folder[key])
     zeit.cms.workflow.interfaces.IPublish(
         folder).publish_multiple(objects)
+    msg = _('Published')
+    zeit.objectlog.interfaces.ILog(folder).log(msg)
     transaction.commit()
