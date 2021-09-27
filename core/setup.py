81de79c06706dd487c3a9be3388dc08dbe54b804
louika <louisa.huelsen@zeit.de>
2021-09-27 15:32:29 +0200

    Back to development: 4.63.7

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='4.63.6',
+    version='4.63.7.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
