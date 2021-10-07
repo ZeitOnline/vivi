866daa3f9a2912163039f7fcf2f1e91b0f189353
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2021-10-07 13:35:58 +0200

    ZO-354: Move environ-base logging config mechanics here from zeit.web

---
+++ core/src/zeit/cms/tests/test_logging.py
@@ -0,0 +1,34 @@
+import unittest
+import zeit.cms.logging
+
+
+class LoggingConfigTest(unittest.TestCase):
+
+    def test_convert_dotted_keys_to_nested_dicts(self):
+        convert = zeit.cms.logging.convert_dotted_keys_to_nested_dicts
+        config = {
+            'one.a.a1': 'leaf1',
+            'one.a.a2': 'leaf2',
+            'one.b.b1': 'leaf3',
+            'one.c.c1__literal__': '123',
+        }
+        self.assertEqual({'one': {
+            'a': {'a1': 'leaf1', 'a2': 'leaf2'},
+            'b': {'b1': 'leaf3'},
+            'c': {'c1': 123}
+        }}, convert(config))
+
+    def test_logging_syntax_fixes(self):
+        config = {
+            'root': {'handlers': 'one, two'},
+            'loggers': {'zope_interface': {'level': 'DEBUG'}},
+            'formatters': {'foo': {'class': 'myclass'}},
+        }
+        zeit.cms.logging.apply_logging_syntax_fixes(config)
+        self.assertEqual({
+            'version': 1,
+            'disable_existing_loggers': False,
+            'root': {'handlers': ['one', 'two']},
+            'loggers': {'zope.interface': {'level': 'DEBUG'}},
+            'formatters': {'foo': {'()': 'myclass'}},
+        }, config)
