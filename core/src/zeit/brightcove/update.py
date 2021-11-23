cb43bc2bfebad4dfc1ec9bcf89f74a7e152a1016
Thomas Baumann <thomas.baumann@zeit.de>
2021-11-23 14:41:30 +0100

    ZO-303: delete video thumbnail and still image with deleting video

--- core/src/zeit/brightcove/update.py
+++ core/src/zeit/brightcove/update.py
@@ -4,6 +4,7 @@ from zeit.cms.workflow.interfaces import IPublish, IPublishInfo
 from zeit.content.video.interfaces import IVideo
 import gocept.runner
 import logging
+import transaction
 import z3c.celery.celery
 import zeit.brightcove.convert
 import zeit.brightcove.session
@@ -73,6 +74,20 @@ class import_video(import_base):
         if IPublishInfo(self.cmsobj).published:
             IPublish(self.cmsobj).retract(background=False)
         del self.bcobj.__parent__[self.bcobj.id]
+        try:
+            still = self.cmsobj.cms_video_still
+            del still.__parent__[still.__name__]
+            transaction.commit()
+        except Exception:
+            log.info('vdieo still image for  %s not deleted', self.bcobj)
+            pass
+        try:
+            thumb = self.cmsobj.cms_thumbnail
+            del thumb.__parent__[thumb.__name__]
+            transaction.commit()
+        except Exception:
+            log.info('video thumbnail for  %s not deleted', self.bcobj)
+            pass
         return True
 
     def _publish(self):
