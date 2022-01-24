a07bef86daaead1b5485e005669bebe7dcdebfdf
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2022-01-24 11:58:53 +0100

    ZO-441: Use zcml features instead of lots of small zcml files for connector test registrations

--- core/src/zeit/cms/testing.py
+++ core/src/zeit/cms/testing.py
@@ -154,13 +154,14 @@ class ZCMLLayer(plone.testing.Layer):
 
     defaultBases = (LOGGING_LAYER,)
 
-    def __init__(self, config_file='ftesting.zcml',
+    def __init__(self, config_file='ftesting.zcml', features=(),
                  name='ZCMLLayer', module=None, bases=()):
         if module is None:
             module = inspect.stack()[1][0].f_globals['__name__']
         if not config_file.startswith('/'):
             config_file = pkg_resources.resource_filename(module, config_file)
         self.config_file = config_file
+        self.features = features
         super(ZCMLLayer, self).__init__(
             name=name, module=module, bases=self.defaultBases + bases)
 
@@ -175,7 +176,7 @@ class ZCMLLayer(plone.testing.Layer):
         # context.execute_actions()
         self['zcaRegistry'] = plone.testing.zca.pushGlobalRegistry()
         self.assert_non_browser_modules_have_no_browser_zcml()
-        zeit.cms.zope._load_zcml(self.config_file)
+        zeit.cms.zope._load_zcml(self.config_file, self.features)
 
     def assert_non_browser_modules_have_no_browser_zcml(self):
         # Caveat emptor: This whole method is a bunch of heuristics, but
@@ -530,7 +531,11 @@ cms_product_config = """\
     base=pkg_resources.resource_filename(__name__, ''))
 
 
-CONFIG_LAYER = ProductConfigLayer(cms_product_config)
+CONFIG_LAYER = ProductConfigLayer(
+    cms_product_config, patches={'zeit.connector': {
+        'repository-path': pkg_resources.resource_filename(
+            'zeit.connector', 'testcontent')
+    }})
 ZCML_LAYER = ZCMLLayer('ftesting.zcml', bases=(CONFIG_LAYER,))
 ZOPE_LAYER = ZopeLayer(bases=(ZCML_LAYER,))
 WSGI_LAYER = WSGILayer(bases=(ZOPE_LAYER,))
