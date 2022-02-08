e21483ed8ed1171a74911d9f1e9bbed6d88051b4
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2022-02-08 08:37:25 +0100

    MAINT: Declare pytest markers in code instead of external configuration
    We also _use_ them in code, so this makes way more sense.

--- core/src/zeit/conftest.py
+++ core/src/zeit/conftest.py
@@ -2,6 +2,12 @@ import os
 
 
 def pytest_configure(config):
+    config.addinivalue_line(
+        'markers', 'slow: This is a non-unit test and thus is not run by '
+        'default. Use ``-m slow`` to run these, or ``-m 1`` to run all tests.')
+    config.addinivalue_line(
+        'markers', 'selenium: Selenium tests are not run by default.')
+
     os.environ.setdefault('GOCEPT_WEBDRIVER_BROWSER', 'firefox')
 
     if config.getoption('visible'):
