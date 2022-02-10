7cb3221ed1377aed4087d647ff87a75335b4cf1f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2022-02-10 09:32:22 +0100

    Update changelog

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='5.9.3.dev0',
+    version='5.10.0.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
