38ca7dbbb8bd6991f4235c249e4f00eb55b64765
Thomas Baumann <thomas.baumann@zeit.de>
2022-04-04 17:02:45 +0200

    ZO-929: sync twitter field with teaser for genre=news

--- core/src/zeit/content/article/edit/browser/resources/sync.js
+++ core/src/zeit/content/article/edit/browser/resources/sync.js
@@ -3,12 +3,14 @@
 var copy_to = function(target) {
     return function() {
         var src = $(this).val();
-        if (! $(target).val()) {
-            $(target).val(src).focusout();
+        if ((! $(target).val()) &&
+            (target != '#social\\.short_text' ||
+            $('#metadata-genre\\.genre option:selected').text() == 'Nachricht')) {
+                $(target).val(src).focusout();
+                text_color();
         }
     };
 };
-
 
 $(document).bind('fragment-ready', function(event) {
     $('#teaser-supertitle\\.teaserSupertitle', event.__target).bind(
@@ -19,6 +21,9 @@ $(document).bind('fragment-ready', function(event) {
 
     $('#teaser-text\\.teaserText', event.__target).bind(
         'change', copy_to('#article-content-head\\.subtitle'));
+
+    $('#teaser-text\\.teaserText', event.__target).bind(
+        'change', copy_to('#social\\.short_text'));
 });
 
 }(jQuery));
