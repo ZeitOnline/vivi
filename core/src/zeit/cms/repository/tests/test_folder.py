04634530145751fbb81e9d9002fa4b32c50c9ccc
louika <louisa.huelsen@zeit.de>
2022-04-11 16:26:32 +0200

    ZO-165: Test folder has publication dependencies

--- core/src/zeit/cms/repository/tests/test_folder.py
+++ core/src/zeit/cms/repository/tests/test_folder.py
@@ -1,4 +1,6 @@
 import zeit.cms.testing
+import zeit.cms.interfaces
+import zeit.workflow.interfaces
 import zope.copypastemove.interfaces
 
 
@@ -10,3 +12,16 @@ class RenameFolderTest(zeit.cms.testing.ZeitCmsTestCase):
         renamer.renameItem('online', 'offline')
         self.assertNotIn('online', self.repository)
         self.assertIn('offline', self.repository)
+
+
+class FolderDependenciesTest(zeit.cms.testing.ZeitCmsTestCase):
+
+    def test_folder_dependencies(self):
+        folder = zeit.cms.interfaces.ICMSContent(
+            'http://xml.zeit.de/online/2007')
+        dependencies = list(zeit.workflow.interfaces.IPublicationDependencies(
+            folder).get_dependencies())
+        self.assertEqual(
+            'http://xml.zeit.de/online/2007/01/', dependencies[0].uniqueId)
+        self.assertEqual(
+            'http://xml.zeit.de/online/2007/02/', dependencies[1].uniqueId)
