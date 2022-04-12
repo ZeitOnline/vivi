eab59cd1b1ce9471866384f97afe0685ab4a23bd
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2022-04-12 10:45:55 +0200

    ZO-121: Fix writing kpi fields to topicpage.xml (belongs to 3277108)

--- core/src/zeit/retresco/connection.py
+++ core/src/zeit/retresco/connection.py
@@ -406,7 +406,7 @@ TOPIC_PAGE_ATTRIBUTES = [
     ('topic_type', 'type'),
 ]
 for i in range(1, 31):
-    TOPIC_PAGE_ATTRIBUTES.append(('kpi_%s' % i, '', int))
+    TOPIC_PAGE_ATTRIBUTES.append(('kpi_%s' % i, None, int))
 
 
 def _build_topic_xml(topicpages):
@@ -419,20 +419,16 @@ def _build_topic_xml(topicpages):
         for attr in TOPIC_PAGE_ATTRIBUTES:
             if len(attr) == 1:
                 vivi = tms = attr[0]
-                typ = None
             elif len(attr) == 2:
                 vivi, tms = attr
-                typ = None
             elif len(attr) == 3:
-                vivi, tms, typ = attr
-            if not vivi:
-                vivi = tms
+                vivi, tms, _ = attr
+                if not tms:
+                    tms = vivi
             value = row.get(tms)
             if value is None:
                 continue
-            if typ is not None:
-                value = typ(value)
-            attributes[vivi] = value
+            attributes[vivi] = str(value)
         root.append(E.topic(row['title'], **attributes))
     return root
 
--- core/src/zeit/retresco/tests/test_connection.py
+++ core/src/zeit/retresco/tests/test_connection.py
@@ -345,12 +345,14 @@ class TopiclistUpdateTest(zeit.retresco.testing.FunctionalTestCase):
             'id': 'berlin',
             'title': 'Berlin',
             'topic_type': 'location',
+            'kpi_1': 42,
         }]
         xml = zeit.retresco.connection._build_topic_xml(pages)
         self.assertEqual('topics', xml.tag)
         topics = xml.xpath('//topic')
         self.assertEqual(1, len(topics))
         self.assertEqual('Berlin', topics[0].text)
+        self.assertEqual('42', topics[0].get('kpi_1'))
 
     def test_topiclist_excludes_pages_with_redirect(self):
         pages = [{
