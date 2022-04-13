553746f0043b229075078524261442eaa8add577
buildbert <zon-backend@zeit.de>
2022-04-13 15:03:32 +0200

    Back to development: 5.13.2

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='5.13.1',
+    version='5.13.2.dev0',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
