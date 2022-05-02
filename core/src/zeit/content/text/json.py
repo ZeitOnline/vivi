f07067d866340714e3518c8b954a1403956373e3
louika <louisa.huelsen@zeit.de>
2022-05-02 16:43:10 +0200

    ZO-1094: Get schema to use for validation

--- core/src/zeit/content/text/json.py
+++ core/src/zeit/content/text/json.py
@@ -1,10 +1,18 @@
 from zeit.cms.i18n import MessageFactory as _
+
 import commentjson
+import logging
+import openapi_schema_validator
+import requests
+import yaml
+
 import zeit.cms.content.dav
 import zeit.cms.interfaces
 import zeit.content.text.interfaces
 import zeit.content.text.text
 import zope.interface
+
+log = logging.getLogger(__name__)
 
 
 @zope.interface.implementer(zeit.content.text.interfaces.IJSON)
@@ -13,6 +21,15 @@ class JSON(zeit.content.text.text.Text):
     @property
     def data(self):
         return commentjson.loads(self.text)
+
+    def get_schema(self):
+        info = zeit.content.text.interfaces.IValidationSchema(self)
+        try:
+            schema = requests.get(info.schema_url)  # yaml schema file expected
+            return yaml.safe_load(schema.text)
+        except requests.exceptions.RequestException as err:
+            status = getattr(err.response, 'status_code', None)
+            log.error('%s returned %s', info.schema_url, status, exc_info=True)
 
 
 class JSONType(zeit.content.text.text.TextType):
