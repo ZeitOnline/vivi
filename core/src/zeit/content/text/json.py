4a6fbeef55114585ff7e8f39ed07ccfcefdcb965
louika <louisa.huelsen@zeit.de>
2022-05-04 19:10:16 +0200

    ZO-1094: Validate json data against schema

--- core/src/zeit/content/text/json.py
+++ core/src/zeit/content/text/json.py
@@ -1,5 +1,9 @@
 from zeit.cms.i18n import MessageFactory as _
+
 import commentjson
+import jsonschema.validators
+import openapi_schema_validator
+
 import zeit.cms.content.dav
 import zeit.cms.interfaces
 import zeit.content.text.interfaces
@@ -13,6 +17,17 @@ class JSON(zeit.content.text.text.Text):
     @property
     def data(self):
         return commentjson.loads(self.text)
+
+    def validate_data(self):
+        validation = zeit.content.text.interfaces.IValidationSchema(self)
+        if validation.schema:
+            schema = commentjson.loads(validation.schema)
+            ref_resolver = jsonschema.validators.RefResolver.from_schema(
+                schema)
+            openapi_schema_validator.validate(
+                self.data,
+                schema[validation.field],
+                resolver=ref_resolver)
 
 
 class JSONType(zeit.content.text.text.TextType):
