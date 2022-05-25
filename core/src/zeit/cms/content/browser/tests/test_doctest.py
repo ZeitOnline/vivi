9ff7a534500fc2a42b2eb7042b0af67344d5ce30
Thomas Baumann <thomas.baumann@zeit.de>
2022-05-25 10:50:16 +0200

    ZO-1211: rm meanwhile unnecessary doctest

--- core/src/zeit/cms/content/browser/tests/test_doctest.py
+++ core/src/zeit/cms/content/browser/tests/test_doctest.py
@@ -5,7 +5,6 @@ import zeit.cms.testing
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(zeit.cms.testing.FunctionalDocFileSuite(
-        'form.txt',
         'template.txt',
         'typechange.txt',
         'widget.txt',
