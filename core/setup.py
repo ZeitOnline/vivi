997ef7562a0a4186b0ca4e49a49500fdebba961d
buildbert <zon-backend@zeit.de>
2023-02-21 10:52:58 +0100

    Preparing release 5.22.17

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vivi.core',
-    version='5.22.17.dev0',
+    version='5.22.17',
     author='gocept, Zeit Online',
     author_email='zon-backend@zeit.de',
     url='http://www.zeit.de/',
