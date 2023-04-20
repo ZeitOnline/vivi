3da7de59d537f71faa9a2cfc51ea9c8ba2d02878
Thomas Baumann <thomas.baumann@zeit.de>
2023-04-20 19:30:07 +0200

    ZO-3028: fix: import entity type for topicpages

--- core/src/zeit/retresco/connection.py
+++ core/src/zeit/retresco/connection.py
@@ -420,11 +420,11 @@ def _build_topic_xml(topicpages):
             if len(attr) == 1:
                 vivi = tms = attr[0]
             elif len(attr) == 2:
-                vivi, tms = attr
+                tms, vivi = attr
             elif len(attr) == 3:
-                vivi, tms, _ = attr
-                if not tms:
-                    tms = vivi
+                tms, vivi, _ = attr
+                if not vivi:
+                    vivi = tms
             value = row.get(tms)
             if value is None:
                 continue
