2e02a7eb896be19aecefb4f0cd5d5600e5168ad6
kabbeck <tobias.kabbeck@gmail.com>
2023-05-17 15:41:29 +0200

    ZO-2452: Unify format of imports

--- core/src/zeit/content/article/edit/tests/test_animatedheader.py
+++ core/src/zeit/content/article/edit/tests/test_animatedheader.py
@@ -1,22 +1,22 @@
+from lxml.objectify import E
+from zeit.content.animation.animation import Animation
+from zeit.content.article.article import Article
 from zeit.content.article.edit.animatedheader import AnimatedHeader
-import lxml.objectify
-import zeit.content.animation.animation
-import zeit.content.article.testing
-import zeit.content.article.article
-import zeit.content.video.video
+from zeit.content.article.testing import FunctionalTestCase, MOCK_LAYER
+from zeit.content.video.video import Video
 
 
-class TestAnimatedHeader(zeit.content.article.testing.FunctionalTestCase):
+class TestAnimatedHeader(FunctionalTestCase):
 
-    layer = zeit.content.article.testing.MOCK_LAYER
+    layer = MOCK_LAYER
 
     def test_animatedheader(self):
-        self.repository['article'] = zeit.content.article.article.Article()
-        self.repository['video'] = zeit.content.video.video.Video()
-        animation = zeit.content.animation.animation.Animation()
+        self.repository['article'] = Article()
+        self.repository['video'] = Video()
+        animation = Animation()
         animation.article = self.repository['article']
         animation.video = self.repository['video']
         self.repository['animation'] = animation
-        animatedheader = AnimatedHeader(None, lxml.objectify.E.animatedheader())
+        animatedheader = AnimatedHeader(None, E.animatedheader())
         animatedheader.animation = self.repository['animation']
         assert animatedheader.xml.get('href') == 'http://xml.zeit.de/animation'
