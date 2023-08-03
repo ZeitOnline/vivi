973433d76a6e5c23b2f724202ee19035dea6095a
Stefan H. Holek <stefan.holek@zeit.de>
2023-08-03 09:31:22 +0200

    ZO-3578: Add webhook for Simplecast

---
+++ core/src/zeit/simplecast/json/webhook.py
@@ -0,0 +1,16 @@
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class Notification:
+    """This view is a receiver for notification events. We register it as a
+    webhook in the Simplecast API, and _they_ will call it each time a
+    podcast or episode is added/changed/deleted.
+    """
+
+    def __call__(self):
+        body = self.request.bodyStream.read(
+            int(self.request['CONTENT_LENGTH']))
+        log.debug(body)
