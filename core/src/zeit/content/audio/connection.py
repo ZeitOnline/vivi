15f1110483317a522f01ec4e158dc59733a88651
louika <louisa.huelsen@zeit.de>
2023-08-16 09:03:30 +0200

    ZO-3701: Product config for simplecast

--- core/src/zeit/content/audio/connection.py
+++ core/src/zeit/content/audio/connection.py
@@ -1,6 +1,7 @@
 import grokcore.component as grok
 import logging
 import requests
+import zope.app.appsetup.product
 
 import zeit.content.audio.interfaces
 
@@ -10,6 +11,11 @@ log = logging.getLogger(__name__)
 
 @grok.implementer(zeit.content.audio.interfaces.ISimplecast)
 class Simplecast(grok.GlobalUtility):
+
+    def __init__(self):
+        config = zope.app.appsetup.product.getProductConfiguration(
+            'zeit.content.audio')
+        self.api_url = config['simplecast-url']
 
     def get_episode(self, episode_id):
         response = self._request('GET', f'episodes/{episode_id}')
@@ -22,7 +28,7 @@ class Simplecast(grok.GlobalUtility):
             raise
 
     def _request(self, verb, path):
-        url = f'https://testapi.simplecast.com/{path}'
+        url = f'{self.api_url}{path}'
         response = requests.request(verb.lower(), url)
         if response.status_code == 404:
             log.error((
--- core/src/zeit/content/audio/testing.py
+++ core/src/zeit/content/audio/testing.py
@@ -1,9 +1,18 @@
 import zeit.cms.testing
 
 
+product_config = """\
+<product-config zeit.content.audio>
+  simplecast-url https://testapi.simplecast.com/
+</product-config>
+"""
+
+CONFIG_LAYER = zeit.cms.testing.ProductConfigLayer(
+    product_config,
+    bases=(zeit.cms.testing.CONFIG_LAYER,))
 ZCML_LAYER = zeit.cms.testing.ZCMLLayer(
     'ftesting.zcml',
-    bases=(zeit.cms.testing.CONFIG_LAYER,))
+    bases=(CONFIG_LAYER,))
 ZOPE_LAYER = zeit.cms.testing.ZopeLayer(bases=(ZCML_LAYER,))
 
 
