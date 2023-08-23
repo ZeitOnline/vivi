67dde122ef5ba219e089c04b23c50d4353dab3c6
Stefan H. Holek <stefan.holek@zeit.de>
2023-08-23 14:05:43 +0200

    ZO-3662: Add Audio factory

--- core/src/zeit/content/audio/audio.py
+++ core/src/zeit/content/audio/audio.py
@@ -1,8 +1,11 @@
 from zeit.cms.i18n import MessageFactory as _
 import zope.interface
+import zope.component
 import zeit.cms.interfaces
 import zeit.cms.content.dav
 import zeit.cms.content.xmlsupport
+import zeit.cms.repository.interfaces
+import zeit.cms.repository.folder
 import zeit.cms.type
 import zeit.content.audio.interfaces
 
@@ -24,6 +27,15 @@ class Audio(zeit.cms.content.xmlsupport.XMLContentBase):
     url = zeit.cms.content.dav.DAVProperty(
         zeit.content.audio.interfaces.IAudio['url'],
         zeit.cms.interfaces.DOCUMENT_SCHEMA_NS, 'url')
+
+
+def add_audio(container, info):
+    audio = Audio()
+    audio.title = info['title']
+    audio.episodeId = info['id']
+    audio.url = info['audio_file_url']
+    container[audio.episodeId] = audio
+    return audio
 
 
 class AudioType(zeit.cms.type.XMLContentTypeDeclaration):
