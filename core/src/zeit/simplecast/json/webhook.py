7f3b32c368555828337c2cf3d3d013a50cf17c63
Stefan H. Holek <stefan.holek@zeit.de>
2023-08-30 17:02:22 +0200

    ZO-3744: Move webhook impl to celery task

--- core/src/zeit/simplecast/json/webhook.py
+++ core/src/zeit/simplecast/json/webhook.py
@@ -5,6 +5,8 @@ import zeit.content.audio.audio
 import zeit.simplecast.interfaces
 import zeit.cms.checkout.helper
 import zope.component
+import celery.result
+import celery.states
 
 log = logging.getLogger(__name__)
 
@@ -31,32 +33,38 @@ class Notification:
         log.info(body)
 
         body = json.loads(body).get('data')
-        log.info(body)
 
-        simplecast = zope.component.getUtility(
-            zeit.simplecast.interfaces.ISimplecast)
-        log.info('Simplecast utility %s', simplecast)
+        # SIMPLECAST_WEBHOOK_TASK.apply_async(body)
+        result = SIMPLECAST_WEBHOOK_TASK(body)
+        celery.result.EagerResult('eager', result, celery.states.SUCCESS)
 
-        if body.get('event') == 'episode_created':
-            log.info('Create episode from simplecast request.')
-            info = simplecast.fetch_episode(body.get('episode_id'))
-            container = zeit.content.audio.audio.audio_container(create=True)
-            zeit.content.audio.audio.add_audio(container, info)
 
-        elif body.get('event') == 'episode_updated':
-            log.info('Update episode from simplecast request.')
-            info = simplecast.fetch_episode(body.get('episode_id'))
-            container = zeit.content.audio.audio.audio_container()
-            if container is not None:
-                with zeit.cms.checkout.helper.checked_out(
-                        container[body.get('episode_id')]) as episode:
-                    episode.update(info)
+@zeit.cms.celery.task
+def SIMPLECAST_WEBHOOK_TASK(body):
+    simplecast = zope.component.getUtility(
+        zeit.simplecast.interfaces.ISimplecast)
+    log.info('Simplecast utility %s', simplecast)
 
-        elif body.get('event') == 'episode_deleted':
-            log.info('Delete episode from simplecast request.')
-            container = zeit.content.audio.audio.audio_container()
-            if container is not None:
-                zeit.content.audio.audio.remove_audio(
-                    container[body.get('episode_id')])
-        else:
-            log.info('No episode processed.')
+    if body.get('event') == 'episode_created':
+        log.info('Create episode from simplecast request.')
+        info = simplecast.fetch_episode(body.get('episode_id'))
+        container = zeit.content.audio.audio.audio_container(create=True)
+        zeit.content.audio.audio.add_audio(container, info)
+
+    elif body.get('event') == 'episode_updated':
+        log.info('Update episode from simplecast request.')
+        info = simplecast.fetch_episode(body.get('episode_id'))
+        container = zeit.content.audio.audio.audio_container()
+        if container is not None:
+            with zeit.cms.checkout.helper.checked_out(
+                    container[body.get('episode_id')]) as episode:
+                episode.update(info)
+
+    elif body.get('event') == 'episode_deleted':
+        log.info('Delete episode from simplecast request.')
+        container = zeit.content.audio.audio.audio_container()
+        if container is not None:
+            zeit.content.audio.audio.remove_audio(
+                container[body.get('episode_id')])
+    else:
+        log.info('No episode processed.')
