c2108b12ac8fbb33c68ee4f7785dde84ad1c4f6f
Louisa v. Hülsen <louisa.huelsen@zeit.de>
2023-09-06 10:39:47 +0200

    ZO-3744: Simplify

--- core/src/zeit/simplecast/json/webhook.py
+++ core/src/zeit/simplecast/json/webhook.py
@@ -41,46 +41,47 @@ class Notification:
         log.info(body)
 
         body = json.loads(body).get('data')
+        episode_id = body.get('episode_id')
+        event = body.get('event')
 
         self.execute_task(
-            body, queue='simplecast', background=self.background)
+            event=event, episode_id=episode_id, background=self.background)
 
-    def execute_task(self, body, queue, background):
+    def execute_task(self, event, episode_id, background):
         if background:
             SIMPLECAST_WEBHOOK_TASK.delay(
-                body, queue=queue, _principal_id_=self._principal)
+                event, episode_id, _principal_id_=self._principal)
         else:
-            log.info('No episode processed.')
-            result = SIMPLECAST_WEBHOOK_TASK(body)
+            result = SIMPLECAST_WEBHOOK_TASK(event, episode_id)
             celery.result.EagerResult('eager', result, celery.states.SUCCESS)
 
 
-@zeit.cms.celery.task
-def SIMPLECAST_WEBHOOK_TASK(body):
+@zeit.cms.celery.task(queue='simplecast')
+def SIMPLECAST_WEBHOOK_TASK(event, episode_id):
     simplecast = zope.component.getUtility(
         zeit.simplecast.interfaces.ISimplecast)
 
-    if body.get('event') == 'episode_created':
+    if event == 'episode_created':
         log.info('Create episode from simplecast request.')
-        info = simplecast.fetch_episode(body.get('episode_id'))
+        info = simplecast.fetch_episode(episode_id)
         container = simplecast.folder(info['created_at'])
         audio = zeit.content.audio.audio.add_audio(container, info)
         log.info('Audio %s successfully created.', audio.uniqueId)
 
-    elif body.get('event') == 'episode_updated':
+    elif event == 'episode_updated':
         log.info('Update episode from simplecast request.')
-        info = simplecast.fetch_episode(body.get('episode_id'))
+        info = simplecast.fetch_episode(episode_id)
         container = simplecast.folder(info['created_at'])
         if container is not None:
             with zeit.cms.checkout.helper.checked_out(
-                    container[body.get('episode_id')]) as episode:
+                    container[episode_id]) as episode:
                 episode.update(info)
                 log.info(
                     'Audio %s successfully updated.', episode.uniqueId)
 
-    elif body.get('event') == 'episode_deleted':
+    elif event == 'episode_deleted':
         log.info('Delete episode from simplecast request.')
-        audio = simplecast.find_existing_episode(body.get('episode_id'))
+        audio = simplecast.find_existing_episode(episode_id)
         if audio:
             uniqueId = audio.uniqueId
             zeit.content.audio.audio.remove_audio(audio)
@@ -88,7 +89,7 @@ def SIMPLECAST_WEBHOOK_TASK(body):
         else:
             log.warning(
                 'No podcast episode %s found. No episode deleted.',
-                body.get('episode_id'))
+                episode_id)
 
     else:
         log.info('No episode processed.')
