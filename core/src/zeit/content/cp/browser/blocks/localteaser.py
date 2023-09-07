f5441d9c394a5f1d66b052a51b30e68bdbf9b15a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2023-09-07 15:26:24 +0200

    ZO-215: Add a content landing zone to the edit tab of local-teaser

--- core/src/zeit/content/cp/browser/blocks/localteaser.py
+++ core/src/zeit/content/cp/browser/blocks/localteaser.py
@@ -6,5 +6,5 @@ class EditCommon(zeit.content.cp.browser.blocks.teaser.EditCommon):
 
     form_fields = zope.formlib.form.FormFields(
         zeit.content.cp.interfaces.ILocalTeaserBlock).select(
-            'teaserSupertitle', 'teaserTitle', 'teaserText',
+            'references', 'teaserSupertitle', 'teaserTitle', 'teaserText',
             'image', 'fill_color', 'force_mobile_image')
