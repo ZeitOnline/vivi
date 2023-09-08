e2c52eb8809bef028539ba7f08ca9156f9c43103
Louisa v. Hülsen <louisa.huelsen@zeit.de>
2023-09-08 19:57:46 +0200

    ZO-3770: Show new properties in ui

--- core/src/zeit/content/audio/browser/form.py
+++ core/src/zeit/content/audio/browser/form.py
@@ -10,7 +10,7 @@ class Base:
 
     form_fields = zope.formlib.form.FormFields(
         zeit.content.audio.interfaces.IAudio).select(
-            'title', 'episode_id', 'url')
+            'title', 'serie', 'episode_id', 'url', 'image', 'duration')
 
     field_groups = (
         gocept.form.grouped.RemainingFields(
