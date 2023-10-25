6a608f637e4b6783081abf94f45f67e50eb038b6
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2023-10-25 10:52:22 +0200

    ZO-4081: Support packing relstorage via zodburi

--- core/src/zeit/cms/relstorage.py
+++ core/src/zeit/cms/relstorage.py
@@ -1,4 +1,7 @@
 from relstorage.zodburi_resolver import RelStorageURIResolver, Resolver
+import ZODB.serialize
+import zeit.cms.cli
+import zodburi
 
 
 class PsqlServiceResolver(Resolver):
@@ -12,3 +15,13 @@ class PsqlServiceResolver(Resolver):
 
 
 psql_resolver = RelStorageURIResolver(PsqlServiceResolver())
+
+
+def zodbpack():
+    settings = zeit.cms.cli.parse_paste_ini()
+    storage_factory, db_kw = zodburi.resolve_uri(settings['zodbconn.uri'])
+    storage = storage_factory()
+    # We use keep_history=False, so we run pack only for garbage collection
+    timestamp = None
+    storage.pack(timestamp, ZODB.serialize.referencesf)
+    storage.close()
