6527af5282dc73944413e783a3a535110616ec37
Martin Stolle <martin.stolle@zeit.de>
2023-12-05 08:30:53 +0100

    ZO-4329: add adapter for podcast setting to real image

--- core/src/zeit/content/audio/audio.py
+++ core/src/zeit/content/audio/audio.py
@@ -8,14 +8,15 @@ import zope.component
 import zope.interface
 
 import zeit.cms.content.dav
-import zeit.cms.content.property
 import zeit.cms.content.interfaces
 import zeit.cms.content.metadata
+import zeit.cms.content.property
 import zeit.cms.interfaces
 import zeit.cms.repository.folder
 import zeit.cms.repository.interfaces
 import zeit.cms.type
 import zeit.content.audio.interfaces
+import zeit.content.image.interfaces
 
 
 log = logging.getLogger(__name__)
@@ -70,3 +71,25 @@ class AudioType(zeit.cms.type.XMLContentTypeDeclaration):
     interface = IAudio
     title = _('Audio')
     type = 'audio'  # Wert für {http://namespaces.zeit.de/CMS/meta}type
+
+
+@grok.implementer(zeit.content.image.interfaces.IImages)
+class PodcastImage(grok.Adapter):
+    grok.context(IAudio)
+
+    def _get_podcast(self):
+        if self.context.audio_type == 'podcast':
+            info = IPodcastEpisodeInfo(self.context)
+            return info.podcast
+
+    @property
+    def fill_color(self):
+        podcast = self._get_podcast()
+        if podcast:
+            return podcast.color
+
+    @property
+    def image(self):
+        podcast = self._get_podcast()
+        if podcast and podcast.image:
+            return zeit.cms.interfaces.ICMSContent(podcast.image, None)
