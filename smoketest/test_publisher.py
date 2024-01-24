af65620134970a1c42a3474b6173ef3f2f09806d
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-01-24 13:44:37 +0100

    ZO-4519, ZO-3922: Move smoketests here from vivi-deployment

---
+++ smoketest/test_publisher.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timezone
+from time import sleep
+
+import pytest
+
+
+def test_publisher_invalidates_fastly(vivi, http):
+    article = '/data/nightwatch-publish.txt'
+
+    expected = datetime.now().isoformat()
+
+    vivi.put(article, expected)
+    vivi.publish(article)
+
+    # vivi runs the publisher asynchronously from the API call.
+    timeout = 60
+    for i in range(timeout):
+        sleep(1)
+        r = http(article)
+        current = r.text.strip()
+        if current == expected:
+            break
+    else:
+        pytest.fail('Expected %s, got %s after %s seconds' % (expected, current, timeout))
+
+
+@pytest.mark.parametrize(
+    'content',
+    [
+        '/wirtschaft/2010-01/automarkt-usa-deutschland-smart',
+        '/2010/01/index',
+    ],
+)
+def test_publisher_updates_metadata(vivi, http, config, content):
+    before = datetime.now(timezone.utc)
+    sleep(1)
+
+    vivi.publish(content)
+
+    # vivi runs the publisher asynchronously from the API call.
+    timeout = 60
+    for i in range(timeout):
+        sleep(1)
+        r = http(
+            config['elasticsearch'],
+            json={
+                'query': {'bool': {'filter': [{'term': {'url': content}}]}},
+                '_source': ['payload.workflow.date_last_published'],
+            },
+        )
+        try:
+            hit = r.json()['hits']['hits'][0]['_source']
+            current = hit['payload']['workflow']['date_last_published']
+            current = datetime.fromisoformat(current)
+        except Exception:
+            current = datetime.min
+        if current > before:
+            break
+    else:
+        pytest.fail('%s did not increase after %s seconds' % (current, timeout))
