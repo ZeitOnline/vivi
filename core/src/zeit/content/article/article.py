f0d06532c703c5d0bcae2a2734cf61d461b4b2cf
Thomas Baumann <thomas.baumann@zeit.de>
2024-01-30 12:10:16 +0100

    ZO-4225: print article checksum object as its checksum

--- core/src/zeit/content/article/article.py
+++ core/src/zeit/content/article/article.py
@@ -515,6 +515,9 @@ class Speechbert(zeit.cms.content.dav.DAVPropertiesAdapter):
     def __ne__(self, other):
         return not self.__eq__(other)
 
+    def __str__(self):
+        return self.checksum
+
 
 @grok.subscribe(
     zeit.content.article.interfaces.IArticle, zeit.cms.workflow.interfaces.IBeforePublishEvent
