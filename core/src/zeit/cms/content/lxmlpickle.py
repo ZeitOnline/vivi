65f018867df7a200701c0bb9d5e3380b0420e153
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-02-09 10:10:38 +0100

    ZO-4627: Support lxml.etree for content objects instead of lxml.objectify

--- core/src/zeit/cms/content/lxmlpickle.py
+++ core/src/zeit/cms/content/lxmlpickle.py
@@ -8,26 +8,30 @@ import lxml.objectify
 log = logging.getLogger(__name__)
 
 
-def treeFactory(state):
-    """Un-Pickle factory."""
+def deserialize(state):
     try:
-        return lxml.objectify.fromstring(state)
+        return lxml.etree.fromstring(state)
     except Exception as e:
         log.error('Error during unpickling', exc_info=True)
-        return lxml.objectify.fromstring(
-            '<error><!-- XML-FEHLER: %s\n\n%s\n\n--></error>' % (e, state)
-        )
+        return lxml.etree.fromstring('<error><!-- XML-FEHLER: %s\n\n%s\n\n--></error>' % (e, state))
 
 
-copyreg.constructor(treeFactory)
+copyreg.constructor(deserialize)
 
 
-def reduceObjectifiedElement(object):
-    """Reduce function for lxml.objectify trees.
+def serialize(obj):
+    """Reduce function for lxml trees.
     See http://docs.python.org/lib/pickle-protocol.html for details.
     """
-    state = lxml.etree.tostring(object.getroottree())
-    return (treeFactory, (state,))
+    state = lxml.etree.tostring(obj.getroottree())
+    return (
+        deserialize,
+        (state,),
+    )
 
 
-copyreg.pickle(lxml.objectify.ObjectifiedElement, reduceObjectifiedElement, treeFactory)
+copyreg.pickle(lxml.etree._Element, serialize)
+
+# BBB
+treeFactory = deserialize
+copyreg.pickle(lxml.objectify.ObjectifiedElement, serialize)
