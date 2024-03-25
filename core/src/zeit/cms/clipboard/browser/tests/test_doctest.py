325cce2c7da6bc5e8249936b22cd8a7622a26c9a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-03-25 15:01:26 +0100

    FIX: Remove doctest without layer
    Mixing doctest suites with different layers in the same module was
    never supported properly (as evidenced by the comment in
    zeit.connector:test_doctest2), but it turns out, it's even worse, and
    the layer-less tests were *never collected at all* and thus never ran,
    for several years now, sigh.

--- core/src/zeit/cms/clipboard/browser/tests/test_doctest.py
+++ core/src/zeit/cms/clipboard/browser/tests/test_doctest.py
@@ -1,12 +1,7 @@
-import unittest
-
 import zeit.cms.testing
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.DocFileSuite('clip.txt', package='zeit.cms.clipboard.browser'))
-    suite.addTest(
-        zeit.cms.testing.FunctionalDocFileSuite('README.txt', package='zeit.cms.clipboard.browser')
+    return zeit.cms.testing.FunctionalDocFileSuite(
+        'README.txt', 'clip.txt', package='zeit.cms.clipboard.browser'
     )
-    return suite
--- core/src/zeit/cms/content/tests/test_doctest.py
+++ core/src/zeit/cms/content/tests/test_doctest.py
@@ -1,23 +1,17 @@
-import unittest
-
 import zeit.cms.testing
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(zeit.cms.testing.DocFileSuite('property.txt', package='zeit.cms.content'))
-    suite.addTest(
-        zeit.cms.testing.FunctionalDocFileSuite(
-            'contentmemo.txt',
-            'contentuuid.txt',
-            'dav.txt',
-            'field.txt',
-            'liveproperty.txt',
-            'lxmlpickle.txt',
-            'metadata.txt',
-            'sources.txt',
-            'xmlsupport.txt',
-            package='zeit.cms.content',
-        )
+    return zeit.cms.testing.FunctionalDocFileSuite(
+        'contentmemo.txt',
+        'contentuuid.txt',
+        'dav.txt',
+        'field.txt',
+        'liveproperty.txt',
+        'lxmlpickle.txt',
+        'metadata.txt',
+        'property.txt',
+        'sources.txt',
+        'xmlsupport.txt',
+        package='zeit.cms.content',
     )
-    return suite
