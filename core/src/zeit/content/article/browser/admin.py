a978d94909f0d33e5402770c736697246b566f33
kabbeck <tobias.kabbeck@gmail.com>
2024-04-22 14:49:53 +0200

    ZO-4983: Move avoid_create_summary to non admin area (#708)
    * ZO-4983: Move avoid_create_summary to non admin area
    
    * ZO-4983: Make linter happy
    
    * ZO-4983: Add changelog entry
    
    * ZO-4983: Remove avoid_create_summary from metadata, already defined in article

--- core/src/zeit/content/article/browser/admin.py
+++ core/src/zeit/content/article/browser/admin.py
@@ -7,4 +7,4 @@ import zeit.content.article.interfaces
 @grok.adapter(zeit.content.article.interfaces.IArticle, name='zeit.content.article')
 @grok.implementer(zeit.cms.admin.interfaces.IAdditionalFieldsCO)
 def additional_fields_co(context):
-    return (zeit.content.article.interfaces.IArticle, ['has_audio', 'avoid_create_summary'])
+    return (zeit.content.article.interfaces.IArticle, ['has_audio'])
