3e623f42f636fc124a19594b0e053a5225ef1684
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-05-27 11:14:40 +0200

    ZO-5455: Explicitly create git branch

--- core/src/zeit/cms/history.py
+++ core/src/zeit/cms/history.py
@@ -70,12 +70,12 @@ def main():
     if create_repo:
         cmd('git config user.email zon-ops@zeit.de')
         cmd('git config user.name zon-ops')
+        cmd('git checkout -b main')
         cmd('git remote add origin git+ssh://git@github.com/ZeitOnline/vivi-config-history')
         cmd(f'git config core.sshCommand "ssh -i {options.sshkey}"')
 
     cmd('git fetch --depth=1')
     cmd('git reset --hard origin/main')
-    cmd('git branch -u origin/main main')
 
     sync_content_to_filesystem(options.uniqueid, output)
 
