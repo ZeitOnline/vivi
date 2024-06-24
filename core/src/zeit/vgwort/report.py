406324638624f02668ddb8748bfe14b9a0bb12c2
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-06-24 15:04:11 +0200

    MAINT: Remove file-based locking, concurrent cronjobs are disabled via k8s runtime now

--- core/src/zeit/vgwort/report.py
+++ core/src/zeit/vgwort/report.py
@@ -1,12 +1,9 @@
 import datetime
 import logging
-import os.path
 import sys
-import tempfile
 
 import grokcore.component as grok
 import pytz
-import zc.lockfile
 import ZODB.POSException
 import zope.app.appsetup.product
 import zope.interface
@@ -104,13 +101,6 @@ class ReportInfo(zeit.cms.content.dav.DAVPropertiesAdapter):
 
 @zeit.cms.cli.runner(principal=zeit.cms.cli.from_config('zeit.vgwort', 'token-principal'))
 def report_new_documents():
-    lock_file_name = os.path.join(tempfile.gettempdir(), 'vgwort-run-lock')
-    try:
-        lock = zc.lockfile.LockFile(lock_file_name)
-    except zc.lockfile.LockError:
-        sys.stderr.write('VGWort report alredy running? Could not lock {}\n'.format(lock_file_name))
-        sys.exit(1)
-
     log.info('Report start')
     now = datetime.datetime.now()
     today = datetime.datetime(now.year, now.month, now.day)
@@ -121,23 +111,20 @@ def report_new_documents():
         sys.exit(2)
 
     vgwort = zope.component.getUtility(zeit.vgwort.interfaces.IMessageService)
-    try:
-        source = zope.component.getUtility(zeit.vgwort.interfaces.IReportableContentSource)
-        for i, content in enumerate(source):
+    source = zope.component.getUtility(zeit.vgwort.interfaces.IReportableContentSource)
+    for i, content in enumerate(source):
+        try:
+            report(content)
+        except Exception as e:
             try:
-                report(content)
-            except Exception as e:
-                try:
-                    if e.args[0][0] == 401:
-                        raise
-                except IndexError:
-                    pass
-                log.warning('Error reporting %s, ignoring', content, exc_info=True)
-            # XXX vgwort returns 401 after some requests for unknown reasons.
-            if i % 6 == 0 and 'client' in vgwort.__dict__:
-                del vgwort.client
-    finally:
-        lock.close()
+                if e.args[0][0] == 401:
+                    raise
+            except IndexError:
+                pass
+            log.warning('Error reporting %s, ignoring', content, exc_info=True)
+        # XXX vgwort returns 401 after some requests for unknown reasons.
+        if i % 6 == 0 and 'client' in vgwort.__dict__:
+            del vgwort.client
     log.info('Report end')
 
 
