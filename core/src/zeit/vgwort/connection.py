3910f5d0d4827c180615f77cf1bf2f66e3d10177
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-06-24 15:12:59 +0200

    ZO-5637: Adjust maintenance window definition to actually observed downtimes (503 for wsdl until 07:30)

--- core/src/zeit/vgwort/connection.py
+++ core/src/zeit/vgwort/connection.py
@@ -244,5 +244,5 @@ def in_daily_maintenance_window():
     # https://tom.vgwort.de/Documents/pdfs/dokumentation/metis/DOC_Verlagsmeldung.pdf
     now = pendulum.now('Europe/Berlin')
     start = now.replace(hour=2, minute=50, second=0, microsecond=0)
-    end = now.replace(hour=6, minute=10, second=0, microsecond=0)
+    end = now.replace(hour=7, minute=40, second=0, microsecond=0)
     return start <= now <= end
