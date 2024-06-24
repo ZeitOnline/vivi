51a0d9b099541ffacc82bdefe0f252b7c21837eb
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-06-24 15:12:36 +0200

    ZO-5637: Adjust maintenance window definition to current docs (03:00-06:00)

--- core/src/zeit/vgwort/connection.py
+++ core/src/zeit/vgwort/connection.py
@@ -243,6 +243,6 @@ def in_daily_maintenance_window():
     # See "METIS Integrationsbeschreibung 3.2.1.1"
     # https://tom.vgwort.de/Documents/pdfs/dokumentation/metis/DOC_Verlagsmeldung.pdf
     now = pendulum.now('Europe/Berlin')
-    start = now.replace(hour=3, minute=50, second=0, microsecond=0)
+    start = now.replace(hour=2, minute=50, second=0, microsecond=0)
     end = now.replace(hour=6, minute=10, second=0, microsecond=0)
     return start <= now <= end
