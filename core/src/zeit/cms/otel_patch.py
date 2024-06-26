628d43b7e0424c381c19ee0c139eacb423c193eb
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-06-26 08:39:04 +0200

    ZO-5394: Remove workaround, bug is fixed in upstream 0.46b0
    Since in the same release the metric name will change to `_seconds`
    instead of `_ms`, we don't have to coordinate updating otel and
    removing the workaround.
    
    This reverts 935f852

--- core/src/zeit/cms/otel_patch.py
+++
@@ -1,4 +0,0 @@
-def record_with_requests_bugfix(self, amount, attributes=None):
-    if self._metric._name == 'http_client_duration_ms':
-        amount /= 1000
-    return self._old_record(amount, attributes)
