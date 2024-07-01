9a94df2f6135ebace52bd4212ef6105145a7514f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-07-01 11:43:24 +0200

    ZO-4281: Import application code only when needed to generate a migration diff

--- core/src/zeit/connector/migrations/env.py
+++ core/src/zeit/connector/migrations/env.py
@@ -8,13 +8,10 @@ from sqlalchemy import create_engine, pool
 # Use env-var based zeit.cms.logging instead?
 logging.basicConfig(level='INFO', format='%(asctime)s %(levelname)-5.5s %(name)s %(message)s')
 
-target_metadata = None
-
 
 def run_migrations_offline() -> None:
     context.configure(
         url='postgresql://unused',
-        target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={'paramstyle': 'named'},
     )
@@ -24,6 +21,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
+    if getattr(context.config.cmd_opts, 'autogenerate', False):
+        from zeit.connector.postgresql import METADATA
+    else:
+        METADATA = None
+
     args = context.get_x_argument(as_dictionary=True)
     pgservice = args.get('service')
     if pgservice:
@@ -33,7 +35,7 @@ def run_migrations_online() -> None:
 
     engine = create_engine(dsn, poolclass=pool.NullPool)
     with engine.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(connection=connection, target_metadata=METADATA)
 
         with context.begin_transaction():
             context.run_migrations()
