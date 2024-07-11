a4875af52032b147c2829d867e8e7c1c6ef808b1
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-07-11 16:30:36 +0200

    ZO-141: Remove VM-only stopfile mechanism

--- core/src/zeit/cms/browser/healthcheck.py
+++ core/src/zeit/cms/browser/healthcheck.py
@@ -1,13 +1,3 @@
-import os.path
-
-import zope.app.appsetup.product
-
-
 class HealthCheck:
     def __call__(self):
-        config = zope.app.appsetup.product.getProductConfiguration('zeit.cms')
-        stopfile = config.get('stopfile')
-        if stopfile and os.path.exists(stopfile):
-            self.request.response.setStatus(500)
-            return 'fail: stopfile %s present' % stopfile
         return 'OK'
