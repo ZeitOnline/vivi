09f2840ed3e82e6aefcc45e653dd0374edc671d7
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-07-11 17:16:34 +0200

    ZO-141: Remove test for stopfile (belongs to a4875af)

--- core/src/zeit/cms/browser/tests/test_healthcheck.py
+++ core/src/zeit/cms/browser/tests/test_healthcheck.py
@@ -1,9 +1,3 @@
-import os
-import tempfile
-import urllib.error
-
-import zope.app.appsetup.product
-
 import zeit.cms.testing
 
 
@@ -19,20 +13,3 @@ class HealthCheckTest(zeit.cms.testing.ZeitCmsBrowserTestCase):
         b.open(self.check)
         self.assertEqual('200 Ok', b.headers['status'])
         self.assertEqual('OK', b.contents)
-
-    def test_should_fail_if_stopfile_exists(self):
-        handle, filename = tempfile.mkstemp()
-        os.close(handle)
-        os.unlink(filename)
-        config = zope.app.appsetup.product.getProductConfiguration('zeit.cms')
-        config['stopfile'] = filename
-
-        b = self.browser
-        with self.assertNothingRaised():
-            b.open(self.check)
-
-        open(filename, 'w').close()
-        with self.assertRaises(urllib.error.HTTPError) as info:
-            b.open(self.check)
-            self.assertEqual(500, info.exception.status)
-            self.assertEqual('fail: stopfile %s present' % filename)
