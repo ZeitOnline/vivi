246174b3c777bd9222c541fb4a8acef61ec4155c
Martin Stolle <martin.stolle@zeit.de>
2024-09-17 11:06:47 +0200

    WCM-285: add migrations

---
+++ core/src/zeit/connector/migrations/postdeploy/20240912_1058-6cc99f5afdc5_add_channels_columns_.py
@@ -0,0 +1,34 @@
+"""add channels columns index
+
+Revision ID: 6cc99f5afdc5
+Revises: cf24009572b7
+Create Date: 2024-09-12 10:58:00.181930
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6cc99f5afdc5'
+down_revision: Union[str, None] = 'cf24009572b7'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with op.get_context().autocommit_block():
+        op.create_index(
+            'ix_properties_channels',
+            'properties',
+            ['channels'],
+            unique=False,
+            postgresql_using='gin',
+            postgresql_concurrently=True,
+            if_not_exists=True,
+        )
+
+
+def downgrade() -> None:
+    op.drop_index('ix_properties_channels', table_name='properties', postgresql_using='gin')
