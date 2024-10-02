1b93cea76bc56587f8bad7f62e691afef21de58f
Jannis Jahr <jannis.jahr@zeit.de>
2024-10-02 16:24:24 +0200

    WCM-401: add support for images in audios that are not podcasts

--- core/src/zeit/content/audio/audio.py
+++ core/src/zeit/content/audio/audio.py
@@ -17,6 +17,7 @@ import zeit.cms.repository.interfaces
 import zeit.cms.type
 import zeit.content.article.interfaces
 import zeit.content.audio.interfaces
+import zeit.content.image.imagereference
 import zeit.content.image.interfaces
 
 
@@ -72,9 +73,18 @@ class AudioType(zeit.cms.type.XMLContentTypeDeclaration):
     type = 'audio'  # Wert für {http://namespaces.zeit.de/CMS/meta}type
 
 
+@grok.adapter(IAudio)
 @grok.implementer(zeit.content.image.interfaces.IImages)
-class PodcastImage(grok.Adapter):
-    grok.context(IAudio)
+def audio_image(context):
+    if context.audio_type == 'podcast':
+        return PodcastImage(context)
+    return zeit.content.image.imagereference.ImagesAdapter(context)
+
+
+@zope.interface.implementer(zeit.content.image.interfaces.IImages)
+class PodcastImage:
+    def __init__(self, context):
+        self.context = context
 
     def _get_podcast(self):
         if self.context.audio_type == 'podcast':
