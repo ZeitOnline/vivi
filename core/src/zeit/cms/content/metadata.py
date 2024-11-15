8f6e8e8d3431ac162bee54363ed3be59ea42e7ec
Martin Stolle <martin.stolle@zeit.de>
2024-11-15 11:01:18 +0100

    MAINT: add property printRessort until zeit.web vivi version upgrade

--- core/src/zeit/cms/content/metadata.py
+++ core/src/zeit/cms/content/metadata.py
@@ -118,6 +118,11 @@ class CommonMetadata(zeit.cms.content.xmlsupport.XMLContentBase):
             return None
         return value
 
+    # XXX remove after zeit.web update
+    @property
+    def printRessort(self):
+        return self.print_ressort
+
     @color_scheme.setter
     def color_scheme(self, value):
         self._color_scheme = value
