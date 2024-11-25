738db12d4a5fca9ed91aa2b0ee3400eb968aa0ad
Jannis Jahr <jannis.jahr@zeit.de>
2024-11-25 14:31:28 +0100

    ZO-6370: add tests for image row

---
+++ core/src/zeit/content/article/edit/tests/test_image_row.py
@@ -0,0 +1,34 @@
+import lxml.builder
+
+from zeit.content.article.edit.image_row import ImageRow
+import zeit.content.article.testing
+import zeit.edit.interfaces
+
+
+class ImageRowTest(zeit.content.article.testing.FunctionalTestCase):
+    def get_image_row(self):
+        image_row = ImageRow(None, lxml.builder.E.image_row())
+        return image_row
+
+    def test_image_row_should_be_set(self):
+        from zeit.content.image.testing import create_image_group_with_master_image
+
+        # (<zeit.content.image.imagegroup.ImageGroup http://xml.zeit.de/2024-11/hund>, None, None)
+        image_row = self.get_image_row()
+        image_row.display_mode = 'square'
+        image_row.variant_name = 'default'
+        image_group = create_image_group_with_master_image()
+        image_row.images = (
+            (
+                image_group,
+                'Fortune of Count Olaf',
+                'fortune-count-olaf',
+            ),
+        )
+        self.assertEqual('square', image_row.xml.xpath('.')[0].get('display_mode'))
+        self.assertEqual('default', image_row.xml.xpath('.')[0].get('variant_name'))
+        self.assertEqual(
+            'http://xml.zeit.de/group/', image_row.xml.xpath('./image')[0].get('base-id')
+        )
+        self.assertEqual('Fortune of Count Olaf', image_row.xml.xpath('./image')[0].get('caption'))
+        self.assertEqual('fortune-count-olaf', image_row.xml.xpath('./image')[0].get('alt_text'))
