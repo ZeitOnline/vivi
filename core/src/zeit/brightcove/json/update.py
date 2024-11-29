761a751a9cb6107dd791e1bbef7c6c35dee26190
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-11-29 10:49:11 +0100

    WCM-568: Record brightcove webhook event body for tracing, to aid debugging

--- core/src/zeit/brightcove/json/update.py
+++ core/src/zeit/brightcove/json/update.py
@@ -1,6 +1,8 @@
 import json
 import logging
 
+import opentelemetry.trace
+
 import zeit.brightcove.update
 import zeit.cms.config
 
@@ -26,9 +28,13 @@ class Notification:
 
     def __call__(self):
         body = self.request.bodyStream.read(int(self.request['CONTENT_LENGTH']))
+        current_span = opentelemetry.trace.get_current_span()
+        current_span.set_attributes({'http.body': body})
+
         data = json.loads(body)
         if data.get('event') != 'video-change' or not data.get('video'):
             return
+
         zeit.brightcove.update.import_video_async.delay(
             data.get('video'),
             _principal_id_=zeit.cms.config.required('zeit.brightcove', 'index-principal'),
