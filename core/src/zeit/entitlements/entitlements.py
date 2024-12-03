98d5ca030e1b99c68f6e8ddae15d80602192735a
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-12-03 16:30:28 +0100

    WCM-532: Implement the two currently known rules for calculating entitlements

--- core/src/zeit/entitlements/entitlements.py
+++ core/src/zeit/entitlements/entitlements.py
@@ -1,6 +1,7 @@
 import grokcore.component as grok
 import zope.interface
 
+from zeit.content.audio.interfaces import IAudioReferences
 import zeit.cms.content.interfaces
 import zeit.entitlements.interfaces
 
@@ -17,7 +18,15 @@ class AcceptedEntitlements(grok.Adapter):
             case 'registration':
                 return {'registration'}
             case 'abo':
-                return {'zplus'}
+                result = {'zplus'}
+
+                if content.ressort == 'zeit-magazin' and content.sub_ressort == 'wochenmarkt':
+                    result.add('wochenmarkt')
+
+                if IAudioReferences(content).get_by_type('podcast'):
+                    result.add('podcast')
+
+                return result
             case _:
                 raise ValueError(f'{content} has invalid access value: {content.access}')
 
--- core/src/zeit/entitlements/tests/test_entitlements.py
+++ core/src/zeit/entitlements/tests/test_entitlements.py
@@ -1,4 +1,7 @@
+from zeit.cms.checkout.helper import checked_out
 from zeit.cms.testcontenttype.testcontenttype import ExampleContentType
+from zeit.content.audio.interfaces import IAudioReferences
+from zeit.content.audio.testing import AudioBuilder
 import zeit.entitlements
 import zeit.entitlements.testing
 
@@ -22,4 +25,19 @@ class Entitlements(zeit.entitlements.testing.FunctionalTestCase):
     def test_empty_for_not_commonmetadata(self):
         self.assertEqual(
             set(), zeit.entitlements.accepted(self.repository['2006']['DSC00109_2.JPG.meta'])
+        )
+
+    def test_ressort_wochenmarkt_adds_entitlement(self):
+        self.content.access = 'abo'
+        self.content.ressort = 'zeit-magazin'
+        self.content.sub_ressort = 'wochenmarkt'
+        self.assertEqual({'wochenmarkt', 'zplus'}, zeit.entitlements.accepted(self.content))
+
+    def test_podcast_audio_reference_adds_entitlement(self):
+        audio = AudioBuilder().build(self.repository)
+        with checked_out(self.repository['testcontent']) as co:
+            co.access = 'abo'
+            IAudioReferences(co).add(audio)
+        self.assertEqual(
+            {'podcast', 'zplus'}, zeit.entitlements.accepted(self.repository['testcontent'])
         )
