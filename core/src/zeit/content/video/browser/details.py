6764056d32d4a5979a4468c0c4a3f8cdd10d6596
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-01-17 14:04:01 +0100

    MAINT: Simplify spelling

--- core/src/zeit/content/video/browser/details.py
+++ core/src/zeit/content/video/browser/details.py
@@ -1,20 +1,7 @@
-import importlib.resources
-
-from zope.browserpage import ViewPageTemplateFile
-
-import zeit.cms.related.interfaces
-import zeit.cms.workflow.interfaces
-import zeit.content.video.interfaces
+import zeit.cms.browser.objectdetails
 
 
 class Details(zeit.cms.browser.objectdetails.Details):
-    index = ViewPageTemplateFile(
-        str(importlib.resources.files(__package__) / 'object-details-body.pt')
-    )
-
-    def __call__(self):
-        return self.index()
-
     @property
     def graphical_preview_url(self):
         return self.context.video_still
