82126c479650e6b7bac8e0a704888aed12ff642f
Louisa von Hülsen <louisa.huelsen@zeit.de>
2025-02-03 15:44:09 +0100

    MAINT: Make vivi ui playwright test more robust

--- smoketest/test_vivi.py
+++ smoketest/test_vivi.py
@@ -9,6 +9,13 @@ def test_login_to_vivi_with_firefox(page):
 @pytest.mark.parametrize('name', ['bahn-dumping-schwarzbuch', 'nightwatch-index'])
 def test_checkout_content(page, name):
     page.goto(f'/repository/wirtschaft/2010-01/{name}')
+    page.get_by_role('link', name='Checkout ^O').wait_for()
     page.get_by_role('link', name='Checkout ^O').click()
+    page.get_by_role('link', name='Admin CO').wait_for()
     assert page.locator('.title').first.is_editable()
+    assert page.get_by_role('link', name='Admin CO').is_visible()
+
+    page.get_by_role('link', name='Checkin ^I').wait_for()
     page.get_by_role('link', name='Checkin ^I').click()
+    page.get_by_role('link', name='Workflow').wait_for()
+    assert page.get_by_role('link', name='Workflow').is_visible()
