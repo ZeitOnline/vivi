36e4be299f7839d4741e03b9c84830ddffd9a4d1
Martin Stolle <martin.stolle@zeit.de>
2025-03-24 10:42:48 +0100

    WCM-694: replace elastic call with storage query and add scheduled publish

--- core/src/zeit/workflow/cli.py
+++ core/src/zeit/workflow/cli.py
@@ -1,51 +1,59 @@
 import logging
 
+from sqlalchemy import select
+from sqlalchemy import text as sql
 import z3c.celery.celery
 import zope.component
 
-from zeit.cms.cli import commit_with_retry, runner
+from zeit.cms.cli import commit_with_retry, from_config, runner
+from zeit.connector.models import Content as ConnectorModel
 import zeit.cms.workflow.interfaces
 
 
 log = logging.getLogger(__name__)
 
 
-@runner(principal=zeit.cms.cli.from_config('zeit.workflow', 'retract-timebased-principal'))
-def retract_overdue_objects():
-    import zeit.find.interfaces
-    import zeit.retresco.interfaces
-
-    tms = zope.component.getUtility(zeit.retresco.interfaces.ITMS)
-    es = zope.component.getUtility(zeit.find.interfaces.ICMSSearch)
-    unretracted = es.search(
-        {
-            'query': {
-                'bool': {
-                    'filter': [
-                        {'term': {'payload.workflow.published': True}},
-                        {'range': {'payload.workflow.released_to': {'lt': 'now-15m'}}},
-                    ]
-                }
-            },
-            '_source': ['url', 'doc_id'],
-        },
-        rows=1000,
-    )
-
-    for item in unretracted:
-        uniqueId = 'http://xml.zeit.de' + item['url']
-        content = zeit.cms.interfaces.ICMSContent(uniqueId, None)
-        if content is None:
-            log.info('Not found: %s, deleting from TMS', uniqueId)
-            tms.delete_id(item['doc_id'])
-        else:
-            publish = zeit.cms.workflow.interfaces.IPublish(content)
-            for _ in commit_with_retry():
-                log.info('Retracting %s', content)
-                try:
+def _handle_scheduled_content(action, sql_query):
+    query = select(ConnectorModel)
+    query = query.where(sql(sql_query))
+    repository = zope.component.getUtility(zeit.cms.repository.interfaces.IRepository)
+    results = repository.search(query)
+    for content in results:
+        publish = zeit.cms.workflow.interfaces.IPublish(content)
+        for _ in commit_with_retry():
+            try:
+                if action == 'publish':
+                    publish.publish(background=False)
+                else:
                     publish.retract(background=False)
-                except z3c.celery.celery.HandleAfterAbort as e:
-                    if 'LockingError' in e.message:  # kludgy
-                        log.warning('Skip %s due to %s', content, e.message)
-                        break
-                    raise
+            except z3c.celery.celery.HandleAfterAbort as e:
+                if 'LockingError' in e.message:  # kludgy
+                    log.warning('Skip %s due to %s', content, e.message)
+                    break
+                raise
+
+
+def _retract_scheduled_content():
+    sql_query = """
+        published = true
+        AND date_scheduled_retract <= NOW()
+    """
+    _handle_scheduled_content('retract', sql_query)
+
+
+def _publish_scheduled_content():
+    sql_query = """
+        published = false
+        AND date_scheduled_publish <= NOW()
+    """
+    _handle_scheduled_content('publish', sql_query)
+
+
+@runner(principal=from_config('zeit.workflow', 'retract-timebased-principal'))
+def retract_scheduled_content():
+    _retract_scheduled_content()
+
+
+@runner(principal=from_config('zeit.workflow', 'retract-timebased-principal'))
+def publish_scheduled_content():
+    _publish_scheduled_content()
