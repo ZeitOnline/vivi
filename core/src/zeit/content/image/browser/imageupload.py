e611d5f6c5bff2d8c7c2ef5259bf30f935a593da
Adrian Heine <adrian.heine@zeit.de>
2025-06-24 12:11:24 +0200

    WCM-936: Image upload skeleton implementation

---
+++ core/src/zeit/content/image/browser/imageupload.py
@@ -0,0 +1,56 @@
+from collections.abc import Iterable
+import uuid
+
+from zeit.cms.i18n import MessageFactory as _
+import zeit.cms.browser.view
+import zeit.cms.browser.widget
+import zeit.cms.interfaces
+import zeit.cms.repository.interfaces
+import zeit.content.image.browser.imagegroup
+import zeit.content.image.image
+
+
+class UploadForm(zeit.cms.browser.view.Base):
+    # FIXME: Translations
+    title = _('Upload images')
+
+    def __call__(self):
+        if self.request.method == 'POST':
+            return self.handle_post()
+        return super().__call__()
+
+    def action(self):
+        return self.url(self.context, '@@upload-images')
+
+    def handle_post(self):
+        # Since self.context is potentially checked out, we cannot rely on self.context.__parent__
+        target = zeit.cms.interfaces.ICMSContent(self.context.uniqueId)
+
+        # If we upload in a folder, we don't want to go up
+        if not zeit.cms.repository.interfaces.IFolder.providedBy(target):
+            target = target.__parent__
+
+        files = self.request.form['files']
+        if not isinstance(files, Iterable):
+            files = (files,)
+        for file in files:
+            self._upload_imagegroup(file, target)
+
+        url = self.url(target)
+        self.redirect(url, status=303)
+
+    def _upload_imagegroup(self, file, parent):
+        imagegroup = zeit.content.image.imagegroup.ImageGroup()
+
+        form = zeit.content.image.browser.imagegroup.AddForm(None, None)
+        form.new_object = imagegroup
+        form.adapters = {}
+        imagegroup = form.create({'master_image_blobs': (file,)})
+
+        name = '{0}.tmp'.format(uuid.uuid4())
+        zeit.cms.repository.interfaces.IAutomaticallyRenameable(imagegroup).renameable = True
+        parent[name] = imagegroup
+
+        for image in form.images:
+            if image is not None:
+                imagegroup[image.__name__] = image
---
+++ core/src/zeit/content/image/browser/tests/test_imageupload.py
@@ -0,0 +1,156 @@
+import webtest.forms
+
+from zeit.content.image.testing import fixture_bytes
+import zeit.cms.browser.interfaces
+import zeit.cms.interfaces
+import zeit.content.image.testing
+
+
+# zope.testbrowser.browser.Control.add_file cannot yet handle multiple file inputs as implemented by
+# https://github.com/Pylons/webtest/commit/d1dbc25f53a031d03112cb1e44f4a060cf3665cd
+def add_file_multi(control, files):
+    control._form[control.name] = [
+        webtest.forms.Upload(filename, file, content_type)
+        for (file, filename, content_type) in files
+    ]
+
+
+class ImageUploadBrowserTest(zeit.content.image.testing.BrowserTestCase):
+    def test_article_has_images_upload_form(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+
+    def test_redirects_after_upload(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                )
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        assert b.url.endswith('/2007/01')
+        # FIXME Redirect to multi-edit
+
+    def test_redirects_after_upload_in_folder(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                )
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        assert b.url.endswith('/2007/01')
+        # FIXME Redirect to multi-edit
+
+    def test_can_upload_image(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                )
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
+        img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
+        assert zeit.content.image.interfaces.IImageGroup.providedBy(img)
+
+    def test_can_upload_image_from_folder(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                )
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
+        img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
+        assert zeit.content.image.interfaces.IImageGroup.providedBy(img)
+
+    def test_can_upload_multiple_images(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                ),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
+        images = tuple(x for x in folder.values() if 'tmp' in x.uniqueId)
+        assert len(images) == 2
+        for img in images:
+            assert zeit.content.image.interfaces.IImageGroup.providedBy(img)
+
+    def test_huge_image_is_resized(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('shoppingmeile_2251x4001px.jpg'),
+                    'shoppingmeile_2251x4001px.jpg',
+                    'image/jpg',
+                )
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
+        img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
+        self.assertEqual((2250, 4000), img[img.master_image].getImageSize())
+
+    def test_image_is_automatically_renameble(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                )
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
+        img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
+        self.assertTrue(zeit.cms.repository.interfaces.IAutomaticallyRenameable(img).renameable)
+        self.assertFalse(zeit.cms.repository.interfaces.IAutomaticallyRenameable(img).rename_to)
