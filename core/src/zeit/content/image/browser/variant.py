9b510e0e127ecfc8c27c0e17fd3246d53d4a4e4b
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-06-25 14:39:15 +0200

    WCM-933: Make conflict prevention actually work again (belongs to 7e894c5)

--- core/src/zeit/content/image/browser/variant.py
+++ core/src/zeit/content/image/browser/variant.py
@@ -81,5 +81,6 @@ class Editor:
         # Force generating thumbnail source if does not exist yet, so not each
         # variant preview tries to do it simultaneously later on (which only
         # leads to conflicts).
-        zeit.content.image.interfaces.IThumbnails(self.context).source_image
+        thumbnails = zeit.content.image.interfaces.IThumbnails(self.context)
+        thumbnails.source_image(thumbnails.master_image(''))
         return super().__call__()
