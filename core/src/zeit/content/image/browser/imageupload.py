24c23ff00b94399b8801811c40935c3c26cbad1c
Adrian Heine <adrian.heine@zeit.de>
2025-07-02 12:28:27 +0200

    WCM-936: Flesh out edit-images

--- core/src/zeit/content/image/browser/imageupload.py
+++ core/src/zeit/content/image/browser/imageupload.py
@@ -2,9 +2,14 @@ from collections.abc import Iterable
 import urllib.parse
 import uuid
 
+import lxml.etree
+import PIL.Image
+import zope.formlib.form
+import zope.formlib.widgets
+
 from zeit.cms.i18n import MessageFactory as _
+import zeit.cms.browser.form
 import zeit.cms.browser.view
-import zeit.cms.browser.widget
 import zeit.cms.interfaces
 import zeit.cms.repository.interfaces
 import zeit.content.image.browser.imagegroup
@@ -23,12 +28,16 @@ class UploadForm(zeit.cms.browser.view.Base):
     def action(self):
         return self.url(self.context, '@@upload-images')
 
+    def from_name(self):
+        return self.context.__name__
+
     def handle_post(self):
         # Since self.context is potentially checked out, we cannot rely on self.context.__parent__
         target = zeit.cms.interfaces.ICMSContent(self.context.uniqueId)
 
+        in_folder = zeit.cms.repository.interfaces.IFolder.providedBy(target)
         # If we upload in a folder, we don't want to go up
-        if not zeit.cms.repository.interfaces.IFolder.providedBy(target):
+        if not in_folder:
             target = target.__parent__
 
         files = self.request.form['files']
@@ -40,11 +49,10 @@ class UploadForm(zeit.cms.browser.view.Base):
             result = self._upload_imagegroup(file, target)
             results.append(result)
 
-        url = (
-            self.url(target, '@@edit-images')
-            + '?'
-            + urllib.parse.urlencode((('files', results),), doseq=True)
-        )
+        params = {'files': results}
+        if not in_folder:
+            params['from'] = self.context.__name__
+        url = self.url(target, '@@edit-images') + '?' + urllib.parse.urlencode(params, doseq=True)
         self.redirect(url, status=303)
 
     def _upload_imagegroup(self, file, parent):
@@ -66,4 +74,65 @@ class UploadForm(zeit.cms.browser.view.Base):
 
 
 class EditForm(zeit.cms.browser.view.Base):
-    pass
+    # FIXME: Translations
+    title = _('Edit images')
+
+    def rows(self):
+        from_name = self.request.form.get('from', None)
+        filenames = self.request.form.get('files', ())
+        if isinstance(filenames, str):
+            filenames = (filenames,)
+        result = []
+        name_index = 1
+        for name in filenames:
+            imggroup = self.context[name]
+
+            if from_name:
+                while True:
+                    suffix = ''
+                    if len(filenames) >= 10:
+                        suffix = f'-{name_index:02}'
+                    elif name_index > 1 or len(filenames) > 1:
+                        suffix = f'-{name_index}'
+                    name = f'{from_name}-bild{suffix}'
+                    name_index += 1
+                    if not self.context.get(name):
+                        break
+
+            data = self._image_data(imggroup[imggroup.master_image])
+            data = data.get('xmpmeta', {}).get('RDF', {}).get('Description', {})
+            result.append(
+                {
+                    'cur_name': imggroup.__name__,
+                    'name': name,
+                    'title': data.get('Headline', ''),
+                    'copyright': ' / '.join(
+                        filter(
+                            None,
+                            (
+                                data.get('creator', {}).get('Seq', {}).get('li', None),
+                                data.get('Credit', None),
+                            ),
+                        )
+                    ),
+                    'description': data.get('description', {})
+                    .get('Alt', {})
+                    .get('li', {})
+                    .get('text', ''),
+                    'thumbnail': self.url(imggroup, 'thumbnail'),
+                }
+            )
+        return result
+
+    def _image_data(self, img):
+        try:
+            with zope.security.proxy.removeSecurityProxy(img.open()) as f:
+                PIL.Image.ElementTree = lxml.etree
+                image = PIL.Image.open(f)
+                image.load()
+                return image.getxmp()
+        except IOError:
+            # FIXME: only log
+            raise zeit.content.image.interfaces.ImageProcessingError(
+                'Cannot retrieve XMP metadata from image %s' % img.__name__
+            )
--- core/src/zeit/content/image/browser/tests/test_imageupload.py
+++ core/src/zeit/content/image/browser/tests/test_imageupload.py
@@ -39,7 +39,7 @@ class ImageUploadBrowserTest(zeit.content.image.testing.BrowserTestCase):
         b.getForm(name='imageupload').submit()
         folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
         img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
-        assert b.url.endswith('/2007/01/@@edit-images?files=' + img.__name__)
+        assert b.url.endswith(f'/2007/01/@@edit-images?files={img.__name__}&from=Somalia')
 
     def test_redirects_after_upload_in_folder(self):
         b = self.browser
@@ -58,7 +58,7 @@ class ImageUploadBrowserTest(zeit.content.image.testing.BrowserTestCase):
         b.getForm(name='imageupload').submit()
         folder = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/online/2007/01')
         img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
-        assert b.url.endswith('/2007/01/@@edit-images?files=' + img.__name__)
+        assert b.url.endswith(f'/2007/01/@@edit-images?files={img.__name__}')
 
     def test_can_upload_image(self):
         b = self.browser
@@ -142,6 +142,7 @@ class ImageUploadBrowserTest(zeit.content.image.testing.BrowserTestCase):
         assert '/2007/01/@@edit-images?files=' in b.url
         assert re.search('[&?]files=' + re.escape(images[0].__name__) + '(&|$)', b.url)
         assert re.search('[&?]files=' + re.escape(images[1].__name__) + '(&|$)', b.url)
+        assert re.search('[&?]from=Somalia(&|$)', b.url)
 
     def test_huge_image_is_resized(self):
         b = self.browser
@@ -162,7 +163,7 @@ class ImageUploadBrowserTest(zeit.content.image.testing.BrowserTestCase):
         img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
         self.assertEqual((2250, 4000), img[img.master_image].getImageSize())
 
-    def test_image_is_automatically_renameble(self):
+    def test_imagegroup_is_automatically_renameble(self):
         b = self.browser
         b.open('/repository/online/2007/01/Somalia/@@upload-images')
         file_input = b.getControl(name='files')
@@ -181,3 +182,67 @@ class ImageUploadBrowserTest(zeit.content.image.testing.BrowserTestCase):
         img = next(x for x in folder.values() if 'tmp' in x.uniqueId)
         self.assertTrue(zeit.cms.repository.interfaces.IAutomaticallyRenameable(img).renameable)
         self.assertFalse(zeit.cms.repository.interfaces.IAutomaticallyRenameable(img).rename_to)
+
+    def test_editimages_correctly_names_single_image(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                ),
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        assert b.getControl(name='name[0]').value == 'Somalia-bild'
+
+    def test_editimages_correctly_names_multiple_images(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                ),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        assert b.getControl(name='name[0]').value == 'Somalia-bild-1'
+        assert b.getControl(name='name[1]').value == 'Somalia-bild-2'
+
+    def test_editimages_correctly_names_many_images(self):
+        b = self.browser
+        b.open('/repository/online/2007/01/Somalia/@@upload-images')
+        file_input = b.getControl(name='files')
+        add_file_multi(
+            file_input,
+            [
+                (
+                    fixture_bytes('new-hampshire-450x200.jpg'),
+                    'new-hampshire-450x200.jpg',
+                    'image/jpg',
+                ),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+                (fixture_bytes('opernball.jpg'), 'opernball.jpg', 'image/jpg'),
+            ],
+        )
+        b.getForm(name='imageupload').submit()
+        assert b.getControl(name='name[0]').value == 'Somalia-bild-01'
+        assert b.getControl(name='name[1]').value == 'Somalia-bild-02'
+        assert b.getControl(name='name[9]').value == 'Somalia-bild-10'
