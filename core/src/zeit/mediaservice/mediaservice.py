e1ba3994006e1e7a7eff36250ba6e4bc8296904c
Adrian Heine <adrian.heine@zeit.de>
2025-07-24 11:34:33 +0200

    WCM-983: Introduce MediaService

--- core/src/zeit/mediaservice/mediaservice.py
+++ core/src/zeit/mediaservice/mediaservice.py
@@ -11,40 +11,52 @@ import zeit.content.audio.audio
 @zeit.cms.celery.task(queue='mediaservice')
 def create_audio_objects(volume_uniqueid):
     volume = zeit.cms.interfaces.ICMSContent(volume_uniqueid)
-    articles = volume.get_articles()
-    ensure_audio_folder(volume)
-    audios = get_audios(volume)
-    _create_audio_objects(volume, articles, audios)
+    mediaservice = MediaService()
+    mediaservice.create_audio_objects(volume)
 
 
-def get_audios(volume):
-    connection = zope.component.getUtility(zeit.mediaservice.interfaces.IConnection)
-    return connection.get_audio_infos(volume.year, volume.volume)
+class MediaService:
+    def create_audio_objects(self, volume):
+        articles = volume.get_articles()
+        folder = self._audio_folder(volume)
+        audios = self._get_audios(volume)
+        self._create_audio_objects(folder, articles, audios)
 
+    def _get_audios(self, volume):
+        connection = zope.component.getUtility(zeit.mediaservice.interfaces.IConnection)
+        return connection.get_audio_infos(volume.year, volume.volume)
 
-def ensure_audio_folder(volume):
-    zeit.cms.content.add.find_or_create_folder(
-        'premium', 'audio', str(volume.year), volume.volume_number
-    )
+    def _audio_folder(self, volume):
+        return zeit.cms.content.add.find_or_create_folder(
+            'premium', 'audio', str(volume.year), volume.volume_number
+        )
 
+    # FIXME: Peek at zeit.speech
+    def _create_audio_objects(self, folder, articles, audios):
+        for article in articles:
+            audio_info = audios.get(article.ir_mediasync_id)
+            if not audio_info:
+                continue
 
-# FIXME: Peek at zeit.speech
-def _create_audio_objects(volume, articles, audios):
-    repository = zope.component.getUtility(zeit.cms.repository.interfaces.IRepository)
-    folder = repository['premium']['audio'][str(volume.year)][volume.volume_number]
-    for article in articles:
-        audio_info = audios.get(article.ir_mediasync_id)
-        if not audio_info:
-            continue
+            audio = self._create_audio_object(article, audio_info)
+            if not audio:
+                continue
 
+            article_uuid = zeit.cms.content.interfaces.IUUID(article)
+            folder[article_uuid.shortened] = audio
+            with checked_out(article, raise_if_error=True) as co:
+                references = zeit.content.audio.interfaces.IAudioReferences(co)
+                # FIXME: Deduplicate, see speech
+                references.add(folder[article_uuid.shortened])
+
+    def _create_audio_object(self, article, audio_info):
         audio_url = audio_info.get('url')
         if not audio_url:
             err = ValueError(f'Premium audio info without URL for {article.ir_mediasync_id}')
             current_span = opentelemetry.trace.get_current_span()
             current_span.record_exception(err)
-            continue
+            return
 
-        article_uuid = zeit.cms.content.interfaces.IUUID(article)
         audio = zeit.content.audio.audio.Audio()
         audio.audio_type = 'premium'
         audio.external_id = article.ir_mediasync_id
@@ -55,9 +67,4 @@ def _create_audio_objects(volume, articles, audios):
             current_span = opentelemetry.trace.get_current_span()
             current_span.record_exception(err)
         audio.duration = audio_duration
-
-        folder[article_uuid.shortened] = audio
-        with checked_out(article, raise_if_error=True) as co:
-            references = zeit.content.audio.interfaces.IAudioReferences(co)
-            # FIXME: Deduplicate, see speech
-            references.add(folder[article_uuid.shortened])
+        return audio
