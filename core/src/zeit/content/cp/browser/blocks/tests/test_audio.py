59680ba216569d79fb494e3e1a0a75a998553e20
Valentin von Guttenberg <valentin.vonguttenberg@zeit.de>
2025-08-01 11:12:11 +0200

    ES-204: Add test

--- core/src/zeit/content/cp/browser/blocks/tests/test_audio.py
+++ core/src/zeit/content/cp/browser/blocks/tests/test_audio.py
@@ -11,3 +11,14 @@ class TestPodcastHeader(zeit.content.cp.testing.BrowserTestCase):
         b.open('http://localhost/++skin++vivi/repository/centerpage/@@checkout')
         b.open('contents')
         self.assertEqual(1, b.contents.count('type-podcastheader'))
+
+
+class TestPodcastMetadata(zeit.content.cp.testing.BrowserTestCase):
+    def test_podcastmetadata_can_be_added(self):
+        self.centerpage = zeit.content.cp.centerpage.CenterPage()
+        self.centerpage.body['lead'].create_item('podcastmetadata')
+        self.repository['centerpage'] = self.centerpage
+        b = self.browser
+        b.open('http://localhost/++skin++vivi/repository/centerpage/@@checkout')
+        b.open('contents')
+        self.assertEqual(1, b.contents.count('type-podcastmetadata'))
