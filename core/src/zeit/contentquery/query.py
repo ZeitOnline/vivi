392a2dcc771af0e3333297e40ce5e7900f5d2233
thomaspuppe <info@thomaspuppe.de>
2025-08-26 14:25:30 +0200

    WCM-1058: Fix order, because id cannot be NULL

--- core/src/zeit/contentquery/query.py
+++ core/src/zeit/contentquery/query.py
@@ -192,15 +192,12 @@ class SQLCustomContentQuery(SQLContentQuery):
 
     @property
     def order(self):
-        # everything date related desc, pages asc
-        direction = 'desc'
-        order_column = self.order_column
+        # everything date related DESC, pages ASC
 
         if self.order_column == 'print_page':
-            direction = 'asc'
-            order_column = 'print_page,id'
+            return 'print_page ASC NULLS LAST, id DESC'
 
-        return f'{order_column} {direction} nulls last'
+        return f'{self.order_column} DESC NULLS LAST'
 
     @property
     def order_column(self):
