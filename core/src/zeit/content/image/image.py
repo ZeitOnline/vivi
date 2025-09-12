6891e53955d7aa30304b3d3c7da121ca44cdc064
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-09-12 09:04:59 +0200

    WCM-861: Move XMP parsing functions into their own module

--- core/src/zeit/content/image/image.py
+++ core/src/zeit/content/image/image.py
@@ -14,6 +14,7 @@ import zope.location.interfaces
 import zope.security.proxy
 
 from zeit.cms.i18n import MessageFactory as _
+from zeit.content.image.xmp import extract_metadata_from_xmp
 import zeit.cms.content.interfaces
 import zeit.cms.interfaces
 import zeit.cms.repository.file
@@ -24,52 +25,6 @@ import zeit.content.image.imagegroup
 import zeit.content.image.interfaces
 import zeit.workflow.interfaces
 import zeit.workflow.timebased
-
-
-def extract_metadata_from_xmp(xmp):
-    result = {'title': None, 'copyright': None, 'caption': None}
-    if 'xapmeta' in xmp:
-        data = xmp['xapmeta']
-        data = data.get('RDF', {}).get('Description', {})
-        if isinstance(data, dict):
-            data = (data,)
-        for item in data:
-            if 'Headline' in item:
-                result['title'] = item['Headline']
-            if 'creator' in item:
-                result['creator'] = item['creator'].get('Seq', {}).get('li', None)
-            if 'Credit' in item:
-                result['credit'] = item['Credit']
-            if 'description' in item:
-                result['caption'] = (
-                    item.get('description', {}).get('Alt', {}).get('li', {}).get('text', None)
-                )
-    if 'xmpmeta' in xmp:
-        data = xmp['xmpmeta']
-        data = data.get('RDF', {}).get('Description', {})
-        if isinstance(data, dict):
-            data = (data,)
-        for item in data:
-            if 'Headline' in item:
-                result['title'] = item['Headline']
-            if 'creator' in item:
-                result['creator'] = item['creator'].get('Seq', {}).get('li', None)
-            if 'Credit' in item:
-                result['credit'] = item['Credit']
-            if 'description' in item:
-                result['caption'] = (
-                    item.get('description', {}).get('Alt', {}).get('li', {}).get('text', None)
-                )
-
-    if 'credit' in result or 'creator' in result:
-        result['copyright'] = '/'.join(
-            filter(None, (result.get('creator', None), result.get('credit', None)))
-        )
-        if 'credit' in result:
-            del result['credit']
-        if 'creator' in result:
-            del result['creator']
-    return result
 
 
 class BaseImage:
