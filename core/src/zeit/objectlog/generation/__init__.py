bcac8fd07ef29c13f0a8271fdfc462087f825ef4
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-09-17 11:51:09 +0200

    MAINT: Extract helper function in zeit.objectlog.generations
    So far we don't import zeit.cms here at all (only in tests), so let's
    maybe not start that now, even though this code is duplicated from
    zeit.cms.generation.

--- core/src/zeit/objectlog/generation/__init__.py
+++ core/src/zeit/objectlog/generation/__init__.py
@@ -1,4 +1,7 @@
+import zope.component
+import zope.component.hooks
 import zope.generations.generations
+import zope.generations.utility
 
 
 minimum_generation = 1
@@ -7,3 +10,13 @@ generation = 1
 manager = zope.generations.generations.SchemaManager(
     minimum_generation, generation, 'zeit.objectlog.generation'
 )
+
+
+def do_evolve(context, func):
+    site = zope.component.hooks.getSite()
+    try:
+        root = zope.generations.utility.getRootFolder(context)
+        zope.component.hooks.setSite(root)
+        func(root)
+    finally:
+        zope.component.hooks.setSite(site)
