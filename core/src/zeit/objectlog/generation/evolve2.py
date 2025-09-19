8d63d8fc5f3a968478a564e87cdd777fd1dc69ff
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-09-19 10:58:26 +0200

    WCM-633: Handle zodb data loss during objectlog migration

--- core/src/zeit/objectlog/generation/evolve2.py
+++ core/src/zeit/objectlog/generation/evolve2.py
@@ -1,6 +1,12 @@
+import logging
+
+import ZODB.POSException
 import zope.component
 
 import zeit.objectlog.generation
+
+
+log = logging.getLogger(__name__)
 
 
 def update(root):
@@ -9,10 +15,13 @@ def update(root):
     for key in list(objectlog):  # production has ~90k, fits into ~200M
         value = objectlog[key]
         id = key.referenced_object
-        for item in value.values():
-            item.uniqueId = id
-            del item.object_reference
-        objectlog[id] = value
+        try:
+            for item in value.values():
+                item.uniqueId = id
+                del item.object_reference
+            objectlog[id] = value
+        except ZODB.POSException.POSKeyError:
+            log.warning('ZODB.POSException.POSKeyError, removing lost key %s', key)
         del objectlog[key]
 
 
