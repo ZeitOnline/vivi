a764ac108d99f0ac1b2d49213fb89e9d513421ef
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-09-23 10:22:54 +0200

    WCM-1122: Add toggle cp_teaser_image_from_thumbnail to evaluate performance without thumbnails here as well

--- core/src/zeit/content/cp/browser/blocks/teaser.py
+++ core/src/zeit/content/cp/browser/blocks/teaser.py
@@ -6,6 +6,7 @@ import zope.formlib.form
 import zope.interface
 import zope.lifecycleevent
 
+from zeit.cms.content.sources import FEATURE_TOGGLES
 from zeit.cms.i18n import MessageFactory as _
 import zeit.cms.browser.interfaces
 import zeit.cms.browser.view
@@ -113,9 +114,17 @@ class Display(zeit.cms.browser.view.Base):
         image = images.image
         if zeit.content.image.interfaces.IImageGroup.providedBy(image):
             repository = zope.component.getUtility(zeit.cms.repository.interfaces.IRepository)
+            if FEATURE_TOGGLES.find('cp_teaser_image_from_thumbnail'):
+                params = {'thumbnail': True}
+            else:
+                # The previously used `thumbnail-source` image had width=1000px
+                params = {
+                    'width': zeit.cms.config.get('zeit.content.cp', 'thumbnail-width', 1000),
+                    'height': 0,
+                }
             return '%s%s/@@raw' % (
                 self.url(repository),
-                image.variant_url(image_pattern, thumbnail=True),
+                image.variant_url(image_pattern, **params),
             )
         else:
             return self.url(image, '@@raw')
