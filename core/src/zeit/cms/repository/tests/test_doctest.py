cda14ffa4c92b239852506e09a4f1ddd075d7d46
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-10-08 12:20:15 +0200

    WCM-1097: Remove long-unused repository tree viewlet
    Was disabled in f473e75

--- core/src/zeit/cms/repository/tests/test_doctest.py
+++ core/src/zeit/cms/repository/tests/test_doctest.py
@@ -3,5 +3,8 @@ import zeit.cms.testing
 
 def test_suite():
     return zeit.cms.testing.FunctionalDocFileSuite(
-        'README.txt', 'cache.txt', 'preference.txt', 'file.txt', package='zeit.cms.repository'
+        'README.txt',
+        'cache.txt',
+        'file.txt',
+        package='zeit.cms.repository',
     )
