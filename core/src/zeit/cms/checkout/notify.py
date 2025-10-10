e3113490e46ecfeeceedae7c0b2dd82a6a1a200f
Louisa von Hülsen <louisa.huelsen@zeit.de>
2025-10-10 15:12:52 +0200

    WCM-810: Invalidate resource when notifying cms as well

--- core/src/zeit/cms/checkout/notify.py
+++ core/src/zeit/cms/checkout/notify.py
@@ -1,14 +1,30 @@
+import logging
+import xmlrpc.client
+
 import zope.event
 import zope.publisher.xmlrpc
 
 import zeit.cms.content.interfaces
 import zeit.cms.interfaces
+import zeit.connector.interfaces
+
+
+log = logging.getLogger(__name__)
 
 
 class Notify(zope.publisher.xmlrpc.XMLRPCView):
     def principal(self):
         return False
 
-    def notify(self, unique_id):
-        content = zeit.cms.interfaces.ICMSContent(unique_id)
+    def notify(self, resource_id):
+        if not isinstance(resource_id, str):
+            raise xmlrpc.client.Fault(
+                100, '`resource_id` must be string type, got %s' % (type(resource_id))
+            )
+        self._notify(resource_id)
+
+    def _notify(self, resource_id):
+        log.info('%s content modified %s.' % (self.request.principal.id, resource_id))
+        zope.event.notify(zeit.connector.interfaces.ResourceInvalidatedEvent(resource_id))
+        content = zeit.cms.interfaces.ICMSContent(resource_id)
         zope.event.notify(zeit.cms.content.interfaces.ContentModifiedEvent(content, self.principal))
