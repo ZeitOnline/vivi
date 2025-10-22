593d76ffb3a312f52cde1d4114dc8323f07d14a3
Jannis Jahr <jannis.jahr@zeit.de>
2025-10-22 09:15:39 +0200

    ENG-453: add volume to followings
    After reading the ticket, i noticed that previous commit was wrong

--- core/src/zeit/workflow/tests/test_publish_3rdparty.py
+++ core/src/zeit/workflow/tests/test_publish_3rdparty.py
@@ -1,3 +1,4 @@
+from _pydatetime import datetime, timezone
 import unittest
 
 import lxml.etree
@@ -733,16 +734,29 @@ class FollowingsPayloadTest(zeit.workflow.testing.FunctionalTestCase):
             self.assertEqual(data['created'], date.isoformat())
 
     def test_followings_volume(self):
-        article = ICMSContent('http://xml.zeit.de/2025/10/anna-sprache')
         self.repository['2025'] = zeit.cms.repository.folder.Folder()
         self.repository['2025']['10'] = zeit.cms.repository.folder.Folder()
-        self.repository['2025']['10']['index'] = zeit.content.cp.centerpage.CenterPage()
-        cp = self.repository['2025']['10']['index']
+        cp = self.repository['2025']['10']['index'] = zeit.content.cp.centerpage.CenterPage()
+        overview_cp = self.repository['2025']['index'] = zeit.content.cp.centerpage.CenterPage()
 
-        date = zeit.cms.workflow.interfaces.IPublishInfo(article).date_first_released
-        expected_uuid = zeit.cms.content.interfaces.IUUID(cp).shortened
-        data = zeit.workflow.testing.publish_json(article, 'followings')
-        self.assertIsNotNone(data, 'Data should not be None when recipe categories are present')
+        with checked_out(cp):
+            cp.year = 2025
+            cp.volume = 10
+            cp.type = 'volume'
+            info = zeit.cms.workflow.interfaces.IPublishInfo(cp)
+            info.published = True
+            info.date_first_released = datetime(2025, 3, 5, 8, 18, tzinfo=timezone.utc)
+
+        with checked_out(overview_cp):
+            overview_cp.year = 2025
+            overview_cp.type = 'volume-overview'
+
+        overview_cp = self.repository['2025']['index']
+
+        data = zeit.workflow.testing.publish_json(cp, 'followings')
+        expected_uuid = zeit.cms.content.interfaces.IUUID(overview_cp).shortened
+        date = zeit.cms.workflow.interfaces.IPublishInfo(cp).date_first_released
+        self.assertIsNotNone(data, 'Data should not be None')
         self.assertEqual(data['parent_uuids'][0], expected_uuid)
         self.assertEqual(data['created'], date.isoformat())
 
