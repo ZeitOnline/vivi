78d3cf1af5181c80cf6ddafba1a1a709d1e322b8
Louisa von Hülsen <louisa.huelsen@zeit.de>
2025-10-28 11:48:07 +0100

    WCM-1177: Do not break on audio references
    when adapting random content to them

--- core/src/zeit/entitlements/tests/test_entitlements.py
+++ core/src/zeit/entitlements/tests/test_entitlements.py
@@ -1,4 +1,3 @@
-import transaction
 import zope.interface
 
 from zeit.cms.checkout.helper import checked_out
@@ -13,42 +12,33 @@ import zeit.entitlements.testing
 class Entitlements(zeit.entitlements.testing.FunctionalTestCase):
     def setUp(self):
         super().setUp()
-        content = ExampleContentType()
-        zope.interface.alsoProvides(content, zeit.content.article.interfaces.IArticle)
-        self.repository['testarticle'] = content
-        self.content = self.repository['testcontent']
-
-    def set_properties(self, name, properties):
-        with checked_out(self.repository[name]) as co:
-            for key, val in properties.items():
-                setattr(co, key, val)
-        transaction.commit()
+        self.content = ExampleContentType()
 
     def test_converts_access_to_entitlements(self):
-        self.assertEqual(set(), zeit.entitlements.accepted(self.repository['testcontent']))
-        self.set_properties('testcontent', {'access': 'free'})
-        self.assertEqual(set(), zeit.entitlements.accepted(self.repository['testcontent']))
-        self.set_properties('testcontent', {'access': 'dynamic'})
-        self.assertEqual(set(), zeit.entitlements.accepted(self.repository['testcontent']))
-        self.set_properties('testcontent', {'access': 'registration'})
-        self.assertEqual(
-            {'registration'}, zeit.entitlements.accepted(self.repository['testcontent'])
-        )
-        self.set_properties('testcontent', {'access': 'abo'})
-        self.assertEqual({'zplus'}, zeit.entitlements.accepted(self.repository['testcontent']))
+        self.assertEqual(set(), zeit.entitlements.accepted(self.content))
+        self.content.access = 'free'
+        self.assertEqual(set(), zeit.entitlements.accepted(self.content))
+        self.content.access = 'dynamic'
+        self.assertEqual(set(), zeit.entitlements.accepted(self.content))
+        self.content.access = 'registration'
+        self.assertEqual({'registration'}, zeit.entitlements.accepted(self.content))
+        self.content.access = 'abo'
+        self.assertEqual({'zplus'}, zeit.entitlements.accepted(self.content))
 
     def test_empty_for_not_commonmetadata(self):
         text = Text()
         self.assertEqual(set(), zeit.entitlements.accepted(text))
 
     def test_ressort_wochenmarkt_adds_entitlement(self):
-        with checked_out(self.content) as co:
-            co.access = 'abo'
-            co.ressort = 'zeit-magazin'
-            co.sub_ressort = 'wochenmarkt'
+        self.content.access = 'abo'
+        self.content.ressort = 'zeit-magazin'
+        self.content.sub_ressort = 'wochenmarkt'
         self.assertEqual({'wochenmarkt', 'zplus'}, zeit.entitlements.accepted(self.content))
 
     def test_podcast_audio_reference_adds_entitlement(self):
+        content = ExampleContentType()
+        zope.interface.alsoProvides(content, zeit.content.article.interfaces.IArticle)
+        self.repository['testarticle'] = content
         AudioBuilder().referenced_by(self.repository['testarticle']).build()
         with checked_out(self.repository['testarticle']) as co:
             co.access = 'abo'
@@ -57,15 +47,10 @@ class Entitlements(zeit.entitlements.testing.FunctionalTestCase):
         )
 
     def test_accepted_entitlements(self):
-        self.set_properties(
-            'testcontent', {'access': 'abo', 'accepted_entitlements': 'podcast,wochenmarkt'}
-        )
-        self.assertEqual(
-            {'podcast', 'wochenmarkt'}, zeit.entitlements.accepted(self.repository['testcontent'])
-        )
-        self.set_properties('testcontent', {'access': 'abo', 'accepted_entitlements': ''})
-        self.assertEqual({'zplus'}, zeit.entitlements.accepted(self.repository['testcontent']))
-
-    def test_accepted_article_entitlements(self):
-        self.set_properties('testarticle', {'access': 'abo', 'accepted_entitlements': 'podcast'})
-        self.assertEqual({'podcast'}, zeit.entitlements.accepted(self.repository['testarticle']))
+        self.content.access = 'abo'
+        self.content.accepted_entitlements = 'podcast,wochenmarkt'
+        self.assertEqual({'podcast', 'wochenmarkt'}, zeit.entitlements.accepted(self.content))
+        self.content.accepted_entitlements = 'podcast'
+        self.assertEqual({'podcast'}, zeit.entitlements.accepted(self.content))
+        self.content.accepted_entitlements = ''
+        self.assertEqual({'zplus'}, zeit.entitlements.accepted(self.content))
