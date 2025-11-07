9a37a2e3887d9ee5089e5357ab59b78e18bd7297
Valentin von Guttenberg <valentin.vonguttenberg@zeit.de>
2025-11-07 12:27:02 +0100

    ES-449: Update & consolidate scrolly image tests

--- core/src/zeit/content/article/edit/tests/test_scrolly_image.py
+++ core/src/zeit/content/article/edit/tests/test_scrolly_image.py
@@ -1,11 +1,13 @@
 import lxml.builder
 import zope.component
 
+import zeit.content.animation.animation
 import zeit.content.article.article
 import zeit.content.article.edit.body
 import zeit.content.article.edit.interfaces
 import zeit.content.article.testing
 import zeit.content.image.testing
+import zeit.content.video.video
 import zeit.edit.interfaces
 
 
@@ -28,46 +30,66 @@ class ScrollyImageTest(zeit.content.article.testing.FunctionalTestCase):
         self.assertTrue(zeit.content.article.edit.interfaces.IScrollyImage.providedBy(scrolly_img))
         self.assertEqual('scrolly_image', scrolly_img.xml.tag)
 
-    def test_text_attribute_should_be_stored_in_xml(self):
+    def test_all_attributes_should_be_stored_in_xml(self):
         from zeit.content.article.edit.scrolly_image import ScrollyImage
 
         scrolly_img = ScrollyImage(None, lxml.builder.E.scrolly_image())
         text = 'This is some scrollytelling text'
+        layout_desktop = 'contain'
+        layout_mobile = 'contain-padded'
+
         scrolly_img.text = text
+        scrolly_img.layout_desktop = layout_desktop
+        scrolly_img.layout_mobile = layout_mobile
+
         self.assertEqual(text, scrolly_img.text)
         self.assertEqual(text, scrolly_img.xml.get('text'))
 
-    def test_layout_desktop_attribute_should_be_stored_in_xml(self):
-        from zeit.content.article.edit.scrolly_image import ScrollyImage
-
-        scrolly_img = ScrollyImage(None, lxml.builder.E.scrolly_image())
-        layout_desktop = 'contain'
-        scrolly_img.layout_desktop = layout_desktop
         self.assertEqual(layout_desktop, scrolly_img.layout_desktop)
         self.assertEqual(layout_desktop, scrolly_img.xml.get('layout-desktop'))
+
+        self.assertEqual(layout_mobile, scrolly_img.layout_mobile)
+        self.assertEqual(layout_mobile, scrolly_img.xml.get('layout-mobile'))
 
     def test_layout_desktop_default_should_be_cover(self):
         scrolly_img = self.create_scrolly_image()
         self.assertEqual('cover', scrolly_img.layout_desktop)
 
-    def test_layout_mobile_attribute_should_be_stored_in_xml(self):
-        from zeit.content.article.edit.scrolly_image import ScrollyImage
-
-        scrolly_img = ScrollyImage(None, lxml.builder.E.scrolly_image())
-        layout_mobile = 'contain-padded'
-        scrolly_img.layout_mobile = layout_mobile
-        self.assertEqual(layout_mobile, scrolly_img.layout_mobile)
-        self.assertEqual(layout_mobile, scrolly_img.xml.get('layout-mobile'))
-
     def test_layout_mobile_default_should_be_cover(self):
         scrolly_img = self.create_scrolly_image()
         self.assertEqual('cover', scrolly_img.layout_mobile)
 
-    def test_references_should_accept_image_group(self):
+    def test_references_should_accept_image(self):
+        from zeit.content.image.testing import create_image
+
         scrolly_img = self.create_scrolly_image()
-        image_ref = self.repository['2006']['DSC00109_2.JPG']
-        scrolly_img.references = image_ref
-        self.assertEqual(image_ref.uniqueId, scrolly_img.xml.get('href'))
+        image = create_image()
+        self.repository['image'] = image
+        scrolly_img.references = image
+        self.assertEqual(image.uniqueId, scrolly_img.xml.get('href'))
+
+    def test_references_should_accept_image_group(self):
+        from zeit.content.image.testing import create_image_group
+
+        scrolly_img = self.create_scrolly_image()
+        image_group = create_image_group()
+        self.repository['imagegroup'] = image_group
+        scrolly_img.references = image_group
+        self.assertEqual(image_group.uniqueId, scrolly_img.xml.get('href'))
+
+    def test_references_should_accept_animation(self):
+        scrolly_img = self.create_scrolly_image()
+
+        # create animation
+        self.repository['article'] = zeit.content.article.article.Article()
+        self.repository['video'] = zeit.content.video.video.Video()
+        animation = zeit.content.animation.animation.Animation()
+        animation.article = self.repository['article']
+        animation.video = self.repository['video']
+        self.repository['animation'] = animation
+        scrolly_img.references = animation
+
+        self.assertEqual(animation.uniqueId, scrolly_img.xml.get('href'))
 
     def test_references_should_be_retrievable(self):
         scrolly_img = self.create_scrolly_image()
