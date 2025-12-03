a092583bdf4676449b84eae13a7439244d7262fd
Martin Stolle <martin.stolle@zeit.de>
2025-12-03 13:23:24 +0100

    WCM-799: toggle events
    by default active

--- core/src/zeit/workflow/scheduled/checkout.py
+++ core/src/zeit/workflow/scheduled/checkout.py
@@ -10,6 +10,7 @@ import logging
 
 import grokcore.component as grok
 
+from zeit.cms.content.sources import FEATURE_TOGGLES
 from zeit.workflow.scheduled.operations import (
     SQLScheduledOperationsStorage,
     ZODBScheduledOperationsStorage,
@@ -25,6 +26,9 @@ log = logging.getLogger(__name__)
 @grok.subscribe(zeit.cms.interfaces.ICMSContent, zeit.cms.checkout.interfaces.IAfterCheckoutEvent)
 def copy_operations_to_workingcopy(context, event):
     if not zeit.cms.checkout.interfaces.ILocalContent.providedBy(context):
+        return
+
+    if not FEATURE_TOGGLES.find('use_scheduled_operations'):
         return
 
     content_id = zeit.cms.content.interfaces.IUUID(context).shortened
@@ -63,6 +67,9 @@ def sync_operations_to_repository(context, event):
     impact should be negligible.
     """
     if not zeit.cms.checkout.interfaces.ILocalContent.providedBy(context):
+        return
+
+    if not FEATURE_TOGGLES.find('use_scheduled_operations'):
         return
 
     content_id = zeit.cms.content.interfaces.IUUID(context).shortened
