861cd31156f61110a2f5f56f18d2763ff9dfb3d5
Martin Stolle <martin.stolle@zeit.de>
2026-01-02 10:59:49 +0100

    WCM-1249: add main to equality check or the changes will not be saved
    if we later add the UI implementation

--- core/src/zeit/cms/tagging/tag.py
+++ core/src/zeit/cms/tagging/tag.py
@@ -83,11 +83,12 @@ class Tag:
     def __eq__(self, other):
         # XXX this is not a generic equality check. From a domain perspective,
         # two tags are the same when their codes are the same. However, since
-        # we want to edit ``pinned``, and formlib compares the *list* of
-        # keywords, which uses == on the items, we need to include pinned here.
+        # we want to edit ``pinned`` and ``main``, and formlib compares the
+        # *list* of keywords, which uses == on the items, we need to include
+        # pinned and main here.
         if not zeit.cms.tagging.interfaces.ITag.providedBy(other):
             return False
-        return self.code == other.code and self.pinned == other.pinned
+        return self.code == other.code and self.pinned == other.pinned and self.main == other.main
 
     def __ne__(self, other):
         return not self.__eq__(other)
